997c5019b5d814570a00558d5a36166ae550860b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:16 +0000

    Add package tfmake/0.7.3/tfmake-0.7.3.tar.gz

---
+++ packages/tfmake/tfmake-0.7.3.tar.gz/tfmake-0.7.3/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['tfmake', 'tfmake.custom']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=7.0,<8.0', 'pathlib>=1.0.1,<2.0.0', 'toml>=0.10.0,<0.11.0']
+
+entry_points = \
+{'console_scripts': ['tfmake = tfmake.cli:main']}
+
+setup_kwargs = {
+    'name': 'tfmake',
+    'version': '0.7.3',
+    'description': 'Python based Makefile wrapper for terraform projects',
+    'long_description': None,
+    'author': 'Pascal Prins',
+    'author_email': 'pascal.prins@foobar-it.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, !=3.6.*',
+}
+
+
+setup(**setup_kwargs)
