0c51677fb09ace58fd2e79ce5b3288c1c0cf0e76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:28 +0000

    Add package nesterxkw/1.0.0/nesterxkw-1.0.0.tar.gz

---
+++ packages/nesterxkw/nesterxkw-1.0.0.tar.gz/nesterxkw-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+	name = "nesterxkw",
+	version = "1.0.0",
+	py_modules =["nesterxkw"],
+	author = "jack",
+        author_email = "xkwwangyi@163.com",
+	description = " a modules for printing the content of list"
+	)
