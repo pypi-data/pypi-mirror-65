98e707febf41e45f00070670454c496d096505a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:16 +0000

    Add package typish/1.5.3/typish-1.5.3-py3-none-any.whl

---
+++ packages/typish/typish-1.5.3-py3-none-any.whl/typish/__init__.py
@@ -0,0 +1,40 @@
+import typing
+
+import typish
+from typish._classes import (
+    SubscriptableType,
+    Something,
+    TypingType,
+    ClsDict,
+    ClsFunction,
+)
+from typish._functions import (
+    subclass_of,
+    instance_of,
+    get_origin,
+    get_args,
+    get_alias,
+    get_type,
+    common_ancestor,
+    common_ancestor_of_types,
+    get_args_and_return_type,
+    get_type_hints_of_callable,
+    is_type_annotation,
+)
+from typish._meta import (
+    __version__,
+)
+from typish._types import (
+    T,
+    KT,
+    VT,
+    Empty,
+    Unknown,
+    Module,
+    NoneType,
+    Ellipsis_,
+    EllipsisType,
+)
+
+# As of Python 3.8, Literal is in the typing module.
+Literal = getattr(typing, 'Literal', typish._classes.Literal)
