edd56eefc9bb197b70a904e1b841febc9eb2f202
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:27 +0000

    Add package hklab2/0.25/hklab2-0.25.tar.gz

---
+++ packages/hklab2/hklab2-0.25.tar.gz/hklab2-0.25/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name="hklab2",
+      version="0.25",
+      url="http://github.com/kimhyokwan/hklab2",
+      license="MIT",
+      author="kimhyokwan",
+      author_email="haiteam@kopo.ac.kr",
+      keywords = ["calendar","isoweek","listsum"],
+      description = "isoweek calculation and so on",
+      packages=["hklab2"],
+      install_requires=["isoweek"] 
+     )
