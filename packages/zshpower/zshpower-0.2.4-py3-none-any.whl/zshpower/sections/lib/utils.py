3ab708a0e43729aca9f85fe248cffbf2a072a5a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:43 +0000

    Add package zshpower/0.2.4/zshpower-0.2.4-py3-none-any.whl

---
+++ packages/zshpower/zshpower-0.2.4-py3-none-any.whl/zshpower/sections/lib/utils.py
@@ -0,0 +1,92 @@
+import os
+# from snakypy import FG
+# from snakypy.ansi import NONE as s_none
+from subprocess import check_output
+
+
+# class Color:
+#     """Colors bash"""
+#
+#     NONE = s_none
+#
+#     def __init__(self, set_color="none"):
+#         self.set_color = set_color
+#         self.color = {"green": FG.GREEN,
+#                       "red": FG.RED,
+#                       "blue": FG.BLUE,
+#                       "yellow": FG.YELLOW,
+#                       "cyan": FG.CYAN,
+#                       "magenta": FG.MAGENTA,
+#                       "white": FG.WHITE,
+#                       "black": FG.BLACK,
+#                       "none": ""}
+#
+#     def __str__(self):
+#         return self.color.get(self.set_color, "")
+
+
+# class Color:
+#     """Color application compatible only with Oh My ZSH."""
+#     NONE = "%{$reset_color%}"
+#
+#     def __init__(self, set_color=None):
+#         self.color = f"%{{$fg[{set_color}]%}}"
+#
+#     def __str__(self):
+#         return self.color
+
+class Color:
+    """Color application compatible only ZSH."""
+    NONE = "%f"
+
+    def __init__(self, set_color=None):
+        self.color = f"%F{{{set_color}}}"
+
+    def __str__(self):
+        return self.color
+
+
+def choice_symbol(config, extra, spacing=" "):
+    # TODO: Add icon in SSH by options true/false - Next version
+    if config != "":
+        config += spacing
+    if "SSH_CONNECTION" in os.environ:
+        config = extra
+    return config
+
+
+def abspath_link():
+    cwd = check_output("pwd -L", shell=True, universal_newlines=True).strip()
+    return cwd
+
+
+def git_status(*, porcelain=False, branch=False):
+    porcelain_set = "--porcelain" if porcelain else ""
+    branch_set = "--branch" if branch else ""
+    if porcelain and not branch:
+        status = check_output(f"git status {porcelain_set}",
+                              shell=True, universal_newlines=True).split()
+    elif branch and not porcelain:
+        data = check_output(f"git status {branch_set}",
+                            shell=True, universal_newlines=True).split()
+        if data[0] == "HEAD":
+            status = f"{Color('red')}HEAD{Color().NONE}"
+        else:
+            status = data[2]
+    else:
+        status = check_output(f"git status {porcelain_set} {branch_set}",
+                              shell=True, universal_newlines=True).split()
+    return status
+
+
+def separator(config, spacing=" "):
+    data = config["general"]["separator"]
+    if data != "":
+        data += spacing
+    return data
+
+
+def prefix_text_formatting(obj, spacing=" "):
+    if obj != "":
+        obj += spacing
+    return obj
