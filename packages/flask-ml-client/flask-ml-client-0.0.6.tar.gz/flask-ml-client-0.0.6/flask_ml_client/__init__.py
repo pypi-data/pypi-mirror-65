f1ffed904604c46251ec5ae821fd8eb7a7683f16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:12 +0000

    Add package flask-ml-client/0.0.6/flask-ml-client-0.0.6.tar.gz

---
+++ packages/flask-ml-client/flask-ml-client-0.0.6.tar.gz/flask-ml-client-0.0.6/flask_ml_client/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+    flask_ml_client
+    ~~~~~
+    A microframework for machine learning based on Flask.
+    Designed at UMass
+    :copyright: Jagath Jai Kumar
+    :license: MIT
+"""
+
+from .MLClient import MLClient
+
+__version__ = "0.0.4"
