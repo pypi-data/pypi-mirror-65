f8969047416e5967483dc5544b8296ab26fa9476
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:39 +0000

    Add package shaonutil/0.0.0.62.1/shaonutil-0.0.0.62.1-py3.8.egg

---
+++ packages/shaonutil/shaonutil-0.0.0.62.1-py3.8.egg/shaonutil/strings.py
@@ -0,0 +1,15 @@
+"""String"""
+import pprint
+
+
+def nicely_print(dictionary,print=True):
+	"""Prints the nicely formatted dictionary - shaonutil.strings.nicely_print(object)"""
+	if print: pprint.pprint(dictionary)
+
+	# Sets 'pretty_dict_str' to 
+	return pprint.pformat(dictionary)
+
+def change_dic_key(dic,old_key,new_key):
+	"""Change dictionary key with new key"""
+	dic[new_key] = dic.pop(old_key)
+	return dic
