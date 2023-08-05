959a02d563999fe73f7c102c8f3b8369b87ca31c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:07 +0000

    Add package brudercropper/0.0.7/brudercropper-0.0.7.tar.gz

---
+++ packages/brudercropper/brudercropper-0.0.7.tar.gz/brudercropper-0.0.7/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="brudercropper", # Replace with your own username
+    version="0.0.7",
+    author="Niggo",
+    # author_email="niggo@420.bayern",
+    description="Croppt Zeug auf 62mm für Bruderlabeldrucker",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # url="https://",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        # "Development Status :: 3 - Alpha"
+        "Programming Language :: Python :: 3",
+        # "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
