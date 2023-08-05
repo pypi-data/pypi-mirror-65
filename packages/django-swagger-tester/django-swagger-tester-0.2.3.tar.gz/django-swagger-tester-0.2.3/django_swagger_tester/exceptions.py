16dcf84d7bcb71fb08848e7356dab314612508d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:37 +0000

    Add package django-swagger-tester/0.2.3/django-swagger-tester-0.2.3.tar.gz

---
+++ packages/django-swagger-tester/django-swagger-tester-0.2.3.tar.gz/django-swagger-tester-0.2.3/django_swagger_tester/exceptions.py
@@ -0,0 +1,6 @@
+class SwaggerDocumentationError(Exception):
+    """
+    Custom exception raised when package tests fail.
+    """
+
+    pass
---
+++ packages/django-swagger-tester/django-swagger-tester-0.2.3.tar.gz/django-swagger-tester-0.2.3/django_swagger_tester/utils.py
@@ -0,0 +1,49 @@
+import logging
+import re
+from typing import List
+
+from rest_framework.schemas.generators import EndpointEnumerator
+
+logger = logging.getLogger('django_swagger_tester')
+
+
+def get_paths() -> List[str]:
+    """
+    Returns a list of endpoint paths.
+    """
+    return list(set(endpoint[0] for endpoint in EndpointEnumerator().get_api_endpoints()))  # noqa: C401
+
+
+def convert_resolved_url(resolved_url: str) -> str:
+    """
+    Converts an url:
+
+        - from /api/v1/<vehicle_type:vehicle_type>/ to /api/v1/{vehicle_type}/, and
+        - from /api/v1/<vehicle_type>/ to /api/v1/{vehicle_type}/
+
+    :return: Converted url
+    """
+    patterns = [
+        {
+            'pattern': r'<\w+:\w+>',
+            'string_pattern': '<{keyword}:{keyword}>',
+            'first_index': '<',
+            'second_index': ':'
+        },
+        {
+            'pattern': r'<\w+>',
+            'string_pattern': '<{keyword}>',
+            'first_index': '<',
+            'second_index': '>'
+        }]
+
+    for item in patterns:
+        matches = re.findall(item['pattern'], resolved_url)
+        if matches:
+            url = resolved_url
+            for dynamic_url in matches:
+                keyword = dynamic_url[dynamic_url.index(item['first_index']) + 1: dynamic_url.index(item['second_index'])]
+                url = url.replace(item['string_pattern'].format(keyword=keyword), f'{{{keyword}}}')
+            logger.debug('Converted resolved url from `%s` to `%s`', resolved_url, url)
+            resolved_url = url
+    return resolved_url
