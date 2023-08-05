878c4f863e150d34ecd5f6fd31498ef5b5d9cb97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:50 +0000

    Add package ejcli/1.6.0/ejcli-1.6.0.tar.gz

---
+++ packages/ejcli/ejcli-1.6.0.tar.gz/ejcli-1.6.0/ejcli/commands/info.py
@@ -0,0 +1,20 @@
+from ejcli.http import task_list, task_ids, problem_info
+from ejcli.error import EJError
+
+def do_info(self, cmd):
+    """
+    usage: info <task>
+
+    Show testing information and problem statement for a task.
+    """
+    cmd = cmd.strip()
+    if not cmd:
+        return self.do_help('info')
+    tasks = task_list(self.url, self.cookie)
+    try: task_id = task_ids(self.url, self.cookie)[tasks.index(cmd)]
+    except (ValueError, IndexError):
+        raise EJError("No such task.")
+    a, b = problem_info(self.url, self.cookie, task_id)
+    for k, v in a.items(): print(k+': '+v)
+    print()
+    print(b)
