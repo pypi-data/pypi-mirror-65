8f678379e16a2c2a01d93e9caa91df7cb7d1e141
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:45 +0000

    Add package boschshcpy/0.1.1/boschshcpy-0.1.1-py3-none-any.whl

---
+++ packages/boschshcpy/boschshcpy-0.1.1-py3-none-any.whl/boschshcpy/__init__.py
@@ -0,0 +1,2 @@
+from .session import SHCSession
+from .device_helper import SHCDeviceHelper, SHCCameraEyes, SHCShutterContact, SHCShutterControl, SHCSmartPlug, SHCSmokeDetector, SHCIntrusionDetectionSystem
