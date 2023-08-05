41564f32cf7cdd0321505faf59b0ddcbf5e5fa02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:05 +0000

    Add package pandemic/0.1.3/pandemic-0.1.3-py3-none-any.whl

---
+++ packages/pandemic/pandemic-0.1.3-py3-none-any.whl/pandemic/plotting.py
@@ -0,0 +1,20 @@
+from pandemic.conventions import STATE_DESCRIPTIONS, STATE_COLORS
+from collections import Counter
+
+def plot_points(plt, positions, status, title=None):
+    x = [p[0] for p in positions]
+    y = [p[1] for p in positions]
+    if status is not None:
+        c = [ STATE_COLORS[s] for s in status ]
+        scatter = plt.scatter(x=x, y=y, c=status )
+        running = Counter(status)
+        descriptions = list(STATE_DESCRIPTIONS.values())
+        running_totals  = [ (descriptions[k]+ " ("+str(v)+")",k) for k,v in running.items() ]
+        running_totals.sort( key=lambda rt: rt[1] )
+        labels = [ desc for desc,_ in running_totals ]
+        plt.legend(handles=scatter.legend_elements()[0], labels=labels, loc='upper left')
+        if title:
+            plt.title(title)
+    else:
+        scatter = plt.scatter(x=x, y=y)
+    return scatter
