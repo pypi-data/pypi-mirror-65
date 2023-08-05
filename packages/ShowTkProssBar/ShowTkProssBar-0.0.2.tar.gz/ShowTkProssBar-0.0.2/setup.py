a8731496c75cf6bbdb306b9fe7ed384972a214be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:57 +0000

    Add package ShowTkProssBar/0.0.2/ShowTkProssBar-0.0.2.tar.gz

---
+++ packages/ShowTkProssBar/ShowTkProssBar-0.0.2.tar.gz/ShowTkProssBar-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='ShowTkProssBar',
+      version='0.0.2',
+      description='show a tk prossbar',
+      url='https://44202439.org',
+      author='glwwanggs',
+      author_email='442024339@qq.com',
+      license='MIT',
+      packages=['ShowTkProssBar'])
