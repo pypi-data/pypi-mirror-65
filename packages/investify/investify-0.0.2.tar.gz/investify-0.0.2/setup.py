1e74144eedaed87fdc4d6c84295094c0c63226a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:15 +0000

    Add package investify/0.0.2/investify-0.0.2.tar.gz

---
+++ packages/investify/investify-0.0.2.tar.gz/investify-0.0.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['investify']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=7.1.1,<8.0.0', 'requests>=2.23.0,<3.0.0', 'twilio>=6.38.0,<7.0.0']
+
+setup_kwargs = {
+    'name': 'investify',
+    'version': '0.0.2',
+    'description': 'Get automated price alert based on investing.com prices',
+    'long_description': None,
+    'author': 'Abhishek Guha',
+    'author_email': 'abhi.workspace@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
