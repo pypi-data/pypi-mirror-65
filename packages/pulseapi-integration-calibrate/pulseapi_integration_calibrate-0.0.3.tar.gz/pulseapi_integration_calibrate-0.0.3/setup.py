e8721ac544c80d3f5e6d754f87f16bd779dc02d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:22 +0000

    Add package pulseapi-integration-calibrate/0.0.3/pulseapi_integration_calibrate-0.0.3.tar.gz

---
+++ packages/pulseapi-integration-calibrate/pulseapi_integration_calibrate-0.0.3.tar.gz/pulseapi_integration_calibrate-0.0.3/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pulseapi_integration_calibrate",
+    version="0.0.3",
+    author="Matskevich",
+    author_email="matskevichivan98@gmail.com",
+    description="Calibrate plane",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Matskevichivan/Calibrate",
+    packages=["pulseapi_integration_calibrate"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=['pulseapi>=1.6',
+                      'numpy>=1.18',
+                      'pulseapi_integration>=0.1.0']
+)
