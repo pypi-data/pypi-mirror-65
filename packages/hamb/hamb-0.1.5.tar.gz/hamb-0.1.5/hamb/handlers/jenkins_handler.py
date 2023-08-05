0a111e936da95d883d472cae14b5b500e068f666
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:38 +0000

    Add package hamb/0.1.5/hamb-0.1.5.tar.gz

---
+++ packages/hamb/hamb-0.1.5.tar.gz/hamb-0.1.5/hamb/handlers/jenkins_handler.py
@@ -0,0 +1,18 @@
+"""
+fail the jenkins build if tests fail
+"""
+
+import os
+
+
+class Handler:
+    @staticmethod
+    def run(result, conf=None):
+        print("DQ check failed, calling jenkins handler")
+        # raise RuntimeError('TEST FAILED, STOPPING JENKINS!')
+        # raise NameError('TESTS FAILED!')
+        exit(1)
+        print("YOU SHALL NOT SEE THIS MESSAGE!")
+
+
+# Handler.run('result')
---
+++ packages/hamb/hamb-0.1.5.tar.gz/hamb-0.1.5/hamb/handlers/sns_handler.py
@@ -0,0 +1,12 @@
+"""
+sns handler - future use
+"""
+
+import logging, logging.handlers
+
+
+class Handler(object):
+    @staticmethod
+    def run(result, conf):
+        # just a shell for now
+        print("send sns")
