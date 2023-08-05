aa002184309338dd37d44a22a104b2625f2ef05c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:33 +0000

    Add package clifold/0.2.3/clifold-0.2.3.tar.gz

---
+++ packages/clifold/clifold-0.2.3.tar.gz/clifold-0.2.3/clifold/commands/clifold_pkg.py
@@ -0,0 +1,45 @@
+"""Install packages with pip."""
+# import os
+import subprocess
+# import shutil
+import shlex
+
+
+def pip(pkg, project_path, pname):
+    '''
+    Packages installation function.
+
+    Args:
+        pkg: True or False
+        project_path: absolute path to the project
+        pname: project name
+
+    Returns:
+        Packages installed or not installed
+    '''
+    if pkg:
+        pip_path = f"{pname}/bin/pip3"
+        # shell_path = os.environ['SHELL']
+        # shell = os.path.basename(shell_path)
+        # if shell == "bash" or "zsh":
+        #     cmd = f"{pname}/bin/activate"
+        # elif shell == "csh" or "tcsh":
+        #     cmd = f"{pname}/bin/activate.csh"
+        # elif shell == "fish":
+        #     cmd = f"{pname}/bin/activate.fish"
+        # else:
+        #     raise OSError("\nUnknown Shell!")
+        pkg = input("\n📦 Packages you want to install: ")
+        pkgs = shlex.split(pkg)
+        args = [pip_path, "install"]
+        for i in range(len(args)):
+            pkgs.insert(i, args[i])
+
+        # subprocess.run(["source", cmd], shell=True, executable=shell_path)
+        # os.chdir(project_path)
+        print('\n🔰 Downloading packages...\n')
+        subprocess.run(pkgs, check=True)
+        return 1
+    else:
+        print("\nNo package downloaded...")
+        return 0
