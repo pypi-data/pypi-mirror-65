60df1b1f8f3f19368f0dc0467be0c1222d05b2ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:23 +0000

    Add package bgdf-distributions/0.1/bgdf_distributions-0.1.tar.gz

---
+++ packages/bgdf-distributions/bgdf_distributions-0.1.tar.gz/bgdf_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='bgdf_distributions',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      packages=['bgdf_distributions'],
+      zip_safe=False)
