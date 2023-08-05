68607c71d30982d5964aaf3c12ccdc3ab0e0b261
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:30 +0000

    Add package pytse/1.1.0/pytse-1.1.0.tar.gz

---
+++ packages/pytse/pytse-1.1.0.tar.gz/pytse-1.1.0/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+from pathlib import Path
+setuptools.setup(
+    name="pytse",
+    version="1.1.0",
+    long_description=Path("README.md").read_text(),
+    long_description_content_type='text/markdown',
+    description="A small web crawler for tsetmc.com",
+    packages=setuptools.find_packages(exclude=["tests","data"]),
+    project_urls={
+        'Source': 'https://github.com/miladj/PyTse',
+    },
+    install_requires=[
+          'requests',
+      ]
+)
