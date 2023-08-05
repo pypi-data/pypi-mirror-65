9ea23884cee39438eb431cd31a10f7c699f1e81e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:02 +0000

    Add package distribution-of-probability/1.2/distribution_of_probability-1.2.tar.gz

---
+++ packages/distribution-of-probability/distribution_of_probability-1.2.tar.gz/distribution_of_probability-1.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distribution_of_probability',
+      version='1.2',
+      description='Gaussian distributions',
+      packages=['distribution_of_probability'],
+      zip_safe=False)
