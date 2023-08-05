d7cc427f021b9095a50149766be4a3fc7be0605a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:50 +0000

    Add package dsnd-ml-nano-distributions/1.0/dsnd_ml_nano_distributions-1.0.tar.gz

---
+++ packages/dsnd-ml-nano-distributions/dsnd_ml_nano_distributions-1.0.tar.gz/dsnd_ml_nano_distributions-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dsnd_ml_nano_distributions',
+      version='1.0',
+      description='Gaussian distributions, Binomial distributions',
+      packages=['dsnd_ml_nano_distributions'],
+      author='Isaac Tan',
+      zip_safe=False)
