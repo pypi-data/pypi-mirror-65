ac423ff5d1fced58d178ec92312bf15df62ca39a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:01 +0000

    Add package eziocon/0.1.1/eziocon-0.1.1.tar.gz

---
+++ packages/eziocon/eziocon-0.1.1.tar.gz/eziocon-0.1.1/README.md
@@ -0,0 +1,2 @@
+# eziocon
+This repository consists of all the files which is a wrapper of python objects for generic functions for interacting with the Databases such as oracle, sql, mongodb
---
+++ packages/eziocon/eziocon-0.1.1.tar.gz/eziocon-0.1.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+
+
+setup(
+    name="eziocon", # Replace with your own username
+    version="0.1.1",
+    author="iyappan",
+    author_email="iyappan.akp@gmail.com",
+    description="A common package for doing basic operations over sql structured databases ",
+    url="https://github.com/iyappan24/eziocon",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+    install_requires=[
+         'pandas',
+         'pymysql',
+         'cx_Oracle',
+          'json'
+      ]
+)
+
+
