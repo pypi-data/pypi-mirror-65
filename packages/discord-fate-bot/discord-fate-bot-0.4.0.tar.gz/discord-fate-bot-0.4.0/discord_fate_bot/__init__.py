cd22c1030b964c2b455ca375dd2d42661f1f96b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:13 +0000

    Add package discord-fate-bot/0.4.0/discord-fate-bot-0.4.0.tar.gz

---
+++ packages/discord-fate-bot/discord-fate-bot-0.4.0.tar.gz/discord-fate-bot-0.4.0/discord_fate_bot/__init__.py
@@ -0,0 +1,5 @@
+__all__ = [ 'main' ]
+__version__ = '0.4.0'
+
+from .console_script import main
+
---
+++ packages/discord-fate-bot/discord-fate-bot-0.4.0.tar.gz/discord-fate-bot-0.4.0/discord_fate_bot/extensions/log_invite_url.py
@@ -0,0 +1,29 @@
+import logging
+
+from discord import Permissions
+from discord.ext.commands import Cog
+from discord.utils import oauth_url
+
+def setup(bot):
+    bot.add_cog(LogInviteUrlCog(bot))
+
+logger = logging.getLogger(__name__)
+
+class LogInviteUrlCog(Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @Cog.listener()
+    async def on_connect(self):
+        permissions = Permissions(
+            add_reactions = True,
+            manage_messages = True,
+            read_message_history = True,
+            read_messages = True,
+            send_messages = True,
+        )
+
+        url = oauth_url(self.bot.user.id, permissions)
+
+        logger.info('Invite URL: %s', url)
+
