4b606e6d1a93abdb4be73d65c6a31a5801744c79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:35 +0000

    Add package data-mine/0.0.3/data_mine-0.0.3.tar.gz

---
+++ packages/data-mine/data_mine-0.0.3.tar.gz/data_mine-0.0.3/data_mine/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import
+
+__author__ = """Pirtoaca George-Sebastian (sebisebi)"""
+__email__ = 'gpirtoaca@gmail.com'
+__version__ = '0.0.3'
+
+
+from .collection import Collection
+
+# Quick dataset loader methods.
+def RACE(*args, **kwargs):
+    from data_mine.nlp.RACE import RACEDataset
+    return RACEDataset(*args, **kwargs)
