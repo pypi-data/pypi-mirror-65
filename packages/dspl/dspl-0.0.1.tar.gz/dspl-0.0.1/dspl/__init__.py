822d963c1dfd323f403fd3212c947a086de5ce5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:36 +0000

    Add package dspl/0.0.1/dspl-0.0.1.tar.gz

---
+++ packages/dspl/dspl-0.0.1.tar.gz/dspl-0.0.1/dspl/__init__.py
@@ -0,0 +1,1 @@
+from dspl.validation import DataSplitter
---
+++ packages/dspl/dspl-0.0.1.tar.gz/dspl-0.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("./requirements.txt") as f:
+    required = f.read().splitlines()
+
+with open("./README.md") as f:
+    readme = f.read()
+
+setup(
+    name="dspl",
+    packages=["dspl"],
+    version="0.0.1",
+    install_required=required,
+    zip_safe=True,
+    author="Nikita Varganov",
+    author_email="nikita.varganov.ml@gmail.com",
+    license="MIT",
+    description="Library for using in DS-Platform",
+    url="https://github.com/NV-27/dspl",
+    download_url="https://github.com/NV-27/dspl/archive/0.0.1.tar.gz",
+    keywords=["dspl", "ds_platform", "ds_template"],
+    ling_description=readme,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.6",
+        "Operating System :: OS Independent"
+    ]
+)
