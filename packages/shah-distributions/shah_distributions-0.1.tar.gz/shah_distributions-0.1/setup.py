7a39999078f0a228e26132d87c07ca162cd92e8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:43 +0000

    Add package shah-distributions/0.1/shah_distributions-0.1.tar.gz

---
+++ packages/shah-distributions/shah_distributions-0.1.tar.gz/shah_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='shah_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['shah_distributions'],
+      author='Shahzeb Akhtar',
+      author_email = 'shahzeb.akhtar@gmail.com',
+      zip_safe=False)
