2caf71c6b8407ba43f8b80713fea05310dd25979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:53 +0000

    Add package followthemoney-enrich/1.28.0/followthemoney-enrich-1.28.0.tar.gz

---
+++ packages/followthemoney-enrich/followthemoney-enrich-1.28.0.tar.gz/followthemoney-enrich-1.28.0/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='followthemoney-enrich',
+    version='1.28.0',
+    long_description="Data enrichment toolkit",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ],
+    keywords='',
+    author='OCCRP',
+    author_email='data@occrp.org',
+    url='https://occrp.org',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'test']),
+    namespace_packages=[],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'followthemoney',
+        'mwclient',  # wikipedia
+        'rdflib',  # wikidata
+        'SPARQLWrapper',  # wikidata
+        'zeep',  # bvd orbis (soap)
+        'requests',
+        'alephclient'
+    ],
+    entry_points={
+        'followthemoney.enrich': [
+            'aleph = followthemoney_enrich.aleph:AlephEnricher',
+            'occrp = followthemoney_enrich.aleph:OccrpEnricher',
+            'orbis = followthemoney_enrich.orbis:OrbisEnricher',
+            'opencorporates = followthemoney_enrich.opencorporates:OpenCorporatesEnricher',  # noqa
+        ],
+        'followthemoney.cli': [
+            'enrich = followthemoney_enrich.cli:enrich',
+        ],
+    },
+    test_suite='nose.collector',
+    tests_require=['coverage', 'nose']
+)
