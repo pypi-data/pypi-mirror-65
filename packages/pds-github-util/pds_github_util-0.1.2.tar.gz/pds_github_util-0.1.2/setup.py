c4b4d76d44dd2fd7617d2c729102cf922f17887b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:37 +0000

    Add package pds-github-util/0.1.2/pds_github_util-0.1.2.tar.gz

---
+++ packages/pds-github-util/pds_github_util-0.1.2.tar.gz/pds_github_util-0.1.2/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pds_github_util", # Replace with your own package name
+    version="0.1.2",
+    license="apache-2.0",
+    author="thomas loubrieu",
+    author_email="loubrieu@jpl.nasa.gov",
+    description="util functions for software life cycle enforcement on github",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/NASA-PDS-Incubator/pds-github-util",
+    download_url = "https://github.com/NASA-PDS-Incubator/pds-github-util/releases/download/0.0.1/pds_github_util-0.0.1.tar.gz",
+    packages=setuptools.find_packages(),
+    keywords=['github', 'action', 'github action', 'snapshot', 'release', 'maven'],
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'github3.py',
+        'lxml'
+    ],
+    entry_points={
+        # snapshot-release for backward compatibility
+        'console_scripts': ['snapshot-release=pds_github_util.maven_snapshot_release:main',
+                            'maven-snapshot-release=pds_github_util.maven_snapshot_release:main',
+                            'python-snapshot-release=pds_github_util.python_snapshot_release:main'
+                            ],
+    },
+
+
+)
