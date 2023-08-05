e61234f7486b716e68a9504fe76344b07b0e1fee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:23 +0000

    Add package pacifica-notifications/0.5.0/pacifica-notifications-0.5.0.tar.gz

---
+++ packages/pacifica-notifications/pacifica-notifications-0.5.0.tar.gz/pacifica-notifications-0.5.0/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""Setup and install the pacifica service."""
+from os import path
+try:  # pip version 9
+    from pip.req import parse_requirements
+except ImportError:
+    from pip._internal.req import parse_requirements
+from setuptools import setup, find_packages
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+INSTALL_REQS = parse_requirements('requirements.txt', session='hack')
+
+setup(
+    name='pacifica-notifications',
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    description='Pacifica Notifications Service',
+    url='https://github.com/pacifica/pacifica-notifications/',
+    long_description=open(path.join(
+        path.abspath(path.dirname(__file__)),
+        'README.md')).read(),
+    long_description_content_type='text/markdown',
+    author='David Brown',
+    author_email='dmlb2000@gmail.com',
+    packages=find_packages(include=['pacifica.*']),
+    namespace_packages=['pacifica'],
+    entry_points={
+        'console_scripts': [
+            'pacifica-notifications=pacifica.notifications.__main__:main',
+            'pacifica-notifications-cmd=pacifica.notifications.__main__:cmd'
+        ]
+    },
+    install_requires=[str(ir.req) for ir in INSTALL_REQS]
+)
