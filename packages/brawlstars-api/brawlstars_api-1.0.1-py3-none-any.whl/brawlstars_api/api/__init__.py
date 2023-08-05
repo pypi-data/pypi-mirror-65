0900b805263e1e91647b232f8dea4b2b420a2ec4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:57 +0000

    Add package brawlstars-api/1.0.1/brawlstars_api-1.0.1-py3-none-any.whl

---
+++ packages/brawlstars-api/brawlstars_api-1.0.1-py3-none-any.whl/brawlstars_api/api/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from swagger_client.api.brawlers_api import BrawlersApi
+from swagger_client.api.clubs_api import ClubsApi
+from swagger_client.api.players_api import PlayersApi
+from swagger_client.api.rankings_api import RankingsApi
