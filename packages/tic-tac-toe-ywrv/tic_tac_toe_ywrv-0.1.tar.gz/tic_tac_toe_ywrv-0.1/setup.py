aa2f724bb89fac64a354b10831a049cd52846a32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:38 +0000

    Add package tic-tac-toe-ywrv/0.1/tic_tac_toe_ywrv-0.1.tar.gz

---
+++ packages/tic-tac-toe-ywrv/tic_tac_toe_ywrv-0.1.tar.gz/tic_tac_toe_ywrv-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name = 'tic_tac_toe_ywrv',
+    version = '0.1',
+    packages = ['tic_tac_toe_ywrv'],
+    author = 'Yifei Wang, Rahul Vaswani',
+    zip_safe = False)
