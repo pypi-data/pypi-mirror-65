f89f9a7991c4b664205b715cab08cebffe2cd9f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:53 +0000

    Add package cfapackages-cfa/0.0.1/cfapackages-cfa-0.0.1.tar.gz

---
+++ packages/cfapackages-cfa/cfapackages-cfa-0.0.1.tar.gz/cfapackages-cfa-0.0.1/README.md
@@ -0,0 +1,1 @@
+This is a simple example package. You can use to write your content. [Github-flavored Markdown](https://guides.github.com/features/mastering-markdown/) 
---
+++ packages/cfapackages-cfa/cfapackages-cfa-0.0.1.tar.gz/cfapackages-cfa-0.0.1/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cfapackages-cfa", # Replace with your own username
+    version="0.0.1",
+    author="Omolewa Adaramola",
+    author_email="omolewa.davids@gmail.com",
+    description="Chick-fil-A packages",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
