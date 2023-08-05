738a78e24ea1835924144b2ba55a5d177b97dcd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:50 +0000

    Add package granitepy/0.3.0a0/granitepy-0.3.0a0.tar.gz

---
+++ packages/granitepy/granitepy-0.3.0a0.tar.gz/granitepy-0.3.0a0/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+with open("README.md") as f:
+    readme = f.read()
+
+
+setuptools.setup(
+    name="granitepy",
+    author="MrRandom#9258 and twitch 🔋#7443",
+    version="0.3.0a0",
+    url="https://github.com/iDevision/granitepy",
+    packages=setuptools.find_packages(),
+    licence="AGPL-3.0",
+    description="A python client for the audio provider Andesite for use with discord.py.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    install_requires=requirements,
+    extra_require=None,
+    classifiers=[
+        "Framework :: AsyncIO",
+        'Operating System :: OS Independent',
+        'Natural Language :: English',
+        'Intended Audience :: Developers',
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Libraries',
+        "Topic :: Internet"
+    ],
+    python_requires='>=3.6',
+    keywords=['andesite', 'granitepy', "discord.py"],
+)
