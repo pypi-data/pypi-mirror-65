ecdc8314d5640389f7aeb2193ee151074d1b3cd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:04 +0000

    Add package pystachio/0.8.7/pystachio-0.8.7.tar.gz

---
+++ packages/pystachio/pystachio-0.8.7.tar.gz/pystachio-0.8.7/pystachio/__init__.py
@@ -0,0 +1,18 @@
+__author__ = 'Brian Wickman'
+__version__ = '0.8.3'
+__license__ = 'MIT'
+
+
+import sys
+
+from .base import Environment
+from .basic import Boolean, Enum, Float, Integer, String
+from .choice import Choice
+from .composite import Default, Empty, Required, Struct
+from .container import List, Map
+from .naming import Namable, Ref
+from .parsing import MustacheParser
+from .typing import Type, TypeCheck, TypeFactory
+
+if sys.version_info < (2, 6, 5):
+  raise ImportError("pystachio requires Python >= 2.6.5")
