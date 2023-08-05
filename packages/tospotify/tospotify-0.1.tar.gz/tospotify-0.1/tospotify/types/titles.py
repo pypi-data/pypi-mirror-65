9f6e6a0a0d561ce2b92c5eff82a127fb8b130336
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:08 +0000

    Add package tospotify/0.1/tospotify-0.1.tar.gz

---
+++ packages/tospotify/tospotify-0.1.tar.gz/tospotify-0.1/tospotify/types/titles.py
@@ -0,0 +1,36 @@
+from ..processing import clean_title
+
+
+class Title:
+    """ Base class for a song title """
+    def __init__(self, title: str) -> None:
+        self.title = title
+
+    def makes_sense(self) -> bool:
+        """ Checks whether it makes sense to compile a query given this song title
+
+        :return: True if a query would make sense, False otherwise
+        :rtype: bool
+        """
+        return True
+
+    def process(self) -> str:
+        """ Processes the title according to the implemented rules.
+
+        :return: processed title
+        :rtype: str
+        """
+        return self.title
+
+
+class CleanedTitle(Title):
+    """ Title class which will clean the song title if it makes sense,
+    e.g. Every breath feat. Sting -> Every breath
+    """
+    def makes_sense(self) -> bool:
+        return clean_title(self.title) != self.title
+
+    def process(self) -> str:
+        title = clean_title(self.title)
+
+        return title
