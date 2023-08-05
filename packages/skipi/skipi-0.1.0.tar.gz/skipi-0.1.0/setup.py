448cc7851387ffa30b1384f9379a3b74c8a6d354
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:51 +0000

    Add package skipi/0.1.0/skipi-0.1.0.tar.gz

---
+++ packages/skipi/skipi-0.1.0.tar.gz/skipi-0.1.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='skipi',
+    description='Intuitive package to easily work with mathematical functions',
+    version=__import__('skipi').__version__,
+    author='Alexander Book',
+    author_email='alexander.book@frm2.tum.de',
+    license='MIT',
+    url='https://github.com/TUM-E21-ThinFilms/skipi',
+    packages=find_packages(),
+    keywords=['scientific', 'mathematical transforms'],
+    include_package_data=True,
+    install_requires=['numpy', 'scipy', 'matplotlib'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
