c900282afb3d4f20a6ef8734802b35b6d91fb41f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:48 +0000

    Add package odoo-env/0.9.14/odoo-env-0.9.14.tar.gz

---
+++ packages/odoo-env/odoo-env-0.9.14.tar.gz/odoo-env-0.9.14/odoo_env/repos.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+class Repo(object):
+    def __init__(self, value):
+        self._dict = value
+
+    @property
+    def name(self):
+        return self._dict.get('repo')
+
+    @property
+    def dir_name(self):
+        return self._dict.get('repo')
+
+    @property
+    def branch(self):
+        return self._dict.get('branch')
+
+    @property
+    def url(self):
+        if self._dict.get('ssh', False):
+            template = 'git@{}:{}/{}'
+        else:
+            template = 'https://{}/{}/{}'
+
+        return template.format(self._dict.get('host', 'github.com'),
+                                     self._dict.get('usr'),
+                                     self._dict.get('repo'))
+
+    @property
+    def formatted(self):
+        aaa = self._dict['usr'] + '/' + self._dict['repo']
+        ret = 'b ' + self._dict['branch'].ljust(7) + ' ' + aaa.ljust(30)
+        return ret
+
+    @property
+    def clone(self):
+        return 'clone --depth 1 -b {} {}'.format(self.branch, self.url)
+
+    @property
+    def pull(self):
+        return 'pull'
