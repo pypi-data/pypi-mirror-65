0f242f557b761a281079442f8aee5bec50b9b852
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:56 +0000

    Add package nnunet/1.1.1/nnunet-1.1.1.tar.gz

---
+++ packages/nnunet/nnunet-1.1.1.tar.gz/nnunet-1.1.1/nnunet/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+print("\nPlease cite the following paper when using nnUNet:\nIsensee, Fabian, et al. \"nnU-Net: Breaking the Spell on "
+      "Successful Medical Image Segmentation.\" arXiv preprint arXiv:1904.08128 (2019).")
+print("If you have questions or suggestions, feel free to open an issue at https://github.com/MIC-DKFZ/nnUNet\n")
+
+from . import *
