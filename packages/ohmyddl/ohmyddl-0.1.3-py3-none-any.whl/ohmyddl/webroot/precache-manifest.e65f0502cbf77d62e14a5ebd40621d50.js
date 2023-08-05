6f4755cef354cd79e89d0a360e3d67a4c583aee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:52 +0000

    Add package ohmyddl/0.1.3/ohmyddl-0.1.3-py3-none-any.whl

---
+++ packages/ohmyddl/ohmyddl-0.1.3-py3-none-any.whl/ohmyddl/webroot/precache-manifest.e65f0502cbf77d62e14a5ebd40621d50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10388168f95044f9442c4dbceba5c813",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c2db820a4f3ce4d4f2c",
+    "url": "/static/css/main.ea0700e7.chunk.css"
+  },
+  {
+    "revision": "1d40e95cfb0f370033e1",
+    "url": "/static/js/2.af9020f9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.af9020f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c2db820a4f3ce4d4f2c",
+    "url": "/static/js/main.654a3e47.chunk.js"
+  },
+  {
+    "revision": "68ed935b5956b9ea3c84",
+    "url": "/static/js/runtime-main.63261781.js"
+  }
+]);
