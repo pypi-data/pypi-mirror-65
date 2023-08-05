f3707dbecde80deb5a2c143a4238f17ef061acf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:19 +0000

    Add package databear/1.2.0/databear-1.2.0.tar.gz

---
+++ packages/databear/databear-1.2.0.tar.gz/databear-1.2.0/databear/errors.py
@@ -0,0 +1,36 @@
+'''
+DataBear Exceptions
+'''
+
+class MeasureError(Exception):
+    '''
+    Exception for measurement failures
+    This class is designed to store information
+    associated with several failed measurements
+    '''
+    def __init__(self,failures,messages):
+        '''
+        Inputs
+        failures - a list of measurement names that failed
+        messages - a dictionary of messages associated with measurement names
+        '''
+        self.measurements = failures
+        self.messages = messages
+
+class SensorConfigError(Exception):
+    pass
+
+class DataLogConfigError(Exception):
+    pass
+
+
+#Testing
+if __name__ == "__main__":
+    x = 1
+    y = 2
+
+    if x<y:
+        msg = {'x':'Testing','y':'Testing'}
+        raise MeasureError(['x','y'],msg)
+
+    print('hello')
