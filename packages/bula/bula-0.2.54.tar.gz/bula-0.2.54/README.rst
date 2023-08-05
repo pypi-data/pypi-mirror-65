b7944285dd0c51747c1e2a001dd508a06feb89ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:25 +0000

    Add package bula/0.2.54/bula-0.2.54.tar.gz

---
+++ packages/bula/bula-0.2.54.tar.gz/bula-0.2.54/README.rst
@@ -0,0 +1,11 @@
+Bula
+=======
+
+
+A `Bulma`_-based `Pelican`_ blog theme; clean, flexible and responsive.
+
+For more information, see `the GitHub repo`_.
+
+ .. _Bulma: http://bulma.io/
+ .. _Pelican: http://docs.getpelican.com/en/stable/
+ .. _the GitHub repo: https://github.com/textbook/bulrush#readme
---
+++ packages/bula/bula-0.2.54.tar.gz/bula-0.2.54/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    author='Toshiyuki Yoshida',
+    author_email='yostos@yostos.org',
+    maintainer='Toshiyuki Yoshida',
+    maintainer_email='yostos@yostos.org',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Pelican :: Themes',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    description='Bula theme for Pelican',
+    install_requires=['markupsafe', 'webassets', 'yuicompressor'],
+    license='ISC',
+    name='bula',
+    package_data={
+        'bula': [
+            'templates/*.html',
+            'static/css/*.less',
+        ]
+    },
+    packages=['bula'],
+    test_suite='tests',
+    tests_require=['pelican'],
+    url='https://github.com/yostos/bula',
+    version='0.2.54',
+)
