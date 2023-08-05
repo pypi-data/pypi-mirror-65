e9190d937e2ab3cd42550f34c978dfeac7e0b9be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:36 +0000

    Add package tenvs/1.0.4/tenvs-1.0.4.tar.gz

---
+++ packages/tenvs/tenvs-1.0.4.tar.gz/tenvs-1.0.4/setup.py
@@ -0,0 +1,42 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+def read_install_requires():
+    reqs = [
+            'beautifulsoup4',
+            'gym',
+            'lxml>=3.8.0',
+            'matplotlib==3.1.3',
+            'msgpack>=0.5.6',
+            'numpy==1.16.0',
+            'pandas==0.25.3',
+            'pyzmq>=16.0.0',
+            'requests>=2.0.0',
+            'scipy',
+            'simplejson>=3.16.0',
+            'tushare']
+    return reqs
+
+
+setup(name='tenvs',
+      version='1.0.4',
+      description='',
+      url='https://github.com/tradingAI/tenvs',
+      author='liuwen',
+      author_email='liuwen.w@qq.com',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        ],
+      python_requires='>=3',
+      install_requires=read_install_requires(),
+      )
