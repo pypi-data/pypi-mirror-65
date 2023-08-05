3fefd32b73ad7ac12be32a14004510b2b0804876
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:35 +0000

    Add package fsds/0.2.4/fsds-0.2.4-py2.py3-none-any.whl

---
+++ packages/fsds/fsds-0.2.4-py2.py3-none-any.whl/fsds/__init__.py
@@ -0,0 +1,11 @@
+"""Top-level package for fsds."""
+
+__author__ = """James Irving"""
+__email__ = 'james.irving.phd@gmail.com'
+__version__ = '0.2.4'
+
+from .default import *
+
+from fsds_100719 import datasets
+from fsds_100719 import quick_refs
+# from .fsds import imports 
