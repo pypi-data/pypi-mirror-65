691858df2c274c80c99ca4d05397707641e83564
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:25 +0000

    Add package Sphinx-Substitution-Extensions/2020.4.5.0/Sphinx%20Substitution%20Extensions-2020.4.5.0.tar.gz

---
+++ packages/Sphinx-Substitution-Extensions/Sphinx%20Substitution%20Extensions-2020.4.5.0.tar.gz/Sphinx Substitution Extensions-2020.4.5.0/sample/source/conf.py
@@ -0,0 +1,11 @@
+"""
+Sample ``conf.py``.
+"""
+
+extensions = [
+    'sphinx_substitution_extensions',
+    'sphinx-prompt',
+]
+
+_substitution_pair = ('|author|', 'Eleanor')
+substitutions = [_substitution_pair]
