485c58f9128cd7366bc8b84402ee335bd18db05c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:04 +0000

    Add package hipims-io/0.2.3/hipims_io-0.2.3-py3-none-any.whl

---
+++ packages/hipims-io/hipims_io-0.2.3-py3-none-any.whl/hipims_io/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+init
+To do:
+    initialize a package
+Created on Wed Apr  1 14:56:15 2020
+
+@author: Xiaodong Ming
+"""
+from .input_setup import InputHipims
+from .output_setup import OutputHipims
+from .Raster import Raster
+from .Boundary import Boundary
