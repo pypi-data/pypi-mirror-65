ddc7b1d9f5e5fa10efc262117c57f25706c9b008
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:14 +0000

    Add package vk-dev/0.1.1/vk_dev-0.1.1.tar.gz

---
+++ packages/vk-dev/vk_dev-0.1.1.tar.gz/vk_dev-0.1.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="vk_dev",
+    version="0.1.1",
+    author="Yan",
+    author_email="deknowny@gmail.com",
+    description="Package for creating VK bots",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Rhinik/vk_bot",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
