86dc4d51281d907ab34ba4226f9e4c52bcc45656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:48 +0000

    Add package ayc/0.1/ayc-0.1.tar.gz

---
+++ packages/ayc/ayc-0.1.tar.gz/ayc-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ayc',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/aycandv/package-test',
+      author='Aycan Deniz Vit',
+      author_email='avit16@ku.edu.tr',
+      license='MIT',
+      packages=['ayc'],
+      zip_safe=False)
