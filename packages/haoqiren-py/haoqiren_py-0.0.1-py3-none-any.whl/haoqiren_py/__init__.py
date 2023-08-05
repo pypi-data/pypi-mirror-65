a41d187e84a5d28faf00e72583eb60b0090c812d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:16 +0000

    Add package haoqiren-py/0.0.1/haoqiren_py-0.0.1-py3-none-any.whl

---
+++ packages/haoqiren-py/haoqiren_py-0.0.1-py3-none-any.whl/haoqiren_py/__init__.py
@@ -0,0 +1,9 @@
+from . import base
+from . import io
+from . import math
+from . import net
+from . import office
+from . import ui
+
+packages = ['haoqiren_py', 'haoqiren_py.base', 'haoqiren_py.io', 'haoqiren_py.math', 'haoqiren_py.office',
+            'haoqiren_py.ui', 'haoqiren_py.net']
