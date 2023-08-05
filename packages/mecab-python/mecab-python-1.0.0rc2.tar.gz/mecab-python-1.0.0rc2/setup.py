cc3b0cb8d21e77ac59dd29bc4cbd303289213b2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:59 +0000

    Add package mecab-python/1.0.0rc2/mecab-python-1.0.0rc2.tar.gz

---
+++ packages/mecab-python/mecab-python-1.0.0rc2.tar.gz/mecab-python-1.0.0rc2/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name = "mecab-python",
+      description = "Redirect to mecab-python3",
+      long_description = "Use mecab-python3 instead.",
+      long_description_content_type = "text/markdown",
+      maintainer = "Paul O'Leary McCann",
+      maintainer_email = "polm@dampfkraft.com",
+      url = "https://github.com/SamuraiT/mecab-python3",
+      version='1.0.0rc2',
+      setup_requires=['mecab-python3'],
+      install_requires=['mecab-python3'],
+      )
