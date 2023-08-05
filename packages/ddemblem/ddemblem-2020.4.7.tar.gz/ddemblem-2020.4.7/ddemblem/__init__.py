a3fa1e3c7b21e247204011213c3e84f9cf9d7816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:20 +0000

    Add package ddemblem/2020.4.7/ddemblem-2020.4.7.tar.gz

---
+++ packages/ddemblem/ddemblem-2020.4.7.tar.gz/ddemblem-2020.4.7/ddemblem/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "2020.4.7"
+
+from ddemblem.Base import Roster as Roster
+from ddemblem.Base import createHeaders as CreateHeaders
+from ddemblem.Base import getSearchMsg as GetSearchMsg
+from ddemblem.Engine import Receiver as Receiver
+from ddemblem.Proxy import Proxy as Proxy
