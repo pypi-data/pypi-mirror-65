eb7a76cd7e2c6d308a84f2ea5bb0bf28a401e9c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:38 +0000

    Add package pangeamt-nlp/0.4.6/pangeamt_nlp-0.4.6-py3-none-any.whl

---
+++ packages/pangeamt-nlp/pangeamt_nlp-0.4.6-py3-none-any.whl/pangeamt_nlp/processor/validator/parallel_symbol.py
@@ -0,0 +1,37 @@
+from pangeamt_nlp.processor.base.validator_base import ValidatorBase
+from pangeamt_nlp.seg import Seg
+
+
+class ParallelSymbol(ValidatorBase):
+    NAME = "parallel_symbol"
+
+    DESCRIPTION_TRAINING = """
+            Remove a trans-unit if in source or target there is different number of symbols
+        """
+
+    DESCRIPTION_DECODING = """
+            Validators do not apply to decoding.
+        """
+
+    def __init__(self, src_lang: str, tgt_lang: str) -> None:
+        super().__init__(src_lang, tgt_lang)
+        self._chars = {
+            ':',
+            ';',
+            '#',
+            '%',
+            '[',
+            ']',
+            '(',
+            ')',
+            '{',
+            '}',
+            '<',
+            '>'
+        }
+
+    def validate(self, seg: Seg) -> bool:
+        for char in self._chars:
+            if seg.src.count(char) != seg.tgt.count(char):
+                return False
+        return True
