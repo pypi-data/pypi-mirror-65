da8082ff374a458dcc584838246966449906ba85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:37 +0000

    Add package mq-consumer/1.0.15/mq_consumer-1.0.15.tar.gz

---
+++ packages/mq-consumer/mq_consumer-1.0.15.tar.gz/mq_consumer-1.0.15/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+import mq_consumer
+
+setup(
+    name='mq_consumer',
+    version=mq_consumer.__version__,
+    author='Ruzzy Rullezz',
+    author_email='ruslan@lemimi.ru',
+    packages=find_packages(),
+    package_dir={'mq_consumer': 'mq_consumer'},
+    install_requires=[
+        'pika==1.1.0',
+    ],
+)
