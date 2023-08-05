cf0eaedcf0523bbb99983cba8390c4723e87182e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:23 +0000

    Add package jopkf/0.1.2/jopkf-0.1.2.tar.gz

---
+++ packages/jopkf/jopkf-0.1.2.tar.gz/jopkf-0.1.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, Extension, find_packages
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+with open('HISTORY.md') as history_file:
+    HISTORY = history_file.read()
+
+setup_args = dict(
+    name='jopkf',
+    version='0.1.2',
+    description='Useful tools to work with Elastic stack in Python',
+    long_description_content_type="text/markdown",
+    long_description=README + '\n\n' + HISTORY,
+    license='MIT',
+    packages=find_packages(),
+    author='Thuc Nguyen',
+    author_email='gthuc.nguyen@gmail.com',
+    keywords=['Elastic', 'ElasticSearch', 'ElasticStack'],
+    url='https://github.com/ncthuc/elastictools',
+    download_url='https://pypi.org/project/jopkf/'
+)
+
+install_requires = [
+    'elasticsearch>=6.0.0,<7.0.0',
+    'jinja2'
+]
+
+if __name__ == '__main__':
+    setup(**setup_args, install_requires=install_requires)
