13f02de784412084079c633f937b000f7986f2cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:39 +0000

    Add package pds-github-util/0.1.3/pds_github_util-0.1.3.tar.gz

---
+++ packages/pds-github-util/pds_github_util-0.1.3.tar.gz/pds_github_util-0.1.3/pds_github_util/python_snapshot_release.py
@@ -0,0 +1,46 @@
+import os
+import re
+import glob
+import logging
+from .snapshot_release import snapshot_release_publication
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+SNAPSHOT_TAG_SUFFIX = "+dev"
+
+
+def python_get_version():
+    setup_path = os.path.join(os.environ.get('GITHUB_WORKSPACE'), 'setup.py')
+    prog = re.compile("version=.*")
+    with open(setup_path, 'r') as f:
+        for line in f:
+            line = line.strip()
+            if prog.match(line):
+                return line[9:-2]
+
+
+def python_upload_assets(repo_name, tag_name, release):
+    """
+          Upload packages produced by python setup.py
+
+    """
+    package_pattern = os.path.join(os.environ.get('GITHUB_WORKSPACE'),
+                                        'dist',
+                                        '*')
+    packages = glob.glob(package_pattern)
+    for package in packages:
+        with open(package, 'rb') as f_asset:
+            asset_filename = os.path.basename(package)
+            logger.info(f"Upload asset file {asset_filename}")
+            release.upload_asset('application/zip',
+                                 asset_filename,
+                                 f_asset)
+
+
+def main():
+    snapshot_release_publication(SNAPSHOT_TAG_SUFFIX, python_get_version, python_upload_assets)
+
+
+if __name__ == "__main__":
+    main()
