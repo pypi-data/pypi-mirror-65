c909e60714b864f2cba9eff4c6ec4699f02c0321
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:12 +0000

    Add package compute-overtime/0.3/compute_overtime-0.3.tar.gz

---
+++ packages/compute-overtime/compute_overtime-0.3.tar.gz/compute_overtime-0.3/compute_overtime/__init__.py
@@ -0,0 +1,1 @@
+from .ComputeOvertime import OverTimeCompute
---
+++ packages/compute-overtime/compute_overtime-0.3.tar.gz/compute_overtime-0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='compute_overtime',
+      version='0.3',
+      description='Computation of Worked Overtime',
+      packages=['compute_overtime'],
+      author = 'Ikenna Nwosu',
+      author_email = 'ikenna.nwosu@kositechconsultingltd.com',
+      zip_safe=False)
