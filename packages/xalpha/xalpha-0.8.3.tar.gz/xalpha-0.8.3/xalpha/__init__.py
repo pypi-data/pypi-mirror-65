1886b2fe33fc6a65bf3b23b04d7d4d390517d4f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:23 +0000

    Add package xalpha/0.8.3/xalpha-0.8.3.tar.gz

---
+++ packages/xalpha/xalpha-0.8.3.tar.gz/xalpha-0.8.3/xalpha/__init__.py
@@ -0,0 +1,15 @@
+__version__ = "0.8.3"
+__author__ = "refraction-ray"
+__name__ = "xalpha"
+
+import xalpha.policy
+import xalpha.remain
+from xalpha.evaluate import evaluate
+from xalpha.info import fundinfo, indexinfo, cashinfo, mfundinfo, FundReport
+from xalpha.multiple import mul, mulfix, imul
+from xalpha.realtime import rfundinfo, review
+from xalpha.record import record, irecord
+from xalpha.trade import trade, itrade
+from xalpha.universal import get_daily, get_rt, get_bar, set_backend, set_handler
+from xalpha.provider import show_providers, set_proxy
+from xalpha.toolbox import PEBHistory, Compare, SWPEBHistory, QDIIPredict, set_holdings
