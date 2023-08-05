f828c39de563502234bc77575d97bf9c753baa28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:03 +0000

    Add package eupy/0.0.2/eupy-0.0.2.zip

---
+++ packages/eupy/eupy-0.0.2.zip/eupy-0.0.2/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+# from distutils.core import setup
+from setuptools import setup
+
+setup(name = 'eupy',
+      version = '0.0.2',
+      description = 'Python set of utils and libraries',
+      url = 'http://github.com/fivosts/eupy',
+      download_url = 'https://github.com/fivosts/eupy/archive/0.0.2.tar.gz',
+      author = 'Foivos Tsimpourlas',
+      author_email = 'fivos_ts@hotmail.com',
+      license = 'MIT',
+      install_requires = [ "datetime",
+                           "matplotlib",
+                           "numpy",
+                           "scrapy>=2.0.0",
+                           "seaborn",
+                           "pathlib",
+                        ],
+      package_dir = {'hermes'   : 'eupy/hermes',
+                     'mrcrawley': 'eupy/mrcrawley',
+                     'native'   : 'eupy/native',
+                    },
+      packages = ['mrcrawley', 'native', 'hermes'],
+      classifiers = [
+           "Programming Language :: Python :: 3.7",
+      ]
+      )
