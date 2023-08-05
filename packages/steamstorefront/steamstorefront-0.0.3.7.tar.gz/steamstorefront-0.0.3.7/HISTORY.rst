ab2887cc773c9748b6f56f105a5f75aa15581e77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:28 +0000

    Add package steamstorefront/0.0.3.7/steamstorefront-0.0.3.7.tar.gz

---
+++ packages/steamstorefront/steamstorefront-0.0.3.7.tar.gz/steamstorefront-0.0.3.7/HISTORY.rst
@@ -0,0 +1,17 @@
+=======
+History
+=======
+
+0.0.3 (2020-01-12)
+------------------
+- fixed bundle.py
+- added getLink()
+- general improvements
+
+0.0.2 (2020-01-10)
+------------------
+- requirement fixes.
+
+0.0.1 (2020-01-08)
+------------------
+* Initial build.
---
+++ packages/steamstorefront/steamstorefront-0.0.3.7.tar.gz/steamstorefront-0.0.3.7/steamstorefront/__init__.py
@@ -0,0 +1,12 @@
+"""Top-level package for Steam Store Front."""
+
+__author__ = """Santosh Panna"""
+__email__ = 'santoshpanna.sp@gmail.com'
+__version__ = '0.0.3.7'
+
+import builtins
+
+from .steamstorefront import SteamStoreFront
+from .errors import InvalidArgument
+
+builtins.InvalidArgument = InvalidArgument
