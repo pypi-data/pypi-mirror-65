c804a0a2320d7e2577b9dde59e58ed113a7a3de6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:46 +0000

    Add package jcfg/0.8.0/jcfg-0.8.0-py3-none-any.whl

---
+++ packages/jcfg/jcfg-0.8.0-py3-none-any.whl/jcfg/__init__.py
@@ -0,0 +1,5 @@
+from .json_config import JsonCfg
+
+from .error import JCfgInvalidKeyError, JCfgInvalidValueError, JCfgKeyNotFoundError, JCfgValueTypeMismatchError, JCfgInvalidSetValueError
+
+
---
+++ packages/jcfg/jcfg-0.8.0-py3-none-any.whl/jcfg/error.py
@@ -0,0 +1,24 @@
+
+
+class JCfgError(Exception):
+    pass
+
+
+class JCfgInvalidKeyError(JCfgError):
+    pass
+
+class JCfgInvalidValueError(JCfgError):
+    pass
+
+class JCfgKeyNotFoundError(JCfgError):
+    pass
+
+
+class JCfgValueTypeMismatchError(JCfgError):
+    pass
+
+class JCfgInvalidSetValueError(JCfgError):
+    pass
+
+class JCfgEmptyConfigError(JCfgError):
+    pass
