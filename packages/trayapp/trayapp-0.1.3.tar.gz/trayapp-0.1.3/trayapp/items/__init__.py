7e7681b11b7714a0a922398cc2ec4831859a902c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:01 +0000

    Add package trayapp/0.1.3/trayapp-0.1.3.tar.gz

---
+++ packages/trayapp/trayapp-0.1.3.tar.gz/trayapp-0.1.3/trayapp/items/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+__author__ = "Robin 'r0w' Weiland"
+__date__ = "2020-03-02"
+__version__ = "0.1.3"
+
+"""Library for creating system tray applications
+based on Moses Palmér's 'pystray' library
+See README for insntructions"""
+
+__all__ = ('RadioButtonGroup', 'SubMenu',)
+
+from trayapp.items.radiobutton import RadioButtonGroup
+from trayapp.items.submenu import SubMenu
+
+if __name__ == '__main__': pass
