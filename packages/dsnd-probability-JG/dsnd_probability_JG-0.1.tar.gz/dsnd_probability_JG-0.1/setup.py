1e87da9f71f66f471f27522a501a6334e7eb5588
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:40 +0000

    Add package dsnd-probability-JG/0.1/dsnd_probability_JG-0.1.tar.gz

---
+++ packages/dsnd-probability-JG/dsnd_probability_JG-0.1.tar.gz/dsnd_probability_JG-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_JG',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd_probability_JG'],
+      author='Joao Granja',
+      author_email='jgranja10+JG@gmail.com',
+      zip_safe=False)
