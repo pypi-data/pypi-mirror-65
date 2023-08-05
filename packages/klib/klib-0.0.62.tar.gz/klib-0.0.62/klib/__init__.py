f87ff13db7ad90bd70b5776773370b76ee1c2a7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:59 +0000

    Add package klib/0.0.62/klib-0.0.62.tar.gz

---
+++ packages/klib/klib-0.0.62.tar.gz/klib-0.0.62/klib/__init__.py
@@ -0,0 +1,13 @@
+"""
+Data Science Module for Python
+==================================
+klib is a Python module ... ...
+"""
+
+__author__ = """Andreas Kanz"""
+
+# from ._version import __version__
+from .describe import corr_plot, missingval_plot
+from .clean import convert_datatypes, data_cleaning, drop_missing
+
+__all__ = ['corr_plot', 'missingval_plot', 'convert_datatypes', 'data_cleaning', 'drop_missing']
