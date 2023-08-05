c27aee5d8b00b5393076a5b974d9653c49d7d87d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:14 +0000

    Add package timidity/0.1.0/timidity-0.1.0.tar.gz

---
+++ packages/timidity/timidity-0.1.0.tar.gz/timidity-0.1.0/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md") as fh:
+    long_desc = fh.read()
+
+setuptools.setup(
+    name="timidity",
+    version="0.1.0",
+    description="A lightweight Python MIDI player",
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JinayJain/timidity",
+    author="Jinay Jain",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "simpleaudio",
+    ]
+)
---
+++ packages/timidity/timidity-0.1.0.tar.gz/timidity-0.1.0/timidity/__main__.py
@@ -0,0 +1,31 @@
+from timidity.parser import parse_midi
+from timidity.player import play_notes
+from glob import glob
+from scipy.io import wavfile
+import os
+from scipy.signal import sawtooth, square
+import numpy as np
+
+
+def run():
+    midi_list = glob("songs/*.mid")
+
+    i = 1
+    for filename in midi_list:
+        print(f"{i}.) {filename}")
+        i += 1
+    select_idx = int(input("Select a file: ")) - 1
+    selection = midi_list[select_idx]
+
+    notes, tpq, bpm = parse_midi(selection)
+    audio, player = play_notes(notes, tpq, bpm, np.sin)
+
+    basename = os.path.splitext(os.path.basename(selection))[0]
+    wavfile.write(f"wav/{basename}.wav", 44100, audio)
+
+    player.wait_done()
+    print("Done!")
+
+
+if __name__ == "__main__":
+    run()
