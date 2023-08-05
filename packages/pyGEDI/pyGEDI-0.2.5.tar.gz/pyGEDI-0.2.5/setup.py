1da8541a5da3927c9ce626fc53cd669cbe4282c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:18 +0000

    Add package pyGEDI/0.2.5/pyGEDI-0.2.5.tar.gz

---
+++ packages/pyGEDI/pyGEDI-0.2.5.tar.gz/pyGEDI-0.2.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+ 
+setup(
+    name='pyGEDI',
+    packages=['pyGEDI'], 
+    version='0.2.5',
+    description='pyGED is a Python Package for NASA Global Ecosystem Dynamics Investigation (GEDI) mission data extraction, analysis, processing and visualization.',
+    author='EduinHSERNA',
+    url='https://github.com/EduinHSERNA/pyGEDI',
+    keywords='GEDI'
+)
