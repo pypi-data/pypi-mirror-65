208a12bfc684297f8ae18416501f4d363b6547ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:00 +0000

    Add package dimsdk/0.3.13/dimsdk-0.3.13.tar.gz

---
+++ packages/dimsdk/dimsdk-0.3.13.tar.gz/dimsdk-0.3.13/dimsdk/crypto/utils.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+#
+#   DIM-SDK : Decentralized Instant Messaging Software Development Kit
+#
+#                                Written in 2019 by Moky <albert.moky@gmail.com>
+#
+# ==============================================================================
+# MIT License
+#
+# Copyright (c) 2019 Albert Moky
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ==============================================================================
+
+import hashlib
+
+from binascii import b2a_hex, a2b_hex
+
+
+def sha1(data: bytes) -> bytes:
+    """ SHA1 Digest """
+    return hashlib.sha1(data).digest()
+
+
+def hex_encode(data: bytes) -> str:
+    """ HEX Encode """
+    return b2a_hex(data).decode('utf-8')
+
+
+def hex_decode(string: str) -> bytes:
+    """ HEX Decode """
+    return a2b_hex(string)
