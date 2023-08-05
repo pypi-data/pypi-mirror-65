7d49adab8bc8d1310f3202b698948b155c4b720d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:37 +0000

    Add package polysdk/0.0.3/polysdk-0.0.3-py3-none-any.whl

---
+++ packages/polysdk/polysdk-0.0.3-py3-none-any.whl/polysdk/polyclient.py
@@ -0,0 +1,100 @@
+import requests
+import json
+import logging
+import csv
+import sys
+
+from polysdk.models.maskeddata import MaskedData
+from polysdk.models.unmaskeddata import UnmaskedData
+
+
+class PolyClient:
+    __base_url = "https://v1.polymerapp.io:4560"
+    __mask_api_url = __base_url + "/v1/pub/mask"
+    __unmask_api_url = __base_url + "/v1/pub/unmask"
+
+    __api_token = ""
+    __api_headers = {
+        'Content-Type': 'application/json',
+        'Public-Token': ''
+    }
+
+    @staticmethod
+    def __validate_args(text, key):
+        return text is not '' and key is not ''
+
+    @staticmethod
+    def __validate_file_args(file_path, key):
+        return file_path is not '' and key is not ''
+
+    def __init__(self, api_token):
+        if api_token is '' or len(api_token) != 32:
+            exception_msg = "api_token is empty or invalid."
+            logging.exception(exception_msg)
+            raise Exception(exception_msg)
+
+        self.__api_token = api_token
+        self.__api_headers['Public-Token'] = self.__api_token
+
+    def mask_text(self, text, key):
+        if not self.__validate_args(text, key):
+            raise Exception("text or key is empty.")
+
+        payload = {
+            'text': text,
+            'key': key
+        }
+
+        r = requests.post(self.__mask_api_url, data=json.dumps(payload), headers=self.__api_headers)
+
+        try:
+            logging.info('Status code: ' + str(r.status_code))
+            logging.info('Response body: ' + str(r.json()))
+            response_body = r.json()
+
+            data = MaskedData(text=response_body['text'])
+            return data
+        except Exception:
+            logging.exception('Error: ' + r.text)
+            raise Exception(r.text)
+
+    def unmask_text(self, text, key):
+        if not self.__validate_args(text, key):
+            raise Exception("text or key is empty.")
+
+        payload = {
+            'text': text,
+            'key': key
+        }
+
+        r = requests.post(self.__unmask_api_url, data=json.dumps(payload), headers=self.__api_headers)
+
+        try:
+            logging.info('Status code: ' + str(r.status_code))
+            logging.info('Response body: ' + str(r.json()))
+            response_body = r.json()
+
+            data = UnmaskedData(text=response_body['text'])
+
+            return data
+        except Exception:
+            logging.exception('Error: ' + r.text)
+            raise Exception(r.text)
+
+    def mask_file(self, file_path, key):
+        if not self.__validate_file_args(file_path, key):
+            raise Exception("file_path or key is empty.")
+
+        with open(file_path) as file:
+            text = file.read()
+            masked_data = self.mask_text(text=text, key=key)
+            return masked_data
+
+    def unmask_file(self, file_path, key):
+        if not self.__validate_file_args(file_path, key):
+            raise Exception("file_path or key is empty.")
+
+        with open(file_path) as file:
+            text = file.read()
+            unmasked_data = self.unmask_text(text=text, key=key)
+            return unmasked_data
