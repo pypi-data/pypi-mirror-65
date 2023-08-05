7b783273529d5bb99863bdaf7ae72da8dfcf7754
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:35 +0000

    Add package dmoj-tool-dessertion/0.0.5/dmoj-tool-dessertion-0.0.5.tar.gz

---
+++ packages/dmoj-tool-dessertion/dmoj-tool-dessertion-0.0.5.tar.gz/dmoj-tool-dessertion-0.0.5/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="dmoj-tool-dessertion",
+    version="0.0.5",
+    author="Desertion",
+    author_email="73731354pi@gmail.com",
+    description="CLI submission to DMOJ",
+    scripts=['bin/dmoj-tool'],
+    packages=setuptools.find_packages(),
+    install_requires=['beautifulsoup4','bs4','certifi','chardet','idna','requests','soupsieve','urllib3'],
+    python_requires='>=3.6'
+)
