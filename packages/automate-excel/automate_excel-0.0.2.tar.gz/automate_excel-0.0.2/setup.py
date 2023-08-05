f5e0bd73659e8e1bf8dcd400941cd0459f850d9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:37 +0000

    Add package automate-excel/0.0.2/automate_excel-0.0.2.tar.gz

---
+++ packages/automate-excel/automate_excel-0.0.2.tar.gz/automate_excel-0.0.2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+import src
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as req:
+    reqs = req.read().splitlines()
+
+setuptools.setup(
+    name="automate_excel",
+    version=src.__version__,
+    author="Chris Charlton",
+    author_email="chrispcharlton@gmail.com",
+    description="A library for automating existing spreadsheets.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/chrispcharlton/automate_excel",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    install_requires=reqs,
+    python_requires='>=3.7',
+)
