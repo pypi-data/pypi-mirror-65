92149565019beb1205d542a19781cacb18cd8493
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:33 +0000

    Add package gcloud-rest-kms/3.2.0/gcloud_rest_kms-3.2.0-py2.py3-none-any.whl

---
+++ packages/gcloud-rest-kms/gcloud_rest_kms-3.2.0-py2.py3-none-any.whl/gcloud/rest/kms/__init__.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+from future import standard_library
+standard_library.install_aliases()
+from pkg_resources import get_distribution
+__version__ = get_distribution('gcloud-rest-kms').version
+
+from gcloud.rest.kms.kms import KMS
+from gcloud.rest.kms.kms import SCOPES
+from gcloud.rest.kms.utils import decode
+from gcloud.rest.kms.utils import encode
+
+
+__all__ = ['__version__', 'decode', 'encode', 'KMS', 'SCOPES']
