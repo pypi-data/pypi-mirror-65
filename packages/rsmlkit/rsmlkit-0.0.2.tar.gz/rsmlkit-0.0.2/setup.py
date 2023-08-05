6a3053771505ea74c1a538bbd53dd721bb4d14f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:37 +0000

    Add package rsmlkit/0.0.2/rsmlkit-0.0.2.tar.gz

---
+++ packages/rsmlkit/rsmlkit-0.0.2.tar.gz/rsmlkit-0.0.2/setup.py
@@ -0,0 +1,44 @@
+import setuptools
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+__version__ = '0.0.2'
+url = 'https://github.com/raeidsaqur/rsmlkit'
+
+install_requires = [
+    'numpy',
+    'scipy',
+    'networkx',
+    'scikit-learn',
+    'scikit-image',
+    'requests',
+    'plyfile',
+    'pandas',
+    'h5py'
+]
+
+setup_requires = ['pytest-runner']
+tests_require = ['pytest', 'pytest-cov', 'mock']
+
+setup(
+    name="rsmlkit",
+    version=__version__,
+    author="Raeid Saqur",
+    author_email="rsaqur@cs.princeton.edu",
+    description="Auxialiary package for running experiments",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=url,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=install_requires,
+    setup_requires=setup_requires,
+    tests_require=tests_require,
+    packages=find_packages(),
+)
