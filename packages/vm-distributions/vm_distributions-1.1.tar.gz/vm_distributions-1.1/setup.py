77aa197ae087c67a6039749c08e68740bf75b25f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:22 +0000

    Add package vm-distributions/1.1/vm_distributions-1.1.tar.gz

---
+++ packages/vm-distributions/vm_distributions-1.1.tar.gz/vm_distributions-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='vm_distributions',
+      version='1.1',
+      description='Gaussian distributions',
+      packages=['vm_distributions'],
+      author = "Vishal Madheshia",
+      author_email = "vishalmadheshia@gmail.com",
+      zip_safe=False)
