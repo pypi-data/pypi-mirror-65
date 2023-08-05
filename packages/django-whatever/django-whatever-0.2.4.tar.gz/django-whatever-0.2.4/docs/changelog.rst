17690d781a82059264f52f27b0ce15765b5d3d1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:32 +0000

    Add package django-whatever/0.2.4/django-whatever-0.2.4.tar.gz

---
+++ packages/django-whatever/django-whatever-0.2.4.tar.gz/django-whatever-0.2.4/docs/changelog.rst
@@ -0,0 +1,53 @@
+.. _changelog:
+
+Changelog
+=========
+
+0.2.4
+~~~~~
+
+* A bunch of maintenance updates that happened during the "unmaintained" mode.
+* The last version with Django 1.11, 2.0, and 2.1 support
+
+0.2.3
+~~~~~
+
+* Values for custom fields are created with their parent generator if the field is not registered (#22)
+
+0.2.2
+~~~~~
+
+* Fixed pip installation
+* Added ability to login_as arbitrary user
+
+0.2.1
+~~~~~
+
+* Added xunit reference docs
+* Fixed ``any_text_field`` return value
+* Updated setup.py to use versiontools
+* Minor updates and bugfixes in docs and LICENCE
+
+0.2.0
+~~~~~
+
+* Fixed ``ImportError: cannot import name _strclass`` for python 2.7
+* Added ``any_model_with_defaults`` function to generate models with default values where acceptible
+* Fixed tests for django 1.4 compatibility
+* Added support for GenericIPAddressField in both model and forms (django 1.4 and above)
+* Multiple minor updates and bugfixes in docs
+
+0.1.0
+~~~~~
+
+* Forked django-any and renamed to django-whatever
+* Created complete documentation for package
+* Models with ``GenericForeignKey`` can be created with ``any_model(MyModel, content_object=object)``
+* Self-referencing models no longer produce ``"RuntimeError: maximum recursion depth exceeded"``
+* ``ImageField`` and naive callable ``upload_to`` support.
+
+
+django-any changelog
+~~~~~~~~~~~~~~~~~~~~
+
+Not maintained
---
+++ packages/django-whatever/django-whatever-0.2.4.tar.gz/django-whatever-0.2.4/setup.py
@@ -0,0 +1,47 @@
+from os import path
+import codecs
+import os
+from setuptools import setup
+
+os.environ['DJANGO_SETTINGS_MODULE'] = "testproject.settings"
+
+read = lambda filepath: codecs.open(filepath, 'r', 'utf-8').read()
+
+setup(
+    name='django-whatever',
+    version=":versiontools:django_any:",
+    description='Unobtrusive test models creation for django.',
+    long_description=read(path.join(path.dirname(__file__), 'README.rst')),
+    author='Sergey Fursov',
+    author_email='geyser85@gmail.com',
+    url='http://github.com/ivelum/django-whatever',
+    packages=['django_any', 'django_any.contrib'],
+    include_package_data=True,
+    test_suite="tests.manage",
+    zip_safe=False,
+    setup_requires=[
+        'versiontools >= 1.8',
+    ],
+    license='MIT License',
+    platforms=['any'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
+        'Topic :: Software Development :: Testing :: Mocking',
+    ]
+)
