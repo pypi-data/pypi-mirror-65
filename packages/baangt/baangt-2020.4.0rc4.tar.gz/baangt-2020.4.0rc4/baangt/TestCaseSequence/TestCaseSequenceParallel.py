4c3ccc0d78995bbf4d263e4842eafcf5b08ddc43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:20 +0000

    Add package baangt/2020.4.0rc4/baangt-2020.4.0rc4.tar.gz

---
+++ packages/baangt/baangt-2020.4.0rc4.tar.gz/baangt-2020.4.0rc4/baangt/TestCaseSequence/TestCaseSequenceParallel.py
@@ -0,0 +1,42 @@
+import multiprocessing
+import logging
+from baangt.TestSteps import Exceptions
+from baangt.base import GlobalConstants as GC
+from datetime import datetime
+import time
+import gevent.queue
+logger = logging.getLogger("pyC")
+
+
+class TestCaseSequenceParallel:
+    def __init__(self, sequenceNumber: int, tcNumber: int, testcaseSequence=None, **kwargs):
+        self.sequenceNumber = sequenceNumber
+        self.dataRecord = kwargs.get(GC.KWARGS_DATA)
+        self.tcNumber = tcNumber
+        self.testcaseSequence = testcaseSequence
+        self.kwargs = kwargs
+
+    def one_sequence(self, results: gevent.queue.Queue):
+        dataRecord = self.dataRecord
+        currentRecordNumber = self.tcNumber
+        testcaseSequence = self.testcaseSequence
+        parallelizationSequenceNumber = self.sequenceNumber
+        
+        logger.info(f"Starting one_sequence with SequenceNumber = {parallelizationSequenceNumber}, "
+                    f"CurrentRecordNumber is {currentRecordNumber}")
+
+        if not dataRecord:
+            logger.warning("dataRecord was empty - doing nothing")
+            return
+
+        try:
+            self.kwargs[GC.KWARGS_TESTRUNINSTANCE].executeDictSequenceOfClasses(testcaseSequence,
+                                                                                GC.STRUCTURE_TESTCASE,
+                                                                                **self.kwargs)
+
+        except Exceptions.baangtTestStepException as e:
+            logger.critical(f"Unhandled Error happened in parallel run {parallelizationSequenceNumber}: " + str(e))
+            dataRecord[GC.TESTCASESTATUS] = GC.TESTCASESTATUS_ERROR
+        finally:
+            d_t = datetime.fromtimestamp(time.time())
+            results.put([{self.tcNumber: dataRecord}, {self.sequenceNumber:  [self.tcNumber, d_t]}])
