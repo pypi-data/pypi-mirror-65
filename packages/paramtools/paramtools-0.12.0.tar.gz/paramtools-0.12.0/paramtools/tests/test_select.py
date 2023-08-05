662f023583674e4fd4fdfb98c5474523dd7d68fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:45 +0000

    Add package paramtools/0.12.0/paramtools-0.12.0.tar.gz

---
+++ packages/paramtools/paramtools-0.12.0.tar.gz/paramtools-0.12.0/paramtools/tests/test_select.py
@@ -0,0 +1,41 @@
+import pytest
+
+from paramtools.select import select_eq, select_gt, select_lt
+
+
+@pytest.fixture
+def vos():
+    return [
+        {"d0": 1, "d1": "hello", "value": 1},
+        {"d0": 1, "d1": "world", "value": 1},
+        {"d0": 2, "d1": "hello", "value": 1},
+        {"d0": 3, "d1": "world", "value": 1},
+    ]
+
+
+def test_select_eq(vos):
+    assert list(select_eq(vos, True, labels={"d0": 1, "d1": "hello"})) == [
+        {"d0": 1, "d1": "hello", "value": 1}
+    ]
+
+    assert list(
+        select_eq(vos, True, labels={"d0": [1, 2], "d1": "hello"})
+    ) == [
+        {"d0": 1, "d1": "hello", "value": 1},
+        {"d0": 2, "d1": "hello", "value": 1},
+    ]
+
+
+def test_select_gt(vos):
+    assert list(select_gt(vos, True, labels={"d0": 1})) == [
+        {"d0": 2, "d1": "hello", "value": 1},
+        {"d0": 3, "d1": "world", "value": 1},
+    ]
+
+
+def test_select_lt(vos):
+    assert list(select_lt(vos, True, labels={"d0": 3})) == [
+        {"d0": 1, "d1": "hello", "value": 1},
+        {"d0": 1, "d1": "world", "value": 1},
+        {"d0": 2, "d1": "hello", "value": 1},
+    ]
