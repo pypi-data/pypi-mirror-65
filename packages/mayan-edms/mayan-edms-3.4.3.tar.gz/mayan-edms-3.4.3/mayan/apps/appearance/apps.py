46747016b8614db54a3300975d533c1cdd8e6804
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:51 +0000

    Add package mayan-edms/3.4.3/mayan-edms-3.4.3.tar.gz

---
+++ packages/mayan-edms/mayan-edms-3.4.3.tar.gz/mayan-edms-3.4.3/mayan/apps/appearance/apps.py
@@ -0,0 +1,13 @@
+from django.utils.translation import ugettext_lazy as _
+
+from mayan.apps.common.apps import MayanAppConfig
+
+
+class AppearanceApp(MayanAppConfig):
+    app_url = 'appearance'
+    has_tests = True
+    name = 'mayan.apps.appearance'
+    verbose_name = _('Appearance')
+
+    def ready(self):
+        super(AppearanceApp, self).ready()
