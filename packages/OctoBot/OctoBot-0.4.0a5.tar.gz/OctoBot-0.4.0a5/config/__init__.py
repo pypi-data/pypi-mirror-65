1430e4fa458b8f4af780a943ac1ee108484643a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:25 +0000

    Add package OctoBot/0.4.0a5/OctoBot-0.4.0a5.tar.gz

---
+++ packages/OctoBot/OctoBot-0.4.0a5.tar.gz/OctoBot-0.4.0a5/config/__init__.py
@@ -0,0 +1,45 @@
+#  Drakkar-Software OctoBot
+#  Copyright (c) Drakkar-Software, All rights reserved.
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 3.0 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library.
+
+PROJECT_NAME = "OctoBot"
+SHORT_VERSION = "0.4.0"  # major.minor.revision
+PATCH_VERSION = ""  # patch : pX
+VERSION_DEV_PHASE = "a"  # alpha : a / beta : b / release candidate : rc
+VERSION_PHASE = "5"  # XX
+VERSION = f"{SHORT_VERSION}{VERSION_DEV_PHASE}{VERSION_PHASE}"
+LONG_VERSION = f"{SHORT_VERSION}{PATCH_VERSION}{VERSION_DEV_PHASE}{VERSION_PHASE}"
+
+# logs
+LOGS_FOLDER = "logs"
+
+# terms of service
+CONFIG_ACCEPTED_TERMS = "accepted_terms"
+
+# DEBUG
+CONFIG_DEBUG_OPTION = "DEV-MODE"
+FORCE_ASYNCIO_DEBUG_OPTION = False
+
+# Files
+CONFIG_FILE = "config.json"
+TEMP_RESTORE_CONFIG_FILE = "temp_config.json"
+CONFIG_FOLDER = "config"
+SCHEMA = "schema"
+CONFIG_FILE_SCHEMA = f"{CONFIG_FOLDER}/config_{SCHEMA}.json"
+DEFAULT_CONFIG_FILE = f"{CONFIG_FOLDER}/default_config.json"
+LOGGING_CONFIG_FILE = f"{CONFIG_FOLDER}/logging_config.ini"
+LOG_FILE = f"{LOGS_FOLDER}/{PROJECT_NAME}.log"
+
+OCTOBOT_KEY = b'uVEw_JJe7uiXepaU_DR4T-ThkjZlDn8Pzl8hYPIv7w0='
