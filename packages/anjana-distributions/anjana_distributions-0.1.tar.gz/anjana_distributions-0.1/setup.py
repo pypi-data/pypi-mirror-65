1174b9323741339c7bfa700970a921a1120d6c97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:23 +0000

    Add package anjana-distributions/0.1/anjana_distributions-0.1.tar.gz

---
+++ packages/anjana-distributions/anjana_distributions-0.1.tar.gz/anjana_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='anjana_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['anjana_distributions'],
+      zip_safe=False)
