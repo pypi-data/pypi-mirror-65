1f7f9d033975d953778a74e2080a634531425291
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:07 +0000

    Add package dagster/0.7.6rc0/dagster-0.7.6rc0.tar.gz

---
+++ packages/dagster/dagster-0.7.6rc0.tar.gz/dagster-0.7.6rc0/dagster_tests/core_tests/definitions_tests/test_preset_definition.py
@@ -0,0 +1,102 @@
+import pytest
+
+from dagster import (
+    Bool,
+    DagsterInvalidDefinitionError,
+    DagsterInvariantViolationError,
+    ModeDefinition,
+    PipelineDefinition,
+    PresetDefinition,
+    execute_pipeline_with_preset,
+    lambda_solid,
+    solid,
+)
+from dagster.utils import file_relative_path
+
+
+def test_presets():
+    @solid(config={'error': Bool})
+    def can_fail(context):
+        if context.solid_config['error']:
+            raise Exception('I did an error')
+        return 'cool'
+
+    @lambda_solid
+    def always_fail():
+        raise Exception('I always do this')
+
+    pipeline = PipelineDefinition(
+        name='simple',
+        solid_defs=[can_fail, always_fail],
+        preset_defs=[
+            PresetDefinition.from_files(
+                'passing',
+                environment_files=[file_relative_path(__file__, 'pass_env.yaml')],
+                solid_subset=['can_fail'],
+            ),
+            PresetDefinition.from_files(
+                'passing_overide_to_fail',
+                environment_files=[file_relative_path(__file__, 'pass_env.yaml')],
+                solid_subset=['can_fail'],
+            ).with_additional_config({'solids': {'can_fail': {'config': {'error': True}}}}),
+            PresetDefinition(
+                'passing_direct_dict',
+                environment_dict={'solids': {'can_fail': {'config': {'error': False}}}},
+                solid_subset=['can_fail'],
+            ),
+            PresetDefinition.from_files(
+                'failing_1',
+                environment_files=[file_relative_path(__file__, 'fail_env.yaml')],
+                solid_subset=['can_fail'],
+            ),
+            PresetDefinition.from_files(
+                'failing_2', environment_files=[file_relative_path(__file__, 'pass_env.yaml')]
+            ),
+        ],
+    )
+
+    with pytest.raises(DagsterInvalidDefinitionError):
+        PresetDefinition.from_files(
+            'invalid_1', environment_files=[file_relative_path(__file__, 'not_a_file.yaml')]
+        )
+
+    with pytest.raises(DagsterInvariantViolationError):
+        PresetDefinition.from_files(
+            'invalid_2',
+            environment_files=[file_relative_path(__file__, 'test_repository_definition.py')],
+        )
+
+    assert execute_pipeline_with_preset(pipeline, 'passing').success
+
+    assert execute_pipeline_with_preset(pipeline, 'passing_direct_dict').success
+    assert (
+        execute_pipeline_with_preset(pipeline, 'failing_1', raise_on_error=False).success == False
+    )
+
+    assert (
+        execute_pipeline_with_preset(pipeline, 'failing_2', raise_on_error=False).success == False
+    )
+
+    with pytest.raises(DagsterInvariantViolationError, match='Could not find preset'):
+        execute_pipeline_with_preset(pipeline, 'not_failing', raise_on_error=False)
+
+    assert (
+        execute_pipeline_with_preset(
+            pipeline, 'passing_overide_to_fail', raise_on_error=False
+        ).success
+        == False
+    )
+
+
+def test_invalid_preset():
+    @lambda_solid
+    def lil_solid():
+        return ';)'
+
+    with pytest.raises(DagsterInvalidDefinitionError, match='mode "mode_b" which is not defined'):
+        PipelineDefinition(
+            name="preset_modes",
+            solid_defs=[lil_solid],
+            mode_defs=[ModeDefinition(name="mode_a")],
+            preset_defs=[PresetDefinition(name="preset_b", mode="mode_b")],
+        )
