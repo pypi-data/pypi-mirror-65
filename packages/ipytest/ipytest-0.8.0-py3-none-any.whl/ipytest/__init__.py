a032713863b8bca3e6d763e3ce4b43bbf565d596
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:01 +0000

    Add package ipytest/0.8.0/ipytest-0.8.0-py3-none-any.whl

---
+++ packages/ipytest/ipytest-0.8.0-py3-none-any.whl/ipytest/__init__.py
@@ -0,0 +1,39 @@
+from ._config import config, autoconfig
+from ._unittest_support import (
+    run_tests,
+    collect_tests,
+    assert_equals,
+    get_assert_function,
+    unittest_assert_equals,
+)
+from ._util import clean_tests, reload, emit_deprecation_warning, running_as_test
+
+try:
+    import pytest as _pytest  # noqa: F401
+
+except ImportError:
+    emit_deprecation_warning("ipytest will require pytest in future releases")
+    _has_pytest = False
+
+else:
+    from ._pytest_support import run, run_pytest  # noqa: F401
+
+    _has_pytest = True
+
+
+# the pytest exit code
+exit_code = None
+
+
+__all__ = (["run_pytest", "run"] if _has_pytest else []) + [
+    "config",
+    "exit_code",
+    "run_tests",
+    "clean_tests",
+    "collect_tests",
+    "assert_equals",
+    "get_assert_function",
+    "unittest_assert_equals",
+    "reload",
+    "running_as_test",
+]
