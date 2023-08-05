57e0e52b4d77d1ef1b429fcfde1620fda2e5b723
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:44 +0000

    Add package pebaz/0.0.65/pebaz-0.0.65-cp38-cp38-win_amd64.whl

---
+++ packages/pebaz/pebaz-0.0.65-cp38-cp38-win_amd64.whl/pebaz/foozle.py
@@ -0,0 +1,12 @@
+import nimporter
+from pebaz.bazzle import func1
+from pebaz.barzzle import func2
+
+def foo():
+    print('foo')
+
+def bar():
+    print('bar' + str(func1()))
+
+def baz():
+    print('baz' + str(func2()))
