f4d063268e80cc91d0ec4fc2f953a6270ebab78a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:09 +0000

    Add package piconlib/0.0.1/piconlib-0.0.1.tar.gz

---
+++ packages/piconlib/piconlib-0.0.1.tar.gz/piconlib-0.0.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="piconlib", # Replace with your own username
+    version="0.0.1",
+    author="Tau5",
+    author_email="tau0@tutanota.com",
+    description="A library to interact with Picon's system tree",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Tau5/piconlib",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
