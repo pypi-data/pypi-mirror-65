ca3979e3ef827be847c14ec62a47d9cea1d4e650
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:42 +0000

    Add package dspl/0.0.3/dspl-0.0.3.tar.gz

---
+++ packages/dspl/dspl-0.0.3.tar.gz/dspl-0.0.3/setup.py
@@ -0,0 +1,34 @@
+import os
+import os.path
+from setuptools import setup, find_packages
+
+with open("README.md") as f:
+    readme = f.read()
+
+def find_requires():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    requirements = []
+    with open("{0}/requirements.txt".format(dir_path), "r") as reqs:
+        requirements = reqs.readlines()
+    return requirements    
+
+setup(
+    name="dspl",
+    packages=["dspl"],
+    version="0.0.3",
+    install_required=find_requires(),
+    zip_safe=True,
+    author="Nikita Varganov",
+    author_email="nikita.varganov.ml@gmail.com",
+    license="MIT",
+    description="Library for using in DS-Platform",
+    url="https://github.com/NV-27/dspl",
+    download_url="https://github.com/NV-27/dspl/archive/0.0.3.tar.gz",
+    keywords=["dspl", "ds_platform", "ds_template"],
+    ling_description=readme,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.6",
+        "Operating System :: OS Independent"
+    ]
+)
