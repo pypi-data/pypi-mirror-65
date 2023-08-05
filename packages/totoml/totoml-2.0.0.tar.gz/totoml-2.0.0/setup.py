62b8dfbd69d7cd3a03b4d95a52f16331edf8604e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:04 +0000

    Add package totoml/2.0.0/totoml-2.0.0.tar.gz

---
+++ packages/totoml/totoml-2.0.0.tar.gz/totoml-2.0.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools 
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="totoml", # Replace with your own username
+    version="2.0.0",
+    author="nicolasances",
+    author_email="nicolasances@gmail.com",
+    description="TotoML SDK for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nicolasances/py-totoml",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
