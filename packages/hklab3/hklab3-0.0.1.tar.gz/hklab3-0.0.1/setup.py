2f954f5fb2542398dd8c0a0dcb0afd3b272cb75f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:32 +0000

    Add package hklab3/0.0.1/hklab3-0.0.1.tar.gz

---
+++ packages/hklab3/hklab3-0.0.1.tar.gz/hklab3-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name="hklab3",
+      version="0.0.1",
+      url="http://github.com/kimhyokwan/hklab2",
+      license="MIT",
+      author="kimhyokwan",
+      author_email="haiteam@kopo.ac.kr",
+      keywords = ["calendar","isoweek","listsum"],
+      description = "isoweek calculation and so on",
+      packages=["hklab2","hklab3"],
+      install_requires=["isoweek"] 
+     )
