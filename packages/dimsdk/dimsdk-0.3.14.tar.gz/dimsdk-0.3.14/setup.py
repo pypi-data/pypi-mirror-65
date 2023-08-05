e545e5dea1443884fffb6ad9afe3732bedbc17b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:42 +0000

    Add package dimsdk/0.3.14/dimsdk-0.3.14.tar.gz

---
+++ packages/dimsdk/dimsdk-0.3.14.tar.gz/dimsdk-0.3.14/setup.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    Decentralized Instant Messaging Software Development Kit
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    This is a new protocol designed for instant messaging (IM).
+    The software provides accounts(user identity recognition) and
+    communications between accounts safely by end-to-end encryption.
+"""
+
+from setuptools import setup, find_packages
+
+__version__ = '0.3.14'
+__author__ = 'Albert Moky'
+__contact__ = 'albert.moky@gmail.com'
+
+with open('README.md', 'r') as fh:
+    readme = fh.read()
+
+setup(
+    name='dimsdk',
+    version=__version__,
+    url='https://github.com/dimchat/sdk-py',
+    license='MIT',
+    author=__author__,
+    author_email=__contact__,
+    description='Decentralized Instant Messaging SDK',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    package_data={
+        '': ['*.js']
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        'dimp>=0.8.16',
+        # 'dkd>=0.7.1',
+        # 'mkm>=0.8.10',
+        'apns2',  # 0.4.1
+        'numpy',  # 1.15.4
+    ]
+)
