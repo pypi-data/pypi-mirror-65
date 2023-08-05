e9e5df25c9a07ab89c0619f528274175f63cf2eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:21 +0000

    Add package quickstart-rhy/0.2.3/QuickStart_Rhy-0.2.3.tar.gz

---
+++ packages/quickstart-rhy/QuickStart_Rhy-0.2.3.tar.gz/QuickStart_Rhy-0.2.3/QuickStart_Rhy/imagedeal.py
@@ -0,0 +1,28 @@
+import sys
+import os
+
+
+def set_img_background():
+    from QuickStart_Rhy.ImageTools.ColorTools import transport_back, get_color_from_str
+
+    try:
+        img = sys.argv[2]
+        if img == '-help':
+            raise IndexError
+        if not os.path.exists(img):
+            exit('[ERROR] No Such File: %s' % img)
+        to = sys.argv[3]
+        try:
+            frm = sys.argv[4]
+        except IndexError:
+            frm = '0,0,0,0'
+    except IndexError:
+        print('Usage: qs -stbg picture to_color [from_color: default transparency]')
+        exit(0)
+    else:
+        to = get_color_from_str(to)
+        frm = get_color_from_str(frm)
+        img = transport_back(img, to, frm)
+        iname = sys.argv[2].split('.')
+        iname = iname[0] + '_stbg.' + ''.join(iname[1:])
+        img.save(iname)
