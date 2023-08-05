26bf41d937c4d831b7703a22f197e91a0698c0b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:34 +0000

    Add package pyproftool/0.2.1/pyproftool-0.2.1.tar.gz

---
+++ packages/pyproftool/pyproftool-0.2.1.tar.gz/pyproftool-0.2.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='pyproftool',
+    version='0.2.1',
+    packages=[''],
+    url='https://github.com/conradoad/PyProfTool',
+    license='',
+    author='conradoad',
+    author_email='conradocostaad@yahoo.com.br',
+    description=''
+)
