1ba9776b28dfa1fca146e00c53efce9dbddc9200
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:14 +0000

    Add package ezpp/0.1.0/ezpp-0.1.0-py3-none-any.whl

---
+++ packages/ezpp/ezpp-0.1.0-py3-none-any.whl/zk_logo_maker/__init__.py
@@ -0,0 +1,16 @@
+__all__ = (
+    "__title__", "__summary__", "__uri__", "__version__", "__author__",
+    "__email__", "__license__", "__copyright__",
+)
+
+__title__ = "zk-logo-maker"
+__summary__ = "create logo for zhike"
+__uri__ = "https://github.com/caojianfeng/zk_logo_maker"
+
+__version__ = "1.0.5"
+
+__author__ = "JeffreyCao"
+__email__ = "je255reycao@stufft.io"
+
+__license__ = "MIT Licence"
+__copyright__ = "Copyright 2019 JeffreyCao"
