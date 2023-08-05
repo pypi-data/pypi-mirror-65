7988c7dcaf914a342ccfcc1cbb23a599a8467141
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:44 +0000

    Add package aa-structures/1.1.1/aa-structures-1.1.1.tar.gz

---
+++ packages/aa-structures/aa-structures-1.1.1.tar.gz/aa-structures-1.1.1/structures/models/__init__.py
@@ -0,0 +1,20 @@
+# flake8: noqa
+
+from .owners import Owner
+from .eveuniverse import (        
+    EsiNameLocalization,
+    EveCategory, 
+    EveGroup, 
+    EveType, 
+    EveRegion, 
+    EveConstellation, 
+    EveSolarSystem, 
+    EvePlanet, 
+    EveMoon  
+)
+from .structures import (
+    Structure, StructureService, StructureTag
+)  
+from .notifications import (
+    Webhook, Notification, EveEntity, get_default_notification_types
+)
