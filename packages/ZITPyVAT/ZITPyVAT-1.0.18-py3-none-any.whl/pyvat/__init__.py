9087d91711e4f029f467b7b44110ab7d48458702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:20 +0000

    Add package ZITPyVAT/1.0.18/ZITPyVAT-1.0.18-py3-none-any.whl

---
+++ packages/ZITPyVAT/ZITPyVAT-1.0.18-py3-none-any.whl/pyvat/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/8/3 10:13
+# @Author  : Liu Gang
+# @Site    : 
+# @File    : __init__.py.py
+# @Software: PyCharm
+import sys
+from pyvat.compat import PY_VER
+
+print("Python Version:{0}".format(sys.version))
+
+__VERSION__ = "V1.0.18"
