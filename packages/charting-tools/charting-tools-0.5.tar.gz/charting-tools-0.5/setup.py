9e9bd87f135c401545b21d5ad7dbd1666801075a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:33 +0000

    Add package charting-tools/0.5/charting-tools-0.5.tar.gz

---
+++ packages/charting-tools/charting-tools-0.5.tar.gz/charting-tools-0.5/setup.py
@@ -0,0 +1,22 @@
+#@tjw123hh（QQ：3245813583 加载失败） 的Python文件
+#! python
+import setuptools
+
+
+setuptools.setup(
+    name="charting-tools", # Replace with your own username
+    version="0.5",
+    author="tjw123hh",
+    author_email="3245813583@qq.com",
+    description="A small statistical charting tool",
+    long_description='Please use the help function to view.',
+    long_description_content_type="text/markdown",
+    url=None,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='==3.8',
+)
