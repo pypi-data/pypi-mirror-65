e4bca2db6ffb2cdb8b14bdb453ddb7941f8a2736
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:03 +0000

    Add package ai-django-core/1.2.14/ai_django_core-1.2.14-py3-none-any.whl

---
+++ packages/ai-django-core/ai_django_core-1.2.14-py3-none-any.whl/ai/templatetags/ai_date_tags.py
@@ -0,0 +1,17 @@
+# -*- coding: UTF-8 -*-
+from __future__ import division
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def format_to_minutes(time):
+    """
+    Converts the seconds to minutes
+
+    :param time:
+    :return minutes:
+    """
+
+    return time.seconds // 60
---
+++ packages/ai-django-core/ai_django_core-1.2.14-py3-none-any.whl/ai/templatetags/ai_file_tags.py
@@ -0,0 +1,36 @@
+# -*- coding: UTF-8 -*-
+import os
+
+from django import template
+from django.conf import settings
+
+register = template.Library()
+
+
+@register.filter
+def filename(value, max_length=25):
+    """
+    Shortens the filename to maxlength 25 without loosing the file extension
+
+    :param file:
+    :return filename with a max length of 25
+    """
+    name = os.path.basename(value.url)
+    if len(name) > max_length:
+        ext = name.split('.')[-1]
+        name = u"%s[..].%s" % (name[:max_length], ext)
+    return name
+
+
+@register.filter
+def filesize(value):
+    """
+    Returns the filesize of the filename given in value
+
+    :param value:
+    :return filesize:
+    """
+    try:
+        return os.path.getsize("%s%s" % (settings.MEDIA_ROOT, value))
+    except:
+        return 0
