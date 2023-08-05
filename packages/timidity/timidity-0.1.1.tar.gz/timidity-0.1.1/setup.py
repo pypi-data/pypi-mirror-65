fb860b6e50be6b607abc5763adfe879279339bbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:18 +0000

    Add package timidity/0.1.1/timidity-0.1.1.tar.gz

---
+++ packages/timidity/timidity-0.1.1.tar.gz/timidity-0.1.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md") as fh:
+    long_desc = fh.read()
+
+setuptools.setup(
+    name="timidity",
+    version="0.1.1",
+    description="A lightweight Python MIDI player",
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JinayJain/timidity",
+    author="Jinay Jain",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "simpleaudio",
+        "numpy"
+    ]
+)
