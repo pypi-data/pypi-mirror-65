995292bca0502e96653dd1863b87f46e0352dd5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:33 +0000

    Add package clifold/0.2.3/clifold-0.2.3-py2.py3-none-any.whl

---
+++ packages/clifold/clifold-0.2.3-py2.py3-none-any.whl/clifold/commands/clifold_project.py
@@ -0,0 +1,40 @@
+"""Create a project with venv."""
+import os
+import venv
+import sys
+
+
+def create(pname):
+    '''
+    Project creation function.
+
+    Args:
+        pname: project_name
+
+    Returns:
+        Absolute Project Path
+    '''
+    proj_files = ['__init__', 'main']
+    test_files = ['__init__', 'test_main']
+    dirs_folders = [f'{pname}/{pname}', f'{pname}/tests']
+
+    current_path = os.getcwd()
+    exist_path = os.path.join(current_path, pname)
+    project_path = os.path.join(exist_path, pname)
+    if os.path.exists(exist_path):
+        print("Project {} already exists in {}.\n".format(pname, current_path))
+        sys.exit()
+    else:
+        venv.create(pname, with_pip=True)
+        os.chdir(exist_path)
+        for i in range(len(dirs_folders)):
+            os.makedirs(os.path.join(exist_path, dirs_folders[i]))
+            for j in range(len(dirs_folders)):
+                if i == 0:
+                    open(f"{dirs_folders[i]}/{proj_files[j]}.py", 'x').close()
+                else:
+                    open(f"{dirs_folders[i]}/{test_files[j]}.py", 'x').close()
+
+        print("\n🎉 Project {} created in {}.\n".format(pname, current_path))
+
+    return project_path
