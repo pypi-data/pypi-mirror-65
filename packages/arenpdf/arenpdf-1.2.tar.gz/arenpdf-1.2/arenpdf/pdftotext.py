8ed65493d2a5252720da383f1d807f1dcedbfe23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:12 +0000

    Add package arenpdf/1.2/arenpdf-1.2.tar.gz

---
+++ packages/arenpdf/arenpdf-1.2.tar.gz/arenpdf-1.2/arenpdf/pdftotext.py
@@ -0,0 +1,18 @@
+""" This module provides functions to convert a PDF to text. """
+
+class Converter:
+   """ A simple converter for converting PDFs to text. """
+   
+   def convert(self, path):
+    """ 
+    Convert the given PDF to text. 
+    
+    Parameters:
+    path (str): The path to PDF file.
+  
+    Returns:
+    str: The content of PDF file as text.
+    """
+    print("pdftotext")
+    
+    
