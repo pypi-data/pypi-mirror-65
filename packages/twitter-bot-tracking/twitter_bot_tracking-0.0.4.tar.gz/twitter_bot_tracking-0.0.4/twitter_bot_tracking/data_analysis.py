d976e215888212f2885c68d7936bdda5fb0fd403
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:50 +0000

    Add package twitter-bot-tracking/0.0.4/twitter_bot_tracking-0.0.4.tar.gz

---
+++ packages/twitter-bot-tracking/twitter_bot_tracking-0.0.4.tar.gz/twitter_bot_tracking-0.0.4/twitter_bot_tracking/data_analysis.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import boto3
+import os
+
+# In[4]:
+
+
+class DataAnalysis:
+
+    
+    @staticmethod
+    def calculate_sentiment(data_df):
+        
+        # Add tweet sentiment column to dataframe
+        data_df['sentiment'] = data_df.apply(
+            lambda row: DataAnalysis.get_sentiment(row['text'], 
+                                                   row['lang']), 
+            axis=1)
+        
+        return data_df
+    
+    
+    @staticmethod
+    def get_sentiment(text, language):
+        # Define comprehend object
+        comprehend = boto3.client(service_name='comprehend',
+                                  region_name=os.environ.get("region_name"))
+        # Return undefined if language is not recognized by comprehend
+        if language not in ['hi', 'de', 'zh-TW', 'ko', 'pt', 'en', 
+                            'it', 'fr', 'zh', 'es', 'ar', 'ja']:
+
+            return 'undefined'
+        
+        else:
+            # Get detected sentiment as lower case string
+            return comprehend.detect_sentiment(
+                Text=text, LanguageCode=language)['Sentiment'].lower()
+        
+    @staticmethod
+    def get_top_n(tweet_df, column, top_n):
+        sorted_df = tweet_df.sort_values(column, ascending=False)[:top_n]
+        return sorted_df
+
+
+# In[ ]:
+
+
+
+
