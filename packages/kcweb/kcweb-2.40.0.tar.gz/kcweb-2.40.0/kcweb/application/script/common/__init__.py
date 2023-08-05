7d0f8682465d4e2838e164a0fc372cf7287281f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:15 +0000

    Add package kcweb/2.40.0/kcweb-2.40.0.tar.gz

---
+++ packages/kcweb/kcweb-2.40.0.tar.gz/kcweb-2.40.0/kcweb/application/script/common/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from .win import *
+#下面的方法在当前模块中有效
+G=globals.G
+#下面的方法在当前模块中有效
+def test():
+    return 'test'
---
+++ packages/kcweb/kcweb-2.40.0.tar.gz/kcweb-2.40.0/kcweb/application/script/test.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#脚本文件
+import sys
+sys.path.append("../../")
+from ${appname}.script.common import *
+class TEST:
+    def aa(self):
+        print('aa')
+
+t=TEST()
+t.aa()
