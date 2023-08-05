87a6e273a41135f8797bad8f117d7274514b9014
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:34 +0000

    Add package cloudutils/1.2.3/cloudutils-1.2.3-py3-none-any.whl

---
+++ packages/cloudutils/cloudutils-1.2.3-py3-none-any.whl/cloudutils/model/complex.py
@@ -0,0 +1,51 @@
+# Copyright © 2020 Noel Kaczmarek
+from spyne import String, Integer
+from spyne.model.complex import ComplexModel
+
+
+class UUID(String):
+    __namespace__ = 'cloud'
+    __type_name__ = 'UUID'
+
+    class Attributes(String.Attributes):
+        max_length = 32
+
+
+class Username(String):
+    __namespace__ = 'cloud'
+    __type_name__ = 'Username'
+
+    class Attributes(String.Attributes):
+        max_length = 20
+
+
+class ServiceType(ComplexModel):
+    __namespace__ = 'cloud'
+
+    id = Integer
+    name = String
+    port = Integer
+
+
+class Service(ComplexModel):
+    __namespace__ = 'cloud'
+
+    id = UUID
+    host = String
+    type = ServiceType
+
+
+MandatoryServiceType = ServiceType.customize(nullable=False, min_occurs=1)
+MandatoryService = Service.customize(nullable=False, min_occurs=1)
+
+
+class Session(ComplexModel):
+    __namespace__ = 'cloud'
+
+    id = String
+    user = String
+    username = String
+    key = String
+
+
+MandatorySession = Session.customize(nullable=False, min_occurs=1)
