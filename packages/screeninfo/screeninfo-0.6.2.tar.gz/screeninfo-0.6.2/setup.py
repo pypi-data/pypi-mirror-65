d7354faa128dfa97bf21d47bd88c21ad1db27c51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:21 +0000

    Add package screeninfo/0.6.2/screeninfo-0.6.2.tar.gz

---
+++ packages/screeninfo/screeninfo-0.6.2.tar.gz/screeninfo-0.6.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name="screeninfo",
+    packages=["screeninfo", "screeninfo.enumerators"],
+    version="0.6.2",
+    description="Fetch location and size of physical screens.",
+    author="rr-",
+    author_email="rr-@sakuya.pl",
+    url="https://github.com/rr-/screeninfo",
+    keywords=["screen", "monitor", "desktop"],
+    classifiers=[],
+    install_requires=["dataclasses"],
+)
