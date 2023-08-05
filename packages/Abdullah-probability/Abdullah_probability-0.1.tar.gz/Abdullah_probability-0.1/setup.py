fb7af60ee45641bebbc5b42e40184d66d76b9b07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:07 +0000

    Add package Abdullah-probability/0.1/Abdullah_probability-0.1.tar.gz

---
+++ packages/Abdullah-probability/Abdullah_probability-0.1.tar.gz/Abdullah_probability-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='Abdullah_probability',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['Abdullah_probability'],
+      zip_safe=False)
