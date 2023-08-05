5c3ad1d0bc3cc8bb9be21342c50522632574f599
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:04 +0000

    Add package clifold/0.1.1/clifold-0.1.1.tar.gz

---
+++ packages/clifold/clifold-0.1.1.tar.gz/clifold-0.1.1/README.md
@@ -0,0 +1,27 @@
+<!-- <p align='center'><a href="https://ydcjeff.github.io/clifold" target="_blank" rel="noopener noreferrer"></p> -->
+
+![clifold logo](./docs/Clifold.svg)
+
+<h1 align='center'>Clifold</h1>
+
+[![Build Status](https://travis-ci.com/ydcjeff/clifold.svg?token=Hh6ZhQhxWHRigNVJeYyv&branch=master)](https://travis-ci.com/ydcjeff/clifold)  ![Python package](https://github.com/ydcjeff/clifold/workflows/Python%20package/badge.svg?branch=master)
+
+## Usage:
+```
+usage: clif <arg> [options]
+
+🚀 A CLI tool for scaffolding any Python Projects 🚀
+
+Argument:
+  project_name    Project name to create with venv
+
+Options:
+  -g, --git       Make git initialization
+  -ng, --no-git   Skip git initialization
+  -p, --pkg       Ask packages to install
+  -np, --no-pkg   Skip packages installation
+  -i, --init      Create setup.py file
+  -ni, --no-init  Skip setup.py file
+  -V, --version   Output version number
+  -h, --help      Output usage information
+```
---
+++ packages/clifold/clifold-0.1.1.tar.gz/clifold-0.1.1/clifold/clifold_project.py
@@ -0,0 +1,37 @@
+'''Create a project with venv.'''
+import os
+import venv
+
+
+def create(pname):
+    '''
+    Project creation function.
+
+    Args:
+        pname: project_name
+
+    Returns:
+        Absolute Project Path
+    '''
+    proj_files = ['__init__', 'main']
+    test_files = ['__init__', 'test_main']
+    dirs_folders = [f'src/{pname}', 'tests']
+
+    current_path = os.getcwd()
+    exist_path = os.path.join(current_path, pname)
+    if os.path.exists(exist_path):
+        print("Project {} already exists in {}.\n".format(pname, current_path))
+    else:
+        venv.create(pname)
+        os.chdir(exist_path)
+        for i in range(len(dirs_folders)):
+            os.makedirs(os.path.join(exist_path, dirs_folders[i]))
+            for j in range(len(dirs_folders)):
+                if i == 0:
+                    open(f"{dirs_folders[i]}/{proj_files[j]}.py", 'x').close()
+                else:
+                    open(f"{dirs_folders[i]}/{test_files[j]}.py", 'x').close()
+
+        print("\n🎉 Project {} created in {}.\n".format(pname, current_path))
+
+    return exist_path
