0b37e6cf7aff3f455550e5153e989241259afec3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:15 +0000

    Add package tir-framework/1.15.1/tir_framework-1.15.1.tar.gz

---
+++ packages/tir-framework/tir_framework-1.15.1.tar.gz/tir_framework-1.15.1/tir/technologies/core/config.py
@@ -0,0 +1,59 @@
+import json
+import os
+
+class ConfigLoader:
+    """
+    This class is instantiated to contain all config information used throughout the execution of the methods.
+    """
+    def __init__(self, path="config.json"):
+        valid = os.path.isfile(path)
+
+        if valid:
+            with open(path) as json_data_file:
+                data = json.load(json_data_file)
+        else:
+            data = {}
+
+        self.autostart = True
+        self.ipExec = str(data["ipExec"]) if "ipExec" in data else ""
+        self.url_set_start_exec = str(data["UrlSetStartExec"]) if "UrlSetStartExec" in data else ""
+        self.url_set_end_exec = str(data["UrlSetEndExec"]) if "UrlSetEndExec" in data else ""
+        self.screenshot = bool(data["ScreenShot"]) if "ScreenShot" in data else True
+        self.country = str(data["Country"]) if "Country" in data else "BRA"
+        self.execution_id = str(data["ExecId"]) if "ExecId" in data else ""
+        self.num_exec = str(data["NumExec"]) if "NumExec" in data else ""
+        self.issue = str(data["MotExec"]) if "MotExec" in data else ""
+        self.url = str(data["Url"]) if "Url" in data else ""
+        self.browser = str(data["Browser"]) if "Browser" in data else ""
+        self.environment = str(data["Environment"])  if "Environment" in data else ""
+        self.user = str(data["User"]) if "User" in data else ""
+        self.password = str(data["Password"]) if "Password" in data else ""
+        self.language = str(data["Language"]) if "Language" in data else ""
+        self.skip_environment = ("SkipEnvironment" in data and bool(data["SkipEnvironment"]))
+        self.headless = ("Headless" in data and bool(data["Headless"]))
+        self.log_folder = str(data["LogFolder"]) if "LogFolder" in data else ""
+        self.log_file = ("LogFile" in data and bool(data["LogFile"]))
+        self.debug_log = ("DebugLog" in data and bool(data["DebugLog"]))
+        self.time_out = int(data["TimeOut"]) if "TimeOut" in data else 90
+        self.parameter_menu = str(data["ParameterMenu"]) if "ParameterMenu" in data else ""
+        self.screenshot_folder = str(data["ScreenshotFolder"]) if "ScreenshotFolder" in data else ""
+        self.coverage = ("Coverage" in data  and bool(data["Coverage"]))
+        self.skip_restart = ("SkipRestart" in data and bool(data["SkipRestart"]))
+        self.smart_test = ("SmartTest" in data and bool(data["SmartTest"]))
+        self.smart_erp = ("SmartERP" in data and bool(data["SmartERP"]))
+        self.valid_language = self.language != ""
+        self.initial_program = ""
+        self.routine = ""
+        self.date = ""
+        self.group = ""
+        self.branch = ""
+        self.module = ""
+        self.user_cfg = str(data["UserCfg"]) if "UserCfg" in data else ""
+        self.password_cfg = str(data["PasswordCfg"]) if "PasswordCfg" in data else ""
+        self.electron_binary_path = (str(data["BinPath"]) if "BinPath" in data else "")
+        self.csv_path = (str(data["CSVPath"]) if "CSVPath" in data else "")
+        self.driver_database = str(data["DriverDB"]) if "DriverDB" in data else ""
+        self.server_database = str(data["ServerDB"]) if "ServerDB" in data else ""
+        self.name_database = str(data["NameDB"]) if "NameDB" in data else ""
+        self.user_database = str(data["UserDB"]) if "UserDB" in data else ""
+        self.password_database = str(data["PasswordDB"]) if "PasswordDB" in data else ""
