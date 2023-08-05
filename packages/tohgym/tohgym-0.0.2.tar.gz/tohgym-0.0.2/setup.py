f1a6f880b4150a81166cf27b65b3c3128dcbadf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:48 +0000

    Add package tohgym/0.0.2/tohgym-0.0.2.tar.gz

---
+++ packages/tohgym/tohgym-0.0.2.tar.gz/tohgym-0.0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='tohgym',
+      version='0.0.2',
+      url='https://github.com/xadahiya/toh-gym',
+      author='Akshay Dahiya (forked by Ryan McDermott)',
+      install_requires=['gym']
+      )
