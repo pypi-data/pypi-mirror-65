b64145ab16cc9761dcb4a4de73595f17674124fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:18 +0000

    Add package ivy/2.8.0/ivy-2.8.0.tar.gz

---
+++ packages/ivy/ivy-2.8.0.tar.gz/ivy-2.8.0/ivy/extensions/ivy_ibis.py
@@ -0,0 +1,26 @@
+# ------------------------------------------------------------------------------
+# This extension adds support for Ibis templates.
+# ------------------------------------------------------------------------------
+
+import sys
+from ivy import hooks, templates, site
+
+try:
+    import ibis
+except ImportError:
+    ibis = None
+
+
+# The ibis package is an optional dependency.
+if ibis:
+
+    # Initialize the template loader. 
+    @hooks.register('init')
+    def init():
+        ibis.config.loader = ibis.loaders.FileLoader(site.theme('templates'))
+
+    # Register our template engine callback for files with a .ibis extension.
+    @templates.register('ibis')
+    def callback(page, filename):
+        template = ibis.config.loader(filename)
+        return template.render(page)
