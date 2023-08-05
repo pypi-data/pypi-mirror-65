b489fb426d8db78f211b0767f6c62af797c43d15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:45 +0000

    Add package cdm-connector/0.0.6.27/cdm-connector-0.0.6.27.tar.gz

---
+++ packages/cdm-connector/cdm-connector-0.0.6.27.tar.gz/cdm-connector-0.0.6.27/skypoint_python_cdm/Partition.py
@@ -0,0 +1,19 @@
+from .DataObject import DataObject
+from .SchemaEntry import SchemaEntry
+from .FileFormatSettings import FileFormatSettings
+from .MetadataObject import MetadataObjectCollection
+from .utils import DateTimeOffset
+from .utils import Uri
+
+
+class Partition(DataObject):
+    def __init__(self, schema=[]):
+        self.schema = schema + [
+            SchemaEntry("refreshTime", DateTimeOffset),
+            SchemaEntry("location", Uri)
+        ]
+        super().__init__(self.schema)
+
+
+class PartitionCollection(MetadataObjectCollection):
+    itemType = Partition
