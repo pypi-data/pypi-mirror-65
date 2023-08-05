70bebf3fe6ff5612a9e76d2ea9f8061fac7eaee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:59 +0000

    Add package ephysiopy/1.5.77/ephysiopy-1.5.77.tar.gz

---
+++ packages/ephysiopy/ephysiopy-1.5.77.tar.gz/ephysiopy-1.5.77/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+import os
+from ephysiopy import __version__
+
+with open(os.path.join(os.getcwd(), "readme.rst"), "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ephysiopy",
+    version=__version__,
+    author="Robin Hayman",
+    author_email="robin.hayman@gmail.com",
+    description="Analysis of electrophysiology data",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/rhayman/ephysiopy",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    package_data={"" : ["*.pkl", "*.txt"]},
+    install_requires=[
+        "numpy",
+        "scipy",
+        "matplotlib",
+        "scikit-learn<0.21",
+        "astropy",
+        "scikit-image",
+        "mahotas",
+        "h5py"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
