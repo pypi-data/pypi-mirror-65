f8ba075bc30509505640c1006d34db3b5fe45be6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:47 +0000

    Add package videoguys/0.0.4/videoguys-0.0.4-py3-none-any.whl

---
+++ packages/videoguys/videoguys-0.0.4-py3-none-any.whl/videoguys/clients/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import absolute_import
+
+from videoguys.clients.api import ApiClient
+from videoguys.clients.upload import UploadClient
+from videoguys.clients.download import DownloadClient
