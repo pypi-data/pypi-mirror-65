8a49a696f94c76ea6b0dada05b08cf95ef30b3b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:49 +0000

    Add package transformers-finetuning/0.3.6/transformers_finetuning-0.3.6.tar.gz

---
+++ packages/transformers-finetuning/transformers_finetuning-0.3.6.tar.gz/transformers_finetuning-0.3.6/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     name='transformers_finetuning',  
+     version='0.3.6',
+     author="Ankur Singh",
+     author_email="ankur310794@gmail.com",
+     description="A Package for fine tuning based on transformers",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/Ankur3107/transformers_finetuning",
+     packages=['transformers_finetuning'],
+     install_requires=['transformers'], 
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent"
+     ]
+ )
