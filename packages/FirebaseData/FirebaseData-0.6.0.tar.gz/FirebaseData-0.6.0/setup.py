53df43942994d99ba32e03f4a9d1421527c73ba6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:36 +0000

    Add package FirebaseData/0.6.0/FirebaseData-0.6.0.tar.gz

---
+++ packages/FirebaseData/FirebaseData-0.6.0.tar.gz/FirebaseData-0.6.0/setup.py
@@ -0,0 +1,25 @@
+from os import path
+from setuptools import setup, find_packages
+
+base_dir = path.abspath(path.dirname(__file__))
+
+with open(path.join(base_dir, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='FirebaseData',
+    version='0.6.0',
+    packages=find_packages(),
+    install_requires=[
+        'blinker>=1.4',
+    ],
+    python_requires='>=3, <4',
+    url='https://github.com/heston/firebase-live-data',
+    author='Heston Liebowitz',
+    description=(
+        'Utilities for storing, retrieving, and monitoring Firebase Realtime '
+        'Database objects in Python.'
+    ),
+    long_description=long_description,
+    license='MIT'
+)
