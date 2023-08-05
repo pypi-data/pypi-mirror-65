199ba88816d7f2f1dacc1f56e07010a2a4d41fd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:42 +0000

    Add package pySplitFlap/1.2.0/pySplitFlap-1.2.0-py3-none-any.whl

---
+++ packages/pySplitFlap/pySplitFlap-1.2.0-py3-none-any.whl/splitflap/metadata.py
@@ -0,0 +1,9 @@
+name = "pySplitFlap"
+version = "1.2.0"
+description = "A library for controlling split-flap displays"
+license = "GPLv3"
+author = "Julian Metzler"
+author_email = "git@mezgr.de"
+requires = ['pyserial']
+url = "https://github.com/Mezgrman/pySplitFlap"
+keywords = "sign message board library wrapper serial text display train"
