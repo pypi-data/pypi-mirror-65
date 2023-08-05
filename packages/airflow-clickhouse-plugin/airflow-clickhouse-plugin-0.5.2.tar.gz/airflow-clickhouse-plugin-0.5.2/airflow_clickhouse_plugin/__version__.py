b50653235bab6111ea5da31dd8026dfba1a7f36f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:34 +0000

    Add package airflow-clickhouse-plugin/0.5.2/airflow-clickhouse-plugin-0.5.2.tar.gz

---
+++ packages/airflow-clickhouse-plugin/airflow-clickhouse-plugin-0.5.2.tar.gz/airflow-clickhouse-plugin-0.5.2/airflow_clickhouse_plugin/__version__.py
@@ -0,0 +1,16 @@
+"""
+__version__.py
+~~~~~~~~~~~~~~
+
+Information about the current version of the airflow_clickhouse_plugin package.
+"""
+
+__title__ = 'airflow-clickhouse-plugin'
+__description__ = \
+    'airflow-clickhouse-plugin - ' \
+    'Airflow plugin to execute ClickHouse commands and queries'
+__version__ = '0.5.2'
+__author__ = 'Viktor Taranenko, Anton Bryzgalov'
+__author_email__ = 'viktor@samsungnext.com, tony.bryzgaloff@gmail.com'
+__license__ = 'MIT License'
+__url__ = 'https://github.com/whisklabs/airflow-clickhouse-plugin'
