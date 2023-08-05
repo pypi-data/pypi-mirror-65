e62c11612bf262e9a9dca3b9e73a03e8ed69e85a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:45 +0000

    Add package mkn/0.0.1/mkn-0.0.1.tar.gz

---
+++ packages/mkn/mkn-0.0.1.tar.gz/mkn-0.0.1/mkn/__init__.py
@@ -0,0 +1,4 @@
+from mkn import sample
+
+
+__version__ = "0.0.1"
---
+++ packages/mkn/mkn-0.0.1.tar.gz/mkn-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+from setuptools import find_packages
+
+__name__ = "mkn"
+__version__ = "0.0.1"
+
+setup(
+    name="mkn",
+    version=__version__,
+    description='A sample PyPI project',
+    author="Moji Kimino",
+    url="https://github.com/mojikimino/mkn",
+    packages=["mkn"],
+    include_package_data=True,
+    install_requires=["pytest"]
+)
