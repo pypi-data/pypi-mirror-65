5d8a83e8a97cfe3db6921684402716eee47c224a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:56 +0000

    Add package hd-gliomouse/1.1/hd_gliomouse-1.1.tar.gz

---
+++ packages/hd-gliomouse/hd_gliomouse-1.1.tar.gz/hd_gliomouse-1.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+
+setup(name='hd_gliomouse',
+      version='1.1',
+      packages=["hd_gliomouse"],
+      description='Tool for brain tumor segmentation in mice. This is the result of a joint project between the Department of '
+                  'Neuroradiology at the Heidelberg University Hospital and the Division of Medical Image Computing at '
+                  'the German Cancer Research Center (DKFZ). See readme.md for more information',
+      url='https://github.com/NeuroAI-HD/HD-GLIOMOUSE',
+      python_requires='>=3.5',
+      author='Fabian Isensee',
+      author_email='f.isensee@dkfz.de',
+      license='Apache 2.0',
+      zip_safe=False,
+      install_requires=[
+          'torch',
+          'nnunet>1.0', 'batchgenerators'
+      ],
+      entry_points={
+          'console_scripts': [
+                'hd_gliomouse_predict = hd_gliomouse.hd_gliomouse_predict:main',
+                'hd_gliomouse_predict_folder = hd_gliomouse.hd_gliomouse_predict_folder:main',
+        ],
+      },
+      classifiers=[
+          'Intended Audience :: Science/Research',
+          'Programming Language :: Python',
+          'Topic :: Scientific/Engineering',
+          'Operating System :: Unix'
+      ]
+      )
+
