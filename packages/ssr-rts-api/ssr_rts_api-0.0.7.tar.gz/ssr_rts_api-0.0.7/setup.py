1213510cafd78cffbb02f3916a555237eabf6e0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:52 +0000

    Add package ssr-rts-api/0.0.7/ssr_rts_api-0.0.7.tar.gz

---
+++ packages/ssr-rts-api/ssr_rts_api-0.0.7.tar.gz/ssr_rts_api-0.0.7/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ssr_rts_api",
+    packages=["ssr_rts_api"],
+    version="0.0.7",
+    author="Renato Diaz (rouj)",
+    author_email="renatojour@gmail.com",
+    description="Connect with ease to the public rts/ssr public API.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rerouj/ssr_rts_api",
+    keywords=['ssr', 'rts', 'tsr', 'ssr rts api'],  # Keywords that define your package best
+    install_requires=[
+        'requests',
+        'base64',
+        'urllib',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.6',
+)
