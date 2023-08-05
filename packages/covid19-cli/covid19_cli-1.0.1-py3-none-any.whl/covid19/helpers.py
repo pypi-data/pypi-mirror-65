ba250548f50ebe1aa5fa6e8b36322e792664421f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:08 +0000

    Add package covid19-cli/1.0.1/covid19_cli-1.0.1-py3-none-any.whl

---
+++ packages/covid19-cli/covid19_cli-1.0.1-py3-none-any.whl/covid19/helpers.py
@@ -0,0 +1,12 @@
+import requests
+from .logger import log_error
+
+
+def send_request(API_URI, method="GET"):
+    try:
+        response = requests.get(API_URI)
+        resp_json = response.json()
+        return resp_json
+    except requests.RequestException:
+        log_error("Request Exception Occured!\nPlease check network\
+            connectivity or try again.")
