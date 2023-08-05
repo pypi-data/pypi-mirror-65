0f20648128ae350eeee8dbfed558e571692cffca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:56 +0000

    Add package pymt5adapter/0.1.7/pymt5adapter-0.1.7.tar.gz

---
+++ packages/pymt5adapter/pymt5adapter-0.1.7.tar.gz/pymt5adapter-0.1.7/pymt5adapter/__init__.py
@@ -0,0 +1,10 @@
+import MetaTrader5 as _mt5
+
+from . import types
+from .const import *
+from .core import *
+from .oem import *
+from .context import connected
+
+__version__ = _mt5.__version__
+__author__ = _mt5.__author__
