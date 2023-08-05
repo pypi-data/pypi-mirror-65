bbb8f67bfbab66bf44cfe962fe3468465dced755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:12 +0000

    Add package functional-itertools/0.7.0/functional_itertools-0.7.0-py2.py3-none-any.whl

---
+++ packages/functional-itertools/functional_itertools-0.7.0-py2.py3-none-any.whl/functional_itertools/__init__.py
@@ -0,0 +1,24 @@
+"""Python collections in a functional-programming style."""
+from __future__ import annotations
+
+from functional_itertools.cattrs import CAttrs
+from functional_itertools.classes import CDict
+from functional_itertools.classes import CFrozenSet
+from functional_itertools.classes import CIterable
+from functional_itertools.classes import CList
+from functional_itertools.classes import CSet
+from functional_itertools.errors import EmptyIterableError
+from functional_itertools.errors import MultipleElementsError
+
+
+__version__ = "0.7.0"
+__all__ = [
+    "CAttrs",
+    "CDict",
+    "CFrozenSet",
+    "CIterable",
+    "CList",
+    "CSet",
+    "EmptyIterableError",
+    "MultipleElementsError",
+]
