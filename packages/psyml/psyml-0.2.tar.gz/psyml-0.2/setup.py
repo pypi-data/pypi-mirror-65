e13dc85fa5809e56f983e40e99c7ad92960c1ba4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:02 +0000

    Add package psyml/0.2/psyml-0.2.tar.gz

---
+++ packages/psyml/psyml-0.2.tar.gz/psyml-0.2/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+
+VERSION = "0.2"
+
+
+with open("README.md") as fobj:
+    long_description = fobj.read().strip()
+
+
+if __name__ == "__main__":
+    setup(
+        name="psyml",
+        version=VERSION,
+        author="Kai Xia (夏恺)",
+        author_email="kaix@fastmail.com",
+        url="https://github.com/xiaket/psyml",
+        description="Secrets manager using AWS Parameter Store",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        py_modules=["psyml"],
+        entry_points={"console_scripts": ["psyml = psyml:main"]},
+        classifiers=[
+            "Development Status :: 4 - Beta",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: MIT License",
+            "Topic :: Utilities",
+        ],
+    )
