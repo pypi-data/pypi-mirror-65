af8cbc983547af3670b67563ccb9cb29085a594f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:37 +0000

    Add package madprofiling/0.3/madprofiling-0.3.tar.gz

---
+++ packages/madprofiling/madprofiling-0.3.tar.gz/madprofiling-0.3/madprofiling/madprofiling.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+class madprofiling(df):
+    
+    def madunique(df):
+        names=list(df.columns)
+        unique=[]
+        for var_i in names:
+            unique.append(len(df[var_i].unique()))
+        df_unique=pd.DataFrame(unique,index=list(df.columns))
+        df_unique.columns=['unique']
+        return df_unique
+
+    def madprofiling_sub(df):
+        profiling=pd.DataFrame(df.dtypes)
+        profiling.columns=['type']
+        profiling.loc[profiling.type=='object', 'categorical'] = 1  
+        profiling.loc[profiling.type!='object', 'categorical'] = 0
+        profiling['total']=(len(df))
+        profiling=pd.concat([profiling,madunique(df)],axis=1,sort=False)
+        profiling['null']=pd.DataFrame(df.isnull().sum())
+        profiling['na']=pd.DataFrame(df.isna().sum())
+        profiling['null_pcnt']=round(profiling.null/profiling.total,1)
+        profiling['na_pcnt']=round(profiling.na/profiling.total,1)
+        profiling=profiling.sort_values(by='null_pcnt')
+        return profiling
