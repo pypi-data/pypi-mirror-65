be45d3fa2cedbada32aed87323b5297951d43c72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:02 +0000

    Add package mf2/2020.4.1/mf2-2020.4.1.tar.gz

---
+++ packages/mf2/mf2-2020.4.1.tar.gz/mf2-2020.4.1/docs/source/functions/currin.rst
@@ -0,0 +1,7 @@
+Burrin
+======
+
+.. automodule:: mf2.currin
+    :members:
+    :undoc-members:
+    :show-inheritance:
---
+++ packages/mf2/mf2-2020.4.1.tar.gz/mf2-2020.4.1/mf2/__init__.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+"""
+mf2
+
+A collection of analytical functions with 2 or more available fidelities.
+"""
+import pkg_resources
+
+from .multiFidelityFunction import MultiFidelityFunction
+from .borehole import borehole
+from .currin import currin
+from .park91a import park91a
+from .park91b import park91b
+from .bohachevsky import bohachevsky
+from .branin import branin
+from .booth import booth
+from .forrester import forrester, Forrester
+from .himmelblau import himmelblau, himmelblau_3f
+from .himmelblau_seb import himmelblau_seb
+from .six_hump_camelback import six_hump_camelback
+from .hartmann import hartmann6
+
+import mf2.adjustable
+
+__author__ = 'Sander van Rijn'
+__email__ = 's.j.van.rijn@liacs.leidenuniv.nl'
+__version__ = pkg_resources.get_distribution('mf2').version
+
+
+bi_fidelity_functions = (
+    # 1D
+    forrester,
+    # 2D
+    bohachevsky,
+    booth,
+    branin,
+    currin,
+    himmelblau,
+    six_hump_camelback,
+    # 4D
+    park91a,
+    park91b,
+    # 6D
+    hartmann6,
+    # 8D
+    borehole,
+)
+
+tri_fidelity_functions = (
+    himmelblau_3f,
+    himmelblau_seb,
+)
