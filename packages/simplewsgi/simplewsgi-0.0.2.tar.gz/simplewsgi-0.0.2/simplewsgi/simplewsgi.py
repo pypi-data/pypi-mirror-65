a499e2f9f0f45729890f3571e34e69c7b3bc9f48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:24 +0000

    Add package simplewsgi/0.0.2/simplewsgi-0.0.2.tar.gz

---
+++ packages/simplewsgi/simplewsgi-0.0.2.tar.gz/simplewsgi-0.0.2/simplewsgi/simplewsgi.py
@@ -0,0 +1,52 @@
+import re
+
+def http404(env, start_response):
+    start_response('404 Not Found', [('Content-type', 'text/plain; charset=utf-8')])
+    return [b'404 Not Found']
+
+
+def http405(env, start_response):
+    start_response('405 Method Not Allowed', [('Content-type', 'text/plain; charset=utf-8')])
+    return [b'405 Method Not Allowed']
+
+
+class Router:
+    def __init__(self):
+        self.routes = []
+
+    def add(self, method, path, callback):
+        self.routes.append({
+            'method': method,
+            'path': path,
+            'path_compiled': re.compile(path),
+            'callback': callback
+        })
+
+    def match(self, method, path):
+        error_callback = http404
+        for r in self.routes:
+            matched = r['path_compiled'].match(path)
+            if not matched:
+                continue
+
+            error_callback = http405
+            url_vars = matched.groupdict()
+            if method == r['method']:
+                return r['callback'], url_vars
+        return error_callback, {}
+
+class TestPack:
+    def __init__(self):
+        self.router = Router()
+
+    def route(self, path=None, method='GET', callback=None):
+        def decorator(callback_func):
+            self.router.add(method, path, callback_func)
+            return callback_func
+        return decorator(callback) if callback else decorator
+
+    def __call__(self, env, start_response):
+        method = env['REQUEST_METHOD'].upper()
+        path = env['PATH_INFO'] or '/'
+        callback, kwargs = self.router.match(method, path)
+        return callback(env, start_response, **kwargs)
