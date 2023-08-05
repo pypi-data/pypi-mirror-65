088a069c7e84933fdffd10dcc7fb7d4f4787a1cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:05 +0000

    Add package sciutil/1.0.0/sciutil-1.0.0.tar.gz

---
+++ packages/sciutil/sciutil-1.0.0.tar.gz/sciutil-1.0.0/sciutil/__init__.py
@@ -0,0 +1,7 @@
+__title__ = 'sciutil'
+__description__ = 'A set of utility functions for the sci-packages.'
+__url__ = 'https://github.com/arianemora/sciutil'
+__version__ = '1.0.0'
+__author__ = 'Ariane Mora'
+__author_email__ = 'ariane.mora@uq.net.au'
+__license__ = 'GPL3'
