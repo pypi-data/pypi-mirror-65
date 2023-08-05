e2acbeb43b98d1031bbbbc80d876d5ab7d80c415
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:21 +0000

    Add package facialmask/1.0.1/facialmask-1.0.1.tar.gz

---
+++ packages/facialmask/facialmask-1.0.1.tar.gz/facialmask-1.0.1/facialmask/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for facialmask."""
+
+__author__ = """Devin C. Horvay"""
+__email__ = 'devin.c.horvay@gmail.com'
+__version__ = '1.0.0'
---
+++ packages/facialmask/facialmask-1.0.1.tar.gz/facialmask-1.0.1/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+requirements = ['Click>=7.0', 'numpy', 'pillow', 'opencv-python']
+
+setup(
+    author="Devin C. Horvay",
+    author_email='devin.c.horvay@gmail.com',
+    python_requires='>=3.5',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="Pixelate faces automatically classified in images through a Python CLI.",
+    entry_points={
+        'console_scripts': [
+            'facialmask=facialmask.cli:main',
+        ],
+    },
+    install_requires=requirements,
+    license="BSD license",
+    long_description=readme,
+    keywords='facialmask pixelate facedetection face',
+    name='facialmask',
+    packages=find_packages(include=['facialmask', 'facialmask.*']),
+    url='https://github.com/dhorvay/facial-mask',
+    version='1.0.1',
+    zip_safe=False,
+)
