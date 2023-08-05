eaa2604569e129db96ea4c1758ec364de2f429d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:28 +0000

    Add package wtphm/0.1.1/wtphm-0.1.1.tar.gz

---
+++ packages/wtphm/wtphm-0.1.1.tar.gz/wtphm-0.1.1/wtphm/__init__.py
@@ -0,0 +1,12 @@
+"""
+wtphm package.
+
+GitHub: https://github.com/lkev/wtphm/
+PyPI: https://pypi.org/project/wtphm/
+Docs: https://wtphm.readthedocs.io/en/latest/
+"""
+from wtphm import pred_processing
+from wtphm import clustering
+from wtphm import batch
+
+__version__ = '0.1.1'
