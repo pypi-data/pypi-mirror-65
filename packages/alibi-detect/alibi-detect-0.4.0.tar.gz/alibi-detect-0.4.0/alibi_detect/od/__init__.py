b0a77b3ee14293f21a07b2801c9c71efd609f296
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:19 +0000

    Add package alibi-detect/0.4.0/alibi-detect-0.4.0.tar.gz

---
+++ packages/alibi-detect/alibi-detect-0.4.0.tar.gz/alibi-detect-0.4.0/alibi_detect/od/__init__.py
@@ -0,0 +1,21 @@
+from .aegmm import OutlierAEGMM
+from .isolationforest import IForest
+from .mahalanobis import Mahalanobis
+from .ae import OutlierAE
+from .vae import OutlierVAE
+from .vaegmm import OutlierVAEGMM
+from .prophet import OutlierProphet
+from .seq2seq import OutlierSeq2Seq
+from .sr import SpectralResidual
+
+__all__ = [
+    "OutlierAEGMM",
+    "IForest",
+    "Mahalanobis",
+    "OutlierAE",
+    "OutlierVAE",
+    "OutlierVAEGMM",
+    "OutlierProphet",
+    "OutlierSeq2Seq",
+    "SpectralResidual"
+]
