bc53805d93eaf9a50fff7e1bad13d5e364d7ba83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:21 +0000

    Add package mnl-probability/0.1/mnl_probability-0.1.tar.gz

---
+++ packages/mnl-probability/mnl_probability-0.1.tar.gz/mnl_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = 'mnl_probability',
+      version = '0.1',
+      description = 'Gaussian and Binomial distributions',
+      packages = ['mnl_probability'],
+      author = 'Marcel Lucero',
+      author_email = 'mluceron@gmail.com',
+      zip_safe=False)
