7a69806af03843cff4632b81e76f265dbe3cedcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:31 +0000

    Add package easonc/0.25/easonc-0.25.tar.gz

---
+++ packages/easonc/easonc-0.25.tar.gz/easonc-0.25/README.md
@@ -0,0 +1,8 @@
+# Example Package
+
+This is a simple package create by EasonC
+
+```
+python setup.py sdist
+python setup.py sdist upload
+```
---
+++ packages/easonc/easonc-0.25.tar.gz/easonc-0.25/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='easonc',
+    version='0.25',
+    description='easonc test',
+    url='https://github.com/EasonC13/easonc',
+    author='EasonC',
+    author_email='eason.tw.chen@gmail.com',
+    license='MIT',
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    
+)
