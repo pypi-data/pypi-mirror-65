a69f175b2790b940e36e1501833e4a8c9e871b23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:40 +0000

    Add package vcftoabook/0.3.0/vcftoabook-0.3.0-py3-none-any.whl

---
+++ packages/vcftoabook/vcftoabook-0.3.0-py3-none-any.whl/vcftoabook/cli.py
@@ -0,0 +1,37 @@
+import argparse
+import vcftoabook
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog="vcftoabook",
+        description="Convert .vcf files into addressbook files to be used in "
+                    "abook/Mutt"
+    )
+
+    parser.add_argument(
+        'input',
+        type=str,
+        help="the input path of a .vcf file, or directory full of .vcf files",
+    )
+
+    parser.add_argument(
+        '-o',
+        '--output',
+        type=str,
+        default='addressbook',
+        help="the output path/filename (default: ./addressbook)"
+    )
+
+    parser.add_argument(
+        '-a',
+        '--add',
+        help="append new contacts to existing address book"
+    )
+    args = parser.parse_args()
+
+    vcftoabook.main(args)
+
+
+if __name__ == '__main__':
+    main()
