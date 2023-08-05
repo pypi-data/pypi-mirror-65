87a39fdc3b2810b774ce52500e597f711ec09b80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:58 +0000

    Add package codex-africanus/0.2.1/codex-africanus-0.2.1.tar.gz

---
+++ packages/codex-africanus/codex-africanus-0.2.1.tar.gz/codex-africanus-0.2.1/docs/util-api.rst
@@ -0,0 +1,73 @@
+---------
+Utilities
+---------
+
+Command Line
+~~~~~~~~~~~~
+
+.. currentmodule:: africanus.util.cmdline
+
+.. autosummary::
+    parse_python_assigns
+
+.. autofunction:: africanus.util.cmdline.parse_python_assigns
+
+
+Requirements Handling
+~~~~~~~~~~~~~~~~~~~~~
+
+.. currentmodule:: africanus.util.requirements
+
+.. autosummary::
+    requires_optional
+
+.. autofunction:: africanus.util.requirements.requires_optional
+
+Shapes
+~~~~~~
+
+.. currentmodule:: africanus.util.shapes
+
+.. autosummary::
+    aggregate_chunks
+    corr_shape
+
+.. autofunction:: africanus.util.shapes.aggregate_chunks
+.. autofunction:: africanus.util.shapes.corr_shape
+
+
+Beams
+~~~~~
+
+.. currentmodule:: africanus.util.beams
+
+.. autosummary::
+    beam_filenames
+    beam_grids
+
+
+.. autofunction:: africanus.util.beams.beam_filenames
+.. autofunction:: africanus.util.beams.beam_grids
+
+Code
+~~~~
+
+.. currentmodule:: africanus.util.code
+
+.. autosummary::
+    format_code
+    memoize_on_key
+
+.. autofunction:: africanus.util.code.format_code
+.. autoclass:: africanus.util.code.memoize_on_key
+
+
+CUDA
+~~~~
+
+.. currentmodule:: africanus.util.cuda
+
+.. autosummary::
+    grids
+
+.. autofunction:: africanus.util.cuda.grids
---
+++ packages/codex-africanus/codex-africanus-0.2.1.tar.gz/codex-africanus-0.2.1/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+# Import requirements
+from africanus.install.requirements import (requirements,
+                                            extras_require,
+                                            setup_requirements,
+                                            test_requirements)
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+setup(
+    author="Simon Perkins",
+    author_email='sperkins@ska.ac.za',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    description="Radio Astronomy Building Blocks",
+    entry_points={
+        'console_scripts': [
+            'plot-filter=africanus.filters.plot_filter:main',
+            'plot-taper=africanus.filters.plot_taper:main'],
+    },
+    extras_require=extras_require,
+    install_requires=requirements,
+    license="GNU General Public License v2",
+    long_description=readme,
+    long_description_content_type='text/x-rst',
+    include_package_data=True,
+    keywords='codex-africanus',
+    name='codex-africanus',
+    packages=find_packages(),
+    python_requires=">=3.6",
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/ska-sa/codex-africanus',
+    version='0.2.1',
+    zip_safe=False,
+)
