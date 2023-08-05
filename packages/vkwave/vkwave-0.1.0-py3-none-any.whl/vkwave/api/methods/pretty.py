a39e5fc26048834eed288557c1404aff37770340
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:34 +0000

    Add package vkwave/0.1.0/vkwave-0.1.0-py3-none-any.whl

---
+++ packages/vkwave/vkwave-0.1.0-py3-none-any.whl/vkwave/api/methods/pretty.py
@@ -0,0 +1,156 @@
+from vkwave.types.responses import *
+from ._category import Category
+from ._utils import get_params
+
+
+class Pretty(Category):
+    async def cards_create(
+        self,
+        owner_id: int = None,
+        photo: str = None,
+        title: str = None,
+        link: str = None,
+        price: typing.Optional[str] = None,
+        price_old: typing.Optional[str] = None,
+        button: typing.Optional[str] = None,
+        return_raw_response: bool = False,
+    ) -> typing.Union[dict, PrettyCardsCreateResponse]:
+        """
+        :param owner_id:
+        :param photo:
+        :param title:
+        :param link:
+        :param price:
+        :param price_old:
+        :param button:
+        :param return_raw_response: - return result at dict
+        :return:
+        """
+
+        params = get_params(locals())
+
+        raw_result = await self.api_request("create", params)
+        if return_raw_response:
+            return raw_result
+
+        result = PrettyCardsCreateResponse(**raw_result)
+        return result
+
+    async def cards_delete(
+        self,
+        owner_id: int = None,
+        card_id: int = None,
+        return_raw_response: bool = False,
+    ) -> typing.Union[dict, PrettyCardsDeleteResponse]:
+        """
+        :param owner_id:
+        :param card_id:
+        :param return_raw_response: - return result at dict
+        :return:
+        """
+
+        params = get_params(locals())
+
+        raw_result = await self.api_request("delete", params)
+        if return_raw_response:
+            return raw_result
+
+        result = PrettyCardsDeleteResponse(**raw_result)
+        return result
+
+    async def cards_edit(
+        self,
+        owner_id: int = None,
+        card_id: int = None,
+        photo: typing.Optional[str] = None,
+        title: typing.Optional[str] = None,
+        link: typing.Optional[str] = None,
+        price: typing.Optional[str] = None,
+        price_old: typing.Optional[str] = None,
+        button: typing.Optional[str] = None,
+        return_raw_response: bool = False,
+    ) -> typing.Union[dict, PrettyCardsEditResponse]:
+        """
+        :param owner_id:
+        :param card_id:
+        :param photo:
+        :param title:
+        :param link:
+        :param price:
+        :param price_old:
+        :param button:
+        :param return_raw_response: - return result at dict
+        :return:
+        """
+
+        params = get_params(locals())
+
+        raw_result = await self.api_request("edit", params)
+        if return_raw_response:
+            return raw_result
+
+        result = PrettyCardsEditResponse(**raw_result)
+        return result
+
+    async def cards_get(
+        self,
+        owner_id: int = None,
+        offset: typing.Optional[int] = None,
+        count: typing.Optional[int] = None,
+        return_raw_response: bool = False,
+    ) -> typing.Union[dict, PrettyCardsGetResponse]:
+        """
+        :param owner_id:
+        :param offset:
+        :param count:
+        :param return_raw_response: - return result at dict
+        :return:
+        """
+
+        params = get_params(locals())
+
+        raw_result = await self.api_request("get", params)
+        if return_raw_response:
+            return raw_result
+
+        result = PrettyCardsGetResponse(**raw_result)
+        return result
+
+    async def cards_get_by_id(
+        self,
+        owner_id: int = None,
+        card_ids: typing.List[int] = None,
+        return_raw_response: bool = False,
+    ) -> typing.Union[dict, PrettyCardsGetByIdResponse]:
+        """
+        :param owner_id:
+        :param card_ids:
+        :param return_raw_response: - return result at dict
+        :return:
+        """
+
+        params = get_params(locals())
+
+        raw_result = await self.api_request("getById", params)
+        if return_raw_response:
+            return raw_result
+
+        result = PrettyCardsGetByIdResponse(**raw_result)
+        return result
+
+    async def cards_get_upload_u_r_l(
+        self, return_raw_response: bool = False,
+    ) -> typing.Union[dict, PrettyCardsGetUploadURLResponse]:
+        """
+        :param return_raw_response: - return result at dict
+        :return:
+        """
+
+        params = get_params(locals())
+
+        raw_result = await self.api_request("getUploadURL", params)
+        if return_raw_response:
+            return raw_result
+
+        result = PrettyCardsGetUploadURLResponse(**raw_result)
+        return result
---
+++ packages/vkwave/vkwave-0.1.0-py3-none-any.whl/vkwave/bots/easy/easy_bot.py
@@ -0,0 +1,75 @@
+import asyncio
+import typing
+
+from vkwave.bots.core.dispatching.events.base import BaseEvent
+from vkwave.api.methods import API
+from vkwave.client.default import AIOHTTPClient
+from vkwave.api.token.token import BotSyncSingleToken, Token
+from vkwave.bots.core.tokens.storage import TokenStorage
+from vkwave.bots.core.dispatching.dp.dp import Dispatcher
+from vkwave.bots.core.dispatching.extensions.longpoll_bot import BotLongpollExtension
+from vkwave.bots.core.tokens.types import GroupId
+from vkwave.bots.core.dispatching.router.router import DefaultRouter
+from vkwave.longpoll.bot import BotLongpoll, BotLongpollData
+from vkwave.bots.core.dispatching.filters.builtin import (
+    EventTypeFilter,
+    PayloadFilter,
+    TextFilter,
+    ChatActionFilter,
+    CommandsFilter,
+    RegexFilter,
+)
+
+
+class _APIContextManager:
+    def __init__(self, tokens: typing.Union[str, typing.List[str]]):
+        self.client = AIOHTTPClient()
+        self.tokens = (
+            BotSyncSingleToken(Token(tokens))
+            if isinstance(tokens, str)
+            else [BotSyncSingleToken(Token(token)) for token in tokens]
+        )
+        self.api = API(clients=self.client, tokens=self.tokens)
+
+    async def __aenter__(self):
+        return self.api.get_context()
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await self.close()
+
+    async def close(self):
+        await self.client.close()
+
+
+def create_api_session_aiohttp(token: str) -> _APIContextManager:
+    return _APIContextManager(token)
+
+
+class SimpleLongPollBot:
+    def __init__(self, tokens: typing.Union[str, typing.List[str]], group_id: int):
+        self.BaseEvent = BaseEvent
+        self.api_session = create_api_session_aiohttp(tokens)
+        self.api_context = self.api_session.api.get_context()
+        self._lp = BotLongpoll(self.api_context, BotLongpollData(group_id))
+        self._token_storage = TokenStorage[GroupId]()
+        self.dispatcher = Dispatcher(self.api_session.api, self._token_storage)
+        self._lp = BotLongpollExtension(self.dispatcher, self._lp)
+        self.router = DefaultRouter()
+        self.message_handler = self.router.registrar.with_decorator
+        self.dispatcher.add_router(self.router)
+
+        self.text_filter = TextFilter
+        self.event_type_filter = EventTypeFilter
+        self.payload_filter = PayloadFilter
+        self.chat_action_filter = ChatActionFilter
+        self.command_filter = CommandsFilter
+        self.regex_filter = RegexFilter
+
+    async def run(self):
+        await self.dispatcher.cache_potential_tokens()
+        await self._lp.start()
+
+    def run_forever(self, loop: asyncio.AbstractEventLoop = None):
+        loop = loop or asyncio.get_event_loop()
+        loop.create_task(self.run())
+        loop.run_forever()
