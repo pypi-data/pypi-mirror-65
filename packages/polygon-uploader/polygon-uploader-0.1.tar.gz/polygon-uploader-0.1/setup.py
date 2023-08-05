b33e7179c3dff1873e5824f03f2fe9a2425cee75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:49 +0000

    Add package polygon-uploader/0.1/polygon-uploader-0.1.tar.gz

---
+++ packages/polygon-uploader/polygon-uploader-0.1.tar.gz/polygon-uploader-0.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+setuptools.setup(
+    name="polygon-uploader",
+    version="0.1",
+    description="uploader to polygon.codeforces.com",
+    url="https://github.com/niyaznigmatullin/polygon-uploader",
+    author="Niyaz Nigmatullin",
+    install_requires=[
+        'polygon-api>=1.0a6',
+        'requests',
+        'pyyaml',
+        'progressbar2',
+        'beautifulsoup4',
+    ],
+    packages=['polygon_uploader',
+              'polygon_uploader.common',
+              'polygon_uploader.lojac',
+              'polygon_uploader.usaco',
+    ],
+    entry_points={
+        'console_scripts': [
+            'lojacimport=polygon_uploader.lojac:main',
+            'usacoimport=polygon_uploader.usaco:main',
+        ]
+    },
+)
