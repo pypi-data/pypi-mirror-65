05f64be74b02d7ffcc7450c928ddac2ca32c163e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:01 +0000

    Add package vvlab/0.1.8/vvlab-0.1.8.tar.gz

---
+++ packages/vvlab/vvlab-0.1.8.tar.gz/vvlab-0.1.8/vvlab/__version__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding=utf-8
+"""
+@author: Jiawei Wu
+@create time: 2020-03-18 11:52
+@edit time: 2020-04-07 19:41
+@FilePath: /vvlab/__version__.py
+"""
+
+
+
+VERSION = (0, 1, 8)
+
+__version__ = '.'.join(map(str, VERSION))
