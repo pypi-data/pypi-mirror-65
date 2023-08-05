9ca21668b75ede29ca1f4a801b5832333adcea0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:37 +0000

    Add package mathplot/0.1/mathplot-0.1.tar.gz

---
+++ packages/mathplot/mathplot-0.1.tar.gz/mathplot-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='mathplot',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['mathplot'],
+      author = 'Manoj Aryal',
+      zip_safe=False)
