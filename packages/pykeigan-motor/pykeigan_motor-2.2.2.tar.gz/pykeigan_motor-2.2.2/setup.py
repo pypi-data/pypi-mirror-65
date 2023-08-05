9bece83bd71289b9bcc9d15e4c88883a833a9ac4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:14 +0000

    Add package pykeigan-motor/2.2.2/pykeigan_motor-2.2.2.tar.gz

---
+++ packages/pykeigan-motor/pykeigan_motor-2.2.2.tar.gz/pykeigan_motor-2.2.2/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+from os import path
+import codecs
+this_directory = path.abspath(path.dirname(__file__))
+with codecs.open(path.join(this_directory, 'README.rst'),'r','utf-8') as f:
+    readme = f.read()
+
+setup(
+    name='pykeigan_motor',
+    version='2.2.2',
+    description='Python Library for Keigan Motors (v2)',
+    long_description=readme,
+    long_description_content_type='text/x-rst',
+    install_requires=['pyserial'],
+    extras_require={'ble' : ['bluepy']},
+    author='Tomohiro Takata, Hiroshi Harada',
+    author_email='takata@innovotion.co.jp, harada@keigan.co.jp',
+    url='https://github.com/keigan-motor/pykeigan_motor',
+    license='MIT-LICENSE',
+    packages=find_packages(exclude=('tests', 'docs'))
+)
