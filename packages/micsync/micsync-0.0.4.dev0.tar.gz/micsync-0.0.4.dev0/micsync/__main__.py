f9ab04a22d41c6e81907f38084cb53f0c3bc4f17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:19 +0000

    Add package micsync/0.0.4.dev0/micsync-0.0.4.dev0.tar.gz

---
+++ packages/micsync/micsync-0.0.4.dev0.tar.gz/micsync-0.0.4.dev0/micsync/__main__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import sys
+from .micsync import Micsync
+from .version import _program_name
+name = _program_name
+
+
+class MicsyncFriend(Micsync):
+    def __init__(self, mode_name, options, paths):
+        super().__init__(mode_name, options, paths)
+
+    def init_with_arguments(self, arguments):
+        return super()._init_with_arguments(arguments)
+
+
+program = MicsyncFriend(None, None, None)
+if program.init_with_arguments(sys.argv):
+    program.sync()
+
