acf9761de316a37a92cd999880441d2f6689cf4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:11 +0000

    Add package fsds/0.2.1/fsds-0.2.1.tar.gz

---
+++ packages/fsds/fsds-0.2.1.tar.gz/fsds-0.2.1/fsds/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for fsds."""
+
+__author__ = """James Irving"""
+__email__ = 'james.irving.phd@gmail.com'
+__version__ = '0.2.1'
+
+from .default import *
+# from .fsds import imports 
