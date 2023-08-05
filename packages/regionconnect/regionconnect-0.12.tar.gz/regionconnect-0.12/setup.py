f856c2dd019c4caa97ed6f35eb023b4febcfb61e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:22 +0000

    Add package regionconnect/0.12/regionconnect-0.12.tar.gz

---
+++ packages/regionconnect/regionconnect-0.12.tar.gz/regionconnect-0.12/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open("README.md","r") as fh:
+      long_description=fh.read()
+
+setup(name='regionconnect',
+      version='0.12',
+      description='IIT Human Brain Atlas regionconnect tool',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/SheridanQ/regionconnect.git',
+      author='Xiaoxiao Qi',
+      author_email='xqi10@hawk.iit.edu',
+      license='MIT',
+      packages=['regionconnect'],
+      install_requires=[
+      'numpy',
+      'nibabel'],
+      classifiers=['Development Status :: 3 - Alpha', 
+                  'Intended Audience :: Developers',
+                  'Topic :: Software Development :: Build Tools'],
+      entry_points={'console_scripts':['regionconnect=regionconnect:regionconnect']})
