fc15e11c18d2ad65b3459aa81e2dc1ecf4476ad3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:08 +0000

    Add package autoastro/0.7.1/autoastro-0.7.1.tar.gz

---
+++ packages/autoastro/autoastro-0.7.1.tar.gz/autoastro-0.7.1/autoastro/exc.py
@@ -0,0 +1,14 @@
+class CosmologyException(Exception):
+    pass
+
+
+class GalaxyException(Exception):
+    pass
+
+
+class PlottingException(Exception):
+    pass
+
+
+class UnitsException(Exception):
+    pass
