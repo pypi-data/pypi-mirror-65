4d97b1f12d39cbce6c0b7e8d4b732230074515c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:51 +0000

    Add package dff-calc/0.2.0/dff_calc-0.2.0-py3-none-any.whl

---
+++ packages/dff-calc/dff_calc-0.2.0-py3-none-any.whl/dff_calc/__init__.py
@@ -0,0 +1,7 @@
+"""dff_calc - Simple dF/F calculation for neural calcium traces"""
+
+from dff_calc.df_f_calculation import dff_calc
+
+__version__ = '0.2'
+__author__ = 'Hagai Har-Gil <hagaihargil@protonmail.com>'
+__all__ = ['dff_calc']
