e014b43495cdba2f24d217eb4deec42947862eb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:23 +0000

    Add package django-progressive/1.0.3/django-progressive-1.0.3.tar.gz

---
+++ packages/django-progressive/django-progressive-1.0.3.tar.gz/django-progressive-1.0.3/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open("README.md") as fh:
+    long_description = fh.read()
+
+setup(
+    name='django-progressive',
+    version='1.0.3',
+    packages=['django_pwa', 'django_pwa.static.django_pwa', 'django_pwa.templates'],
+    package_data={"django_pwa.templates": ["*"],
+                  "django_pwa.static.django_pwa": ["*"]},
+    license='GPL License',
+    description='Library for adding progressive web app functionality in your django project.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/m19t12/django-pwa',
+    author='Manolis Tsoukalas',
+    author_email='emmtsoukalas@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 3.0',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
