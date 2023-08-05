235842a8c1b7cd7a9e53c96f23c2f97f5bc5486d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:02 +0000

    Add package peach-invasion/0.0.1/peach_invasion-0.0.1-py3-none-any.whl

---
+++ packages/peach-invasion/peach_invasion-0.0.1-py3-none-any.whl/peach_invasion/settings.py
@@ -0,0 +1,67 @@
+class Settings:
+    """ Stores game settings """
+
+    def __init__(self):
+        # Player
+        self.player_image = 'player.bmp'
+        self.player_speed = 4.5
+
+        # Bullets
+        self.bullet_image = 'bullet.bmp'
+        self.bullet_speed = 6.5
+
+        # Enemy
+        self.enemy_image = 'enemy.bmp'
+        self.enemy_x_speed = 1.0
+        self.enemy_y_speed = .15
+        self.enemy_density = .5
+        self.enemy_vertical_space_occupied = .5
+
+        # Ammo
+        self.ammo_image = 'ammo.bmp'
+        self.ammo_limit = 2
+
+        # Health
+        self.health_image = 'health.bmp'
+        self.health_limit = 3
+
+        # Background
+        self.bg_color = (20, 60, 30)
+        self.bg_grass_image = 'grass.bmp'
+        self.bg_grass_number = 35
+
+        # Scoreboard
+        self.scoreboard_txt_color = (210, 240, 220)
+        self.scoreboard_font_size = 42
+        self.scoreboard_margin = 10
+
+        # Button
+        self.btn_size = (250, 70)
+        self.btn_color = (210, 240, 220)
+        self.btn_txt_color = (20, 60, 30)
+        self.btn_font_size = 64
+
+        # Gameplay
+        self.speedup_rate = 1.1
+
+        # Sounds
+        self.sound_background_music = 'background.wav'
+        self.sound_fire = 'fire.wav'
+        self.sound_no_ammo = 'no_ammo.wav'
+        self.sound_death = 'death.wav'
+        self.sound_victory = 'victory.ogg'
+        self.sound_set_enemy_death = (
+            'enemy_death_1.ogg',
+            'enemy_death_2.wav',
+            'enemy_death_3.wav',
+            'enemy_death_4.wav',
+            'enemy_death_5.wav',
+        )
+
+    @staticmethod
+    def image_absolute_path(rel_path):
+        return f'static/images/{rel_path}'
+
+    @staticmethod
+    def audio_absolute_path(rel_path):
+        return f'static/audio/{rel_path}'
