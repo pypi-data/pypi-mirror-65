91b88527fb2717d0cb947ddbb452975fe8c21e48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:38 +0000

    Add package SPyQR/0.0.2/SPyQR-0.0.2.tar.gz

---
+++ packages/SPyQR/SPyQR-0.0.2.tar.gz/SPyQR-0.0.2/SPyQR/__init__.py
@@ -0,0 +1,8 @@
+"""
+SPyQR: A bad package for bad Romans.
+
+SPyQR is an inclusive python project that dreams of allowing all backgrounds to be able to program in python, even
+if all you speak is ancient Latin. <3
+"""
+
+from SPyQR.SPyQR import *
