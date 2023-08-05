e8f45f3427778f0ff0d4d514ec647ea55138b620
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:35 +0000

    Add package chiaoupload/0.0.3/chiaoupload-0.0.3.tar.gz

---
+++ packages/chiaoupload/chiaoupload-0.0.3.tar.gz/chiaoupload-0.0.3/chiaoupload/__init__.py
@@ -0,0 +1,1 @@
+from chiaoupload import hello
---
+++ packages/chiaoupload/chiaoupload-0.0.3.tar.gz/chiaoupload-0.0.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from os import path
+from io import open
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='chiaoupload',  # Required
+    version='0.0.3',  # Required
+    description='test upload package',  # Optional
+    #long_description=long_description,  # Optional
+    #long_description_content_type='text/markdown',  # Optional (see note above)
+    url='https://github.com/luckchiao',  # Optional
+    author='chiao',  # Optional
+    author_email='luckchiao@gmail.com',  # Optional
+    classifiers=[  # Optional
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='python',  # Optional
+    packages=find_packages(exclude=['pandas']),
+)
