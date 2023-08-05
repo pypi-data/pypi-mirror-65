4699357bfab2db06b8531c9f28f6f774cd996ed8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:28 +0000

    Add package floydpink.jsii-code-samples/1.0.6/floydpink.jsii-code-samples-1.0.6.tar.gz

---
+++ packages/floydpink.jsii-code-samples/floydpink.jsii-code-samples-1.0.6.tar.gz/floydpink.jsii-code-samples-1.0.6/README.md
@@ -0,0 +1,3 @@
+# jsii-code-samples
+
+> An example jsii package authored in TypeScript that gets published as GitHub packages for Node.js, Python, Java and dotnet.
---
+++ packages/floydpink.jsii-code-samples/floydpink.jsii-code-samples-1.0.6.tar.gz/floydpink.jsii-code-samples-1.0.6/src/floydpink/jsii_code_samples/__init__.py
@@ -0,0 +1,40 @@
+"""
+# jsii-code-samples
+
+> An example jsii package authored in TypeScript that gets published as GitHub packages for Node.js, Python, Java and dotnet.
+"""
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+__jsii_assembly__ = jsii.JSIIAssembly.load("jsii-code-samples", "1.0.6", __name__, "jsii-code-samples@1.0.6.jsii.tgz")
+
+
+class HelloWorld(metaclass=jsii.JSIIMeta, jsii_type="jsii-code-samples.HelloWorld"):
+    def __init__(self) -> None:
+        jsii.create(HelloWorld, self, [])
+
+    @jsii.member(jsii_name="fibonacci")
+    def fibonacci(self, num: jsii.Number) -> jsii.Number:
+        """
+        :param num: -
+        """
+        return jsii.invoke(self, "fibonacci", [num])
+
+    @jsii.member(jsii_name="sayHello")
+    def say_hello(self, name: str) -> str:
+        """
+        :param name: -
+        """
+        return jsii.invoke(self, "sayHello", [name])
+
+
+__all__ = ["HelloWorld", "__jsii_assembly__"]
+
+publication.publish()
