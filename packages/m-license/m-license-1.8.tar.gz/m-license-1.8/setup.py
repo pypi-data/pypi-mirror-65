014e13f8752777f5e6e7ca7d5d8d23acf41d5bb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:30 +0000

    Add package m-license/1.8/m-license-1.8.tar.gz

---
+++ packages/m-license/m-license-1.8.tar.gz/m-license-1.8/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" Author: ChungNT
+    Company: MobioVN
+    Date created: 04/10/2019
+"""
+
+from setuptools import setup
+
+setup(name='m-license',
+      version='1.8',
+      description='Mobio libraries',
+      url='https://github.com/mobiovn',
+      author='MOBIO',
+      author_email='contact@mobio.vn',
+      license='MOBIO',
+      packages=['mobio/libs/license'],
+      package_data={'': ['*.so']},
+      install_requires=['python-jose',
+                        'pycryptodome==3.4.3',
+                        'numpy'])
