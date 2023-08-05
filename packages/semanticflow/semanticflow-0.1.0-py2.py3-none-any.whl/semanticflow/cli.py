548fcba875e0fd84e3cf0b8ca1375ab6846ccfc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:22 +0000

    Add package semanticflow/0.1.0/semanticflow-0.1.0-py2.py3-none-any.whl

---
+++ packages/semanticflow/semanticflow-0.1.0-py2.py3-none-any.whl/semanticflow/cli.py
@@ -0,0 +1,16 @@
+"""Console script for semanticflow."""
+import sys
+import click
+
+
+@click.command()
+def main(args=None):
+    """Console script for semanticflow."""
+    click.echo("Replace this message by putting your code into "
+               "semanticflow.cli.main")
+    click.echo("See click documentation at https://click.palletsprojects.com/")
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
