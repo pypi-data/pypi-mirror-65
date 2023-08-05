238c0b969d0d8b150ee03375eb172e603df5dd29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:45 +0000

    Add package noun-phrase-ua/0.0.5/noun-phrase-ua-0.0.5.tar.gz

---
+++ packages/noun-phrase-ua/noun-phrase-ua-0.0.5.tar.gz/noun-phrase-ua-0.0.5/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name='noun-phrase-ua',
+    version='0.0.5',
+    packages=setuptools.find_packages(),
+    url='https://github.com/artemkramov/np-extractor-ua',
+    author='Artem Kramov',
+    author_email='artemkramov@gmail.com',
+    description='Noun phrase extractor for the Ukrainian language',
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    install_requires=[
+          'ufal.udpipe', 'dateparser'
+    ],
+    include_package_data=True
+)
