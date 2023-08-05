2d08b22c38ab5e0813bc579067398605ddac3e9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:49 +0000

    Add package lax/1.0.0/lax-1.0.0.tar.gz

---
+++ packages/lax/lax-1.0.0.tar.gz/lax-1.0.0/setup.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="lax",
+      version="1.0.0",
+      packages=["liblax"],
+      scripts=['lax'],
+      author="Iury O. G. Figueiredo",
+      author_email="ioliveira@id.uff.br")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
