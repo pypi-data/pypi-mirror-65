19f7b0506be61b9e6a5e2768960436cf38a7d164
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:59 +0000

    Add package sbiRandM/1.0.1/sbiRandM-1.0.1-py3-none-any.whl

---
+++ packages/sbiRandM/sbiRandM-1.0.1-py3-none-any.whl/sbiRandM/__init__.py
@@ -0,0 +1,14 @@
+from .sbiRandM.exceptions import *
+from .sbiRandM.superimp import *
+from .sbiRandM.modeller_comparison import *
+from .sbiRandM.mainSuperimp import *
+from .sbiRandM.check_stoichometry import *
+from .sbiRandM.interaction_module import *
+from .sbiRandM.data import *
+from .sbiRandM.GUI.mainWindow import *
+from .sbiRandM.mainModeller import *
+from .sbiRandM.models import *
+from .sbiRandM.pairwise_generator import *
+
+
+
---
+++ packages/sbiRandM/sbiRandM-1.0.1-py3-none-any.whl/sbiRandM/sbiRandM/mainModeller.py
@@ -0,0 +1,7 @@
+import argparse, warnings
+from sbiRandM.sbiRandM.modeller_comparison import *
+from sbiRandM.sbiRandM.mainSuperimp import *
+
+args = parse_arguments()
+
+mainMod(args)
