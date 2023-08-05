b4d771219dc5f64dc711a6e6347587c30ec40207
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:39 +0000

    Add package outerspace/0.2.7/outerspace-0.2.7.tar.gz

---
+++ packages/outerspace/outerspace-0.2.7.tar.gz/outerspace-0.2.7/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='outerspace',
+    version='0.2.7',
+    description='',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/sirbiscuit/outerspace',
+    author='sirbiscuit',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'opentsne~=0.3.11',
+        'ipywidgets~=7.5.1',
+        'bokeh~=1.2.0',
+    ],
+)
