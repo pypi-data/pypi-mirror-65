735d8d4792070521d3a564c790e01a6874021c51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:02 +0000

    Add package ttictoc/0.5.4/ttictoc-0.5.4-py3-none-any.whl

---
+++ packages/ttictoc/ttictoc-0.5.4-py3-none-any.whl/ttictoc/__init__.py
@@ -0,0 +1,9 @@
+from .tictoc import Timer,tic,toc,tic2,toc2
+
+#def __get_version():
+#  import json
+#  with open('version.json') as f:
+#    version = json.load(f)['version']
+#  return version
+
+__version__ = '0.5.4'#__get_version()
