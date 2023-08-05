0404207b9f343cbd2ffce9cf66a7949ed471abbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:34 +0000

    Add package wtphm/0.1.0/wtphm-0.1.0.tar.gz

---
+++ packages/wtphm/wtphm-0.1.0.tar.gz/wtphm-0.1.0/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wtphm", # Replace with your own username
+    version="0.1.0",
+    author="Kevin Leahy",
+    description="SCADA data pre-processing library for prognostics and health"\
+                "management and fault detection of wind turbines",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/lkev/wtphm",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "pandas", "numpy", "scipy", "itertools", "collections", "matplotlib",
+        "math", "sklearn", "warnings"],
+    include_package_data=True,
+    python_requires='>=3.6'
+)
+setuptools.find_packages()
