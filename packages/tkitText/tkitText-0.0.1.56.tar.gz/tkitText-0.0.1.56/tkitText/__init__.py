84915d64e66dce2aec1a0d140eb4656b328e4fea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:33 +0000

    Add package tkitText/0.0.1.56/tkitText-0.0.1.56.tar.gz

---
+++ packages/tkitText/tkitText-0.0.1.56.tar.gz/tkitText-0.0.1.56/tkitText/__init__.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+# __init__.py
+#from .a import A
+#from .b import B
+"""
+# Terry_toolkit
+===
+各种工具合集
+文档地址
+http://terry-toolkit.terrychan.org/zh/master/
+# 安装
+> pip3 install Terry_toolkit
+"""
+#多文件导入以后只用导入 import Terry-toolkit 就可以使用所有类
+#>>> import mymodule
+#>>> a = mymodule.A()
+#>>> a.spam()
+
+
+from.text import *
+# from.tjson import *
+# # https://www.helplib.com/GitHub/article_112235
+# # https://unqlite-python.readthedocs.io/en/latest/quickstart.html#key-value-features
+# from unqlite import *
+# # https://github.com/RaRe-Technologies/sqlitedict
+# from sqlitedict import SqliteDict
+
+# """
+# #cacheout缓存模块
+# # from cacheout import Cache# 如果选择LFUCache 就导入即可
+# # https://pypi.org/project/cacheout/
+# """
+# from cacheout import *
+# # cache = LFUCache()
+# from .sentence_parser import *
+# from .triple_extraction import *
+# #三元组获取
+# from .ie import *
+
+# # from .bootstrap import *
+
+# #加载各种的内置资源
+# from .resources import *
+
+
+# from .pkl import *
+
+
+# from .search import *
