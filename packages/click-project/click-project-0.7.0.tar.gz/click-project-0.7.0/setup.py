448c317a6edf105fdb4eb0ac6bb96f3f2ca4eaa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:09 +0000

    Add package click-project/0.7.0/click-project-0.7.0.tar.gz

---
+++ packages/click-project/click-project-0.7.0.tar.gz/click-project-0.7.0/setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import versioneer
+from setuptools import setup, find_packages
+import fastentrypoint  # NOQA
+
+versions = versioneer.get_versions()
+version = versions["version"]
+if version == u"0+unknown":
+    version = u"sha:{}".format(versions["full-revisionid"])
+
+cmdclass = versioneer.get_cmdclass()
+
+setup(
+    name='click-project',
+    cmdclass=cmdclass,
+    version=version,
+    author='Samuel Loury',
+    author_email='konubinixweb@gmail.com',
+    description='A framework to help you have an awesome CLI',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[
+        "click>=6.4",
+        "click-log",
+        "click-didyoumean",
+        "click-completion>=0.5.0",
+        "six",
+        "glob2",
+        "colorama",
+        "python-dateutil",
+        "keyring",
+        "tabulate",
+        "networkx",
+        "pydotplus",
+        "pluginbase",
+        "appdirs",
+        "ipdb",
+        "humanize",
+        "Pygments",
+    ],
+    entry_points = {
+        'console_scripts':
+        [
+            'clk=click_project.main:main',
+        ]
+    },
+)
