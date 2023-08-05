3ac426ecbb35a80b7f7a16ebfe021ba56eaa9bb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:36 +0000

    Add package FormulaLab/0.1.0/FormulaLab-0.1.0.tar.gz

---
+++ packages/FormulaLab/FormulaLab-0.1.0.tar.gz/FormulaLab-0.1.0/README.md
@@ -0,0 +1,60 @@
+# FormulaLab
+[FormulaLab](https://azizalqasem.github.io/FormulaLab/) is a package that allows 
+you to **derive** new formulas, **search** in formulas database, and **mange** 
+your formulas database to connect it effeciently to your project code.
+
+
+## Installation
+FormulaLab depends on: 
+* python +3.4
+* Sympy
+* Pandas  
+
+To install [FormulaLab](https://azizalqasem.github.io/FormulaLab/):
+```python
+$ pip install FormulaLab
+```
+
+
+## Usages
+
+```python
+>>> import FormulaLab as fl
+
+>>> Physics_formulas = ['F = m * a', 'v = a * t']
+>>> phy_search = fl.FormulaSearch(data=Physics_formulas)
+
+# Now say you want to derive F as a function of t
+>>> Force = phy_search.derive('F', 't')
+>>> Force
+[m*v/t]
+
+# Now you want to convert it to a python function
+>>> Force_py = fl.function(Force)
+>>> Force_py(m = 2, v = 3, t = 2)
+3.0
+
+# Now you want to find the the value of "t" in a direct search (no subtitution)
+>>> phy_search.find('t')
+[v/a]
+
+>>> phy_search.find('a')
+[F/m, v/t]
+
+# What if you want "a" as a function of "t" and "v", only in a direct search:
+>>> phy_search.find('a', ['t','v'])    # Also you can say here: phy_search.get('a', 't') 
+[v/t] 
+
+```
+
+## Tutorals and documentations
+visit [FormulaLab](https://azizalqasem.github.io/FormulaLab/)
+
+
+### Author
+Abdulaziz Alqasem
+Aziz_Alqasem@hotmail.com
+
+
+### References
+FormulLab is bult upon [SymPy](https://www.sympy.org/en/index.html)
---
+++ packages/FormulaLab/FormulaLab-0.1.0.tar.gz/FormulaLab-0.1.0/setup.py
@@ -0,0 +1,61 @@
+import setuptools
+from FormulaLab import __version__
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="FormulaLab", 
+    version=__version__,
+    author="Abdulaziz Alqasem",
+    author_email="Aziz_Alqasem@hotmail.com",
+    description="Search Engine of Mathmatical Formulas Database",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://azizalqasem.github.io/FormulaLab/index.html",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 5 - Production/Stable",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Topic :: Database",
+        "Topic :: Documentation :: Sphinx",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Scientific/Engineering :: Physics",    
+    ],
+    python_requires='>=3.4',
+    keywords=('FormulaLab', 'Formulas Search Engine', 'Deriving Formulas', 
+              'Formulas', 'Search', 'derivation', 'database', 'math', 
+              'physics', 'Science', 'Engineering', 'Solving equations',
+              'equations', 'Formula', 'Scientific Research'),
+    project_urls={
+    'Documentation': 'https://azizalqasem.github.io/FormulaLab/index.html',
+    'Source': 'https://github.com/AzizAlqasem/FormulaLab'},
+    install_requires=['sympy', 'pandas', 'numpy'],
+)
+
+#packages=find_packages(include=['FormulaLab', 'FormulaLab.*'], exclude = []),
+
+"""
+Reference:
+# Naming versions conventions
+1.2.0.dev1  # Development release
+1.2.0a1     # Alpha Release
+1.2.0b1     # Beta Release
+1.2.0rc1    # Release Candidate
+1.2.0       # Final Release
+1.2.0.post1 # Post Release
+15.10       # Date based release
+23          # Serial release
+
+# Increment version convention
+MAJOR version when they make incompatible API changes,
+MINOR version when they add functionality in a backwards-compatible manner, and
+MAINTENANCE version when they make backwards-compatible bug fixes.
+"""
