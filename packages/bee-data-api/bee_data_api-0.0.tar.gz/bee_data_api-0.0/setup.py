fdddae7575b9a8551d39a16addd164406e87aeb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:47 +0000

    Add package bee-data-api/0.0/bee_data_api-0.0.tar.gz

---
+++ packages/bee-data-api/bee_data_api-0.0.tar.gz/bee_data_api-0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='bee_data_api',
+      version='0.0',
+      description='None',
+      packages=['bee_data_api'],
+      author_email='steep1183@gmail.com',
+      zip_safe=False,
+      install_requires=['requests'])
+
