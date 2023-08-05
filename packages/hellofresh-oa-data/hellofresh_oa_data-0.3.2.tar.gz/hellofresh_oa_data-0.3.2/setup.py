6587e3c1ab93fc0f5a2f85d69b17fce139755ca1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:53 +0000

    Add package hellofresh-oa-data/0.3.2/hellofresh_oa_data-0.3.2.tar.gz

---
+++ packages/hellofresh-oa-data/hellofresh_oa_data-0.3.2.tar.gz/hellofresh_oa_data-0.3.2/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="hellofresh_oa_data",
+    version="0.3.2",
+    author="US OA Team",
+    author_email="sca-automation@hellofresh.com",
+    long_description="Package with helper function to support functions of OA team in regarding to data",
+    url="https://github.com/hellofresh/us-ops-tech-scripts/hellofresh-oa-data",
+    packages=setuptools.find_packages(),
+    zip_safe=False
+)
