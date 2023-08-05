6ffe0f81d68f01504ae9d852212cb67c5fd068c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:30 +0000

    Add package mle-udacity-dist-test/0.1/mle_udacity_dist_test-0.1.tar.gz

---
+++ packages/mle-udacity-dist-test/mle_udacity_dist_test-0.1.tar.gz/mle_udacity_dist_test-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='mle_udacity_dist_test',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['mle_udacity_dist_test'],
+      author = 'coding maggie',
+      zip_safe=False)
