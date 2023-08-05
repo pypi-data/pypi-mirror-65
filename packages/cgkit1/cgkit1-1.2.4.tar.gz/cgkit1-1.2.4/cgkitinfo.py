90e6a51dcbc1bc60bdb69d0366710aa0bdb5042c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:06 +0000

    Add package cgkit1/1.2.4/cgkit1-1.2.4.tar.gz

---
+++ packages/cgkit1/cgkit1-1.2.4.tar.gz/cgkit1-1.2.4/cgkitinfo.py
@@ -0,0 +1,20 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+######################################################################
+# cgkitinfo
+#
+# Copyright (C) 2004, Matthias Baas (baas@ira.uka.de)
+#
+# http://cgkit.sourceforge.net
+#
+# You may distribute under the terms of the BSD license, as
+# specified in the file license.txt.
+####################################################################
+
+version_info = (1, 2, 4, "final")  # also update setup.py
+
+# The following line is replaced by the setup script
+version = "1.2.4"
