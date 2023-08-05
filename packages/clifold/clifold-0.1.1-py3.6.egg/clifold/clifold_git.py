a1eb781ca30ba96014afd116caa363c43ffb2052
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:03 +0000

    Add package clifold/0.1.1/clifold-0.1.1-py3.6.egg

---
+++ packages/clifold/clifold-0.1.1-py3.6.egg/clifold/clifold_git.py
@@ -0,0 +1,26 @@
+'''Git initialization in project folder.'''
+import os
+import shutil
+import subprocess
+
+
+def git_init(git, project_path):
+    '''
+    Git initialization function.
+
+    Args:
+        git: True or False
+        project_path: absolute path to the project
+
+    Returns:
+        print Success or Error message
+    '''
+    if git:
+        git_path = shutil.which('git')
+        if os.path.exists(git_path):
+            os.chdir(project_path)
+            subprocess.run([git_path, 'init'], check=True)
+        else:
+            raise OSError("Git not found!")
+    else:
+        print("Git not initialized...")
