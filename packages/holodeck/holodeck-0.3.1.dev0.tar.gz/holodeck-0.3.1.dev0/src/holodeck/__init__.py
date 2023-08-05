420362ccf09e329b2bb935c14dff7c9bc03bb8d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:36 +0000

    Add package holodeck/0.3.1.dev0/holodeck-0.3.1.dev0.tar.gz

---
+++ packages/holodeck/holodeck-0.3.1.dev0.tar.gz/holodeck-0.3.1.dev0/src/holodeck/__init__.py
@@ -0,0 +1,8 @@
+"""Holodeck is a high fidelity simulator for reinforcement learning.
+"""
+__version__ = '0.3.1dev'
+
+from holodeck.holodeck import make
+from holodeck.packagemanager import *
+
+__all__ = ['agents', 'environments', 'exceptions', 'holodeck', 'make', 'packagemanager', 'sensors']
