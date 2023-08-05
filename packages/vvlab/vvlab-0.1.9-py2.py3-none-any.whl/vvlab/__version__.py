c5e1efbf4a3bad6a0b14514c8b5b6d9b12b66e0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:13 +0000

    Add package vvlab/0.1.9/vvlab-0.1.9-py2.py3-none-any.whl

---
+++ packages/vvlab/vvlab-0.1.9-py2.py3-none-any.whl/vvlab/__version__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding=utf-8
+"""
+@author: Jiawei Wu
+@create time: 2020-03-18 11:52
+@edit time: 2020-04-07 19:59
+@FilePath: /vvlab/__version__.py
+"""
+
+
+
+VERSION = (0, 1, 9)
+
+__version__ = '.'.join(map(str, VERSION))
