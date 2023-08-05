bc953b839f59912c3164109c0682cae9cbdeb7a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:04 +0000

    Add package rftg/0.0.3/rftg-0.0.3.tar.gz

---
+++ packages/rftg/rftg-0.0.3.tar.gz/rftg-0.0.3/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rftg",
+    version="0.0.3",
+    author="Damien Carol",
+    author_email="damien.carol@gmail.com",
+    description="AI research project about Race for the Galaxy board game",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/damiencarol/rftg",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
