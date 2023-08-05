ddf64ec24dcfb3d3730d21ff2f27a90408480144
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:51 +0000

    Add package proofaday/0.2.4/proofaday-0.2.4-py3-none-any.whl

---
+++ packages/proofaday/proofaday-0.2.4-py3-none-any.whl/proofaday/constants.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+URL = "https://proofwiki.org/wiki/"
+RANDOM = "Special:Random"
+
+NPREFETCH = 10
+LOG_PATH = Path(__file__).parent
+LOG_FILE = "proofaday.log"
+STATUS_PATH = Path(__file__).parent
+STATUS_FILE = ".proofaday.status"
+
+HOST = "localhost"
+PORT = 48484
+
+CLIENT_TIMEOUT = 3
