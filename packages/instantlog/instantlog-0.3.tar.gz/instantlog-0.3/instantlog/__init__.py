65220cbb0d1cd917def7c49ec1ba52b1571369d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:18 +0000

    Add package instantlog/0.3/instantlog-0.3.tar.gz

---
+++ packages/instantlog/instantlog-0.3.tar.gz/instantlog-0.3/instantlog/__init__.py
@@ -0,0 +1,1 @@
+from instantlog.instantlog import InstantLog
---
+++ packages/instantlog/instantlog-0.3.tar.gz/instantlog-0.3/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+setup(
+  name = 'instantlog',        
+  packages = ['instantlog'],   
+  version = '0.3',      
+  license='MIT',       
+  description = 'Simple colorful logger info/waring/error; Screen and file logs',  
+  author = 'Wojciech Jankiewicz',
+  author_email = 'wojtekjankiewicz@gmail.com',
+  url = 'https://github.com/jan-wo/instantlog',  
+  download_url = 'https://github.com/jan-wo/instantlog/archive/v_0.3.tar.gz',#
+  keywords = ['LOGGER', 'COLORS', 'INSTANT'],
+  install_requires=[
+          'colorama'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
