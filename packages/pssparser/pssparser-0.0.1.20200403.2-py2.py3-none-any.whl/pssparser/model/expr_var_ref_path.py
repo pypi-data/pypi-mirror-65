c8bd0296bfbbf1e7b118312bb4c0182540e42f87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:36 +0000

    Add package pssparser/0.0.1.20200403.2/pssparser-0.0.1.20200403.2-py2.py3-none-any.whl

---
+++ packages/pssparser/pssparser-0.0.1.20200403.2-py2.py3-none-any.whl/pssparser/model/expr_var_ref_path.py
@@ -0,0 +1,20 @@
+'''
+Created on Mar 18, 2020
+
+@author: ballance
+'''
+from pssparser.model.expr_type import ExprType
+from pssparser.model.expr_hierarchical_id import ExprHierarchicalId
+
+class ExprVarRefPath(ExprType):
+    
+    def __init__(self, hid : ExprHierarchicalId):
+        self.hid = hid
+        self.lhs : ExprType = None
+        self.rhs : ExprType = None
+        self.target = None
+        
+    def accept(self, v):
+        v.visit_expr_var_ref_path(self)
+        
+        
