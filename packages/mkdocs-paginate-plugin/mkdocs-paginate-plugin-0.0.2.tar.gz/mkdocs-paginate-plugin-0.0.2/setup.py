00399337fa4f82362f1b7dd311cf79d7d5a7a886
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:46 +0000

    Add package mkdocs-paginate-plugin/0.0.2/mkdocs-paginate-plugin-0.0.2.tar.gz

---
+++ packages/mkdocs-paginate-plugin/mkdocs-paginate-plugin-0.0.2.tar.gz/mkdocs-paginate-plugin-0.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mkdocs-paginate-plugin',
+    version='0.0.2',
+    description="Mkdocs paginate plugin",
+    url="https://github.com/kubilus1/mkdocs-paginate-plugin",
+    author='Matt Kubilus',
+    install_requires=[
+        'mkdocs>=0.17',
+    ],
+    packages=find_packages(exclude=['*.tests']),
+    entry_points={
+        'mkdocs.plugins': [
+            'paginate = paginate.plugin:PaginatePlugin'
+        ]
+    }
+)
