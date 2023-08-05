8ea0cb32fe07c7b3e21c8ec3c378c8e0d5e1c8b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:34 +0000

    Add package netblocks3/0.0.13rc0/netblocks3-0.0.13rc0.tar.gz

---
+++ packages/netblocks3/netblocks3-0.0.13rc0.tar.gz/netblocks3-0.0.13rc0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['netblocks3']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'netblocks3',
+    'version': '0.0.13rc0',
+    'description': 'Get the Cloud Provider netblocks (Python3 version)',
+    'long_description': None,
+    'author': 'tomasfse',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
