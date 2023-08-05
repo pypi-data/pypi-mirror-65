36cf7ee2ae777ffc454a8aef20d6d02fdce5c9f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:02 +0000

    Add package pymoo/0.4.0/pymoo-0.4.0-cp37-cp37m-macosx_10_7_x86_64.whl

---
+++ packages/pymoo/pymoo-0.4.0-cp37-cp37m-macosx_10_7_x86_64.whl/pymoo/usage/termination/usage_constr_violation.py
@@ -0,0 +1,16 @@
+from pymoo.algorithms.so_genetic_algorithm import GA
+from pymoo.factory import get_problem
+from pymoo.optimize import minimize
+from pymoo.util.termination.constr_violation import ConstraintViolationToleranceTermination
+
+problem = get_problem("g05")
+algorithm = GA(pop_size=100, return_least_infeasible=True)
+
+res = minimize(problem,
+               algorithm,
+               ConstraintViolationToleranceTermination(),
+               seed=1,
+               verbose=True)
+
+print(res.CV[0])
+print(res.F[0])
