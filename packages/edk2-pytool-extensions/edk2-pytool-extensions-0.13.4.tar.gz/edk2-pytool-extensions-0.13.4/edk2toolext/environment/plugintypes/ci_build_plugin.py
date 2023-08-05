7955a62c14e16c60edc2d1a05015baf140d8a40e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:34 +0000

    Add package edk2-pytool-extensions/0.13.4/edk2-pytool-extensions-0.13.4.tar.gz

---
+++ packages/edk2-pytool-extensions/edk2-pytool-extensions-0.13.4.tar.gz/edk2-pytool-extensions-0.13.4/edk2toolext/environment/plugintypes/ci_build_plugin.py
@@ -0,0 +1,103 @@
+# @file ci_build_plugin
+# Plugin that supports adding tests or operations to the ci environment
+##
+# Copyright (c) Microsoft Corporation
+#
+# SPDX-License-Identifier: BSD-2-Clause-Patent
+##
+
+
+import os
+import logging
+
+
+class ICiBuildPlugin(object):
+
+    ##
+    # External function of plugin.  This function is used to perform the task of the CiBuild Plugin
+    #
+    #   - package is the edk2 path to package.  This means workspace/package path relative.
+    #   - edk2path object configured with workspace and packages path
+    #   - PkgConfig Object (dict) for the pkg
+    #   - EnvConfig Object
+    #   - Plugin Manager Instance
+    #   - Plugin Helper Obj Instance
+    #   - tc - test case that needs state configured for reporting by plugin.
+    #   - output_stream the StringIO output stream from this plugin via logging
+    #
+    #   Returns  >0 : number of errors found
+    #             0 : passed successfully
+    #            -1 : skipped for missing prereq
+    #
+    #
+    def RunBuildPlugin(self, packagename, Edk2pathObj, pkgconfig, environment, PLM, PLMHelper, tc, output_stream):
+        pass
+
+    ##
+    # Return tuple (string, string) that is the (test case name, test case base class name)
+    #
+    #
+    def GetTestName(self, packagename, environment):
+        pass
+
+    ##
+    # Returns a list of edk2 TARGETs that this plugin would like to run on
+    #
+    # KNOWN TARGET VALUES:
+    #  DEBUG
+    #  RELEASE
+    #  NOOPT
+    #  NO-TARGET
+    #
+    # If the plugin is not Target specific it should return a list of
+    # one element of "NO-TARGET"
+    ##
+    def RunsOnTargetList(self):
+        return ["NO-TARGET"]
+
+    #
+    # Walks a directory for all items ending in certain extension
+    # Default is to walk all of workspace
+    #
+    def WalkDirectoryForExtension(self, extensionlist, directory, ignorelist=None):
+        if not isinstance(extensionlist, list):
+            logging.critical("Expected list but got " + str(type(extensionlist)))
+            return -1
+
+        if directory is None:
+            logging.critical("No directory given")
+            return -2
+
+        if not os.path.isabs(directory):
+            logging.critical("Directory not abs path")
+            return -3
+
+        if not os.path.isdir(directory):
+            logging.critical("Invalid find directory to walk")
+            return -4
+
+        if ignorelist is not None:
+            ignorelist_lower = list()
+            for item in ignorelist:
+                ignorelist_lower.append(item.lower())
+
+        extensionlist_lower = list()
+        for item in extensionlist:
+            extensionlist_lower.append(item.lower())
+
+        returnlist = list()
+        for Root, Dirs, Files in os.walk(directory):
+            for File in Files:
+                for Extension in extensionlist_lower:
+                    if File.lower().endswith(Extension):
+                        ignoreIt = False
+                        if(ignorelist is not None):
+                            for c in ignorelist_lower:
+                                if(File.lower().startswith(c)):
+                                    ignoreIt = True
+                                    break
+                        if not ignoreIt:
+                            logging.debug(os.path.join(Root, File))
+                            returnlist.append(os.path.join(Root, File))
+
+        return returnlist
---
+++ packages/edk2-pytool-extensions/edk2-pytool-extensions-0.13.4.tar.gz/edk2-pytool-extensions-0.13.4/edk2toolext/invocables/edk2_platform_build.py
@@ -0,0 +1,136 @@
+# @file edk2_platform_build
+# Invocable class that does a build.
+# Needs a child of UefiBuilder for pre/post build steps.
+##
+# Copyright (c) Microsoft Corporation
+#
+# SPDX-License-Identifier: BSD-2-Clause-Patent
+##
+import os
+import sys
+import logging
+from edk2toolext import edk2_logging
+from edk2toolext.environment import plugin_manager
+from edk2toolext.environment.plugintypes.uefi_helper_plugin import HelperFunctions
+from edk2toolext.environment import self_describing_environment
+from edk2toolext.environment.uefi_build import UefiBuilder
+from edk2toolext.edk2_invocable import Edk2Invocable
+from edk2toollib.utility_functions import locate_class_in_module
+
+
+class BuildSettingsManager():
+    ''' Platform settings will be accessed through this implementation. '''
+
+    def GetActiveScopes(self):
+        ''' return tuple containing scopes that should be active for this process '''
+        raise NotImplementedError()
+
+    def GetWorkspaceRoot(self):
+        ''' get WorkspacePath '''
+        raise NotImplementedError()
+
+    def GetPackagesPath(self):
+        ''' Return a list of workspace relative paths that should be mapped as edk2 PackagesPath '''
+        raise NotImplementedError()
+
+    def GetName(self):
+        ''' Get the name of the repo, platform, or product being build '''
+        pass
+
+    def AddCommandLineOptions(self, parserObj):
+        ''' Implement in subclass to add command line options to the argparser '''
+        pass
+
+    def RetrieveCommandLineOptions(self, args):
+        '''  Implement in subclass to retrieve command line options from the argparser '''
+        pass
+
+    def GetLoggingLevel(self, loggerType):
+        ''' Get the logging level for a given type
+        base == lowest logging level supported
+        con  == Screen logging
+        txt  == plain text file logging
+        md   == markdown file logging
+        '''
+        pass
+
+
+class Edk2PlatformBuild(Edk2Invocable):
+    ''' Imports UefiBuilder and calls go '''
+
+    def AddCommandLineOptions(self, parserObj):
+        ''' adds command line options to the argparser '''
+
+        # PlatformSettings could also be a subclass of UefiBuilder, who knows!
+        if isinstance(self.PlatformSettings, UefiBuilder):
+            self.PlatformBuilder = self.PlatformSettings
+        else:
+            try:
+                # if it's not, we will try to find it in the module that was originally provided.
+                self.PlatformBuilder = locate_class_in_module(self.PlatformModule, UefiBuilder)()
+            except (TypeError):
+                raise RuntimeError(f"UefiBuild not found in module:\n{dir(self.PlatformModule)}")
+
+        self.PlatformBuilder.AddPlatformCommandLineOptions(parserObj)
+
+    def RetrieveCommandLineOptions(self, args):
+        '''  Retrieve command line options from the argparser '''
+        self.PlatformBuilder.RetrievePlatformCommandLineOptions(args)
+
+    def GetSettingsClass(self):
+        '''  Providing BuildSettingsManager  '''
+        return BuildSettingsManager
+
+    def GetLoggingFileName(self, loggerType):
+        name = self.PlatformSettings.GetName()
+        if name is not None:
+            return f"BUILDLOG_{name}"
+        return "BUILDLOG"
+
+    def Go(self):
+        logging.info("Running Python version: " + str(sys.version_info))
+
+        Edk2PlatformBuild.collect_python_pip_info()
+
+        (build_env, shell_env) = self_describing_environment.BootstrapEnvironment(
+            self.GetWorkspaceRoot(), self.GetActiveScopes())
+
+        # Bind our current execution environment into the shell vars.
+        ph = os.path.dirname(sys.executable)
+        if " " in ph:
+            ph = '"' + ph + '"'
+        shell_env.set_shell_var("PYTHON_HOME", ph)
+        # PYTHON_COMMAND is required to be set for using edk2 python builds.
+        # todo: work with edk2 to remove the bat file and move to native python calls
+        pc = sys.executable
+        if " " in pc:
+            pc = '"' + pc + '"'
+        shell_env.set_shell_var("PYTHON_COMMAND", pc)
+
+        # Load plugins
+        logging.log(edk2_logging.SECTION, "Loading Plugins")
+        pm = plugin_manager.PluginManager()
+        failedPlugins = pm.SetListOfEnvironmentDescriptors(
+            build_env.plugins)
+        if failedPlugins:
+            logging.critical("One or more plugins failed to load. Halting build.")
+            for a in failedPlugins:
+                logging.error("Failed Plugin: {0}".format(a["name"]))
+            raise Exception("One or more plugins failed to load.")
+
+        helper = HelperFunctions()
+        if(helper.LoadFromPluginManager(pm) > 0):
+            raise Exception("One or more helper plugins failed to load.")
+        #
+        # Now we can actually kick off a build.
+        #
+        logging.log(edk2_logging.SECTION, "Kicking off build")
+        ret = self.PlatformBuilder.Go(self.GetWorkspaceRoot(),
+                                      os.pathsep.join(self.PlatformSettings.GetPackagesPath()),
+                                      helper, pm)
+        logging.log(edk2_logging.SECTION, f"Log file is located at: {self.log_filename}")
+        return ret
+
+
+def main():
+    Edk2PlatformBuild().Invoke()
