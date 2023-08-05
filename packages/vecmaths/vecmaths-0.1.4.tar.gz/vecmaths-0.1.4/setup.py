381c298c7131feaa2fc8d74869cc6fd18dd56b64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:12 +0000

    Add package vecmaths/0.1.4/vecmaths-0.1.4.tar.gz

---
+++ packages/vecmaths/vecmaths-0.1.4.tar.gz/vecmaths-0.1.4/setup.py
@@ -0,0 +1,17 @@
+"""Pip installation script."""
+
+from setuptools import find_packages, setup
+
+setup(
+    name='vecmaths',
+    version="0.1.4",
+    description="A collection of (mostly vectorised) maths functions in Python.",
+    author='Adam J Plowman, Maria S Yankova',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta'
+    ],
+)
