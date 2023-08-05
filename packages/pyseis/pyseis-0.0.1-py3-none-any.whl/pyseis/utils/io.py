33602a2ef53256251bb75aeda4937984f21b0947
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:17 +0000

    Add package pyseis/0.0.1/pyseis-0.0.1-py3-none-any.whl

---
+++ packages/pyseis/pyseis-0.0.1-py3-none-any.whl/pyseis/utils/io.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+"""
+
+Utilities to read and write files. (Mostly read)
+
+"""
+
+
+
+def readfiles(list: file_list):
+    """ Takes in a list of strings and tries to read them.
+
+    :param list:
+    :return: `obspy.Stream`
+    """
