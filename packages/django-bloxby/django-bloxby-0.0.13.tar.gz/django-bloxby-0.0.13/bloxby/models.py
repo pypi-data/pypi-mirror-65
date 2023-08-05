17a8fe0f138b52eae3552547a59869dfb99bd593
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:43 +0000

    Add package django-bloxby/0.0.13/django-bloxby-0.0.13.tar.gz

---
+++ packages/django-bloxby/django-bloxby-0.0.13.tar.gz/django-bloxby-0.0.13/bloxby/models.py
@@ -0,0 +1,60 @@
+from django.conf import settings
+from django.db import models
+
+from .managers import UserBridgeManager
+from .functions import Bloxby
+
+bloxby = Bloxby()
+
+
+class UserBridge(models.Model):
+    """You can create this at post_save of PurchasedPackage or something similar, a model that represents that the user
+    has purchased a package, and delete it also at post_delete"""
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    bloxby_id = models.IntegerField(blank=True, null=True)
+    autologin_token = models.CharField(max_length=20, blank=True, null=True)
+    active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    objects = UserBridgeManager()
+
+    def __str__(self):
+        return f'{self.user.email} - {self.bloxby_id}'
+
+    def delete(self, using=None, keep_parents=False):
+        bloxby.Users.update(self.bloxby_id, status='Inactive')
+        self.active = False
+        self.save()
+
+    @classmethod
+    def create(cls, package_id, user):
+        """
+        Package_id is the bloxby package id not the internal one
+        Simply call UserBridge.create(package_id, user);
+        """
+        # Check if user already exists in bloxby
+        existings = cls.objects.filter(user=user)
+        if existings.exists():
+            obj = existings.first()
+            obj.active = True
+            obj.save()
+            new = obj
+            bloxby.Users.update(obj.bloxby_id, status='Active')
+        else:
+            # Check if email already exists there
+            new = cls.objects.create(user=user)
+            data, success = bloxby.Users.create(
+                user.first_name, user.last_name, user.email, bloxby.generate_random_password(), package_id
+            )
+            if success:
+                new.bloxby_id = data['data']['user']['id']
+                new.autologin_token = data['data']['user']['auto_login_token']
+            else:
+                if 'email already used' in str(data):
+                    # Hack to find the user again. Not efficient but we have to work with what we have
+                    for bu in bloxby.Users.all():
+                        if user.email in bu['email']:
+                            new.bloxby_id = bu['id']
+                            new.autologin_token = bu['auto_login_token']
+            new.save()
+        return new
