0c0fd63254d6acb6983d2d441dad11e5af0bcfb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:02 +0000

    Add package spectra3D/0.0.1.3.1/spectra3D-0.0.1.3.1.tar.gz

---
+++ packages/spectra3D/spectra3D-0.0.1.3.1.tar.gz/spectra3D-0.0.1.3.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="spectra3D",
+    version="0.0.1.3.1",
+    author="Ben Knight",
+    author_email="bknight@i3drobotics.com",
+    description="Adding spectrum data to 3D",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/i3drobotics/Spectra3D",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'numpy','matplotlib','plyfile'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
