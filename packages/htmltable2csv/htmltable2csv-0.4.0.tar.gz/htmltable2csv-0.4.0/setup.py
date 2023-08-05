cd55f11902442fdec3f2c34a551c044b0cda69d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:29 +0000

    Add package htmltable2csv/0.4.0/htmltable2csv-0.4.0.tar.gz

---
+++ packages/htmltable2csv/htmltable2csv-0.4.0.tar.gz/htmltable2csv-0.4.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="htmltable2csv",
+    version="0.4.0",
+    author="Aqib",
+    author_email="aqib@codecrud.com",
+    description="Transform HTML tables in a Web Page to CSV files.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aqib-git/table2csv",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
