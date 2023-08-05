9fbcb6560409265b343bb9fc73d3bc63ae4ad408
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:10 +0000

    Add package eyalev-test1/0.1.2/eyalev-test1-0.1.2.tar.gz

---
+++ packages/eyalev-test1/eyalev-test1-0.1.2.tar.gz/eyalev-test1-0.1.2/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['eyalev_test1']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['test1-cli = eyalev_test1.main:main']}
+
+setup_kwargs = {
+    'name': 'eyalev-test1',
+    'version': '0.1.2',
+    'description': '',
+    'long_description': None,
+    'author': 'Eyal Levin',
+    'author_email': 'eyalev@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+}
+
+
+setup(**setup_kwargs)
