a56da51ee03e39a217514d3dac5a94d822324ec4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:09 +0000

    Add package pythonspell/0.6/pythonspell-0.6.tar.gz

---
+++ packages/pythonspell/pythonspell-0.6.tar.gz/pythonspell-0.6/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(name='pythonspell',
+    version='0.6',
+    description='A simple python spellchecker built on BK Trees and DamerauLevenshtein distance',
+    url='https://github.com/AidanJSmith',
+    author='Aidan Smith',
+    author_email='100023755@mvla.net',
+    license='MIT',
+    keywords='spelling corrector autocorrect',
+    packages=['pyspell'],
+    zip_safe=False)
