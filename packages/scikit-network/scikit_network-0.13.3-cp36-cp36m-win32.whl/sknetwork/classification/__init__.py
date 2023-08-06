a3fdb846a62931b5660af553cae315f93631c091
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:44 +0000

    Add package scikit-network/0.13.3/scikit_network-0.13.3-cp36-cp36m-win32.whl

---
+++ packages/scikit-network/scikit_network-0.13.3-cp36-cp36m-win32.whl/sknetwork/classification/__init__.py
@@ -0,0 +1,5 @@
+"""classification module"""
+from sknetwork.classification.base import BaseClassifier, BaseBiClassifier
+from sknetwork.classification.knn import KNN, BiKNN
+from sknetwork.classification.pagerank import BiPageRankClassifier, PageRankClassifier, CoPageRankClassifier
+from sknetwork.classification.diffusion import BiDiffusionClassifier, DiffusionClassifier
