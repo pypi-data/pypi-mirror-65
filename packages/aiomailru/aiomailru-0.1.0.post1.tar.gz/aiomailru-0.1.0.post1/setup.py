acf5afe74d8146cae5449b840c63623168c4d8b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:39 +0000

    Add package aiomailru/0.1.0.post1/aiomailru-0.1.0.post1.tar.gz

---
+++ packages/aiomailru/aiomailru-0.1.0.post1.tar.gz/aiomailru-0.1.0.post1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='aiomailru',
+    version='0.1.0.post1',
+    author='Konstantin Togoi',
+    author_email='konstantin.togoi@protonmail.com',
+    url='https://github.com/KonstantinTogoi/aiomailru',
+    description='Python Mail.Ru API wrapper',
+    long_description=open('README.rst').read(),
+    license='BSD',
+    packages=find_packages(),
+    python_requires='>=3.5',
+    install_requires=['aiohttp>=3.0.0', 'yarl'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest-asyncio', 'pytest-dotenv', 'pytest-localserver'],
+    extras_require={'scrapers': ['pyppeteer<=0.0.25']},
+    keywords=['mail.ru rest api scrapers asyncio'],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
