b936c0da0a480450a37af3ec44d3d488829d1bab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:54 +0000

    Add package covid19-cli/1.0.3/covid19_cli-1.0.3-py3.5.egg

---
+++ packages/covid19-cli/covid19_cli-1.0.3-py3.5.egg/covid19/__main__.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python3
+
+import argparse
+from covid19 import functions
+
+
+def main():
+    VERSION = "1.0.3"
+
+    parser = argparse.ArgumentParser(
+        description="A CLI for fetching covid19 info."
+    )
+
+    # adding arguments
+    parser.add_argument(
+            "-e",
+            "--emergency",
+            help="print emergency contact numbers and helpful links",
+            action="store_true"
+        )
+    parser.add_argument(
+            "-s",
+            "--state",
+            help="(string) state name (correct, case-insensetive) to track"
+        )
+    parser.add_argument(
+        "--speech",
+        default=False,
+        help="enable or disable speech module",
+        action='store_true'
+    )
+
+    args = parser.parse_args()
+
+    functions.print_covid19_cli_info(VERSION)
+    functions.fetch_cases(args, VERSION)
+    functions.print_covid19_cli_credits()
+
+
+if __name__ == '__main__':
+    main()
