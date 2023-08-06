94e1f39638b01e328ac58845d5284a6f8ca2615b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:25 +0000

    Add package CaaS/0.4/CaaS-0.4.tar.gz

---
+++ packages/CaaS/CaaS-0.4.tar.gz/CaaS-0.4/CaaS/__init__.py
@@ -0,0 +1,7 @@
+from CaaS.caas import text_compression
+from CaaS.caas import text_decompression
+from CaaS.caas import audio_compression
+from CaaS.caas import image_compression
+from CaaS.caas import video_compression
+from CaaS.errors import Error
+from Caas.errors import InvalidAuthKeyError
