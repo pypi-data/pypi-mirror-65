fda762da17d8037b5e20c485cdec08d3a8c1b4b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:30 +0000

    Add package sunnyportal-py/0.0.2/sunnyportal_py-0.0.2-py3-none-any.whl

---
+++ packages/sunnyportal-py/sunnyportal_py-0.0.2-py3-none-any.whl/sunnyportal/pandasclient.py
@@ -0,0 +1,12 @@
+import datetime as dt
+import pandas as pd
+
+from .client import Plant
+
+
+class PandasPlant(Plant):
+    def month_overview(self, date: dt.date) -> pd.DataFrame:
+        req = super(PandasPlant, self).month_overview(date=date)
+        df = pd.DataFrame(req.days)
+        df.set_index('timestamp', inplace=True)
+        return df
