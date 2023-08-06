2e3c1a3a6af15b962f1d4ed378208ba6019384c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:00 +0000

    Add package gridsim/0.1/gridsim-0.1.tar.gz

---
+++ packages/gridsim/gridsim-0.1.tar.gz/gridsim-0.1/README.md
@@ -0,0 +1,13 @@
+# Gridsim
+
+*Simple grid-based robot simulator*
+
+**[Documentation](https://gridsim.readthedocs.io/)**
+
+I'm planning on using this as a simple test-bed for my algorithms.
+
+This package will be a package/library for the simulator itself, not the robot code. This follows a similar structure (with a World, Robot, Logger, and Viewer) as Kilosim.
+
+I plan to package this with PyPi so I can easily use it with separate repositories for the algorithms.
+
+For reference on package structure, see the [package-boilerplate](https://github.com/jtebert/package-boilerplate).
---
+++ packages/gridsim/gridsim-0.1.tar.gz/gridsim-0.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='gridsim',
+    version=0.1,
+    description='Simple grid-based robot simulator',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    python_requires='>=3.6',
+    author='Julia Ebert',
+    author_email='julia@juliaebert.com',
+    url='https://github.com/jtebert/gridsim',
+    packages=find_packages(exclude=('tests', 'docs')),
+    license=license,
+    install_requires=[
+        # List of dependencies
+        "pygame",
+        "numpy",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Framework :: Robot Framework",
+        "Development Status :: 3 - Alpha"
+    ],
+    zip_safe=False,
+)
