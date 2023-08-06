74ea16b41b5f9beb6bf2097f632e340e0ff0c3d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:01 +0000

    Add package notiforward/0.0.1/notiforward-0.0.1.tar.gz

---
+++ packages/notiforward/notiforward-0.0.1.tar.gz/notiforward-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+from pathlib import Path
+
+GEN_version = "0.0.1"
+READ_name = "notiforward"
+
+here = Path(__file__).parent.resolve()
+
+setup(
+    name=READ_name,
+    version=GEN_version,
+    author="rendaw",
+    url="https://gitlab.com/rendaw/notiforward",
+    download_url="https://gitlab.com/rendaw/notiforward/-/archive/v{v}/notiforward-v{v}.tar.gz".format(
+        v=GEN_version
+    ),
+    license="MIT",
+    description="Forward DBUS notifications",
+    long_description=(here / "README.md").read_text(),
+    long_description_content_type="text/markdown",
+    classifiers=[],
+    packages=["notiforward"],
+    entry_points={"console_scripts": ["notiforward=notiforward:main",],},
+)
