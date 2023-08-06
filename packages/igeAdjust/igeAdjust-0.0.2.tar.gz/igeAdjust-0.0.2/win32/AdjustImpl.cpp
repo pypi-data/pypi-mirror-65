eea485d0638fcef7098b2325dc0c49cf2e55bd97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:32 +0000

    Add package igeAdjust/0.0.2/igeAdjust-0.0.2.tar.gz

---
+++ packages/igeAdjust/igeAdjust-0.0.2.tar.gz/igeAdjust-0.0.2/win32/AdjustImpl.cpp
@@ -0,0 +1,15 @@
+#include "AdjustImpl.h"
+#include "Adj.h"
+
+
+void AdjustImpl::Init(bool debug)
+{
+}
+
+void AdjustImpl::Release()
+{
+}
+
+void AdjustImpl::logEvent(const char* eventType, std::map<std::string, std::string> eventValue)
+{
+}
