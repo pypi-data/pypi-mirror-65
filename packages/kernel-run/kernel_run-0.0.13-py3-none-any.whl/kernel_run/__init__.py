f9b9f3454f95ddb32cc83f0e220792164e418f58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:55 +0000

    Add package kernel-run/0.0.13/kernel_run-0.0.13-py3-none-any.whl

---
+++ packages/kernel-run/kernel_run-0.0.13-py3-none-any.whl/kernel_run/__init__.py
@@ -0,0 +1,59 @@
+import json
+import os
+import string
+import webbrowser
+
+import requests
+
+from kernel_run.utils.creds import read_creds
+from kernel_run.utils.jupyter import read_nbfile, sanitize_nb
+from kernel_run.utils.misc import DEFAULT_PREFIX, is_url, url_filename
+from kernel_run.utils.network import download_rawlink, push_kernel
+
+API_URL = 'https://www.kaggle.com/api/v1'
+
+
+def create_kernel(path_or_url, public=False, no_browser=False, new=False,
+                  strip_output=False, prefix=DEFAULT_PREFIX, creds_path=None, name=None):
+    """Instantly create and run a Kaggle kernel from a Jupyter notebook (local file or URL)
+
+    Arguments:
+        path_or_url (string): Path/URL to the Jupyter notebook
+        public (bool, optional): If true, creates a public kernel. A private kernel
+            is created by default.
+        no_browser (bool, optional): If true, does not attempt to automatically open 
+            a browser tab to edit the created Kernel
+        new (bool, optional): If true, creates a new Kernel by adding a random 
+            5-letter string at the end of the title
+        prefix (string, optional): A prefix added to the Kernel title, to indicate that
+            the Kernel was created using kernel-run
+        creds_path (string, optional): Path to the 'kaggle.json' credentials file 
+            (defaults to '~/.kaggle/kaggle.json')
+        strip_output (bool, optional): Clear output cells before uploading notebook.
+    """
+    creds = read_creds(creds_path)
+
+    # Read notebook & filename
+    if is_url(path_or_url):
+        nbtext = download_rawlink(path_or_url)
+        fname = url_filename(path_or_url)
+    else:
+        fname, nbtext = read_nbfile(path_or_url)
+
+    if name is not None:
+        fname = name
+
+    nbtext = sanitize_nb(nbtext, strip_output)
+
+    # Create the kernel
+    res_json = push_kernel(nbtext, fname, creds, public, new, prefix)
+
+    # Output link to edit created kernel
+    kernel_url = res_json['url'] + "/edit"
+    print('Kernel created successfully: ' + kernel_url)
+
+    # Launch browser window to edit created kernel
+    if not no_browser:
+        webbrowser.open_new_tab(kernel_url)
+
+    return res_json
---
+++ packages/kernel-run/kernel_run-0.0.13-py3-none-any.whl/kernel_run/cli.py
@@ -0,0 +1,41 @@
+import argparse
+from argparse import RawTextHelpFormatter
+
+from kernel_run import create_kernel
+from kernel_run.utils.misc import DEFAULT_PREFIX
+
+CLI_DESC = """Instantly create and run a Kaggle kernel from a Jupyter notebook (local file or URL). Examples:
+$ kernel-run path/to/jupyter_notebook.py
+$ kernel-run https://raw.githubusercontent.com/aakashns/deep-learning-workbook/master/examples/00_mnist_basic.ipynb
+"""
+
+PUBLIC_MSG = "Create a public kernel"
+NEW_MSG = "Create a new kernel, if a kernel with the same name exists"
+BROWSER_MSG = "Don't open a browser window automatically"
+PREFIX_MSG = "Prefix added to kernel title to easy identification (defaults to 'kr/')"
+OUTPUT_MSG = "Clear output cells before uploading notebook"
+NAME_MSG = "Provide a name for kernel (defaults to filename)"
+
+def main():
+    parser = argparse.ArgumentParser(
+        description=CLI_DESC, formatter_class=RawTextHelpFormatter)
+    parser.add_argument('notebook_path_or_url',
+                        help="Path/URL of the Jupyter notebook")
+    parser.add_argument('--public', help=PUBLIC_MSG, action="store_true")
+    parser.add_argument('--new', help=NEW_MSG, action='store_true')
+    parser.add_argument('--no-browser', help=BROWSER_MSG, action="store_true")
+    parser.add_argument('--strip-output', help=OUTPUT_MSG, action="store_true")
+    parser.add_argument('--prefix', help=PREFIX_MSG, default=DEFAULT_PREFIX)
+    parser.add_argument('--name', help=NAME_MSG)
+    args = parser.parse_args()
+    create_kernel(path_or_url=args.notebook_path_or_url,
+                  public=args.public,
+                  no_browser=args.no_browser,
+                  new=args.new,
+                  strip_output=args.strip_output,
+                  prefix=args.prefix,
+                  name=args.name)
+
+
+if __name__ == '__main__':
+    main()
