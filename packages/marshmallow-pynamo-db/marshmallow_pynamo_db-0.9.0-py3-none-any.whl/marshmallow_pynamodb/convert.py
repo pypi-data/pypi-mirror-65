7d2eb2dedf893bda9d086b7f39ead5ef267264a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:24 +0000

    Add package marshmallow-pynamo-db/0.9.0/marshmallow_pynamo_db-0.9.0-py3-none-any.whl

---
+++ packages/marshmallow-pynamo-db/marshmallow_pynamo_db-0.9.0-py3-none-any.whl/marshmallow_pynamodb/convert.py
@@ -0,0 +1,29 @@
+from marshmallow import fields
+
+from pynamodb import attributes
+
+from marshmallow_pynamodb import fields as custom_fields
+
+PYNAMODB_TYPE_MAPPING = {
+    attributes.NumberAttribute: fields.Number,
+    attributes.JSONAttribute: fields.Raw,
+    attributes.UnicodeAttribute: fields.String,
+    attributes.BooleanAttribute: fields.Boolean,
+    attributes.UTCDateTimeAttribute: fields.DateTime,
+    attributes.AttributeContainerMeta: custom_fields.PynamoNested,
+    attributes.ListAttribute: fields.List,
+    attributes.NullAttribute: fields.Raw,
+    attributes.UnicodeSetAttribute: custom_fields.UnicodeSet,
+    attributes.NumberSetAttribute: custom_fields.NumberSet,
+}
+
+
+def attribute2field(attribute):
+    try:
+        pynamodb_type = type(attribute)
+        field = PYNAMODB_TYPE_MAPPING[pynamodb_type]
+    except KeyError:
+        pynamodb_type = type(pynamodb_type)
+        field = PYNAMODB_TYPE_MAPPING[pynamodb_type]
+
+    return field
