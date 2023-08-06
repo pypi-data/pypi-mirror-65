523d84e91a384d55f1fbd6c267db61af5b20e552
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:37 +0000

    Add package moic/0.3.1/moic-0.3.1.tar.gz

---
+++ packages/moic/moic-0.3.1.tar.gz/moic-0.3.1/moic/cli/completion/base.py
@@ -0,0 +1,55 @@
+from moic.config import JiraInstance
+
+
+def autocomplete_users(ctx, args, incomplete):
+    """Get Jira users list completion"""
+    try:
+        jira = JiraInstance()
+        users = jira.session.search_users(incomplete)
+        return [user.name for user in users]
+    except Exception:
+        return []
+
+
+def autocomplete_priorities(ctx, args, incomplete):
+    """Get Jira priorities list completion"""
+    try:
+        jira = JiraInstance()
+        return [priority.name for priority in jira.session.priorities() if incomplete in priority.name]
+    except Exception:
+        return []
+
+
+def autocomplete_issues(ctx, args, incomplete):
+    """Get Jira issues list completion"""
+    try:
+        jira = JiraInstance()
+        if "-" in incomplete:
+            issues = jira.session.search_issues(f"project = {incomplete.split('-')[0]}", 0, 100)
+            return [(issue.key, issue.fields.summary) for issue in issues if issue.key.startswith(incomplete.upper())]
+        else:
+            return []
+    except Exception:
+        return []
+
+
+def autocomplete_transitions(ctx, args, incomplete):
+    """Get Jira translations available for an issue"""
+    try:
+        jira = JiraInstance()
+        issue_key = args[-1:][0]
+        issue = jira.session.issue(issue_key)
+        transitions = jira.session.transitions(issue)
+        return [t.name for t in transitions if t.name.startswith(incomplete)]
+    except Exception:
+        return []
+
+
+def autcomplete_projects(ctx, args, incomplete):
+    """Get Jira projects list completion"""
+    try:
+        jira = JiraInstance()
+        projects = jira.session.project()
+        return [p.name for p in projects if incomplete in p.name]
+    except Exception:
+        return []
---
+++ packages/moic/moic-0.3.1.tar.gz/moic-0.3.1/moic/cli/utils/__init__.py
@@ -0,0 +1,9 @@
+from .base import (
+    get_issue_commits,
+    get_template,
+    print_issue,
+    print_issues,
+    print_status,
+    validate_issue_key,
+    validate_priority,
+)
