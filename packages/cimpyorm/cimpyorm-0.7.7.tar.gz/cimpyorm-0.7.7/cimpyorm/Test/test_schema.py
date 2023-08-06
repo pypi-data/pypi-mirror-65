9e94d70de0d55983a625b00f46af288d7f97fceb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:17 +0000

    Add package cimpyorm/0.7.7/cimpyorm-0.7.7.tar.gz

---
+++ packages/cimpyorm/cimpyorm-0.7.7.tar.gz/cimpyorm-0.7.7/cimpyorm/Test/test_schema.py
@@ -0,0 +1,32 @@
+from cimpyorm.Model.Elements import CIMClass
+
+
+def test_persisted_classes(cgmes_schema):
+    schema = cgmes_schema
+    # Make sure we have all CIMClasses
+    assert len(schema.Elements["CIMClass"]) == 397
+    assert schema.Elements["CIMClass"]["ACLineSegment"] is \
+           schema.session.query(CIMClass).filter(CIMClass.name == "ACLineSegment").one()
+
+
+def test_summary(cgmes_schema):
+    schema = cgmes_schema
+    assert schema.model.classes.ACLineSegment.property_table().shape == (27, 7)
+
+
+def test_description_CIMClass(cgmes_schema):
+    from cimpyorm import describe
+    describe(cgmes_schema.model.classes.TopologicalNode)
+    cgmes_schema.model.classes.TopologicalNode.describe()
+
+
+def test_description_CIMClasswType(cgmes_schema):
+    from cimpyorm import describe
+    describe(cgmes_schema.model.classes.OperationalLimitType)
+    cgmes_schema.model.classes.OperationalLimitType.describe()
+
+
+def test_description_parseable(cgmes_schema):
+    from cimpyorm import describe
+    describe(cgmes_schema.model.TopologicalNode)
+    cgmes_schema.model.TopologicalNode.describe()
