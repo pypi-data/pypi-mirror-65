b57191b24ca033cf68f4ba8b8da2b51e083d0b6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:46 +0000

    Add package omnitools/0.0.34/omnitools-0.0.34.tar.gz

---
+++ packages/omnitools/omnitools-0.0.34.tar.gz/omnitools-0.0.34/omnitools/encoding/utf8.py
@@ -0,0 +1,27 @@
+from ..type import *
+
+__ALL__ = ["utf8e", "utf8d", "try_utf8d", "try_utf8e"]
+
+
+def utf8e(s: str) -> bytes:
+    return s.encode("utf-8")
+
+
+def utf8d(b: bytes) -> str:
+    return b.decode("utf-8")
+
+
+def try_utf8d(b: bytes) -> str_or_bytes:
+    try:
+        return utf8d(b)
+    except:
+        return b
+
+
+def try_utf8e(s: str) -> bytes:
+    try:
+        return utf8e(s)
+    except:
+        return s
+
+
