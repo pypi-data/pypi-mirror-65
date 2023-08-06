f81b9378e963b5862a618bbc34709e4ff1d4bc68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:29 +0000

    Add package swolfpy/0.1.6/swolfpy-0.1.6-py3-none-any.whl

---
+++ packages/swolfpy/swolfpy-0.1.6-py3-none-any.whl/swolfpy/__init__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 28 12:22:24 2020
+
+@author: msardar2
+"""
+
+
+__all__ = [
+    'Distance',
+    'project',
+    'import_methods',
+    'ParallelData',
+    'MyQtApp',
+    'swolfpy'
+]
+
+__version__ = '0.1.6'
+
+# module level doc-string
+__doc__ = """
+Solid Waste Optimization Life-cycle Framework in Python(SwolfPy)
+"""
+
+
+from .Distance import Distance
+from .project_class import project
+from .SWOLF_method import import_methods
+from .building_matrices import ParallelData
+from .UI.PySWOLF_run import MyQtApp
+from PySide2 import QtCore, QtGui, QtWidgets
+
+class swolfpy():
+    def __init__(self):
+        self.app = QtWidgets.QApplication()
+        self.qt_app  = MyQtApp()
+        self. qt_app.show()
+        self.app.exec_()
+
+
