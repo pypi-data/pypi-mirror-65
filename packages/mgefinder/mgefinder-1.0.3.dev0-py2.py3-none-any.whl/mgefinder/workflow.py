c419ddb71b9377eb60c9e611e4195dad3cf106cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:22 +0000

    Add package mgefinder/1.0.3.dev0/mgefinder-1.0.3.dev0-py2.py3-none-any.whl

---
+++ packages/mgefinder/mgefinder-1.0.3.dev0-py2.py3-none-any.whl/mgefinder/workflow.py
@@ -0,0 +1,26 @@
+import sys
+from snakemake import shell
+try:
+    from snakemake import shell
+except ImportError:
+    print("MISSING DEPENDENCY: Snakemake is not installed. You can install with the command \"conda install -c bioconda -c conda-forge snakemake\" or \"pip3 install snakemake\"")
+    sys.exit()
+
+
+
+def _workflow(workdir, snakefile, configfile, cores, memory, unlock, rerun_incomplete, keep_going):
+
+    if unlock == True:
+        cmd = 'snakemake -s {snakefile} --configfile {configfile} --config wd={workdir} --unlock'.format(
+              snakefile=snakefile, configfile=configfile, workdir=workdir
+        )
+    cmd = 'snakemake -s {snakefile} --config wd={workdir} memory={memory} ' \
+          '--cores {cores} --configfile {configfile} '
+    if rerun_incomplete:
+        cmd += '--rerun-incomplete '
+    if keep_going:
+        cmd += '--keep-going '
+    
+    cmd = cmd.format(snakefile=snakefile, configfile=configfile, workdir=workdir, memory=memory, cores=cores)
+    print('COMMAND:', cmd)
+    shell(cmd)
