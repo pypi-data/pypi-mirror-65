415adf48de157a8ec243c9b4bc927a5c421ceaaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:00 +0000

    Add package firmware-variables/0.0.3/firmware_variables-0.0.3.tar.gz

---
+++ packages/firmware-variables/firmware_variables-0.0.3.tar.gz/firmware_variables-0.0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='firmware_variables',
+    version='0.0.3',
+    description='Windows library for controlling UEFI firmware variables',
+    author='Netanel Dziubov',
+    packages=['firmware_variables'],
+    package_dir={'': 'src'},
+    install_requires=['pywin32'],
+    url="https://github.com/netaneld122/firmware-variables",
+    python_requires='>=3.6',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Microsoft :: Windows',
+    ],
+)
