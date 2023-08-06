6e9266cc1cf4c5329be2dbc47b0d3e42412dfb51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:22 +0000

    Add package prob-dists/0.1/prob_dists-0.1.tar.gz

---
+++ packages/prob-dists/prob_dists-0.1.tar.gz/prob_dists-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='prob_dists',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['prob_dists'],
+      author = 'Malladi,Yeswanth chowdary',
+      author_email = 'malladi.yaswanth@gmail.com',
+      zip_safe = False)
