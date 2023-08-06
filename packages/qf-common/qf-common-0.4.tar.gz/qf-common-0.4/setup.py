91e856d911b0eb10f424d44eb97b591ab787ff43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:19 +0000

    Add package qf-common/0.4/qf-common-0.4.tar.gz

---
+++ packages/qf-common/qf-common-0.4.tar.gz/qf-common-0.4/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='qf-common',
+    version='0.4',
+    author='Anton Stefanovich',
+    description='Useful things for QF projects',
+    url='https://github.com/quality-first/qf-common',
+    author_email='anton.stefanovich@gmail.com',
+    packages=['qf_common'],
+    zip_safe=False,
+    license='MIT',
+)
