bfe6d253e7af5f9f854880e8df47309c05737aa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:10 +0000

    Add package adhesive/1.3.2/adhesive-1.3.2-py3-none-any.whl

---
+++ packages/adhesive/adhesive-1.3.2-py3-none-any.whl/adhesive/storage/ensure_folder.py
@@ -0,0 +1,54 @@
+import os
+from typing import Union
+
+from adhesive import config
+
+
+def ensure_folder(item: Union['Workspace', 'ActiveEvent', str]) -> str:
+    """
+    Ensures the folder for the given item exists.
+
+    :param item:
+    :return:
+    """
+    full_path = get_folder(item)
+    os.makedirs(full_path, exist_ok=True)
+
+    return full_path
+
+
+def get_folder(item: Union['Workspace', 'ActiveEvent', str]) -> str:
+    if isinstance(item, Workspace):
+        return os.path.join(
+            config.current.temp_folder,
+            item.execution_id,
+            "workspace")
+
+    if isinstance(item, ActiveEvent):
+        return os.path.join(
+            config.current.temp_folder,
+            item.execution_id,
+            "logs",
+            _get_loop(item),
+            item.task.id,
+            item.token_id)
+
+    if isinstance(item, str):
+        return os.path.join(config.current.temp_folder, item)
+
+    raise Exception(f"Unable to get_folder for {item}.")
+
+
+def _get_loop(event: 'ActiveEvent') -> str:
+    loop = event.context.loop
+    result = ""
+
+    while loop:
+        result += f"_loop_{loop.event_id}_{loop.index}/"
+        loop = loop.parent_loop
+
+    return result
+
+
+from adhesive.model.ActiveEvent import ActiveEvent
+from adhesive.workspace.Workspace import Workspace
