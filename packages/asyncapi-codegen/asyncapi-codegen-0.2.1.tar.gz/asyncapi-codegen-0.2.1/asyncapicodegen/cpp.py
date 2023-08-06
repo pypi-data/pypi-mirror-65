afa69afc278784577a0d0c8861895048485ffde4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:46 +0000

    Add package asyncapi-codegen/0.2.1/asyncapi-codegen-0.2.1.tar.gz

---
+++ packages/asyncapi-codegen/asyncapi-codegen-0.2.1.tar.gz/asyncapi-codegen-0.2.1/asyncapicodegen/cpp.py
@@ -0,0 +1,147 @@
+import abc
+import stringcase
+import datetime
+
+from . import templator
+from . import specwrapper
+from . import _version
+import jsonschemacodegen.cpp
+import jsonschemacodegen.resolver
+
+class GeneratedFiles(object):
+
+    def __init__(self, cppFile=None, hppFile=None):
+        self.cpp = []
+        self.hpp = []
+        if cppFile is not None:
+            self.cpp.append(cppFile)
+        if hppFile is not None:
+            self.hpp.append(hppFile)
+
+    def __iadd__(self, other):
+        assert(isinstance(other, type(self)))
+        self.cpp.extend(other.cpp)
+        self.hpp.extend(other.hpp)
+        return self
+
+
+class ResolverBaseClass(jsonschemacodegen.cpp.ResolverBaseClass):
+    
+    @abc.abstractmethod
+    def cpp_get_client_filename_base(self, spec, uri):
+        pass
+
+    @abc.abstractmethod
+    def cpp_get_client_classname(self, spec, uri):
+        pass
+
+
+
+class SimpleResolver(ResolverBaseClass, jsonschemacodegen.resolver.SimpleResolver):
+
+    def cpp_get_client_filename_base(self, spec, uri):
+        name = stringcase.snakecase(uri.split('.')[-1])
+        return "asyncapi_{}_client".format(name)
+
+    def cpp_get_client_classname(self, uri, spec):
+        return stringcase.pascalcase(self.cpp_get_client_filename_base(uri, spec))
+
+
+class GeneratorFromAsyncApi(object):
+
+    def __init__(self, src_output_dir, header_output_dir, resolver, namespace=[], src_usings=[]):
+        
+        self.output_dir = {
+            "src": src_output_dir,
+            "header": header_output_dir,
+        }
+        self.namespace = namespace
+        self.usings = src_usings
+        self.resolver = resolver
+
+        for u in src_usings:
+            self.resolver.cpp_add_using(u)
+
+        self.srcGenerator = templator.Generator('asyncapicodegen.templates.cpp', self.output_dir['src'])
+        self.headerGenerator = templator.Generator('asyncapicodegen.templates.cpp', self.output_dir['header'])
+
+    def GenerateSchemasForType(self, spec, url, itemType, getSchemaFunc):
+        assert(isinstance(spec, specwrapper.SpecRoot))
+        genFiles = GeneratedFiles()
+        pathBase = "%s#/components/%s/%s"
+        if 'components' not in spec or itemType not in spec['components']:
+            return genFiles
+        for name, obj in spec['components'][itemType].items():
+            ref = pathBase % (url, itemType, name)
+            schemaGenerator = jsonschemacodegen.cpp.GeneratorFromSchema(src_output_dir=self.output_dir['src'],
+                    header_output_dir=self.output_dir['header'],
+                    resolver=self.resolver)
+            output = schemaGenerator.Generate(getSchemaFunc(obj), ref)
+            assert(output is not None), "Generating {} didn't have output".format(name)
+            genFiles += GeneratedFiles(*output)
+        return genFiles
+
+    def GenerateSchemas(self, spec, url):
+        genFiles = GeneratedFiles()
+        genFiles += self.GenerateSchemasForType(spec, url, 'parameters', lambda obj: obj['schema'])
+        genFiles += self.GenerateSchemasForType(spec, url, 'messages', lambda obj: obj['payload'])
+        genFiles += self.GenerateSchemasForType(spec, url, 'schemas', lambda obj: obj)
+        return genFiles
+
+    def GenerateServers(self, spec, url):
+        genFiles = GeneratedFiles()
+        if 'servers' in spec:
+            for serverName, serverObj in spec['servers'].items():
+                path = "{}#/servers/{}".format(url, serverName)
+                headerFilename = self.resolver.cpp_get_header(path)
+                sourceFilename = "{}.cpp".format(self.resolver.cpp_get_filename_base(path))
+                name = stringcase.pascalcase(stringcase.snakecase(serverName))
+                self.srcGenerator.RenderTemplate("broker.cpp.jinja2", 
+                    sourceFilename, 
+                    usings=self.resolver.cpp_get_usings(),
+                    ns=self.namespace, 
+                    resolver=self.resolver,
+                    includes=[headerFilename],
+                    Name=name,
+                    server=serverObj)
+                genFiles += GeneratedFiles(cppFile=sourceFilename)
+                self.headerGenerator.RenderTemplate("broker.hpp.jinja2", 
+                    headerFilename,
+                    ns=self.namespace,
+                    resolver=self.resolver,
+                    Name=name,
+                    server=serverObj)
+                genFiles += GeneratedFiles(hppFile=headerFilename)
+        return genFiles
+
+    def Generate(self, spec, class_name):
+        assert(isinstance(spec, dict))
+        wrappedSpec = specwrapper.SpecRoot(spec, self.resolver)
+        genFiles = GeneratedFiles()
+
+        genFiles += self.GenerateSchemas(wrappedSpec, class_name)
+        genFiles += self.GenerateServers(wrappedSpec, class_name)
+
+        headerFilename = "{}.hpp".format(self.resolver.cpp_get_client_filename_base(wrappedSpec, class_name))
+        sourceFilename = "{}.cpp".format(self.resolver.cpp_get_client_filename_base(wrappedSpec, class_name))
+        self.srcGenerator.RenderTemplate("source.cpp.jinja2", 
+            sourceFilename, 
+            usings=self.usings,
+            ns=self.namespace, 
+            resolver=self.resolver,
+            includes=[headerFilename],
+            Name=class_name,
+            codegenVersion=_version.__version__,
+            codegenDate=datetime.datetime.now().strftime("%c"),
+            spec=wrappedSpec)
+        genFiles += GeneratedFiles(cppFile=sourceFilename)
+        self.headerGenerator.RenderTemplate("header.hpp.jinja2", 
+            headerFilename,
+            ns=self.namespace,
+            resolver=self.resolver,
+            Name=class_name,
+            codegenVersion=_version.__version__,
+            codegenDate=datetime.datetime.now().strftime("%c"),
+            spec=wrappedSpec)
+        genFiles += GeneratedFiles(hppFile=headerFilename)
+        return genFiles
---
+++ packages/asyncapi-codegen/asyncapi-codegen-0.2.1.tar.gz/asyncapi-codegen-0.2.1/asyncapicodegen/python_client.py
@@ -0,0 +1,78 @@
+import abc
+import stringcase
+import yaml
+
+from . import templator
+from . import specwrapper
+from . import python_summary
+import jsonschemacodegen.python
+import jsonschemacodegen.resolver
+
+
+class SimpleResolver(python_summary.SimpleResolver):
+    pass
+
+
+class PackageResolver(SimpleResolver):
+
+    def py_include_statement(self, reference):
+        """Should return the include statement needed to acquire the object representing the
+        schema pointed to at `reference`.  Example, "from schema_foo import Foo"
+        """
+        ref = self._get_reference_parts(reference)
+        return "from . import {type}.{name}".format(**ref)
+
+    def py_class_name(self, reference):
+        """Should return the class name for the object representing the schema pointed to at `reference`.
+        For example, "schema_foo.Foo"
+        """
+        ref = self._get_reference_parts(reference)
+        return "{type}.{name}.{PascalName}".format(**ref)
+
+    def py_filename(self, reference):
+        """Should return the name of the filename holding the python class representing the schema pointed to
+        at `reference`.  For example, "schema_foo.py"
+        """
+        ref = self._get_reference_parts(reference)
+        return "{type}/{name}.py".format(**ref)
+
+    def py_client_filepath(self, client_type, filename):
+        return filename
+
+    def py_package_name(self):
+        return "asyncapi-client"
+
+class GeneratorFromAsyncApi(python_summary.GeneratorFromAsyncApi):
+
+    def Generate(self, spec, class_name, filename_base):
+        assert(isinstance(spec, dict))
+        wrappedSpec = specwrapper.SpecRoot(spec, self.resolver)
+
+        self.GenerateSchemasForType(wrappedSpec, 'messages', lambda obj: obj['payload'])
+        self.GenerateSchemasForType(wrappedSpec, 'schemas', lambda obj: obj)
+        self.GenerateTestsForType(wrappedSpec, 'schemas', lambda obj: obj)
+        self.GenerateTestsForType(wrappedSpec, 'messages', lambda obj: obj['payload'])
+
+        if 'channels' in wrappedSpec:
+            clientType = 'x-client-role' in wrappedSpec and wrappedSpec['x-client-role'] or 'client'
+            outputName = self.resolver.py_client_filepath(clientType, "{}.py".format(filename_base))
+            if outputName is not None:
+                self.generator.RenderTemplate("client.py.jinja2", 
+                    outputName, 
+                    Name = "{}{}".format(stringcase.pascalcase(class_name), stringcase.pascalcase(clientType)),
+                    spec = wrappedSpec,
+                    resolver=self.resolver)
+    
+    def GenerateSetup(self, spec, class_name):
+        assert(isinstance(spec, dict))
+        wrappedSpec = specwrapper.SpecRoot(spec, self.resolver)
+        clientType = 'x-client-role' in wrappedSpec and wrappedSpec['x-client-role'] or 'client'
+        print(f"Generating setup for {clientType}")
+        if clientType in ['provider', 'utilizer', 'library']:
+            self.generator.RenderTemplate("setup.py.jinja2", 
+                    "setup.py", 
+                    Name = "{}".format(stringcase.pascalcase(class_name)),
+                    spec = wrappedSpec,
+                    resolver=self.resolver)
+
+
