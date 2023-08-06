a521ab3925e847df5059fde5fee325e8c39a98fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:00 +0000

    Add package lume/0.1.2/lume-0.1.2-py3-none-any.whl

---
+++ packages/lume/lume-0.1.2-py3-none-any.whl/lume/src/infrastructure/services/logger/emojis_logger.py
@@ -0,0 +1,40 @@
+import emoji
+from lume.src.domain.services.interface_logger import (
+    ILogger,
+    INFO,
+    WARNING,
+    ERROR,
+    HIGHLIGHT,
+    COMMAND,
+)
+from lume.src.infrastructure.services.logger.colors import Colors
+
+LOGGING_LEVEL = {
+    HIGHLIGHT: emoji.emojize("🔥"),
+    COMMAND: emoji.emojize("👩‍💻"),
+    INFO: "",
+    ERROR: emoji.emojize("💩"),
+    WARNING: emoji.emojize("🧐"),
+}
+
+
+class EmojisLogger(ILogger):
+    def log(self, logging_level, message):
+        start, end = self.color_provider(logging_level)
+        print(f"{start}{LOGGING_LEVEL[logging_level]} {message.rstrip()}{end}")
+
+    @staticmethod
+    def color_provider(logging_level):
+        start = ""
+        end = ""
+        if logging_level == ERROR:
+            start = Colors.FAIL
+        elif logging_level == WARNING:
+            start = Colors.WARNING
+        elif logging_level == HIGHLIGHT:
+            start = Colors.OKBLUE
+
+        if start != "":
+            end = Colors.ENDC
+
+        return start, end
