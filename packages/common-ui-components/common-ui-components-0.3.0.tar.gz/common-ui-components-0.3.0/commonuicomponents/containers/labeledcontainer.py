afdb7b332f7aa549b5c4dfae5d24d213a5bc8001
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:01 +0000

    Add package common-ui-components/0.3.0/common-ui-components-0.3.0.tar.gz

---
+++ packages/common-ui-components/common-ui-components-0.3.0.tar.gz/common-ui-components-0.3.0/commonuicomponents/containers/labeledcontainer.py
@@ -0,0 +1,8 @@
+from tkinter.ttk import LabelFrame
+from .basecontainer import BaseContainer
+from .. import CommonUIComponents
+
+class LabeledContainer(BaseContainer, LabelFrame):
+   _DEFAULT_STYLE = BaseContainer._defaultStyle({
+      "background": "pink"
+    } if CommonUIComponents.DEBUG else None)
