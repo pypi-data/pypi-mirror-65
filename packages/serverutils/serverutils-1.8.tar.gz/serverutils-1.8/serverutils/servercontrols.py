11da3ea72a1d009098952c3512c744f6d042bd6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:08 +0000

    Add package serverutils/1.8/serverutils-1.8.tar.gz

---
+++ packages/serverutils/serverutils-1.8.tar.gz/serverutils-1.8/serverutils/servercontrols.py
@@ -0,0 +1,10 @@
+class Controls:
+    def __init__(self,server):
+        self.server=server
+        self.inittasks()
+    def inittasks(self):
+        pass
+    def on_post(self,data):
+        pass
+    def on_get(self,data):
+        pass
