bef922753c059b044a78207f884fb56edc4e9063
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:39 +0000

    Add package skreep/0.0.2/skreep-0.0.2.tar.gz

---
+++ packages/skreep/skreep-0.0.2.tar.gz/skreep-0.0.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="skreep", # Replace with your own username
+    version="0.0.2",
+    author="Korud",
+    author_email="khorud@yahoo.com",
+    description="Scraper Lib",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/khorud",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires= ['selenium'],
+    python_requires= '>=3',
+)
