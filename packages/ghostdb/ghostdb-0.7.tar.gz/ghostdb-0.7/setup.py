275222a59379eaeff114c7c9db647c3d8b0b0254
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:24 +0000

    Add package ghostdb/0.7/ghostdb-0.7.tar.gz

---
+++ packages/ghostdb/ghostdb-0.7.tar.gz/ghostdb-0.7/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+def readme():
+    with open("README.rst") as f:
+        return f.read()
+
+setup(
+    name="ghostdb",
+    version="0.7",
+    description="Python SDK for GhostDB Cache",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    author="Jake Grogan",
+    author_email="jake.grogan8@mail.dcu.ie",
+    license="MIT",
+    packages=["GhostDB"],
+    package_data={
+        "GhostDB": [
+            "tests/ghostdb_sim.conf",
+            "tests/ghostdb_test.conf"
+        ]
+    },
+    install_requires=[
+        "requests",
+    ],
+    zip_safe=False,
+    test_suite='nose.collector',
+    tests_require=['nose'],
+)
