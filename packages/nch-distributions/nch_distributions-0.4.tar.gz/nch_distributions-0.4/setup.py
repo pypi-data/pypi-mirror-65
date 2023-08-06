75afd339cf3b04172ae42b68dc0636d78269812c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:22 +0000

    Add package nch-distributions/0.4/nch_distributions-0.4.tar.gz

---
+++ packages/nch-distributions/nch_distributions-0.4.tar.gz/nch_distributions-0.4/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='nch_distributions',
+      version='0.4',
+      description='Gaussian distributions',
+      packages=['nch_distributions'],
+      author='NCH',
+      zip_safe=False)
