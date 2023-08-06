4c41291ca11a91cc9ddad29c84ff95d3e33a03ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:30 +0000

    Add package ploomber/0.3.2/ploomber-0.3.2-py3-none-any.whl

---
+++ packages/ploomber/ploomber-0.3.2-py3-none-any.whl/ploomber/sources/__init__.py
@@ -0,0 +1,9 @@
+from ploomber.sources.sources import (PythonCallableSource,
+                                      SQLScriptSource,
+                                      SQLQuerySource,
+                                      GenericSource,
+                                      FileSource)
+
+__all__ = ['PythonCallableSource', 'SQLScriptSource',
+           'SQLQuerySource',
+           'GenericSource', 'FileSource']
