75fd325a4dac831e142372ac4089e079ca0ade63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:22 +0000

    Add package zshpower/0.2.7/zshpower-0.2.7.tar.gz

---
+++ packages/zshpower/zshpower-0.2.7.tar.gz/zshpower-0.2.7/zshpower/__init__.py
@@ -0,0 +1,18 @@
+"""
+ZSHPower
+~~~~~~~~
+
+ZSHPower is a theme with a manager for the Oh My ZSH framework.
+
+
+For more information, access: 'https://github.com/snakypy/zshpower'
+
+:copyright: Copyright 2020-2020 by Snakypy team, see AUTHORS.
+:license: MIT license, see LICENSE for details.
+"""
+
+from pathlib import Path
+
+HOME = str(Path.home())
+
+__version__ = "0.2.7"
