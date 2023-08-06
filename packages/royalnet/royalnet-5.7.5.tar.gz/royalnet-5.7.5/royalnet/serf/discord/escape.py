66f5aba5e5e237220b909c86e9955f594dc076b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:40 +0000

    Add package royalnet/5.7.5/royalnet-5.7.5.tar.gz

---
+++ packages/royalnet/royalnet-5.7.5.tar.gz/royalnet-5.7.5/royalnet/serf/discord/escape.py
@@ -0,0 +1,31 @@
+import re
+
+
+def escape(string: str) -> str:
+    """Escape a string to be sent through Discord, and format it using RoyalCode.
+
+    Warning:
+        Currently escapes everything, even items in code blocks."""
+    url_pattern = re.compile(r"\[url=(.*?)](.*?)\[/url]")
+    url_replacement = r'\2 (\1)'
+
+    escaped_string = string.replace("<", "&lt;").replace(">", "&gt;")
+
+    simple_parse = escaped_string \
+        .replace("*", "\\*") \
+        .replace("_", "\\_") \
+        .replace("`", "\\`") \
+        .replace("[b]", "**") \
+        .replace("[/b]", "**") \
+        .replace("[i]", "_") \
+        .replace("[/i]", "_") \
+        .replace("[u]", "__") \
+        .replace("[/u]", "__") \
+        .replace("[c]", "`") \
+        .replace("[/c]", "`") \
+        .replace("[p]", "```") \
+        .replace("[/p]", "```")
+
+    advanced_parse = re.sub(url_pattern, url_replacement, simple_parse)
+
+    return advanced_parse
