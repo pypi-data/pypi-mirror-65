d47b7751ac7096b727e5b7f681e11527891cc661
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:52 +0000

    Add package dblue-mlwatch/0.0.1/dblue-mlwatch-0.0.1.tar.gz

---
+++ packages/dblue-mlwatch/dblue-mlwatch-0.0.1.tar.gz/dblue-mlwatch-0.0.1/setup.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+import sys
+
+from setuptools import find_packages, setup
+from setuptools.command.test import test as TestCommand
+
+from dblue_mlwatch.version import VERSION
+
+
+def requirements():
+    with open('requirements/requirements-base.txt') as f:
+        return f.readlines()
+
+
+def read_readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []  # pylint: disable=attribute-defined-outside-init
+        self.test_suite = True  # pylint: disable=attribute-defined-outside-init
+
+    def run_tests(self):
+        import pytest
+        errcode = pytest.main(self.test_args)
+        sys.exit(errcode)
+
+
+PACKAGE_NAME = 'dblue-mlwatch'
+PACKAGE_VERSION = VERSION
+
+setup(name=PACKAGE_NAME,
+      version=VERSION,
+      description='Dblue MLWatch SDK for real-time machine learning model monitoring',
+      long_description=read_readme(),
+      long_description_content_type="text/markdown",
+      maintainer='Rajesh Hegde',
+      maintainer_email='rh@dblue.ai',
+      author='Rajesh Hegde',
+      author_email='rh@dblue.ai',
+      url='https://github.com/dblueai/dblue-mlwatch-python-sdk',
+      license='Apache2',
+      platforms='any',
+      packages=find_packages(),
+      keywords=[
+          'dblue',
+          'machine-learning',
+          'deep-learning',
+          'monitoring',
+          'mlwatch',
+
+      ],
+      install_requires=requirements(),
+      extras_require={
+          "system-metrics": [
+              "psutil==5.7.0",
+          ],
+      },
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: Apache Software License',
+          'Natural Language :: English',
+          'Programming Language :: Python',
+          'Operating System :: OS Independent',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'Topic :: Scientific/Engineering :: Artificial Intelligence'
+      ],
+      tests_require=[
+          "pytest",
+      ],
+      cmdclass={'test': PyTest})
