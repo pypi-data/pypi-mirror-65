9046fe996bc257d9d8793262651fa9c20ddf3321
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:23 +0000

    Add package chiapos/0.12.4/chiapos-0.12.4.tar.gz

---
+++ packages/chiapos/chiapos-0.12.4.tar.gz/chiapos-0.12.4/windows-build.sh
@@ -0,0 +1,28 @@
+#!/bin/sh -x
+
+# Stop in case of error
+set -e
+
+export CMAKE_GENERATOR='MSYS Makefiles'
+
+#export PATH=$PATH:"C:\msys64\mingw64\bin"
+echo "PATH is $PATH"
+
+echo "PWD is $PWD"
+
+#python3 -m venv venv
+#. venv/bin/activate
+
+#pip wheel . -G "MSYS Makefiles"
+cmake -G "MSYS Makefiles" --build .
+make
+
+echo "Running RunTests.exe"
+$PWD/RunTests.exe
+
+echo "Running py.test -v tests/"
+py.test -v $PWD/tests/
+
+echo "Testing Windows Complete"
+#echo "Trying to build a wheel"
+#pip -vv wheel .
