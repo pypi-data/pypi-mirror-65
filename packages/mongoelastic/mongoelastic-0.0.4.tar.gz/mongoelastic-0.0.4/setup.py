b11a1b84d2a4dcb8a9149173a6007b55e169183b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:15 +0000

    Add package mongoelastic/0.0.4/mongoelastic-0.0.4.tar.gz

---
+++ packages/mongoelastic/mongoelastic-0.0.4.tar.gz/mongoelastic-0.0.4/setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mongoelastic",
+    version="0.0.4",
+    author="Naim Malek",
+    author_email="naim@peerbits.com",
+    description="MongoDB to Elasticsearch. Through this you can import data from mongoDB to elasticsearch.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/naimmalek/mongoelastic",
+    download_url="https://pypi.org/project/mongoelastic/",
+    packages=setuptools.find_packages(),
+    install_requires=["progress >= 1.5",
+                      "elasticsearch >=7.6.0",
+                      "pymongo >=3.10.1"
+                      ],
+    keywords=[
+        "mongodb",
+        "elasticsearch import",
+        "import mongodb to elasticsearch",
+        "elasticsearch",
+        "import mogno to es",
+        "import big data elasticsearch",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+    license="MIT",
+
+)
