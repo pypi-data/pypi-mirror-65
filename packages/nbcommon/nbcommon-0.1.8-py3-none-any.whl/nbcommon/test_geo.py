cb42f1d06623bdd90539c7c5f13b454c7e140cc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:25 +0000

    Add package nbcommon/0.1.8/nbcommon-0.1.8-py3-none-any.whl

---
+++ packages/nbcommon/nbcommon-0.1.8-py3-none-any.whl/nbcommon/test_geo.py
@@ -0,0 +1,13 @@
+from . import geo
+
+def test_parse_poly():
+    thing = """who
+cares
+\t1.2\t3.4
+\t5.6\t7.8
+END 
+END """
+    r =  geo.parse_poly(thing)
+    print(f'r is {r}')
+    assert len(r) == 1
+    assert r[0][0] == [1.2,3.4]
