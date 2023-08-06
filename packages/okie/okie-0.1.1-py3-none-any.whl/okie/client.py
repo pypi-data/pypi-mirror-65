9eb444a58c28db2a416aea3ecb6970575ca831a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:56 +0000

    Add package okie/0.1.1/okie-0.1.1-py3-none-any.whl

---
+++ packages/okie/okie-0.1.1-py3-none-any.whl/okie/client.py
@@ -0,0 +1,92 @@
+from typing import Optional, Union
+
+import httptools
+
+from .ctrl import asyncio
+from ._builders import OkieRequestPart, HTTPRequestFull
+from .types import Headers
+from .response import HTProtocol, Response
+from .enums.http_request import HttpRequestType
+
+
+class Okie(asyncio.AsyncioConnectionController):
+    def __init__(
+        self,
+        timeout: float,
+        ssl_handshake_timeout: Optional[float] = None,
+        headers: Optional[Headers] = None,
+    ):
+        # initialize controllers
+        asyncio.AsyncioConnectionController.__init__(
+            self, ssl_handshake_timeout=ssl_handshake_timeout
+        )
+
+        self.timeout = timeout
+        self.default_headers = headers or Headers()
+
+    async def _request(
+        self,
+        method: Union[str, HttpRequestType],
+        url: str,
+        data_builder: Optional[OkieRequestPart] = None,
+        headers: Optional[Headers] = None,
+    ) -> Response:
+        url: httptools.parser.URL = httptools.parse_url(url.encode())
+
+        async with self.make_connection(
+            url.host,
+            url.port,
+            url.schema.startswith(b"https") if url.schema else False
+        ) as stream:  # type: asyncio.AsyncioStreamType
+            r, w = stream
+            method = HttpRequestType(method)
+            plain_request = HTTPRequestFull(
+                method=str(method.value),
+                host=url.host,
+                path=url.path,
+                sub_builder=data_builder,
+                headers=self.default_headers.get_merged(headers),
+            )
+            w.write(plain_request.full)
+
+            await w.drain()
+
+            pc = HTProtocol()
+            parser = httptools.parser.HttpResponseParser(pc)
+
+            # feed headers
+            parser.feed_data(await r.readuntil(b"\r\n\r\n"))
+            # feed the rest body
+            parser.feed_data(await r.readexactly(len(pc.response)))
+
+            return pc.response
+
+    async def request(
+        self,
+        method: Union[str, HttpRequestType],
+        url: str,
+        timeout: Optional[float] = None,
+        data_builder: Optional[OkieRequestPart] = None,
+        headers: Optional[Headers] = None,
+    ) -> Response:
+        """
+        Makes HTTP request within given timeout if succeeds on time returns Response object
+        otherwise raises `asyncio.TimeoutError`
+
+        ##### Parameters
+        - method `(str, okie.HTTPRequestType)` *HTTP request type*
+        - url `str` *resource identifier, if doesn't have schema, will assume non-ssl request*
+        - timeout `float` *optional timeout within request should be sent/read. (Task gets cancelled on error)*
+        - data_builder `okie._builders.OkieRequestPart` *partial builder object to fill `okie.builders.HTTPFullRequest`*
+        - headers `okie.Headers` *headers object, gets merged with self.default_headers into new Headers before request*
+
+        ##### Returns
+        - `okie.response.Response`
+        """
+
+        return await asyncio.call_with_timeout(
+            timeout=timeout or self.timeout, future=self._request(
+                method=method, url=url, data_builder=data_builder,
+                headers=headers,
+            )
+        )
---
+++ packages/okie/okie-0.1.1-py3-none-any.whl/okie/ctrl/asyncio.py
@@ -0,0 +1,89 @@
+"""
+AsyncioConnectionController pools/manages streams with standard `asyncio` library.
+"""
+
+from typing import Optional, Deque, Set, Any, Tuple
+from contextlib import asynccontextmanager
+import asyncio
+from collections import deque
+
+DEFAULT_SSL_HANDSHAKE_TIMEOUT = 60
+DEFAULT_HTTPS_PORT = 443
+DEFAULT_HTTP_PORT = 80
+
+
+AsyncioStreamType = Tuple[asyncio.StreamReader, asyncio.StreamWriter]
+"""
+Asyncio stream type; Pair of stream reader/writer
+"""
+
+
+async def close_stream(stream: AsyncioStreamType):
+    _, w = stream
+    w.close()
+    await w.wait_closed()
+
+
+class AsyncioConnectionController:
+    def __init__(self, ssl_handshake_timeout: Optional[float] = None):
+        self._connections_deque: Deque[Optional[AsyncioStreamType]] = deque()
+        self._busy_connections: Set[Optional[AsyncioStreamType]] = set()
+        self._semaphore = asyncio.Semaphore()
+        self._closed = False
+        self._ssl_hshk_timeout = ssl_handshake_timeout or DEFAULT_SSL_HANDSHAKE_TIMEOUT
+
+    @asynccontextmanager
+    async def make_connection(
+        self,
+        destination_host: bytes,
+        destination_port: Optional[int],
+        ssl: Any,
+        ssl_handshake_timeout: Optional[float] = None,
+    ) -> AsyncioStreamType:
+        await self._semaphore.acquire()
+        stream = self._connections_deque.popleft() if self._connections_deque else None
+        self._busy_connections.add(stream)
+
+        if stream is None:
+            if destination_port is None:
+                destination_port = DEFAULT_HTTPS_PORT if ssl else DEFAULT_HTTP_PORT
+
+            stream = await asyncio.open_connection(
+                host=destination_host.decode(),
+                port=destination_port,
+                ssl=ssl,
+                ssl_handshake_timeout=(
+                    ssl_handshake_timeout or self._ssl_hshk_timeout
+                ) if ssl else None,
+            )
+
+        try:
+            yield stream
+        finally:
+            self._connections_deque.append(stream)
+            self._busy_connections.discard(stream)
+            self._semaphore.release()
+
+    async def close_all(self):
+        if self._closed:
+            return
+
+        self._closed = True
+        await asyncio.wait(
+            (
+                close_stream(rw)
+                for rw in (
+                    *self._connections_deque,
+                    *self._busy_connections
+                ) if rw
+            )
+        )
+
+        self._connections_deque = deque([None] * len(self._connections_deque))
+        self._busy_connections.clear()
+
+
+async def call_with_timeout(future, timeout: float):
+    return asyncio.wait_for(
+        fut=future, timeout=timeout
+    )
