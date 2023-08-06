d479f30016e6ead073d581d6f900eb25df97a4ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:26 +0000

    Add package injectable/3.0.1/injectable-3.0.1-py3-none-any.whl

---
+++ packages/injectable/injectable-3.0.1-py3-none-any.whl/examples/illustrative_example/illustrative_example.py
@@ -0,0 +1,29 @@
+"""
+This is an illustrative example of basic usage of the injectable framework in a single
+Python file.
+"""
+# sphinx-start
+from examples import Example
+from injectable import injectable, InjectionContainer, autowired, Autowired
+
+
+@injectable
+class Dep:
+    def __init__(self):
+        self.foo = "foo"
+
+
+@injectable  # make examples also injectable for testing
+class IllustrativeExample(Example):
+    @autowired
+    def __init__(self, dep: Autowired(Dep)):
+        self.dep = dep
+
+    def run(self):
+        print(self.dep.foo)
+
+
+if __name__ == "__main__":
+    InjectionContainer.load()
+    example = IllustrativeExample()
+    example.run()
