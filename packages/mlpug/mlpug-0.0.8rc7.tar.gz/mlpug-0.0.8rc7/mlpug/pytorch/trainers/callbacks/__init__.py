37297c4b85a9f90b58cdc9b4850c8d41a75bf15b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:59 +0000

    Add package mlpug/0.0.8rc7/mlpug-0.0.8rc7.tar.gz

---
+++ packages/mlpug/mlpug-0.0.8rc7.tar.gz/mlpug-0.0.8rc7/mlpug/pytorch/trainers/callbacks/__init__.py
@@ -0,0 +1,9 @@
+from .basic import *
+from .callback import *
+from .checkpoint_manager import CheckpointManager
+from .lr_scheduler_wrapper import LRSchedulerWrapper
+from .metrics_logger import TrainingMetricsLogger, TestMetricsLogger
+from .tensorboard import Tensorboard, AutoTensorboard
+
+
+
