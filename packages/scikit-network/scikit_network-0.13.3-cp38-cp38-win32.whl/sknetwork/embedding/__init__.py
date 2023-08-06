1bd02fe2b0f5206d83d0fb9c2fe12df0730f61ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:44 +0000

    Add package scikit-network/0.13.3/scikit_network-0.13.3-cp38-cp38-win32.whl

---
+++ packages/scikit-network/scikit_network-0.13.3-cp38-cp38-win32.whl/sknetwork/embedding/__init__.py
@@ -0,0 +1,5 @@
+"""embedding module"""
+from sknetwork.embedding.base import BaseEmbedding
+from sknetwork.embedding.metrics import cosine_modularity
+from sknetwork.embedding.spectral import Spectral, BiSpectral
+from sknetwork.embedding.svd import SVD, GSVD
