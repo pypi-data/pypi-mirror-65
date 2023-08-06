35db55830b48cd657bb2b6dfe4b7b75dfcbf4593
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:19 +0000

    Add package loggertool/0.0.3/loggertool-0.0.3.tar.gz

---
+++ packages/loggertool/loggertool-0.0.3.tar.gz/loggertool-0.0.3/README.md
@@ -0,0 +1,4 @@
+Logging Tools
+
+This is a tool for logging data and stuff.
+For help email lester123octonal@gmail.com
---
+++ packages/loggertool/loggertool-0.0.3.tar.gz/loggertool-0.0.3/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="loggertool", # Name
+    version="0.0.3",
+    author="OctonalXX",
+    author_email="lester123octonal@gmail.com",
+    description="Tools For Logging Data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
