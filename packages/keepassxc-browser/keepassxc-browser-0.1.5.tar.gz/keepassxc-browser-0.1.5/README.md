fe6ab0d05c382a5d62a79a052a68f680bf5c9afe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:20 +0000

    Add package keepassxc-browser/0.1.5/keepassxc-browser-0.1.5.tar.gz

---
+++ packages/keepassxc-browser/keepassxc-browser-0.1.5.tar.gz/keepassxc-browser-0.1.5/README.md
@@ -0,0 +1,4 @@
+keepassxc_browser
+=================
+
+A Python package to interface with KeePassXC over the KeePassXC Browser API. See `example.py` for example usage.
---
+++ packages/keepassxc-browser/keepassxc-browser-0.1.5.tar.gz/keepassxc-browser-0.1.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='keepassxc-browser',
+    version='0.1.5',
+    packages=find_packages(),
+    install_requires=['pysodium'],
+    description='Access the KeepassXC Browser API from python',
+    url='https://github.com/hrehfeld/python-keepassxc-browser',
+    author='Hauke Rehfeld',
+    author_email='pypi.org@haukerehfeld.de',
+    license='AGPL-3.0',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Operating System :: OS Independent",
+        "Topic :: Security",
+        "Topic :: Software Development :: Libraries",
+    ],
+)
