9feb0c907741682111e5d902223697e3171a1257
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:50 +0000

    Add package cimpyorm/0.7.8/cimpyorm-0.7.8-py3-none-any.whl

---
+++ packages/cimpyorm/cimpyorm-0.7.8-py3-none-any.whl/cimpyorm/Test/Integration/SQLite/test_integration_tests.py
@@ -0,0 +1,43 @@
+import cimpyorm.auxiliary
+from cimpyorm.api import load, parse
+import os
+import pytest
+import cimpyorm
+
+from cimpyorm.backends import SQLite, InMemory
+
+
+def test_parse_inmemory(full_grid):
+    try:
+        cimpyorm.auxiliary.get_path("SCHEMAROOT")
+    except KeyError:
+        pytest.skip(f"Schemata not configured")
+    session, m = parse(full_grid, InMemory)
+    session.close()
+
+
+def test_parse_load(full_grid):
+    try:
+        cimpyorm.auxiliary.get_path("SCHEMAROOT")
+    except KeyError:
+        pytest.skip(f"Schemata not configured")
+    path = os.path.join(full_grid, ".integration_test.db")
+    session, m = parse(full_grid, SQLite(path=path))
+    session.close()
+    session, m = load(path)
+    session.close()
+    os.remove(path)
+
+
+def test_parse_parse(full_grid):
+    try:
+        cimpyorm.auxiliary.get_path("SCHEMAROOT")
+    except KeyError:
+        pytest.skip(f"Schemata not configured")
+    path = os.path.join(full_grid, ".integration_test.db")
+    session, m = parse(full_grid, SQLite(path=path))
+    session.close()
+    session, m = parse(full_grid, SQLite(path=path))
+    assert session.query(m.Terminal).first().ConductingEquipment
+    session.close()
+    os.remove(path)
