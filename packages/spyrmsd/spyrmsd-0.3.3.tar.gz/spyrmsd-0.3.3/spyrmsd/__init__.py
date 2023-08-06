80195e1802eb3711ff837113701ca565d3d80b12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:08 +0000

    Add package spyrmsd/0.3.3/spyrmsd-0.3.3.tar.gz

---
+++ packages/spyrmsd/spyrmsd-0.3.3.tar.gz/spyrmsd-0.3.3/spyrmsd/__init__.py
@@ -0,0 +1,11 @@
+"""
+Python RMSD tool with symmetry correction.
+"""
+
+# Handle versioneer
+from ._version import get_versions
+
+versions = get_versions()
+__version__ = versions["version"]
+__git_revision__ = versions["full-revisionid"]
+del get_versions, versions
