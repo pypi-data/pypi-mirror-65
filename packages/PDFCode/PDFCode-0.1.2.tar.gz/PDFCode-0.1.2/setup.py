658a5aedd182438a6f26a3a2b8423ba8a0a740a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:23 +0000

    Add package PDFCode/0.1.2/PDFCode-0.1.2.tar.gz

---
+++ packages/PDFCode/PDFCode-0.1.2.tar.gz/PDFCode-0.1.2/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+from PDFCode.PDFCode import __version__
+
+with open("README_pypi.md", "r") as fh:
+    long_description = fh.read()
+
+# with open("./PDFCode/version.txt", "r") as fh:
+    # version = fh.read()
+
+setuptools.setup(
+    name="PDFCode", # Replace with your own username
+    version=__version__, 
+    author="xincoder",
+    author_email="xincoder@gmail.com",
+    description="Save code files into PDF format with syntax highlighting.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/xincoder/PDFCode",
+    packages=setuptools.find_packages(),
+    entry_points={
+        'console_scripts': ['pdfcode=PDFCode:main'],
+    },
+    install_requires=[
+        'pygments',
+        'pdfkit',
+        'file-magic',
+        'markdown',
+      ],
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.7',
+)
