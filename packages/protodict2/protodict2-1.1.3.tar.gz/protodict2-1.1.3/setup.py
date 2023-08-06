114bb9b8159ddbb070fe28cf7e44c21420c6334e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:47 +0000

    Add package protodict2/1.1.3/protodict2-1.1.3.tar.gz

---
+++ packages/protodict2/protodict2-1.1.3.tar.gz/protodict2-1.1.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+
+setup(
+    name='protodict2',
+    description='a small Python library for 2 ways conversion between dicts '
+                'and protocol buffers.',
+    version='1.1.3',
+    author='Eugene Van den Bulke',
+    author_email='eugene.vandenbulke@gmail.com',
+    url='https://github.com/alexyvassili/protodict2',
+    license='Public Domain',
+    keywords=['protobuf', 'dict'],
+    install_requires=['protobuf>=2.3.0', 'six'],
+    package_dir={'': 'src'},
+    py_modules=['protodict2'],
+    # setup_requires=['protobuf>=2.3.0', 'nose>=1.0', 'coverage', 'nosexcover'],
+    # test_suite = 'nose.collector',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: Public Domain',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
