f3100fab1c03ff17ad04eb889f4800e5cb7d9e93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:39 +0000

    Add package csv-batch/0.2.0/csv_batch-0.2.0.tar.gz

---
+++ packages/csv-batch/csv_batch-0.2.0.tar.gz/csv_batch-0.2.0/csv_batch/__init__.py
@@ -0,0 +1,1 @@
+from csv_batch.read import read
---
+++ packages/csv-batch/csv_batch-0.2.0.tar.gz/csv_batch-0.2.0/csv_batch/read.py
@@ -0,0 +1,19 @@
+import csv
+from typing import Generator
+
+
+def read(file: str, batchsize: int = None) -> Generator:
+    with open(file, "r") as f:
+        reader = csv.DictReader(f)
+
+        if batchsize is not None:
+            batch = []
+            for index, line in enumerate(reader):
+                if index % batchsize == 0 and index > 0:
+                    yield batch
+                    del batch[:]
+                batch.append(dict(line))
+            yield batch
+        else:
+            for row in reader:
+                yield row
