9d335d6021baa3e9cd1815279986c78f4a571daf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:56 +0000

    Add package cloudutils/1.2.14/cloudutils-1.2.14-py3-none-any.whl

---
+++ packages/cloudutils/cloudutils-1.2.14-py3-none-any.whl/cloudutils/core/network.py
@@ -0,0 +1,33 @@
+# Copyright © 2020 Noel Kaczmarek
+from suds.client import Client
+
+
+class Network(object):
+    def __init__(self, host, port, **kwargs):
+        self.administration_service = (host, port)
+        self.authentication_service = self.administration.getService(2)
+        self.resource_service = self.administration.getService(3)
+        self._administration = None
+        self._authentication = None
+        self._resource = None
+
+    @property
+    def administration(self):
+        if not self._administration:
+            self._administration = Client('http://%s:%s/?wsdl' % self.administration_service)
+
+        return self._administration.service
+
+    @property
+    def authentication(self):
+        if not self._authentication:
+            self._authentication = Client('http://%s:%d/?wsdl' % (self.authentication_service.host, self.authentication_service.type.port))
+
+        return self._authentication.service
+
+    @property
+    def resource(self):
+        if not self._resource:
+            self._resource = Client('http://%s:%d/?wsdl' % (self.resource_service.host, self.resource_service.type.port))
+
+        return self._resource.service
