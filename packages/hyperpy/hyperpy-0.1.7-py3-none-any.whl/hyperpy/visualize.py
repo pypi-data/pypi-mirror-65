dddd0e8048b2e7d2045f9c99ad265ce6e1bb2333
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:30 +0000

    Add package hyperpy/0.1.7/hyperpy-0.1.7-py3-none-any.whl

---
+++ packages/hyperpy/hyperpy-0.1.7-py3-none-any.whl/hyperpy/visualize.py
@@ -0,0 +1,16 @@
+
+import matplotlib.pyplot as plt
+
+def imshow_gray(img):
+	'''
+	returns: image ploted in gray levels
+	parameters: image array matrix 2 dimensional single band
+	'''
+    plt.imshow(img,cmap="gray")
+    plt.show()
+
+def imshow_hist(img):
+	'''
+
+	'''
+	pass
