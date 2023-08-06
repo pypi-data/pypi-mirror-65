d3e506d93ffb0b218cc28b022dbed2e393d69950
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:46 +0000

    Add package rocheml/1.2/rocheml-1.2-py3-none-any.whl

---
+++ packages/rocheml/rocheml-1.2-py3-none-any.whl/rocheml/preprocessing/croppreprocessor.py
@@ -0,0 +1,16 @@
+class CropPreprocessor:
+    def __init__(self, box_origin, box_width, box_height):
+        self.box_origin = box_origin
+        self.box_width = box_width
+        self.box_height = box_height
+
+    def preprocess(self, imgs):
+        crops = []
+
+        for img in imgs:
+            crop = img[self.box_origin[1]:self.box_origin[1] +
+                       self.box_height, self.box_origin[0]:self.box_origin[0] +
+                       self.box_width]
+            crops.append(crop)
+
+        return crops
