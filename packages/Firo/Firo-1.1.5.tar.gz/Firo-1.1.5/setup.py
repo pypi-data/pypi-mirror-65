d67ae4ef538a34d59838b6fa6a94b61c400ddb4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:27 +0000

    Add package Firo/1.1.5/Firo-1.1.5.tar.gz

---
+++ packages/Firo/Firo-1.1.5.tar.gz/Firo-1.1.5/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    longdescription = fh.read()
+
+setuptools.setup(
+    name="Firo", 
+    version="1.1.5",
+    author="Peeled Fruit Studios",
+    author_email="Circutrider21@gmail.com",
+    description="A Database That stores and gets values simply",
+    long_description=longdescription,
+    long_description_content_type="text/markdown",
+    url="https://github.com/circutrider21/FiroDB",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
