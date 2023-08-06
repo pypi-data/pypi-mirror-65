ddb7306b19a5ae9750e8c93269d0d50a43c84ff4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:19 +0000

    Add package Mobydoc/0.0.4/Mobydoc-0.0.4.tar.gz

---
+++ packages/Mobydoc/Mobydoc-0.0.4.tar.gz/Mobydoc-0.0.4/src/Mobydoc/__init__.py
@@ -0,0 +1,8 @@
+from .base import Version, checkVersion, dumps
+from .multimedia.multimedia import Multimedia
+from .multimedia.zone_generale import MultimediaZoneGenerale
+from .provenance.provenance import Provenance
+from .provenance.zone_contexte import ProvenanceZoneContexte
+from .specimen.champs.multimedia import ChampSpecimenZoneMultimediaMultimedia
+from .specimen.specimen import Specimen
+from .specimen.zone_identification import SpecimenZoneIdentification
