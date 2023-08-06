b0c48c9c00aa58d8cec68c9916ef741fd2e39e13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:37 +0000

    Add package dbx-deploy/0.9.5/dbx-deploy-0.9.5.tar.gz

---
+++ packages/dbx-deploy/dbx-deploy-0.9.5.tar.gz/dbx-deploy-0.9.5/src/dbxdeploy/dbc/NotebookConverter.py
@@ -0,0 +1,23 @@
+from dbxdeploy.dbc.CommandConverter import CommandConverter
+
+class NotebookConverter:
+
+    def __init__(
+        self,
+        commandConverter: CommandConverter,
+    ):
+        self.__commandConverter = commandConverter
+
+    def convert(self, commands: list, firstLine: str, cellSeparator: str) -> str:
+        while commands[len(commands) - 1]['command'] == '':
+            commands.pop()
+
+        commands.sort(key=lambda command: command['position'])
+        commands = list(map(self.__commandConverter.convert, commands))
+
+        output = f'\n\n{cellSeparator}\n\n'.join(commands)
+
+        if output[-1:] != '\n':
+            output += '\n'
+
+        return firstLine + '\n' + output
