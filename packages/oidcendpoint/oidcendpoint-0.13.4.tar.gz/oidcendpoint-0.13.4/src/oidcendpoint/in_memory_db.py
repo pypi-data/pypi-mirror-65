8054fd23d9961326ff4be4971119bb4d8a7f03d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:42 +0000

    Add package oidcendpoint/0.13.4/oidcendpoint-0.13.4.tar.gz

---
+++ packages/oidcendpoint/oidcendpoint-0.13.4.tar.gz/oidcendpoint-0.13.4/src/oidcendpoint/in_memory_db.py
@@ -0,0 +1,26 @@
+class InMemoryDataBase(object):
+    def __init__(self):
+        self.db = {}
+
+    def __contains__(self, key):
+        if self.db.get(key):
+            return 1
+
+    def set(self, key, value):
+        self.db[key] = value
+
+    def get(self, key):
+        return self.db.get(key, None)
+
+    def delete(self, key):
+        if self.db.get(key):
+            del self.db[key]
+
+    def keys(self):
+        return self.db.keys()
+
+    def close(self):
+        pass
+
+    def clear(self):
+        self.db = {}
