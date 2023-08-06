40c6288f1addf268385de1158b84c4e8240eb13c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:28 +0000

    Add package DroneController/1.0.1/DroneController-1.0.1-py3-none-any.whl

---
+++ packages/DroneController/DroneController-1.0.1-py3-none-any.whl/drone_controller/main.py
@@ -0,0 +1,15 @@
+"""
+Main Class and entry point for the project.
+"""
+
+
+def welcome() -> None:
+    """
+    Greeting method
+    :return: None
+    """
+    print('Welcome to DroneController. Enjoy your flight.')
+
+
+if __name__ == '__main__':
+    welcome()
