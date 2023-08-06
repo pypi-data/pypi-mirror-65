fa8a9c2d35e2ba2949c2352a12ee09dc412c45d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:09 +0000

    Add package Supplier-API-mock-server/1.3.3/Supplier_API_mock_server-1.3.3-py3-none-any.whl

---
+++ packages/Supplier-API-mock-server/Supplier_API_mock_server-1.3.3-py3-none-any.whl/supplier_server/constants.py
@@ -0,0 +1,8 @@
+PRODUCTS = [
+    {'id': 'A300-FX', 'name': 'A300-FX', 'use_timeslots': True},
+    {'id': 'A400-FX', 'name': 'A400-FX', 'use_timeslots': True, 'description': 'Test timeslot product'},
+    {'id': 'A500-FX', 'name': 'A500-FX', 'use_timeslots': False, 'description': 'Test non timeslot product'},
+    {'id': 'A600-FX', 'name': 'A600-FX', 'use_timeslots': False},
+]
+VARIANTS = ('Adult', 'Child')
+MAX_DATE_RANGE = 6  # in months
