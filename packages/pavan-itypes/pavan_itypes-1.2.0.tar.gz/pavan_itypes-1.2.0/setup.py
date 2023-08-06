0484e1859c0ec2a4d9cda59a4fe8be9137a87d3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:08 +0000

    Add package pavan-itypes/1.2.0/pavan_itypes-1.2.0.tar.gz

---
+++ packages/pavan-itypes/pavan_itypes-1.2.0.tar.gz/pavan_itypes-1.2.0/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+import re
+import os
+import sys
+
+
+def get_version():
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open('itypes.py').read()
+    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
+
+version = get_version()
+
+
+if sys.argv[-1] == 'publish':
+    os.system("python setup.py sdist upload")
+    print("You probably want to also tag the version now:")
+    print("  git tag -a %s -m 'version %s'" % (version, version))
+    print("  git push --tags")
+    sys.exit()
+
+
+setup(
+    name='pavan_itypes',
+    version=version,
+    url='https://github.com/PavanTatikonda/itypes',
+    license='BSD',
+    description='Simple immutable types for python.',
+    author='Pavan Tatikonda',
+    author_email='tvs.pavan@gmail.com',
+    py_modules=['itypes'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
+)
