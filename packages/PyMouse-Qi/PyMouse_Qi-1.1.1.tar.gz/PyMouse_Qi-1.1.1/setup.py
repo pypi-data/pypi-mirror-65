d721411279c9dfa86306d93ecd7af7167c4d656e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:22 +0000

    Add package PyMouse-Qi/1.1.1/PyMouse_Qi-1.1.1.tar.gz

---
+++ packages/PyMouse-Qi/PyMouse_Qi-1.1.1.tar.gz/PyMouse_Qi-1.1.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: iso-8859-1 -*-
+
+#   Copyright 2010 Pepijn de Vos
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+from setuptools import setup
+
+setup(name='PyMouse_Qi',
+      version='1.1.1',
+      description='Cross-platform mouse control',
+      author='Pepijn de Vos , modifier by Qi',
+      author_email='pypimail@protonmail.com',
+      packages = ["PyMouse_Qi"],
+      classifiers = [
+          "Programming Language :: Python",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: Apache Software License",
+          "Operating System :: MacOS :: MacOS X",
+          "Operating System :: Microsoft :: Windows",
+          "Operating System :: Unix",
+          "Topic :: Software Development :: Libraries :: Python Modules"
+      ]
+     )
