b1ad3b0fb67af455f76d0b8beea848fb2c1284c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:19 +0000

    Add package SubhoDataStructure/1.0/SubhoDataStructure-1.0.zip

---
+++ packages/SubhoDataStructure/SubhoDataStructure-1.0.zip/SubhoDataStructure-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='SubhoDataStructure',
+    version='1.0',
+    packages=['SubhoDataStructure'],
+    url='https://github.com/SubhoBasak',
+    license='Open Source',
+    author='Subho Basak',
+    author_email='subhobasak50@gmail.com',
+    description='Common data structures for python. Now it only contain binary tree but in future updates more data structures will be added.'
+)
