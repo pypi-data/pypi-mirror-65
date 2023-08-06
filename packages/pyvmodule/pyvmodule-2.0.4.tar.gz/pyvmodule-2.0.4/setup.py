c64d45d34802a82841dedcb2837bd80e62907219
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:37 +0000

    Add package pyvmodule/2.0.4/pyvmodule-2.0.4.tar.gz

---
+++ packages/pyvmodule/pyvmodule-2.0.4.tar.gz/pyvmodule-2.0.4/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+from distutils.core import setup
+setuptools.setup(
+    name="pyvmodule",
+    version="2.0.4",
+    author="H.Z.Tan",
+    author_email="tanhongze@loongson.cn",
+    description="pyvmodule:an advanded verilog coding toolkit",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tanhongze/pyvmodule",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
