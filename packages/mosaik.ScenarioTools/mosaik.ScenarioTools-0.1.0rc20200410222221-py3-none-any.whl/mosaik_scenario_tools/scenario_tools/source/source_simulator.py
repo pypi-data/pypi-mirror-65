bdc7fcd5c7e07b23757ff0e7a87bd98c4a932d78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:36 +0000

    Add package mosaik.ScenarioTools/0.1.0rc20200410222221/mosaik.ScenarioTools-0.1.0rc20200410222221-py3-none-any.whl

---
+++ packages/mosaik.ScenarioTools/mosaik.ScenarioTools-0.1.0rc20200410222221-py3-none-any.whl/mosaik_scenario_tools/scenario_tools/source/source_simulator.py
@@ -0,0 +1,53 @@
+from mosaik_api import Simulator
+
+from mosaik_scenario_tools.scenario_tools.source.source_model import \
+    SourceModel
+
+
+class SourceSimulator(Simulator):
+    def __init__(self):
+        meta = {
+            'models': {
+                SourceModel.__name__: {
+                    'public': True,
+                    'params': [],
+                    'attrs': [
+                        'my_attribute',  # output
+                    ],
+                },
+            },
+        }
+        super().__init__(meta)
+
+        self._models = []
+
+    def create(self, num, model, **model_params):
+        if num != 1:
+            raise NotImplementedError()
+
+        if model != SourceModel.__name__:
+            raise NotImplementedError()
+
+        if len(model_params) != 0:
+            print(model_params)
+            raise NotImplementedError()
+
+        if len(self._models) != 0:
+            raise NotImplementedError()
+
+        eid = SourceModel.__name__ + '-0'
+        self._models.append(SourceModel(eid=eid))
+
+        return [{'eid': eid, 'type': model}]
+
+    def get_data(self, outputs):
+        for model in self._models:
+            assert model.eid in outputs.keys()
+
+        outputs = {model.eid: model.get_data() for model in self._models}
+
+        return outputs
+
+    def step(self, time, inputs):
+        # Request being scheduled for every time step
+        return time + 1
