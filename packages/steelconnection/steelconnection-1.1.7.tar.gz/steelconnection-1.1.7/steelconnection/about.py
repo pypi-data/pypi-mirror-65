80d8c711ada4b7147c80c86373fa5c17f3f71246
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:14 +0000

    Add package steelconnection/1.1.7/steelconnection-1.1.7.tar.gz

---
+++ packages/steelconnection/steelconnection-1.1.7.tar.gz/steelconnection-1.1.7/steelconnection/about.py
@@ -0,0 +1,13 @@
+"""
+about.py
+"""
+
+name = "steelconnection"
+description = "Simplify access to the Riverbed SteelConnect REST API."
+version = "1.1.7"  # update via bump_version.py
+author = "Greg Mueller"
+author_email = "steelconnection@grelleum.com"
+copyright = "Copyright 2018-2020 Greg Mueller"
+license = "MIT"
+project_home = "https://github.com/grelleum/SteelConnection"
+documentation = "https://steelconnection.readthedocs.io/"
