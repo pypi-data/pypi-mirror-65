852edf3108777ed2bf862a6645c4ebd2c99873e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:28 +0000

    Add package epidemic/0.0.9/epidemic-0.0.9.tar.gz

---
+++ packages/epidemic/epidemic-0.0.9.tar.gz/epidemic-0.0.9/setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup
+
+
+setup(
+  name = 'epidemic',
+  packages = ['epidemic'],
+  version = '0.0.9',
+  license='MIT',
+  description = 'This is a python library that can help predict when will next epidemic happen (relatively). MAY NOT BE ACCURATE',   # Give a short description about your library
+  long_description='See detail description: https://epidemic.readthedocs.io',
+  long_description_content_type="text/markdown",
+  author = 'BOYUAN LIU',
+  author_email = 'boyuanliu6@yahoo.com',
+  url = 'https://github.com/boyuan12/epidemic',
+  download_url = 'https://github.com/boyuan12/epidemic/archive/v0.0.9-alpha.tar.gz',
+  keywords = ['epidemic', 'python', 'health'],
+  install_requires=[
+          'numpy',
+          'matplotlib',
+          'termcolor',
+          'scikit-learn',
+          'requests'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+  ],
+  scripts=['bin/cli']
+)
