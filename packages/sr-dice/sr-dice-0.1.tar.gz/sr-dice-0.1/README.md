d96300234233d1fb69b1dd9a8f841f48d43f4c0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:08 +0000

    Add package sr-dice/0.1/sr-dice-0.1.tar.gz

---
+++ packages/sr-dice/sr-dice-0.1.tar.gz/sr-dice-0.1/README.md
@@ -0,0 +1,19 @@
+# SR-DICE
+is a simple dice simulator for the pen and paper RPG [Shadowrun](https://en.wikipedia.org/wiki/Shadowrun).
+
+## Usage
+```
+sr-dice [-h] [--hist] [--raw] num_dices
+
+positional arguments:
+  num_dices   number of dices to roll
+
+optional arguments:
+  -h, --help  show this help message and exit
+  --hist      print a histogram of the dice rolls
+  --raw       print the raw dice rolls
+```
+
+### Bugs
+Please report to brolf <at> magheute (dot) net
+
---
+++ packages/sr-dice/sr-dice-0.1.tar.gz/sr-dice-0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sr-dice",
+    version="0.1",
+    license='EUPL-1.2',
+    author="Brolf",
+    author_email="brolf@magheute.net",
+    description="A simple dice simulator for Shadowrun",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/br-olf/sr-dice",
+    keywords=['dice','pen and paper','RPG','Shadowrun'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: European Union Public Licence 1.2 (EUPL 1.2)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
