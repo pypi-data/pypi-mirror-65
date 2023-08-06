fb91b95fa6e024a03cc4e71d145e7cefe9b75464
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:29 +0000

    Add package aiojson/0.2/aiojson-0.2.tar.gz

---
+++ packages/aiojson/aiojson-0.2.tar.gz/aiojson-0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+with open("README.rst") as description_file:
+    setup(
+        name="aiojson",
+        version="0.2",
+        description="Simple json request template validator for aiohttp",
+        long_description=description_file.read(),
+        packages=find_packages(),
+        url="https://git.yurzs.dev/yurzs/aiojson",
+    )
