5b0749217d9c6cc774051be85a8a12ab48d4d50b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:54 +0000

    Add package hinpy/0.1.4/hinpy-0.1.4.tar.gz

---
+++ packages/hinpy/hinpy-0.1.4.tar.gz/hinpy-0.1.4/setup.py
@@ -0,0 +1,27 @@
+"""`setup.py`"""
+from setuptools import setup, find_packages
+
+# # Package requirements
+# with open('requirements.txt') as f:
+#     INSTALL_REQUIRES = [l.strip() for l in f.readlines() if l]
+
+INSTALL_REQUIRES=[
+'numpy',
+'scikit-learn',
+'pandas',
+'tqdm',
+'surprise',
+'scipy',
+'scikit-surprise',
+]
+
+setup(name='hinpy',
+      version='0.1.4',
+      description='A framework for the analysis of diversity in Heterogeneous Information Networks.',
+      author='Pedro Ramaciotti Morales',
+      author_email='pedro.ramaciotti@gmail.com',
+      url = 'https://github.com/pedroramaciotti/HINPy',
+      download_url = 'https://github.com/pedroramaciotti/HINPy/archive/0.1.4.tar.gz',
+      keywords = ['Heterogeneous Information Networks','Recommender Systems','Diversity'],
+      packages=find_packages(),
+      install_requires=INSTALL_REQUIRES)
