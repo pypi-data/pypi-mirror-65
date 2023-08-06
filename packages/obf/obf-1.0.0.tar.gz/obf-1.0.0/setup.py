03035da7c06bd810951ede450713c9dc8b3babd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:53 +0000

    Add package obf/1.0.0/obf-1.0.0.tar.gz

---
+++ packages/obf/obf-1.0.0.tar.gz/obf-1.0.0/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open ("obf/VERSION", "r") as fh2:
+    version = fh2.read()
+
+setuptools.setup(
+    name="obf",
+    version=version,
+    author="Hossein Ghodse",
+    author_email="hossein.ghodse@gmail.com",
+    description="an obfuscation tool and library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hossg/obf",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+    entry_points = {
+        'console_scripts': ['obf=obf.commandline:main']
+    },
+    # install_requires=[
+    #       'demjson'
+    # ]
+)
