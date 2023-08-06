fc99740f0acae95c71356b64d20bf12069151ded
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:32 +0000

    Add package amaryllis/1.0.11/amaryllis-1.0.11.tar.gz

---
+++ packages/amaryllis/amaryllis-1.0.11.tar.gz/amaryllis-1.0.11/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='amaryllis',
+    packages=['amaryllis', 'amaryllis.models', 'amaryllis.util', 'amaryllis.survival_rate'], 
+    version='1.0.11',
+    license='MIT', 
+    install_requires=['numpy'], 
+    author='yk-amary-20', 
+    author_email='y.k.83600260@gmail.com', 
+    description='To calculate insurance value', 
+    long_description=long_description, 
+    long_description_content_type='text/markdown',  
+    keywords='amaryllis',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ], 
+)
