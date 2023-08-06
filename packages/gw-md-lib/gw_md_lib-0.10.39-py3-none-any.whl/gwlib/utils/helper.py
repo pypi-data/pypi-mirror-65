91d4d4a45e1a77380eb7d1d9ac17f33dc4f3d72f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:18:05 +0000

    Add package gw-md-lib/0.10.39/gw_md_lib-0.10.39-py3-none-any.whl

---
+++ packages/gw-md-lib/gw_md_lib-0.10.39-py3-none-any.whl/gwlib/utils/helper.py
@@ -0,0 +1,37 @@
+import os
+import smtplib
+from email.mime.text import MIMEText
+
+from passlib.hash import pbkdf2_sha256
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+class Helper:
+
+    @staticmethod
+    def verify_crypt(password, compare_password):
+        print("pass", password, compare_password)
+        return pbkdf2_sha256.verify(password, compare_password)
+
+    @staticmethod
+    def set_crypt(password):
+        print("que pex", password)
+        return pbkdf2_sha256.hash(password)
+
+
+    @staticmethod
+    def send_mail(to, subject, content):
+        mail_user = os.environ.get('MAIL_USER', "")
+        mail_pass = os.environ.get('MAIL_PASS', "groundworx")
+        smtpserver = smtplib.SMTP("email-smtp.us-east-1.amazonaws.com", 587)
+        smtpserver.ehlo()
+        smtpserver.starttls()
+        smtpserver.ehlo()
+        smtpserver.login(mail_user, mail_pass)
+        from_email = 'edominguez@auriz.biz'
+        msg = MIMEText(content)
+        msg['Subject'] = subject
+        msg['From'] = from_email
+        msg['To'] = to
+        smtpserver.sendmail(from_email, [to], msg.as_string())
+        smtpserver.quit()
