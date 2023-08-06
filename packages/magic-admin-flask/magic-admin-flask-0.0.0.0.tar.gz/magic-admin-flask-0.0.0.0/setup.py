6821c585474ef774e8f11c9eb1cfb2ba1f5d4413
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:34 +0000

    Add package magic-admin-flask/0.0.0.0/magic-admin-flask-0.0.0.0.tar.gz

---
+++ packages/magic-admin-flask/magic-admin-flask-0.0.0.0.tar.gz/magic-admin-flask-0.0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='magic-admin-flask',
+    version='0.0.0.0',
+    description='Troll Goat',
+    url='https://magic.link',
+    author='Troll Goat',
+    author_email='arthur@magic.link',
+    license='MIT',
+    packages=['magic_admin_flask'],
+    zip_safe=False,
+)
