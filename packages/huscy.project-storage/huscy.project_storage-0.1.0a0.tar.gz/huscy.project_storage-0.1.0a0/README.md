2d6b87350443a95fe35a41986e1b7e6644cd9761
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:37 +0000

    Add package huscy.project-storage/0.1.0a0/huscy.project_storage-0.1.0a0.tar.gz

---
+++ packages/huscy.project-storage/huscy.project_storage-0.1.0a0.tar.gz/huscy.project_storage-0.1.0a0/README.md
@@ -0,0 +1,2 @@
+huscy.project_storage
+======
---
+++ packages/huscy.project-storage/huscy.project_storage-0.1.0a0.tar.gz/huscy.project_storage-0.1.0a0/setup.py
@@ -0,0 +1,56 @@
+from setuptools import find_namespace_packages, setup
+
+from huscy.project_storage import __version__
+
+
+extras_require = {
+    'development': [
+        'psycopg2-binary',
+    ],
+    'docs': [
+        'django-extensions',
+        'pydot',
+    ],
+    'testing': [
+        'tox',
+        'watchdog==0.9',
+    ],
+}
+
+install_requires = [
+    'Django>=2.0',
+    'djangorestframework>=3.10',
+
+    'huscy.projects',
+]
+
+
+setup(
+    name='huscy.project_storage',
+    version=__version__,
+    license='AGPLv3+',
+
+    author='Khaled Bahlawan',
+    author_email='Khaled_Engineer92@hotmail.com',
+
+    packages=find_namespace_packages(),
+
+    install_requires=install_requires,
+    extras_require=extras_require,
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Framework :: Django',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
+    ],
+)
