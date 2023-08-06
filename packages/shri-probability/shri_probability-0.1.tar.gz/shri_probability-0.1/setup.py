70c03bfea53ea64fc463eddd6e9e1b4dea1dae50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:16:13 +0000

    Add package shri-probability/0.1/shri_probability-0.1.tar.gz

---
+++ packages/shri-probability/shri_probability-0.1.tar.gz/shri_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='shri_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['shri_probability'],
+      author = 'Shridhar Sundaram',
+      author_email = 'myemail@gmail.com',
+      zip_safe=False)
