4adcf39fe6cb61c43dd7313ed32ea3695f573a82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:16:56 +0000

    Add package bsv/0.4.2/bsv-0.4.2.tar.gz

---
+++ packages/bsv/bsv-0.4.2.tar.gz/bsv-0.4.2/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='bsv',
+    version='0.4.2',
+    description='spider project to get buy_sell_volume from 4 Futures Exchanges in Chinese Mainland',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitee.com/ragnaros27/bsv",
+    packages=setuptools.find_packages(),
+    author='ragnaros27',
+    author_email='gooddunk@163.com',
+    project_urls={"Code": "https://gitee.com/ragnaros27/bsv",},
+    install_requires=['beautifulsoup4==4.8.1',
+                      'lxml==4.4.2',
+                      'pandas==0.25.3',
+                      'requests==2.22.0',
+                      ],
+    python_requires='>=3.6',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
