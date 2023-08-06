e12de905c2dc64b15aa2667faaa026988706be4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:02 +0000

    Add package awesome-check-link/0.1.1/awesome-check-link-0.1.1.tar.gz

---
+++ packages/awesome-check-link/awesome-check-link-0.1.1.tar.gz/awesome-check-link-0.1.1/test/test_aclinks.py
@@ -0,0 +1,12 @@
+import aclinks
+
+class TestAclinks:
+    def test_extractLink(self):
+        pass
+
+    def test_getDownLinks(self):
+        pass
+
+    def test_getAllStatus(self):
+        pass
+
