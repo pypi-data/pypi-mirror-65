8a21d89163b34162592d6ba1625dc8393a3f8cf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:22 +0000

    Add package sopel-help/0.1.0/sopel_help-0.1.0-py3-none-any.whl

---
+++ packages/sopel-help/sopel_help-0.1.0-py3-none-any.whl/sopel_help/config.py
@@ -0,0 +1,37 @@
+"""Configuration for the help plugin."""
+from sopel import config
+
+from sopel_help.managers import manager
+
+
+class HelpSection(config.types.StaticSection):
+    """Configuration section for this module."""
+    REPLY_METHODS = [
+        'channel',
+        'query',
+        'notice',
+    ]
+
+    output = config.types.ChoiceAttribute('output',
+                                          manager.provider_names,
+                                          default='base')
+    """The help provider to use for output."""
+    reply_method = config.types.ChoiceAttribute('reply_method',
+                                                REPLY_METHODS,
+                                                default='channel')
+    """Where/how to reply to help commands (public/private)."""
+
+    origin_base_url = config.types.ValidatedAttribute(
+        'origin_base_url',
+        default='')
+    """When using an origin server, what is the base URL to use."""
+
+    origin_output_name = config.types.ValidatedAttribute(
+        'origin_output_name',
+        default='help.html')
+    """How is named the file that will be used to store and publish content."""
+
+    origin_output_dir = config.types.ValidatedAttribute(
+        'origin_output_dir',
+        default='/var/www/html')
+    """Where the file will be put on the server to publish the content."""
