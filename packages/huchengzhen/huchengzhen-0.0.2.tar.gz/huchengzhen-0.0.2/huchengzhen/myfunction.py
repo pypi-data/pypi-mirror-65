b1a48b637cddde6419b8e4a371ea174ac9882228
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:31 +0000

    Add package huchengzhen/0.0.2/huchengzhen-0.0.2.tar.gz

---
+++ packages/huchengzhen/huchengzhen-0.0.2.tar.gz/huchengzhen-0.0.2/huchengzhen/myfunction.py
@@ -0,0 +1,6 @@
+def mysum(*args):
+    s = 0
+    for v in args:
+        i = float(v)
+        s += i
+    print(s)
---
+++ packages/huchengzhen/huchengzhen-0.0.2.tar.gz/huchengzhen-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="huchengzhen",
+    version="0.0.2",
+    keywords=("pip", "testpypi"),
+    description="test pip module",
+    long_description="test how to define pip module and upload to pypi",
+    license="MIT",
+
+    url="https://huchengzhen.com",  # your module home page, such as
+    author="Hu Chengzhen",  # your name
+    author_email="huchengzhen@gmail.com",  # your email
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=[]
+)
