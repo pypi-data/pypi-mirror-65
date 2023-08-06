306153c04e04852c2956ef0f6b0c8fd69142a87e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:22 +0000

    Add package dxseq/0.0/dxseq-0.0.tar.gz

---
+++ packages/dxseq/dxseq-0.0.tar.gz/dxseq-0.0/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+  name = 'dxseq',
+  packages = ['dxseq'],
+  version = '0.0',
+  license='MIT',
+  description = 'Massively Parallel Diagnostic Assay Design and Sequencing',
+  author = 'Ayaan Hossain',
+  author_email = 'auh57@psu.edu',
+  url = 'https://github.com/ayaanhossain/dxseq',
+  download_url = '',
+  keywords = [],
+  install_requires=[],
+
+  # Arbitrary Classification for Now
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
