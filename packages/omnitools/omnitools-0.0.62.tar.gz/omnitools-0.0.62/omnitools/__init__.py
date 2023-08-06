95cc1b619904d7d3ee30d891523a81a2bcaa9326
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:27 +0000

    Add package omnitools/0.0.62/omnitools-0.0.62.tar.gz

---
+++ packages/omnitools/omnitools-0.0.62.tar.gz/omnitools-0.0.62/omnitools/__init__.py
@@ -0,0 +1,10 @@
+from .stdout import *
+from .xtypes import *
+from .encoding import *
+from .hashing import *
+from .rng import *
+from .js import *
+
+version = "0.0.62"
+keywords = ["omnitools utilities"]
+entry = "test:main"
