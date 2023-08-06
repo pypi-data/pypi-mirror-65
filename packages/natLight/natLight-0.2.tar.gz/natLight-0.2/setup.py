cad5904ca36b344d8fff37a9fc5bc010427bfdbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:21 +0000

    Add package natLight/0.2/natLight-0.2.tar.gz

---
+++ packages/natLight/natLight-0.2.tar.gz/natLight-0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+	name='natLight',
+	version='0.2',
+	description='Natural Ligth to RGB Converter',
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url='https://gitlab.com/stroblme/naturalLight.git',
+	author='Melvin Strobl',
+	packages=find_packages(),
+	python_requires='>=3.6'
+)
