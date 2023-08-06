cec077c96206df5a30f6ae79573dab276c8e3f16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:30 +0000

    Add package pandasbt/0.1.1/pandasbt-0.1.1.tar.gz

---
+++ packages/pandasbt/pandasbt-0.1.1.tar.gz/pandasbt-0.1.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as r:
+    long_description = r.read()
+
+setuptools.setup(
+    name="pandasbt",
+    version="0.1.1",
+    author="Janderson FFerreira",
+    author_email="ffjanderson@gmail.com",
+    description="Simple framework to test your strategies ideias with pandas",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Janderson/pandasbt",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ],
+    python_requires='>=3.6',
+)
