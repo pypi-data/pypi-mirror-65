5d37e229fd786fe48d6c89f0a4c3d2a7512c0293
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:42 +0000

    Add package pokemon/0.35/pokemon-0.35.tar.gz

---
+++ packages/pokemon/pokemon-0.35.tar.gz/pokemon-0.35/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import os
+
+with open('README.md') as filey:
+    LONG_DESCRIPTION = filey.read()
+
+setup(
+    # Application name:
+    name="pokemon",
+
+    # Version number:
+    version="0.35",
+
+    # Application author details:
+    author="Vanessa Sochat",
+    author_email="vsochat@stanford.edu",
+
+    # Packages
+    packages=find_packages(),
+
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+
+    # Data files
+    include_package_data=True,
+    zip_safe=False,
+
+    # Details
+    url="http://www.github.com/vsoch/pokemon",
+    license="LICENSE",
+    description="ascii database of pokemon... in python!",
+    keywords="pokemon, avatar, ascii, gravatar",
+    entry_points={"console_scripts": ["pokemon=pokemon.scripts:main",],},
+)
