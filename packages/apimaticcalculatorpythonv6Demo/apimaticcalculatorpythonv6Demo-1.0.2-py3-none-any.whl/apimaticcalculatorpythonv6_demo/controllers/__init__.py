474ebd142c73e64c81ad438b2cc963f5deb949b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:52 +0000

    Add package apimaticcalculatorpythonv6Demo/1.0.2/apimaticcalculatorpythonv6Demo-1.0.2-py3-none-any.whl

---
+++ packages/apimaticcalculatorpythonv6Demo/apimaticcalculatorpythonv6Demo-1.0.2-py3-none-any.whl/apimaticcalculatorpythonv6_demo/controllers/__init__.py
@@ -0,0 +1,4 @@
+__all__ = [
+    'base_controller',
+    'calculator_dev_ops_conf',
+]
---
+++ packages/apimaticcalculatorpythonv6Demo/apimaticcalculatorpythonv6Demo-1.0.2-py3-none-any.whl/apimaticcalculatorpythonv6_demo/models/operation_type.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+"""
+    apimaticcalculatorpythonv6_demo
+
+    This file was automatically generated for testing by APIMATIC v2.0 ( https://apimatic.io ).
+"""
+
+class OperationType(object):
+
+    """Implementation of the 'Operation Type' enum.
+
+    Possible operators are sum, subtract, multiply, divide
+
+    Attributes:
+        SUM: Represents the sum operator
+        SUBTRACT: Represents the subtract operator
+        MULTIPLY: Represents the multiply operator
+        DIVIDE: Represents the divide operator
+
+    """
+
+    SUM = 'SUM'
+
+    SUBTRACT = 'SUBTRACT'
+
+    MULTIPLY = 'MULTIPLY'
+
+    DIVIDE = 'DIVIDE'
+
