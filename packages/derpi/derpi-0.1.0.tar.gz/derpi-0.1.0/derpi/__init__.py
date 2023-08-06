93758bcb77f5d303e41accfcf4d780df8f6ec140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:11 +0000

    Add package derpi/0.1.0/derpi-0.1.0.tar.gz

---
+++ packages/derpi/derpi-0.1.0.tar.gz/derpi-0.1.0/derpi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+__author__ = 'luckydonald'
+
+__version__ = "0.1.0"
+VERSION = __version__
+
+import logging
+logger = logging.getLogger(__name__)
+logger.debug(f'derpi version: {VERSION}')
