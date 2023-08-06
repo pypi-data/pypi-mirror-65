5071b3d893afa0b14d5bd07fe7f1ef2778d09674
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:08 +0000

    Add package lob-pyutil/5.0.0/lob-pyutil-5.0.0.tar.gz

---
+++ packages/lob-pyutil/lob-pyutil-5.0.0.tar.gz/lob-pyutil-5.0.0/pyutil/cvx/util.py
@@ -0,0 +1,15 @@
+import cvxpy as cvx
+
+
+class Solver(object):
+    def __init__(self, problem):
+        assert isinstance(problem, cvx.Problem)
+        self.problem = problem
+        self.parameters = {a.name(): a for a in problem.parameters()}
+        self.variables = {a.name(): a for a in problem.variables()}
+
+    def solve(self, *args, **kwargs):
+        self.problem.solve(*args, **kwargs)
+        # Check that the solver computed an optimal solution
+        assert self.problem.status == "optimal", "Couldn't find an optimal solution. Status {s}".format(
+            s=self.problem.status)
