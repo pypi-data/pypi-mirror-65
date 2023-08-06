7865728c868b7a7e6be9a6bff09216945228e50a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:54 +0000

    Add package rinki-reader/1.0.0/rinki-reader-1.0.0.tar.gz

---
+++ packages/rinki-reader/rinki-reader-1.0.0.tar.gz/rinki-reader-1.0.0/README.md
@@ -0,0 +1,3 @@
+The Real Python Feed Reader is a basic web feed reader that can download the latest Real Python tutorials from the Real Python feed.
+
+For more information see the tutorial How to Publish an Open-Source Python Package to PyPI on Real Python.
---
+++ packages/rinki-reader/rinki-reader-1.0.0.tar.gz/rinki-reader-1.0.0/reader/feed.py
@@ -0,0 +1,10 @@
+import feedparser
+import html2text
+
+_CACHED_FEEDS = dict()
+
+def _feed(url):
+    """Only read a feed once, by caching its contents"""
+    if url not in _CACHED_FEEDS:
+        _CACHED_FEEDS[url] = feedparser.parse(url)
+    return _CACHED_FEEDS[url]
