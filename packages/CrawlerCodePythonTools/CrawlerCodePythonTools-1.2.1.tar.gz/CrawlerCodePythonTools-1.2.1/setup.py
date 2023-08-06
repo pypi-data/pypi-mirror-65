5ef008979b7a70dc3b07c5590a36ff2e9dadecbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:20 +0000

    Add package CrawlerCodePythonTools/1.2.1/CrawlerCodePythonTools-1.2.1.tar.gz

---
+++ packages/CrawlerCodePythonTools/CrawlerCodePythonTools-1.2.1.tar.gz/CrawlerCodePythonTools-1.2.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+def readme():
+    with open("README.rst") as f:
+        README = f.read()
+    return README
+
+
+TYPE = "CORE"  # "CORE" "GUI" "WEBBOT"
+
+packages = ['pythontools.core', 'pythontools.identity', 'pythontools.sockets', 'pythontools.dev']
+install_requires = ["colorama"]
+if TYPE == "GUI": packages.append('pythontools.gui') and install_requires.append('PyQt5')
+if TYPE == "WEBBOT": packages.append('pythontools.webbot') and install_requires.append('selenium')
+
+setup(
+    name='CrawlerCodePythonTools' + ('-Gui' if TYPE == "GUI" else '-WebBot' if TYPE == "WEBBOT" else ''),
+    version='1.2.1',
+    packages=packages,
+    url='https://github.com/CrawlerCode',
+    license='',
+    author='CrawlerCode',
+    author_email='',
+    description='',
+    long_description=readme(),
+    long_description_content_type="text/x-rst",
+    include_package_data=True,
+    install_requires=install_requires
+)
