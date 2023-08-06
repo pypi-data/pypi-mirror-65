779d13f83f3155b3cef6bbb83e39172d9a49c337
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:59 +0000

    Add package fars-cleaner/1.1.0/fars_cleaner-1.1.0.tar.gz

---
+++ packages/fars-cleaner/fars_cleaner-1.1.0.tar.gz/fars_cleaner-1.1.0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fars_cleaner",
+    version="1.1.0",
+    author="Mitchell Abrams",
+    author_email="mitchell.abrams@duke.edu",
+    description="A python package for loading and preprocessing the FARS database",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mzabrams/fars-cleaner",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'pooch',
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+    ),
+)
