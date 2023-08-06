4ac1d79fa860e37278c4decc428b8417cafbef73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:25 +0000

    Add package vsucsprime2/0.2/vsucsprime2-0.2.tar.gz

---
+++ packages/vsucsprime2/vsucsprime2-0.2.tar.gz/vsucsprime2-0.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+ 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+ 
+setup(
+    name='vsucsprime2',
+    version='0.2',
+    description='Demo for building a Python project',
+    author='Lin Chen',
+    author_email='lichen@valdosta.edu',
+    url='http://lin-chen-va.github.io',
+    install_requires=requirements,
+    packages=['primepackage', ],
+    package_dir={'':'src'},
+    scripts=['src/generator',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README').read(),
+    classifiers=[
+      'Development Status :: 4 - Beta',
+      'Environment :: X11 Applications :: GTK',
+      'Intended Audience :: End Users/Desktop',
+      'Intended Audience :: Developers',
+      'License :: OSI Approved :: GNU General Public License (GPL)',
+      'Operating System :: POSIX :: Linux',
+      'Programming Language :: Python',
+      'Topic :: Desktop Environment',
+      'Topic :: Text Processing :: Fonts'
+      ],
+)
