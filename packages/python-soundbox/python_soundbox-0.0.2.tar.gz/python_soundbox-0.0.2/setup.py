c506995a5adda6b00bea44ef3f4a4745dd74b0f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:59 +0000

    Add package python-soundbox/0.0.2/python_soundbox-0.0.2.tar.gz

---
+++ packages/python-soundbox/python_soundbox-0.0.2.tar.gz/python_soundbox-0.0.2/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="python_soundbox",
+    version="0.0.2",
+    author="Zhe Wang",
+    author_email="wangzhetju@gmail.com",
+    description="Python and sound tools",
+    long_description="Python and sound tools",
+    long_description_content_type="text/markdown",
+    url="https://github.com/wangzhe3224/pysound",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
