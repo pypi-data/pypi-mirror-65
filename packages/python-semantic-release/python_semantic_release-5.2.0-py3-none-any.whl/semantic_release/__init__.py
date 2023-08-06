cbc78b2b0e24ac82b911a56a2fb5aa70c543de77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:17:05 +0000

    Add package python-semantic-release/5.2.0/python_semantic_release-5.2.0-py3-none-any.whl

---
+++ packages/python-semantic-release/python_semantic_release-5.2.0-py3-none-any.whl/semantic_release/__init__.py
@@ -0,0 +1,18 @@
+"""Semantic Release
+"""
+__version__ = '5.2.0'
+
+
+from .errors import UnknownCommitMessageStyleError  # noqa
+from .errors import ImproperConfigurationError, SemanticReleaseBaseError  # noqa
+
+
+def setup_hook(argv: list):
+    """
+    A hook to be used in setup.py to enable `python setup.py publish`.
+
+    :param argv: sys.argv
+    """
+    if len(argv) > 1 and argv[1] in ['version', 'publish', 'changelog']:
+        from .cli import main
+        main()
