239c7366ddd96839d76acf090941c5f103ae3cd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:10 +0000

    Add package unique-color/1.0/unique_color-1.0.tar.gz

---
+++ packages/unique-color/unique_color-1.0.tar.gz/unique_color-1.0/README.md
@@ -0,0 +1,5 @@
+[![DOI](https://zenodo.org/badge/DOI/10.5281/zenodo.3746984.svg)](https://doi.org/10.5281/zenodo.3746984)
+
+
+# unique_color
+These are 35 unique colors
---
+++ packages/unique-color/unique_color-1.0.tar.gz/unique_color-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='unique_color',
+    version='1.0',
+    author='Yichao Li',
+    author_email='yl079811@ohio.edu',
+    url='https://github.com/YichaoOU/unique_color',
+	packages=['unique_color'],
+    license='LICENSE',
+    description='Privide 33 unique colors in hex or rgb format',
+)
