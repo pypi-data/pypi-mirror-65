5f04b7d806f03f55938928ae4622a53b16811c87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:34 +0000

    Add package pmlab-lite/0.3.9.4/pmlab_lite-0.3.9.4-py3-none-any.whl

---
+++ packages/pmlab-lite/pmlab_lite-0.3.9.4-py3-none-any.whl/pmlab_lite/alignments/variables.py
@@ -0,0 +1,11 @@
+#will contain some variables
+
+open_list = []
+closed_list = []
+solutions = []
+
+#storing the information of the synchronous Product Net
+incidence_matrix = []
+init_mark_vector = [] 
+final_mark_vector = []
+transitions_by_index = dict()
