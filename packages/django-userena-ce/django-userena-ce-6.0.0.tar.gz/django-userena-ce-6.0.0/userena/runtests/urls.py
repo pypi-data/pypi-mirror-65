4d739b1d1613f9101f90bcfd2f9ce9c3c96d73bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:44 +0000

    Add package django-userena-ce/6.0.0/django-userena-ce-6.0.0.tar.gz

---
+++ packages/django-userena-ce/django-userena-ce-6.0.0.tar.gz/django-userena-ce-6.0.0/userena/runtests/urls.py
@@ -0,0 +1,24 @@
+from django.conf import settings
+from django.conf.urls import url, include
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+admin.autodiscover()
+
+urlpatterns = [
+    url(r"^admin/doc/", include("django.contrib.admindocs.urls")),
+    url(r"^admin/", admin.site.urls),
+    # Demo Override the signup form with our own, which includes a
+    # first and last name.
+    # (r'^accounts/signup/$',
+    #  'userena.views.signup',
+    #  {'signup_form': SignupFormExtra}),
+    url(r"^accounts/", include("userena.urls")),
+    url(r"^messages/", include("userena.contrib.umessages.urls")),
+    url(r"^i18n/", include("django.conf.urls.i18n")),
+]
+
+# Add media and static files
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
