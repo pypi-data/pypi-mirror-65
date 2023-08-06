d62529e2aeb41d6b8cf00c46c704e4e91d8c11cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:55 +0000

    Add package GBdistributions-ML/0.1/GBdistributions_ML-0.1.tar.gz

---
+++ packages/GBdistributions-ML/GBdistributions_ML-0.1.tar.gz/GBdistributions_ML-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='GBdistributions_ML',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['GBdistributions_ML'],
+      zip_safe=False)
