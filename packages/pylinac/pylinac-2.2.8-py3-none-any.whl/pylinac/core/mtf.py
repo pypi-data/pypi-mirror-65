957cded6278694ddf6171711702d53cb7c8ad399
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:04 +0000

    Add package pylinac/2.2.8/pylinac-2.2.8-py3-none-any.whl

---
+++ packages/pylinac/pylinac-2.2.8-py3-none-any.whl/pylinac/core/mtf.py
@@ -0,0 +1,16 @@
+
+import numpy as np
+
+
+class MTF:
+
+    def __init__(self, lp_spacings, lp_maximums, lp_minimums, norm=None):
+        pass
+
+    def calc_at(self, x=50):
+        pass
+
+    @classmethod
+    def from_highcontrast_diskset(cls, diskset):
+        pass
+
