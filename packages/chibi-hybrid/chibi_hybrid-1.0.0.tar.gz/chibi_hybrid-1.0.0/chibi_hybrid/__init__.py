702a959f3944ee396029acc28bb111eedfb9d9c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:23 +0000

    Add package chibi-hybrid/1.0.0/chibi_hybrid-1.0.0.tar.gz

---
+++ packages/chibi-hybrid/chibi_hybrid-1.0.0.tar.gz/chibi_hybrid-1.0.0/chibi_hybrid/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from .chibi_hybrid import Chibi_hybrid
+
+
+__all__ = [ 'Chibi_hybrid' ]
+
+__author__ = """Dem4ply"""
+__email__ = 'dem4ply@gmail.com'
+__version__ = '1.0.0'
