7226879c1b71370ec5cf5c20ae424fbd1b5b6999
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:30 +0000

    Add package rejected/3.20.7/rejected-3.20.7-py2.py3-none-any.whl

---
+++ packages/rejected/rejected-3.20.7-py2.py3-none-any.whl/rejected/__init__.py
@@ -0,0 +1,34 @@
+"""
+Rejected is a Python RabbitMQ Consumer Framework and Controller Daemon
+
+"""
+import logging
+
+# Add NullHandler to prevent logging warnings
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+from rejected.consumer import (
+    Consumer,
+    ConsumerException,
+    MessageException,
+    ProcessingException,
+    PublishingConsumer,
+    SmartConsumer,
+    SmartPublishingConsumer)  # noqa E402
+
+__author__ = 'Gavin M. Roy <gavinmroy@gmail.com>'
+__since__ = '2009-09-10'
+__version__ = '3.20.7'
+
+__all__ = [
+    '__author__',
+    '__since__',
+    '__version__',
+    'Consumer',
+    'ConsumerException',
+    'MessageException',
+    'ProcessingException',
+    'PublishingConsumer',
+    'SmartConsumer',
+    'SmartPublishingConsumer'
+]
