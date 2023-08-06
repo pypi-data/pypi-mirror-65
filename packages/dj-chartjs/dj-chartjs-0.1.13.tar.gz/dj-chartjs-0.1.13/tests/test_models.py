5fd7a3ba0c74cf5df9c2e824b771f32cd830e1a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:52 +0000

    Add package dj-chartjs/0.1.13/dj-chartjs-0.1.13.tar.gz

---
+++ packages/dj-chartjs/dj-chartjs-0.1.13.tar.gz/dj-chartjs-0.1.13/tests/test_models.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_django-charts
+------------
+
+Tests for `django-charts` models module.
+"""
+
+from django.test import TestCase
+
+from dj_chartjs import models
+
+
+class TestDj_chartjs(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
