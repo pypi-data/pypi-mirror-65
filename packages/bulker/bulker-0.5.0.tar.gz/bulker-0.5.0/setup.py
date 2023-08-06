76f2cfdb51721b752d064d5171b42058fbde4865
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:56 +0000

    Add package bulker/0.5.0/bulker-0.5.0.tar.gz

---
+++ packages/bulker/bulker-0.5.0.tar.gz/bulker-0.5.0/setup.py
@@ -0,0 +1,70 @@
+#! /usr/bin/env python
+
+import os
+from setuptools import setup
+import sys
+
+PACKAGE = "bulker"
+
+# Additional keyword arguments for setup().
+extra = {}
+
+# Ordinary dependencies
+DEPENDENCIES = []
+with open("requirements/requirements-all.txt", "r") as reqs_file:
+    for line in reqs_file:
+        if not line.strip():
+            continue
+        DEPENDENCIES.append(line)
+
+extra["install_requires"] = DEPENDENCIES
+
+with open("{}/_version.py".format(PACKAGE), 'r') as versionfile:
+    version = versionfile.readline().split()[-1].strip("\"'\n")
+
+
+
+# Handle the pypi README formatting.
+try:
+    import pypandoc
+    long_description = pypandoc.convert_file('README.md', 'rst')
+    msg = "\033[032mPandoc conversion succeeded.\033[0m"
+except(IOError, ImportError, OSError):
+    msg = "\033[0;31mWarning: pandoc conversion failed!\033[0m"
+    long_description = open('README.md').read()
+
+setup(
+    name=PACKAGE,
+    packages=[PACKAGE],
+    version=version,
+    description="A manager for containerized executables",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: System :: Distributed Computing"
+    ],
+    keywords="docker, containers, reproducibility, bioinformatics, workflow",
+    url="https://bulker.databio.org",
+    author=u"Nathan Sheffield",
+    author_email=u"nathan@code.databio.org", 
+    license="BSD2",
+    entry_points={
+        "console_scripts": [
+            'bulker = bulker.bulker:main'
+        ],
+    },    
+    package_data={"bulker": [os.path.join("bulker", "*")]},
+    include_package_data=True,
+    test_suite="tests",
+    tests_require=(["mock", "pytest"]),
+    setup_requires=(["pytest-runner"] if {"test", "pytest", "ptr"} & set(sys.argv) else []),
+    **extra
+)
+
+
+print(msg)
