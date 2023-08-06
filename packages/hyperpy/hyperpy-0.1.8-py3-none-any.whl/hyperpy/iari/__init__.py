6a37a0665bf70b6fe5242d5b91f1dc0d7a4d545f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:36 +0000

    Add package hyperpy/0.1.8/hyperpy-0.1.8-py3-none-any.whl

---
+++ packages/hyperpy/hyperpy-0.1.8-py3-none-any.whl/hyperpy/iari/__init__.py
@@ -0,0 +1,4 @@
+from .iari import renameh
+from .iari import unziph
+from .iari import raw2nph
+from .iari import pick_hyperdir
---
+++ packages/hyperpy/hyperpy-0.1.8-py3-none-any.whl/hyperpy/visualize.py
@@ -0,0 +1,16 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+def imshow_gray(img):
+	'''
+	returns: image ploted in gray levels
+	parameters: image array matrix 2 dimensional single band
+	'''
+	plt.imshow(img)
+	plt.show()
+
+def imshow_hist(img):
+	'''
+
+	'''
+	pass
