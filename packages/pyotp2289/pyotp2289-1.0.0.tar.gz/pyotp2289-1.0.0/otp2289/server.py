11d1d286b63c95bac5e1ac4a697b69ade2669949
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:37 +0000

    Add package pyotp2289/1.0.0/pyotp2289-1.0.0.tar.gz

---
+++ packages/pyotp2289/pyotp2289-1.0.0.tar.gz/pyotp2289-1.0.0/otp2289/server.py
@@ -0,0 +1,445 @@
+# -*- coding: utf-8 -*-
+# SPDX-License-Identifier: BSD-2-Clause-FreeBSD
+#
+# Copyright (c) 2020, Simeon Simeonov
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""A pure Python implementation of the RFC-2289 OTP server"""
+import binascii
+import hashlib
+
+from .generator import (OTP_ALGO_MD5,
+                        OTPGenerator,
+                        OTPGeneratorException)
+
+
+class OTPStateException(Exception):
+    """OTPStateException class"""
+
+
+class OTPStoreException(Exception):
+    """OTPStoreException class"""
+
+
+class OTPInvalidResponse(Exception):
+    """OTPInvalidResponse class"""
+
+
+class OTPState:
+    """
+    OTPState class
+
+    The OTPState class represents a single state on the server side that can:
+    - generate a challenge
+    - validate the corresponding generated response from the generator
+    """
+
+    def __init__(self, ot_hex, current_step, seed, hash_algo=OTP_ALGO_MD5):
+        """
+        Constructs an OTPState object with the given arguments.
+
+        Keyword Arguments:
+        :param ot_hex: The one-time hex from the last successful authentication
+                       or None for a newly initialized sequence.
+        :type ot_hex: str or None
+
+        :param current_step: The current step that is sent with the challenge
+        :type current_step: int
+
+        :param seed: The seed that is sent with the challenge
+        :type seed: str
+
+        :param hash_algo: The hash algo, defaults to OTP_ALGO_MD5
+        :type hash_algo: int or str
+
+        :raises otp2289.OTPStateException: If the input does not validate
+        """
+        # enforce the rfc2289 constraints
+        try:
+            self._seed = OTPGenerator.validate_seed(seed)
+            self._hash_algo = OTPGenerator.validate_hash_algo(hash_algo)
+            self._step = OTPGenerator.validate_step(current_step)
+        except OTPGeneratorException as exp:
+            raise OTPStateException(exp.args[0])
+        self._current_digest = None
+        if ot_hex is not None:
+            self._current_digest = self.validate_hex(ot_hex)
+        self._new_digest_hex = None  # set upon a successful validation
+
+    def __repr__(self):
+        """repr implementation"""
+        return (f'{self.__class__} at {id(self)} '
+                f'(ot_hex={self._current_digest}, current_step={self._step}, '
+                f'seed={self._seed}, '
+                f'hash_algo={self._hash_algo})')
+
+    @property
+    def challenge_string(self):
+        """challenge_string-property"""
+        # RFC-2289: "...the entire challenge string MUST be
+        # terminated with either a space or a new line."
+        return f'otp-{self._hash_algo} {self._step} {self._seed} '
+
+    @property
+    def current_digest(self):
+        """current_digest-property"""
+        return self._current_digest
+
+    @property
+    def hash_algo(self):
+        """hash_algo-property"""
+        return self._hash_algo
+
+    @property
+    def seed(self):
+        """seed-property"""
+        return self._seed
+
+    @property
+    def step(self):
+        """step-property"""
+        return self._step
+
+    @property
+    def validated(self):
+        """validated-property"""
+        return bool(self._new_digest_hex)
+
+    @classmethod
+    def from_dict(cls, dict_obj):
+        """
+        Returns an OTPState object from the dict-object
+
+        :param dict_obj: The dict object
+        :type dict_obj: dict
+
+        :return: A new OTPState object
+        :rtype: otp2289.OTPStore
+        """
+        return cls(**dict_obj)
+
+    @staticmethod
+    def response_to_bytes(response):
+        """
+        A wrapper that handles/validates the response as specified by RFC-2289.
+
+        The method first checks if response is a token and tries to convert
+        it to bytes. If that fails, the method assumes that response is a hex.
+        If neither of those attempts succeeds OTPInvalidResponse is raised.
+        It is up to the caller to run another iteration and compare the result
+        to an existing digest in this state.
+
+        :param response: The response to this state (its challenge)
+        :type response: str
+
+        :raises otp2289.OTPInvalidResponse: If the response is corrupt/illegal,
+                                            but not if it simply does not
+                                            validate
+
+        :return: The bytes representation of response (if any)
+        :rtype: bytes
+        """
+        try:
+            return OTPGenerator.tokens_to_bytes(response)
+        except OTPGeneratorException:
+            # now assume hex...
+            try:
+                return OTPState.validate_hex(response)
+            except OTPStateException:
+                raise OTPInvalidResponse(
+                    'The response is neither a valid token or hex')
+
+    @staticmethod
+    def validate_hex(ot_hex):
+        """
+        Validates the provided hexidigest.
+
+        :param ot_hex: The one-time hex to validate
+        :type ot_hex: str
+
+        :raises otp2289.OTPStateException: If hex does not validate
+
+        :return: The validated hex (without leading 0x) converted to bytes
+        :rtype: bytes
+        """
+        if not isinstance(ot_hex, str):
+            raise OTPStateException('OT-hex must be a str')
+        if ot_hex.startswith('0x'):
+            ot_hex = ot_hex[2:]
+            ot_hex = ot_hex.strip().lower()
+        if len(ot_hex) != 16:
+            raise OTPStateException('The length of the hex should be 16 '
+                                    '(representing 64 bits digest)')
+        try:
+            return binascii.unhexlify(ot_hex)
+        except binascii.Error:
+            raise OTPStateException('Invalid OT-hex')
+
+    def get_next_state(self):
+        """
+        Returns the next state for a validated OTPState.
+
+        This is a brand new OTPState object with the same hash_algo and seed
+        where step -= 1 and ot_hex = self._new_digest_hex
+
+        :return: The next OTPState if validated, None otherwise
+        :rtype: otp2289.OTPState or None
+        """
+        if self._new_digest_hex is None:
+            return None
+        return OTPState(self._new_digest_hex,
+                        self._step - 1,
+                        self._seed,
+                        self._hash_algo)
+
+    def response_validates(self, response, store_valid_response=True):
+        """
+        Validates the incoming response as specified by RFC-2289.
+
+        :param response: The response to this state (its challenge)
+        :type response: str
+
+        :param store_valid_response: Should a valid response be stored
+        :type store_valid_response: bool
+
+        :raises otp2289.OTPInvalidResponse: If the response does not match
+                                            this state
+
+        :return: Returns True if response validates, False otherwise
+        :rtype: bool
+        """
+        # self.response_to_bytes raises OTPInvalidResponse in case response
+        # is corrupt or in a wrong format
+        response_bytes = self.response_to_bytes(response)
+        if self._hash_algo == 'md5':
+            digest = hashlib.md5(response_bytes).digest()
+            if (
+                    self._current_digest is None or
+                    OTPGenerator.strxor(digest[0:8], digest[8:]) ==
+                    self._current_digest
+            ):
+                if store_valid_response:
+                    self._new_digest_hex = binascii.hexlify(
+                        response_bytes).decode()
+                return True
+            return False
+        if self._hash_algo == 'sha1':
+            digest = hashlib.sha1(response_bytes).digest()
+            if (
+                    self._current_digest is None or
+                    OTPGenerator.sha1_digest_folding(
+                        hashlib.sha1(
+                            response_bytes).digest()) == self._current_digest
+            ):
+                if store_valid_response:
+                    self._new_digest_hex = binascii.hexlify(
+                        response_bytes).decode()
+                return True
+            return False
+        # this should not happen since the hash_algo is validated by the caller
+        raise OTPInvalidResponse(f'Ivalid hash_algo: {self._hash_algo}')
+
+    def to_dict(self):
+        """
+        Returns a dict representation of the object.
+
+        This could be the base for a JSON serialization.
+
+        :return: The dict representation of the object
+        :rtype: dict
+        """
+        ot_hex = self._current_digest
+        if ot_hex is not None:
+            ot_hex = binascii.hexlify(self._current_digest).decode()
+        return {'ot_hex': ot_hex,
+                'current_step': self._step,
+                'seed': self._seed,
+                'hash_algo': self._hash_algo}
+
+
+class OTPStore:
+    """
+    OTPStore class
+
+    A helper / container class that stores OTPState objects in a 2 layered
+    dict structure represented by [domain][key].
+
+    The class could serve as a base class when implementing store backends.
+    """
+    def __init__(self, data=None):
+        """
+        Constructs an OTPStore object from data
+
+        :param data: The data object, defaults to None
+        :type data: object or None
+        """
+        self._data = {}  # {key1: {state1-data...}, key2: {state2-data...}}
+        self._states = {}  # OTPState: (domain, key) - dict
+        if data is not None:
+            self._add_data(data)
+
+    def __contains__(self, state):
+        """membership test"""
+        return state in self._states
+
+    def __iter__(self):
+        """iterator for OTPStore"""
+        return iter(self._data)
+
+    def __len__(self):
+        """len() implementation"""
+        return len(self._data)
+
+    @property
+    def data(self):
+        """
+        data-property
+
+        Exposes the entire raw-data structure (dict).
+        Use the high level methods when possible!
+        """
+        return self._data
+
+    @property
+    def states(self):
+        """
+        states-property
+
+        Exposes the entire states structure (dict).
+        Use the high level methods when possible!
+        """
+        return self._states
+
+    def add_state(self, key, state):
+        """
+        Adds an OTPState object with a given key.
+
+        :param key: The key under which to add the state
+        :type key: str
+
+        :param state: The OTPState object
+        :type state: otp2289.OTPState
+
+        :raises otp2289.OTPStoreException: On failure
+        """
+        if not isinstance(key, str):
+            raise OTPStoreException('key must be a str')
+        if not isinstance(state, OTPState):
+            raise OTPStoreException('state must be an OTPState-object')
+        self._data[key] = state
+        self._states[state] = key
+
+    def get(self, key, default=None):
+        """A wrapper for dict.get"""
+        return self._data.get(key, default)
+
+    def items(self):
+        """A wrapper for dict.items"""
+        return self._data.items()
+
+    def pop_state(self, key):
+        """
+        Removes specified key and returns the corresponding OTPState-object.
+
+        :param key: The key
+        :type key: str
+
+        :raises KeyError: If key does not exist
+
+        :raises otp2289.OTPStoreException: On failure
+
+        :return: The state corresponding to the key
+        :rtype: otp2289.OTPState
+        """
+        if not isinstance(key, str):
+            raise OTPStoreException('key must be a str')
+        state = self._data.pop(key)
+        self._states.pop(state)
+        return state
+
+    def response_validates(self, key, response, store_valid_response=True):
+        """
+        A method that wraps around OTPState.response_validates and
+        OTPState.get_next_state.
+
+        The response is validated against the OTPState object that corresponds
+        to key (if any). If store_valid_response is True, the state is replaced
+        by the next state on successful validation.
+
+        :param key: The key
+        :type key: str
+
+        :param response: The response to this state (its challenge)
+        :type response: str
+
+        :param store_valid_response: Should a valid response be stored
+        :type store_valid_response: bool
+
+        :raises KeyError: If the key is not present
+
+        :raises otp2289.OTPInvalidResponse: If the response does not match
+                                            this state
+
+        :return: Returns True if response validates, False otherwise
+        :rtype: bool
+        """
+        state = self._data[key]
+        rvalue = state.response_validates(response, store_valid_response)
+        if rvalue and store_valid_response:
+            next_state = state.get_next_state()
+            self._data[key] = next_state
+            self._states[next_state] = key
+            self._states.pop(state)
+        return rvalue
+
+    def to_dict(self):
+        """
+        Returns a dict representation of the object.
+
+        This could be the base for a JSON serialization.
+
+        :return: The dict representation of the object
+        :rtype: dict
+        """
+        return {key: state.to_dict() for key, state in self._data.items()}
+
+    def _add_data(self, dict_obj):
+        """
+        Adds data from a dict object (dict_obj).
+
+        This method should probably be either overloaded or wrapped
+        in a child class.
+
+        dict_obj has the following format:
+        {'key': {'ot_hex': val1,
+                 'current_step': val2,
+                 'seed': val3,
+                 'hash_algo': val4},
+         ...., ....}
+
+        :param dict_obj: The dict-object
+        :type dict_obj: dict
+        """
+        if not dict_obj:
+            return
+        for key, state_dict in dict_obj.items():
+            self.add_state(key, OTPState(**state_dict))
---
+++ packages/pyotp2289/pyotp2289-1.0.0.tar.gz/pyotp2289-1.0.0/tests/test_generator.py
@@ -0,0 +1,236 @@
+# -*- coding: utf-8 -*-
+# SPDX-License-Identifier: BSD-2-Clause-FreeBSD
+#
+# Copyright (c) 2020, Simeon Simeonov
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""Tests for otp2289.generator"""
+import pytest
+
+import otp2289
+
+
+def test_caller_exceptions():
+    """Tests the exceptions when calling an initialized object"""
+    gen = otp2289.OTPGenerator('This is a test.'.encode(),
+                               'TeSt',
+                               otp2289.OTP_ALGO_MD5)
+    with pytest.raises(otp2289.OTPGeneratorException) as exc_info:
+        gen.generate_otp_words('3')
+    assert exc_info.type is otp2289.OTPGeneratorException
+    assert exc_info.value.args[0] == 'Step value MUST be an int'
+    with pytest.raises(otp2289.OTPGeneratorException) as exc_info:
+        gen.generate_otp_hexdigest(-1)
+    assert exc_info.type is otp2289.OTPGeneratorException
+    assert exc_info.value.args[0] == 'Step value MUST be >= 0'
+    with pytest.raises(otp2289.OTPChallengeException) as exc_info:
+        gen.generate_otp_hexdigest_from_challenge(b'md5 fbd TeSt')
+    assert exc_info.type is otp2289.OTPChallengeException
+    assert exc_info.value.args[0] == 'Challenge must be str'
+    with pytest.raises(otp2289.OTPChallengeException) as exc_info:
+        gen.generate_otp_hexdigest_from_challenge('md5 fbd TeSt')
+    assert exc_info.type is otp2289.OTPChallengeException
+    assert exc_info.value.args[0] == 'Invalid challenge'
+    with pytest.raises(otp2289.generator.OTPChallengeException) as exc_info:
+        gen.generate_otp_hexdigest_from_challenge('otp-md5 fbd TeSt')
+    assert exc_info.type is otp2289.generator.OTPChallengeException
+    assert exc_info.value.args[0] == 'Invalid challenge'
+
+
+def test_constructor_exceptions():
+    """
+    Tests the exceptions when initializing a new object (in the constructor)
+    """
+    # test the otp2289.OTPGenerator __init__ and validators
+    with pytest.raises(otp2289.OTPGeneratorException) as exc_info:
+        otp2289.OTPGenerator('This is a test.'.encode(),
+                             'TeStø'.encode(),
+                             otp2289.OTP_ALGO_MD5)
+    assert exc_info.type is otp2289.OTPGeneratorException
+    assert exc_info.value.args[0] == 'Seed must be a string'
+    with pytest.raises(otp2289.OTPGeneratorException) as exc_info:
+        otp2289.OTPGenerator('This is a test.'.encode(),
+                             'TeStøtEsTteSTteStTest',
+                             otp2289.OTP_ALGO_SHA1)
+    assert exc_info.type is otp2289.OTPGeneratorException
+    assert exc_info.value.args[0] == ('The seed MUST be of 1 to 16 '
+                                      'characters in length')
+    with pytest.raises(otp2289.OTPGeneratorException) as exc_info:
+        otp2289.OTPGenerator('This is a test.'.encode(),
+                             'TeStø',
+                             otp2289.OTP_ALGO_SHA1)
+    assert exc_info.type is otp2289.OTPGeneratorException
+    assert exc_info.value.args[0] == ('The seed MUST consist of purely '
+                                      'alphanumeric characters')
+    with pytest.raises(otp2289.OTPGeneratorException) as exc_info:
+        otp2289.OTPGenerator('This is a test.'.encode(), 'TeSt', 9)
+    assert exc_info.type is otp2289.OTPGeneratorException
+    assert exc_info.value.args[0] == (
+        'hash_algo is not among the known algorithms')
+    with pytest.raises(otp2289.OTPGeneratorException) as exc_info:
+        otp2289.OTPGenerator('This is a test.'.encode(), 'TeSt', b'md5')
+    assert exc_info.type is otp2289.OTPGeneratorException
+    assert exc_info.value.args[0] == 'hash_algo must be an int or a str'
+    # test the package structure as well
+    with pytest.raises(otp2289.generator.OTPGeneratorException) as exc_info:
+        otp2289.generator.OTPGenerator('This is a test.'.encode(),
+                                       'TeSt',
+                                       'foo')
+    assert exc_info.type is otp2289.generator.OTPGeneratorException
+    assert exc_info.value.args[0] == ('foo is not supported by this version '
+                                      'of the hashlib module')
+    with pytest.raises(otp2289.OTPGeneratorException) as exc_info:
+        otp2289.OTPGenerator('1234567', 'TeSt', otp2289.OTP_ALGO_MD5)
+    assert exc_info.type is otp2289.OTPGeneratorException
+    assert exc_info.value.args[0] == 'Password must be a byte-string'
+    with pytest.raises(otp2289.OTPGeneratorException) as exc_info:
+        otp2289.OTPGenerator('1234567'.encode(),
+                             'TeSt',
+                             otp2289.OTP_ALGO_MD5)
+    assert exc_info.type is otp2289.OTPGeneratorException
+    assert exc_info.value.args[0] == 'Password must be longer than 10 bytes'
+
+
+def test_md5():
+    """
+    Tests the MD5 functionality of the OTPGenerator as described in the RFC
+
+    Those are the tests from 'RFC-2289 Appendix C - OTP Verification Examples'
+    """
+    # We could run this in a loop, but I guess "Readability counts."
+    # pass='This is a test.', seed='TeSt'
+    gen = otp2289.OTPGenerator('This is a test.'.encode(),
+                               'TeSt',
+                               otp2289.OTP_ALGO_MD5)
+    res_words = gen.generate_otp_words(0)
+    res_hex = gen.generate_otp_hexdigest(0)
+    assert isinstance(res_words, str)
+    assert isinstance(res_hex, str)
+    assert res_hex == '0x9e876134d90499dd'
+    assert res_words == 'INCH SEA ANNE LONG AHEM TOUR'
+    # step 1
+    assert gen.generate_otp_hexdigest(1) == '0x7965e05436f5029f'
+    assert gen.generate_otp_words(1) == 'EASE OIL FUM CURE AWRY AVIS'
+    assert gen.generate_otp_hexdigest_from_challenge('otp-md5 1   TeSt') == (
+        '0x7965e05436f5029f')
+    assert gen.generate_otp_words_from_challenge('otp-md5 1 TeSt') == (
+        'EASE OIL FUM CURE AWRY AVIS')
+    # step 99
+    assert gen.generate_otp_hexdigest(99) == '0x50fe1962c4965880'
+    assert gen.generate_otp_words(99) == 'BAIL TUFT BITS GANG CHEF THY'
+    assert gen.generate_otp_hexdigest_from_challenge('otp-md5 99   TeSt') == (
+        '0x50fe1962c4965880')
+    assert gen.generate_otp_words_from_challenge('otp-md5 99   TeSt') == (
+        'BAIL TUFT BITS GANG CHEF THY')
+    # iterator test
+    hexdigests = list(gen.hexdigest_range(105))  # testing the range itself
+    words = list(gen.words_range(99))
+    hexdigests.reverse()
+    words.reverse()
+    assert hexdigests[0] == '0x9e876134d90499dd'
+    assert hexdigests[1] == '0x7965e05436f5029f'
+    assert hexdigests[99] == '0x50fe1962c4965880'
+    assert words[0] == 'INCH SEA ANNE LONG AHEM TOUR'
+    assert words[1] == 'EASE OIL FUM CURE AWRY AVIS'
+    assert words[99] == 'BAIL TUFT BITS GANG CHEF THY'
+    # pass='AbCdEfGhIjK', seed='alpha1'
+    gen = otp2289.OTPGenerator('AbCdEfGhIjK'.encode(),
+                               'alpha1',
+                               otp2289.OTP_ALGO_MD5)
+    assert gen.generate_otp_hexdigest(0) == '0x87066dd9644bf206'
+    assert gen.generate_otp_words(0) == 'FULL PEW DOWN ONCE MORT ARC'
+    assert gen.generate_otp_hexdigest(1) == '0x7cd34c1040add14b'
+    assert gen.generate_otp_words(1) == 'FACT HOOF AT FIST SITE KENT'
+    assert gen.generate_otp_hexdigest(99) == '0x5aa37a81f212146c'
+    assert gen.generate_otp_words(99) == 'BODE HOP JAKE STOW JUT RAP'
+    # pass="OTP's are good", seed='correct'
+    gen = otp2289.OTPGenerator("OTP's are good".encode(),
+                               'correct',
+                               otp2289.OTP_ALGO_MD5)
+    assert gen.generate_otp_hexdigest(0) == '0xf205753943de4cf9'
+    assert gen.generate_otp_words(0) == 'ULAN NEW ARMY FUSE SUIT EYED'
+    assert gen.generate_otp_hexdigest(1) == '0xddcdac956f234937'
+    assert gen.generate_otp_words(1) == 'SKIM CULT LOB SLAM POE HOWL'
+    assert gen.generate_otp_hexdigest(99) == '0xb203e28fa525be47'
+    assert gen.generate_otp_words(99) == 'LONG IVY JULY AJAR BOND LEE'
+
+
+def test_sha1():
+    """
+    Tests the SHA-1 functionality of the OTPGenerator as described in the RFC
+
+    Those are the tests from 'RFC-2289 Appendix C - OTP Verification Examples'
+    """
+    # pass='This is a test.', seed='TeSt'
+    gen = otp2289.OTPGenerator('This is a test.'.encode(),
+                               'TeSt',
+                               otp2289.OTP_ALGO_SHA1)
+    # step=0
+    res_hex = gen.generate_otp_hexdigest(0)
+    res_words = gen.generate_otp_words(0)
+    assert isinstance(res_words, str)
+    assert isinstance(res_hex, str)
+    assert res_hex == '0xbb9e6ae1979d8ff4'
+    assert res_words == 'MILT VARY MAST OK SEES WENT'
+    assert gen.generate_otp_hexdigest(1) == '0x63d936639734385b'
+    assert gen.generate_otp_words(1) == 'CART OTTO HIVE ODE VAT NUT'
+    assert gen.generate_otp_hexdigest_from_challenge('otp-sha1 1 TeSt') == (
+        '0x63d936639734385b')
+    assert gen.generate_otp_words_from_challenge('otp-sha1 1 TeSt') == (
+        'CART OTTO HIVE ODE VAT NUT')
+    assert gen.generate_otp_hexdigest(99) == '0x87fec7768b73ccf9'
+    assert gen.generate_otp_words(99) == 'GAFF WAIT SKID GIG SKY EYED'
+    assert gen.generate_otp_hexdigest_from_challenge('otp-sha1 99   TeSt') == (
+        '0x87fec7768b73ccf9')
+    assert gen.generate_otp_words_from_challenge('otp-sha1 99  TeSt') == (
+        'GAFF WAIT SKID GIG SKY EYED')
+    # iterator test
+    hexdigests = list(gen.hexdigest_range(105))
+    words = list(gen.words_range(99))
+    hexdigests.reverse()
+    words.reverse()
+    assert hexdigests[0] == '0xbb9e6ae1979d8ff4'
+    assert hexdigests[1] == '0x63d936639734385b'
+    assert hexdigests[99] == '0x87fec7768b73ccf9'
+    assert words[0] == 'MILT VARY MAST OK SEES WENT'
+    assert words[1] == 'CART OTTO HIVE ODE VAT NUT'
+    assert words[99] == 'GAFF WAIT SKID GIG SKY EYED'
+    # pass='AbCdEfGhIjK', seed='alpha1'
+    gen = otp2289.OTPGenerator('AbCdEfGhIjK'.encode(),
+                               'alpha1',
+                               otp2289.OTP_ALGO_SHA1)
+    assert gen.generate_otp_hexdigest(0) == '0xad85f658ebe383c9'
+    assert gen.generate_otp_words(0) == 'LEST OR HEEL SCOT ROB SUIT'
+    assert gen.generate_otp_hexdigest(1) == '0xd07ce229b5cf119b'
+    assert gen.generate_otp_words(1) == 'RITE TAKE GELD COST TUNE RECK'
+    assert gen.generate_otp_hexdigest(99) == '0x27bc71035aaf3dc6'
+    assert gen.generate_otp_words(99) == 'MAY STAR TIN LYON VEDA STAN'
+    # pass="OTP's are good", seed='correct'
+    gen = otp2289.OTPGenerator("OTP's are good".encode(),
+                               'correct',
+                               otp2289.OTP_ALGO_SHA1)
+    assert gen.generate_otp_hexdigest(0) == '0xd51f3e99bf8e6f0b'
+    assert gen.generate_otp_words(0) == 'RUST WELT KICK FELL TAIL FRAU'
+    assert gen.generate_otp_hexdigest(1) == '0x82aeb52d943774e4'
+    assert gen.generate_otp_words(1) == 'FLIT DOSE ALSO MEW DRUM DEFY'
+    assert gen.generate_otp_hexdigest(99) == '0x4f296a74fe1567ec'
+    assert gen.generate_otp_words(99) == 'AURA ALOE HURL WING BERG WAIT'
