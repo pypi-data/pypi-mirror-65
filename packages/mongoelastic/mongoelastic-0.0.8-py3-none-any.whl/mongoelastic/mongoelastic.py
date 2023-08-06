bc827902ce540937ab9346b6eb084d734e1757e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:36 +0000

    Add package mongoelastic/0.0.8/mongoelastic-0.0.8-py3-none-any.whl

---
+++ packages/mongoelastic/mongoelastic-0.0.8-py3-none-any.whl/mongoelastic/mongoelastic.py
@@ -0,0 +1,48 @@
+from pymongo import MongoClient
+from elasticsearch import Elasticsearch
+from progress.spinner import Spinner
+
+
+class MongoElastic:
+    def __init__(self, args):
+
+        # mongo db connection
+        self.mongo_host = args.get('mongo_host', 'localhost')
+        self.mongo_port = args.get('mongo_port', None)
+        self.mongo_max_pool_size = args.get('mongo_max_pool_size', 50)
+        self.mongo_db_name = args.get('mongo_db_name', None)
+        self.mongo_document_name = args.get('mongo_document_name', None)
+
+        # elasticsearch connection
+        self.es = args.get('es_connection', None)
+
+    def start(self, m_filter=None):
+        m_filter = dict() if not m_filter else m_filter
+        client = MongoClient(self.mongo_host, self.mongo_port, maxPoolSize=self.mongo_max_pool_size)
+        db = client[self.mongo_db_name]
+        document_name = db[self.mongo_document_name]
+
+        mongo_where = m_filter.get('mongo_condition', {})
+        # get all data from mongoDB db
+        m_data = document_name.find(mongo_where)
+
+        if not self.es:
+            es = Elasticsearch(
+                ['localhost'],
+                use_ssl=False,
+            )
+        i = 1
+        spinner = Spinner('Importing... ')
+        for line in m_data:
+            docket_content = line
+            # remove _id from mongo object
+            del docket_content['_id']
+            try:
+                es.index(index=self.es_index_name, doc_type=self.es_doc_type,
+                         id=i, body=docket_content)
+            except Exception:
+                pass
+            i += 1
+            spinner.next()
+        client.close()
+        return True
