9f0a413e5e3e3f523d3f02ebc6faf54e2c43d1a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:39 +0000

    Add package vncdotool/1.0.0/vncdotool-1.0.0.tar.gz

---
+++ packages/vncdotool/vncdotool-1.0.0.tar.gz/vncdotool-1.0.0/tests/unit/test_client.py
@@ -0,0 +1,236 @@
+from unittest import TestCase
+
+from nose.plugins.skip import SkipTest
+import mock
+
+from vncdotool import client
+from vncdotool import rfb
+
+from .helpers import isolate
+
+
+class TestVNCDoToolClient(TestCase):
+
+    def setUp(self):
+        self.isolation = isolate.object(client.VNCDoToolClient,
+                excludes='math.sqrt,operator.add,client.KEYMAP,rfb')
+        self.isolation.start()
+
+        self.client = client.VNCDoToolClient()
+        self.client.transport = mock.Mock()
+        self.client.factory = mock.Mock()
+
+        # mock out a bunch of base class functions
+        self.client.framebufferUpdateRequest = mock.Mock()
+        self.client.pointerEvent = mock.Mock()
+        self.client.keyEvent = mock.Mock()
+        self.client.setEncodings = mock.Mock()
+
+    def tearDown(self):
+        if self.isolation:
+            self.isolation.stop()
+            self.isolation = None
+
+    def _tryPIL(self):
+        try:
+            import PIL  # noqa
+        except ImportError:
+            raise SkipTest
+
+    def test_vncConnectionMade(self):
+        cli = self.client
+        cli._packet = [b"RFB003.003\n"]
+        cli._handleInitial()
+        cli._handleServerInit(b" " * 24)
+        cli.vncConnectionMade()
+        factory = cli.factory
+        factory.clientConnectionMade.assert_called_once_with(cli)
+        self.client.setEncodings.assert_called_once_with([
+            client.rfb.RAW_ENCODING,
+            client.rfb.PSEUDO_CURSOR_ENCODING,
+            client.rfb.PSEUDO_DESKTOP_SIZE_ENCODING])
+
+    def test_keyPress_single_alpha(self):
+        cli = self.client
+        cli.keyPress('a')
+        cli.keyEvent.assert_any_call(ord('a'), down=1)
+        cli.keyEvent.assert_any_call(ord('a'), down=0)
+
+    def test_keyPress_multiple(self):
+        cli = self.client
+        cli.keyPress('ctrl-alt-del')
+
+        # XXX doesn't ensure correct order
+        cli.keyEvent.assert_any_call(rfb.KEY_ControlLeft, down=1)
+        cli.keyEvent.assert_any_call(rfb.KEY_AltLeft, down=1)
+        cli.keyEvent.assert_any_call(rfb.KEY_Delete, down=1)
+        cli.keyEvent.assert_any_call(rfb.KEY_ControlLeft, down=0)
+        cli.keyEvent.assert_any_call(rfb.KEY_AltLeft, down=0)
+        cli.keyEvent.assert_any_call(rfb.KEY_Delete, down=0)
+
+    def test_captureScreen(self):
+        cli = self.client
+        cli._packet = [b"RFB003.003\n"]
+        cli._handleInitial()
+        cli._handleServerInit(b" " * 24)
+        cli.vncConnectionMade()
+        fname = 'foo.png'
+
+        d = cli.captureScreen(fname)
+        d.addCallback.assert_called_once_with(cli._captureSave, fname)
+        assert cli.framebufferUpdateRequest.called
+
+    def test_captureSave(self):
+        cli = self.client
+        cli.screen = mock.Mock()
+        fname = 'foo.png'
+        r = cli._captureSave(cli.screen, fname)
+        cli.screen.save.assert_called_once_with(fname)
+        assert r == cli
+
+    def test_expectScreen(self):
+        self._tryPIL()
+
+        cli = self.client
+        cli._packet = [b"RFB003.003\n"]
+        cli._handleInitial()
+        cli._handleServerInit(b" " * 24)
+        cli.vncConnectionMade()
+        cli.screen = mock.Mock()
+        cli.screen.size = (1024, 768)
+        fname = 'something.png'
+
+        region = (0 ,0, 11, 22)
+        client.Image.open.return_value.size = region[2:]
+
+        d = cli.expectScreen(fname, 5)
+        assert cli.framebufferUpdateRequest.called
+
+        client.Image.open.assert_called_once_with(fname)
+
+        assert cli.expected == client.Image.open.return_value.histogram.return_value
+        cli.deferred.addCallback.assert_called_once_with(cli._expectCompare, region, 5)
+
+    def test_expectCompareSuccess(self):
+        cli = self.client
+        d = cli.deferred = mock.Mock()
+        cli.expected = [2, 2, 2]
+        cli.screen = mock.Mock()
+        cli.screen.histogram.return_value = [1, 2, 3]
+        cli.screen.crop.return_value = cli.screen
+        result = cli._expectCompare(cli, None, 5)
+        assert result == cli
+
+    def test_expectCompareExactSuccess(self):
+        cli = self.client
+        d = cli.deferred = mock.Mock()
+        cli.expected = [2, 2, 2]
+        cli.screen = mock.Mock()
+        cli.screen.histogram.return_value = [2, 2, 2]
+        cli.screen.crop.return_value = cli.screen
+        result = cli._expectCompare(cli, None, 0)
+        assert result == cli
+
+    def test_expectCompareFails(self):
+        cli = self.client
+        cli.deferred = mock.Mock()
+        cli.expected = [2, 2, 2]
+        cli.framebufferUpdateRequest = mock.Mock()
+        cli.screen = mock.Mock()
+        cli.screen.histogram.return_value = [1, 1, 1]
+        cli.screen.crop.return_value = cli.screen
+
+        result = cli._expectCompare(cli, None, 0)
+
+        assert result != cli
+        assert result == cli.deferred
+        assert not cli.deferred.callback.called
+
+        cli.framebufferUpdateRequest.assert_called_once_with(incremental=1)
+        cli.deferred.addCallback.assert_called_once_with(cli._expectCompare, None, 0)
+
+    def test_expectCompareMismatch(self):
+        cli = self.client
+        cli.deferred = mock.Mock()
+        cli.expected = [2, 2]
+        cli.framebufferUpdateRequest = mock.Mock()
+        cli.screen = mock.Mock()
+        cli.screen.histogram.return_value = [1, 1, 1]
+        cli.screen.crop.return_value = cli.screen
+
+        result = cli._expectCompare(cli, None, 0)
+
+        assert result != cli
+        assert result == cli.deferred
+        assert not cli.deferred.callback.called
+
+        cli.framebufferUpdateRequest.assert_called_once_with(incremental=1)
+        cli.deferred.addCallback.assert_called_once_with(cli._expectCompare, None, 0)
+
+    def test_updateRectangeFullScreen(self):
+        cli = self.client
+        cli.image = mock.Mock()
+        data = mock.Mock()
+
+        cli.updateRectangle(0, 0, 100, 200, data)
+
+        client.Image.frombytes.assert_called_once_with('RGB', (100, 200), data, 'raw', 'RGBX')
+
+        assert cli.screen == client.Image.frombytes.return_value
+
+    def test_updateRectangeRegion(self):
+        cli = self.client
+        cli.image = mock.Mock()
+        cli.screen = mock.Mock()
+        cli.screen.size = (100, 100)
+        data = mock.Mock()
+
+        cli.updateRectangle(20, 10, 50, 40, data)
+
+        client.Image.frombytes.assert_called_once_with('RGB', (50, 40), data, 'raw', 'RGBX')
+
+        paste = cli.screen.paste
+        paste.assert_called_once_with(client.Image.frombytes.return_value, (20, 10))
+
+    def test_commitUpdate(self):
+        rects = mock.Mock()
+        self.deferred = mock.Mock()
+        self.client.deferred = self.deferred
+        self.client.commitUpdate(rects)
+
+        self.deferred.callback.assert_called_once_with(self.client)
+
+    def test_vncRequestPassword_attribute(self):
+        cli = self.client
+        cli.sendPassword = mock.Mock()
+        cli.factory.password = 'mushroommushroom'
+        cli.vncRequestPassword()
+        cli.sendPassword.assert_called_once_with(cli.factory.password)
+
+
+class TestVNCDoToolFactory(TestCase):
+
+    def setUp(self):
+        self.factory = client.VNCDoToolFactory()
+
+    def test_init(self):
+        assert self.factory.deferred
+
+    def test_clientConnectionMade(self):
+        deferred = mock.Mock()
+        protocol = mock.Mock()
+        self.factory.deferred = deferred
+
+        self.factory.clientConnectionMade(protocol)
+
+        deferred.callback.assert_called_once_with(protocol)
+
+    def test_clientConnectionFailed(self):
+        deferred = mock.Mock()
+        self.factory.deferred = deferred
+        reason = mock.Mock()
+        connector = mock.Mock()
+
+        self.factory.clientConnectionFailed(connector, reason)
+
+        deferred.errback.assert_called_once_with(reason)
---
+++ packages/vncdotool/vncdotool-1.0.0.tar.gz/vncdotool-1.0.0/tests/unit/test_command.py
@@ -0,0 +1,239 @@
+from vncdotool import command
+
+import socket
+import unittest
+import mock
+from .helpers import isolate
+
+
+class TestBuildCommandList(unittest.TestCase):
+
+    def setUp(self):
+        super(TestBuildCommandList, self).setUp()
+        self.isolation = isolate.object(command.build_command_list)
+        self.isolation.start()
+        self.factory = mock.Mock()
+        self.client = command.VNCDoCLIClient
+        self.deferred = self.factory.deferred
+
+    def tearDown(self):
+        if self.isolation:
+            self.isolation.stop()
+            self.isolation = None
+
+    def assertCalled(self, fn, *args):
+        self.deferred.addCallback.assert_called_with(fn, *args)
+
+    def call_build_commands_list(self, commands, **kwargs):
+        command.build_command_list(self.factory, commands.split(), **kwargs)
+
+    def test_alphanum_key(self):
+        self.call_build_commands_list('key a')
+        self.assertCalled(self.client.keyPress, 'a')
+
+    def test_control_key(self):
+        self.call_build_commands_list('key ctrl-c')
+        self.assertCalled(self.client.keyPress, 'ctrl-c')
+
+    def test_keyup(self):
+        self.call_build_commands_list('keyup a')
+        self.assertCalled(self.client.keyUp, 'a')
+
+    def test_keydown(self):
+        self.call_build_commands_list('keydown a')
+        self.assertCalled(self.client.keyDown, 'a')
+
+    def test_key_missing(self):
+        pass
+
+    def test_move(self):
+        self.call_build_commands_list('move 100 200')
+        self.assertCalled(self.client.mouseMove, 100, 200)
+
+    def test_move(self):
+        self.call_build_commands_list('mousemove 100 200')
+        self.assertCalled(self.client.mouseMove, 100, 200)
+
+    def test_move_missing(self):
+        pass
+
+    def test_click(self):
+        self.call_build_commands_list('click 1')
+        self.assertCalled(self.client.mousePress, 1)
+
+    def test_click_missing(self):
+        pass
+
+    def test_type(self):
+        self.call_build_commands_list('type foobar')
+        call = self.factory.deferred.addCallback
+        for key in 'foobar':
+            call.assert_any_call(self.client.keyPress, key)
+
+    def test_type_missing(self):
+        pass
+
+    def test_capture(self):
+        command.SUPPORTED_FORMATS = ('png',)
+        command.os.path.splitext.return_value = 'capture', '.png'
+        self.call_build_commands_list('capture foo.png')
+        self.assertCalled(self.client.captureScreen, 'foo.png', 0)
+
+    def test_capture_not_supported(self):
+        command.SUPPORTED_FORMATS = ('png',)
+        command.os.path.splitext.return_value = 'capture', '.mpeg'
+        self.call_build_commands_list('capture foo.mpeg')
+        assert not self.deferred.addCallback.called
+
+    def test_capture_missing_filename(self):
+        pass
+
+    def test_expect(self):
+        self.call_build_commands_list('expect foo.png 10')
+        self.assertCalled(self.client.expectScreen, 'foo.png', 10)
+
+    def test_expect_not_png(self):
+        pass
+
+    def test_expect_missing(self):
+        pass
+
+    def test_chain_key_commands(self):
+        self.call_build_commands_list('type foobar key enter')
+        call = self.factory.deferred.addCallback
+        for key in 'foobar':
+            call.assert_any_call(self.client.keyPress, key)
+        call.assert_any_call(self.client.keyPress, 'enter')
+
+    def test_chain_type_expect(self):
+        self.call_build_commands_list('type username expect password.png 0')
+        call = self.factory.deferred.addCallback
+        for key in 'username':
+            call.assert_any_call(self.client.keyPress, key)
+
+        call.assert_any_call(self.client.expectScreen, 'password.png', 0)
+
+    def test_pause(self):
+        self.call_build_commands_list('pause 0.3')
+        self.assertCalled(self.client.pause, 0.3)
+
+    def test_sleep(self):
+        self.call_build_commands_list('sleep 1')
+        self.assertCalled(self.client.pause, 1)
+
+    def test_pause_warp(self):
+        self.call_build_commands_list('pause 10', warp=5)
+        self.assertCalled(self.client.pause, 2.0)
+
+    def test_mousedown(self):
+        self.call_build_commands_list('mousedown 1')
+        self.assertCalled(self.client.mouseDown, 1)
+
+        self.call_build_commands_list('mdown 2')
+        self.assertCalled(self.client.mouseDown, 2)
+
+    def test_mouseup(self):
+        self.call_build_commands_list('mouseup 1')
+        self.assertCalled(self.client.mouseUp, 1)
+
+        self.call_build_commands_list('mup 2')
+        self.assertCalled(self.client.mouseUp, 2)
+
+    def test_drag(self):
+        self.call_build_commands_list('drag 100 200')
+        self.assertCalled(self.client.mouseDrag, 100, 200)
+
+
+    def test_insert_delay(self):
+        self.call_build_commands_list('click 1 key a', delay=100)
+        expected = [ mock.call(self.client.mousePress, 1),
+                     mock.call(self.client.pause, 0.1),
+                     mock.call(self.client.keyPress, 'a')]
+
+        self.assertEqual(self.deferred.addCallback.call_args_list, expected)
+
+
+class TestParseServer(object):
+    def setUp(self):
+        self.isolation = isolate.object(command.parse_server)
+        self.isolation.start()
+        self.options = mock.Mock()
+        self.options.server = '127.0.0.1'
+        parse_args = command.VNCDoToolOptionParser.return_value.parse_args
+        parse_args.return_value = (self.options, [])
+        command.socket.AF_INET = socket.AF_INET
+        command.socket.AF_UNIX = socket.AF_UNIX
+        command.os.path.exists.return_value = False
+
+    def tearDown(self):
+        if self.isolation:
+            self.isolation.stop()
+            self.isolation = None
+
+    def test_default(self):
+        family, host, port = command.parse_server('')
+        assert family == socket.AF_INET
+        assert host == '127.0.0.1'
+        assert port == 5900
+
+    def test_host_display(self):
+        family, host, port = command.parse_server('10.11.12.13:10')
+        assert family == socket.AF_INET
+        assert host == '10.11.12.13'
+        assert port == 5910
+
+    def test_host_port(self):
+        family, host, port = command.parse_server('10.11.12.13::4444')
+        assert family == socket.AF_INET
+        assert host == '10.11.12.13'
+        assert port == 4444
+
+    def test_just_host(self):
+        family, host, port = command.parse_server('10.11.12.13')
+        assert family == socket.AF_INET
+        assert host == '10.11.12.13'
+        assert port == 5900
+
+    def test_just_display(self):
+        family, host, port = command.parse_server(':10')
+        assert family == socket.AF_INET
+        assert host == '127.0.0.1'
+        assert port == 5910
+
+    def test_just_port(self):
+        family, host, port = command.parse_server('::1111')
+        assert family == socket.AF_INET
+        assert host == '127.0.0.1'
+        assert port == 1111
+
+    def test_unix_socket(self):
+        command.os.path.exists.return_value = True
+        family, host, port = command.parse_server('/some/path/unix.skt')
+        assert family == socket.AF_UNIX
+        assert host == '/some/path/unix.skt'
+        assert port == 5900
+
+
+class TestVNCDoCLIClient(unittest.TestCase):
+
+    def setUp(self):
+        self.isolation = isolate.object(command.VNCDoCLIClient)
+        self.isolation.start()
+        self.client = command.VNCDoCLIClient()
+        self.client.factory = mock.Mock()
+
+    def tearDown(self):
+        if self.isolation:
+            self.isolation.stop()
+            self.isolation = None
+
+    def test_vncRequestPassword_prompt(self):
+        cli = self.client
+        cli.factory.password = None
+        cli.sendPassword = mock.Mock()
+        cli.vncRequestPassword()
+
+        password = command.getpass.getpass.return_value
+        assert command.getpass.getpass.called
+        assert cli.factory.password == password
+        cli.sendPassword.assert_called_once_with(password)
