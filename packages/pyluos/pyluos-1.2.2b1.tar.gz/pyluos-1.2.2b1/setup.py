e7f7f9885f9da4358769465ce0a536a25b429060
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:53 +0000

    Add package pyluos/1.2.2b1/pyluos-1.2.2b1.tar.gz

---
+++ packages/pyluos/pyluos-1.2.2b1.tar.gz/pyluos-1.2.2b1/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import imp
+
+from setuptools import setup, find_packages
+
+version = imp.load_source('pyluos.version', 'pyluos/version.py')
+
+
+setup(name='pyluos',
+      version=version.version,
+      packages=find_packages(),
+      install_requires=['future',
+                        'websocket-client',
+                        'pyserial>3',
+                        'SimpleWebSocketServer',
+                        'zeroconf',
+                        'numpy',
+                        'anytree',
+                        ],
+      extras_require={
+          'tests': ['pytest', 'flake8'],
+          'topographe': ['pyzmq'],
+          'jupyter-integration': ['ipywidgets'],
+          'scratch': ['flask', 'Jinja2'],
+      },
+      entry_points={
+          'console_scripts': [
+              'pyluos-wifi-gate = pyluos.tools.wifi_gate:main',
+              'pyluos-usb-gate = pyluos.tools.usb_gate:main',
+              'pyluos-usb2ws = pyluos.tools.usb2ws:main',
+              'pyluos-usb2scratch = pyluos.tools.usb2scratch:main',
+          ],
+      },
+      )
