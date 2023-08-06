7e762b36b5c22361bcb548d7b4ad773989dda452
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:37 +0000

    Add package z3c.checkversions/1.2/z3c.checkversions-1.2.tar.gz

---
+++ packages/z3c.checkversions/z3c.checkversions-1.2.tar.gz/z3c.checkversions-1.2/CHANGES.rst
@@ -0,0 +1,87 @@
+Changelog
+=========
+
+1.2 (2020-04-13)
+----------------
+
+- Add support for Python 3.8, drop support for Python 3.4.
+
+- Improve error message in case zc.buildout is not installed.
+
+- Improve installation instruction.
+
+- Pass the index url to both the 'installed' and 'buildout' checkers.
+
+
+1.1 (2018-11-03)
+----------------
+
+- Add support for Python 3.7.
+
+- Drop support for `python setup.py test`.
+
+
+1.0 (2018-05-23)
+----------------
+
+- Fix compatibility with setuptools 39 by using an API introduced
+  in setuptools 8. (`GH #8`_)
+
+- Add support for Python 3.5, 3.6 and PyPy3.
+
+- Drop support for Python 2.6 and 3.3.
+
+- Standardize namespace ``__init__``.
+
+.. _GH #8 : https://github.com/zopefoundation/z3c.checkversions/issues/8
+
+0.5 (2014-09-15)
+----------------
+
+- Python 3 support by Nicolas Dietrich (`GH #2`_, `GH #4`_)
+
+.. _GH #2: https://github.com/zopefoundation/z3c.checkversions/pull/2
+.. _GH #4: https://github.com/zopefoundation/z3c.checkversions/pull/4
+
+0.4.2 (2013-10-02)
+------------------
+
+- ignore installed packages while searching for new versions, only look in
+  the package index (makes the test suite more reliable, among other things).
+- fix IndexError: list index out of range when buildout.cfg had a package with
+  a blank version pin.
+- show updates for non-final package versions, if there's a newer non-final
+  version available on PyPI (`GH #1`_)
+
+.. _GH #1: https://github.com/zopefoundation/z3c.checkversions/pull/1
+
+0.4.1 (2010-08-25)
+------------------
+
+- fixed edge case bug where 1.0 was never updated to 1.0.x
+- warn buildout users about the extra requirement
+
+0.4 (2010-07-26)
+----------------
+
+- added a `blacklist` option for passing versions to avoid
+  (possibly coming from a buildbot)
+- added a `incremental` option to suggest only one upgrade
+- remove a temporary file during tests
+
+0.3 (2010-07-09)
+----------------
+
+- don't accumulate old comments
+- prefer final versions
+
+0.2 (2010-05-22)
+----------------
+
+- added a verbose option to print old versions as well
+- updated metadata, doc and license
+
+0.1 (2010-05-16)
+----------------
+
+- Initial release
---
+++ packages/z3c.checkversions/z3c.checkversions-1.2.tar.gz/z3c.checkversions-1.2/setup.py
@@ -0,0 +1,73 @@
+##############################################################################
+#
+# Copyright (c) 2010 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+from setuptools import setup, find_packages
+
+setup(
+    name='z3c.checkversions',
+    version='1.2',
+    description="Find newer package versions on PyPI",
+    long_description=(open("README.rst").read() + "\n" +
+                      open("CHANGES.rst").read()),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Quality Assurance',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Framework :: Buildout',
+        'Framework :: Zope',
+        'Framework :: Zope :: 2',
+        'Framework :: Zope :: 3',
+        'Framework :: Zope :: 4',
+    ],
+    keywords='version, buildout, packages, upgrade, zope, ztk',
+    author='Zope Foundation and Contributors',
+    author_email='zope-dev@zope.org',
+    url='https://github.com/zopefoundation/z3c.checkversions',
+    license='ZPL 2.1',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['z3c'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools >= 8',
+    ],
+    python_requires=', '.join([
+        '>=2.7',
+        '!=3.0.*',
+        '!=3.1.*',
+        '!=3.2.*',
+        '!=3.3.*',
+        '!=3.4.*',
+    ]),
+    extras_require={
+        'buildout': ['zc.buildout'],
+    },
+    entry_points="""
+    [console_scripts]
+    checkversions = z3c.checkversions.main:main
+    """,
+)
