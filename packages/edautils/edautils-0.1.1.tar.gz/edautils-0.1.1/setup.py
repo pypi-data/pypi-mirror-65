28725ac470623974ea47d1d8750eaf8ac4e5a801
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:37 +0000

    Add package edautils/0.1.1/edautils-0.1.1.tar.gz

---
+++ packages/edautils/edautils-0.1.1.tar.gz/edautils-0.1.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+from setuptools import find_packages
+
+with open('README.md', encoding='utf-8') as f:
+    readme = f.read()
+
+print(readme)
+
+with open('LICENSE') as f:
+    license = f.read()
+
+required = ['pandas>=0.25.2', 'numpy>=1.17.0', 'matplotlib>=3.0.0']
+
+extras_required = dict()
+extras_required['tests'] = ['pytest>=4.0.0', 'pytest-pep8']
+
+setup(name='edautils',
+      version='0.1.1',
+      description=
+      'This package contains exploratory data analysis (EDA) helper functions',
+      long_description=readme,
+      long_description_content_type='text/markdown',
+      author='David Furrer',
+      author_email='furrer.david1@gmail.com',
+      url='https://github.com/davidfurrer/edautils',
+      license=license,
+      install_requires=required,
+      extras_require=extras_required,
+      packages=find_packages(exclude=('tests')),
+      include_package_data=True)
+#package_data={'edautils': ['data/*', 'README.md', 'LICENSE']})
