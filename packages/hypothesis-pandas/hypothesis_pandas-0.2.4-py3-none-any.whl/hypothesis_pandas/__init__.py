1f77d12df6e321c88a6fb3833d5f88a442da670f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:48 +0000

    Add package hypothesis-pandas/0.2.4/hypothesis_pandas-0.2.4-py3-none-any.whl

---
+++ packages/hypothesis-pandas/hypothesis_pandas-0.2.4-py3-none-any.whl/hypothesis_pandas/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+from hypothesis_pandas.hypothesis_pandas import dataframes
+from hypothesis_pandas.hypothesis_pandas import range_indices
+from hypothesis_pandas.hypothesis_pandas import series
+
+
+__all__ = [
+    "dataframes",
+    "range_indices",
+    "series",
+]
+__version__ = "0.2.4"
