f9aaa84e0bd84149ba0dd4c5dea23a8e32c34402
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:53 +0000

    Add package cryptoxlib-aio/3.1.0/cryptoxlib-aio-3.1.0.tar.gz

---
+++ packages/cryptoxlib-aio/cryptoxlib-aio-3.1.0.tar.gz/cryptoxlib-aio-3.1.0/cryptoxlib/exceptions.py
@@ -0,0 +1,13 @@
+class CryptoXLibException(Exception):
+    pass
+
+class WebsocketReconnectionException(CryptoXLibException):
+    pass
+
+
+class WebsocketError(CryptoXLibException):
+    pass
+
+
+class WebsocketClosed(CryptoXLibException):
+    pass
