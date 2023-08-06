b4a23aebea35d0e9e780ecf2f5a9b942c68d0256
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:33 +0000

    Add package Flask-Gopher/2.2.1/Flask_Gopher-2.2.1-py3-none-any.whl

---
+++ packages/Flask-Gopher/Flask_Gopher-2.2.1-py3-none-any.whl/flask_gopher/__init__.py
@@ -0,0 +1,24 @@
+r"""
+  _____ _           _          ____             _
+ |  ___| | __ _ ___| | __     / ___| ___  _ __ | |__   ___ _ __
+ | |_  | |/ _` / __| |/ /____| |  _ / _ \| '_ \| '_ \ / _ \ '__|
+ |  _| | | (_| \__ \   <_____| |_| | (_) | |_) | | | |  __/ |
+ |_|   |_|\__,_|___/_|\_\     \____|\___/| .__/|_| |_|\___|_|
+                                         |_|
+"""
+
+__title__ = 'Flask-Gopher'
+__author__ = 'Michael Lazar'
+__license__ = 'GPL-3.0'
+__copyright__ = '(c) 2020 Michael Lazar'
+
+from .__version__ import __version__
+
+from .flask_gopher import GopherMenu
+from .flask_gopher import TextFormatter
+from .flask_gopher import GopherDirectory
+from .flask_gopher import GopherExtension
+from .flask_gopher import GopherRequestHandler
+from .flask_gopher import GopherSessionInterface
+from .flask_gopher import make_gopher_ssl_server
+from .flask_gopher import menu, render_menu, render_menu_template
