7075e80e86ba63d99a36f692f2c815ee52226bff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:51 +0000

    Add package tasslab/0.0.0.2/tasslab-0.0.0.2.tar.gz

---
+++ packages/tasslab/tasslab-0.0.0.2.tar.gz/tasslab-0.0.0.2/phasereset/phase_reset_index.py
@@ -0,0 +1,7 @@
+# A module containing several functions to compute the phase reset index
+# Nebojsa Bozanic <bozanic@stanford.edu> Copyright 2020
+# Distributed under the BSD License
+
+def isi_profile(*args, **kwargs):
+
+ return 0
