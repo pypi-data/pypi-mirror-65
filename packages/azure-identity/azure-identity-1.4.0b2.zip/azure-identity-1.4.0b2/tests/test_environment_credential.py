024ee9601ca5faab3103e64e857f3c1f4eeff09a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:35 +0000

    Add package azure-identity/1.4.0b2/azure-identity-1.4.0b2.zip

---
+++ packages/azure-identity/azure-identity-1.4.0b2.zip/azure-identity-1.4.0b2/tests/test_environment_credential.py
@@ -0,0 +1,55 @@
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+import itertools
+import os
+
+from azure.identity import CredentialUnavailableError, EnvironmentCredential
+from azure.identity._constants import EnvironmentVariables
+import pytest
+
+from helpers import mock
+
+
+ALL_VARIABLES = {
+    _
+    for _ in EnvironmentVariables.CLIENT_SECRET_VARS
+    + EnvironmentVariables.CERT_VARS
+    + EnvironmentVariables.USERNAME_PASSWORD_VARS
+}
+
+
+def test_incomplete_configuration():
+    """get_token should raise CredentialUnavailableError for incomplete configuration."""
+
+    with mock.patch.dict(os.environ, {}, clear=True):
+        with pytest.raises(CredentialUnavailableError) as ex:
+            EnvironmentCredential().get_token("scope")
+
+    for a, b in itertools.combinations(ALL_VARIABLES, 2):  # all credentials require at least 3 variables set
+        with mock.patch.dict(os.environ, {a: "a", b: "b"}, clear=True):
+            with pytest.raises(CredentialUnavailableError) as ex:
+                EnvironmentCredential().get_token("scope")
+
+
+@pytest.mark.parametrize(
+    "credential_name,environment_variables",
+    (
+        ("ClientSecretCredential", EnvironmentVariables.CLIENT_SECRET_VARS),
+        ("CertificateCredential", EnvironmentVariables.CERT_VARS),
+        ("UsernamePasswordCredential", EnvironmentVariables.USERNAME_PASSWORD_VARS),
+    ),
+)
+def test_passes_authority_argument(credential_name, environment_variables):
+    """the credential pass the 'authority' keyword argument to its inner credential"""
+
+    authority = "authority"
+
+    with mock.patch.dict("os.environ", {variable: "foo" for variable in environment_variables}, clear=True):
+        with mock.patch(EnvironmentCredential.__module__ + "." + credential_name) as mock_credential:
+            EnvironmentCredential(authority=authority)
+
+    assert mock_credential.call_count == 1
+    _, kwargs = mock_credential.call_args
+    assert kwargs["authority"] == authority
