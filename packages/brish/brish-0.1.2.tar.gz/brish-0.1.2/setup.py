dcd9936a68919ed773d5e84cfc92af5614627140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:54 +0000

    Add package brish/0.1.2/brish-0.1.2.tar.gz

---
+++ packages/brish/brish-0.1.2.tar.gz/brish-0.1.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['brish']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['IPython>=7.13.0,<8.0.0', 'plumbum>=1.6.9,<2.0.0']
+
+setup_kwargs = {
+    'name': 'brish',
+    'version': '0.1.2',
+    'description': 'A bridge between zsh and Python.',
+    'long_description': None,
+    'author': 'NightMachinary',
+    'author_email': 'rudiwillalwaysloveyou@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
