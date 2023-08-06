de9dd6bdec22f922a69bcad1c76c7ccd40fe0e7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:57 +0000

    Add package sciwing/0.1.0/sciwing-0.1.0.tar.gz

---
+++ packages/sciwing/sciwing-0.1.0.tar.gz/sciwing-0.1.0/sciwing/api/api.py
@@ -0,0 +1,16 @@
+import sciwing.api.conf as config
+from fastapi import FastAPI
+from sciwing.api.routers import parscit
+from sciwing.api.routers import citation_intent_clf
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return {"message": "Welcome To SciWING API"}
+
+
+# add the routers to the main app
+app.include_router(parscit.router)
+app.include_router(citation_intent_clf.router)
