aa8db11489634d4043d0817089760637a12999f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:04 +0000

    Add package BittyTax/0.3.2/BittyTax-0.3.2-py2.py3-none-any.whl

---
+++ packages/BittyTax/BittyTax-0.3.2-py2.py3-none-any.whl/bittytax/conv/parsers/trezor.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# (c) Nano Nano Ltd 2019
+
+from decimal import Decimal
+
+from ...config import config
+from ..out_record import TransactionOutRecord
+from ..dataparser import DataParser
+from ..exceptions import UnknownCryptoassetError, UnexpectedTypeError
+
+WALLET = "Trezor"
+
+def parse_trezor(data_row, parser):
+    in_row = data_row.in_row
+    data_row.timestamp = DataParser.parse_timestamp(in_row[0] + 'T' + in_row[1])
+
+    if not config.args.cryptoasset:
+        raise UnknownCryptoassetError
+
+    if in_row[5] == "IN":
+        data_row.t_record = TransactionOutRecord(TransactionOutRecord.TYPE_DEPOSIT,
+                                                 data_row.timestamp,
+                                                 buy_quantity=in_row[7],
+                                                 buy_asset=config.args.cryptoasset,
+                                                 wallet=WALLET)
+    elif in_row[5] == "OUT" or in_row[5] == "SELF":
+        data_row.t_record = TransactionOutRecord(TransactionOutRecord.TYPE_WITHDRAWAL,
+                                                 data_row.timestamp,
+                                                 sell_quantity=abs(Decimal(in_row[7])),
+                                                 sell_asset=config.args.cryptoasset,
+                                                 wallet=WALLET)
+    else:
+        raise UnexpectedTypeError(5, parser.in_header[5], in_row[5])
+
+DataParser(DataParser.TYPE_WALLET,
+           "Trezor",
+           ['Date', 'Time', 'TX id', 'Address', 'Address Label', 'TX type', 'Value', 'TX total',
+            'Balance'],
+           worksheet_name="Trezor",
+           row_handler=parse_trezor)
