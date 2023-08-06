54293816f181afea14adcdc0fb0fd902464406e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:48 +0000

    Add package pulsemixer/1.5.1/pulsemixer-1.5.1.tar.gz

---
+++ packages/pulsemixer/pulsemixer-1.5.1.tar.gz/pulsemixer-1.5.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='pulsemixer',
+    version='1.5.1',
+    description='pulsemixer - CLI and curses mixer for PulseAudio',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/GeorgeFilipkin/pulsemixer',
+    author='George Filipkin',
+    author_email='botebotebot@gmail.com',
+    license='MIT',
+    scripts=['pulsemixer'],
+    classifiers=[
+        'Environment :: Console',
+        'Environment :: Console :: Curses',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Multimedia :: Sound/Audio :: Mixers',
+        'Topic :: Utilities',
+    ],
+)
