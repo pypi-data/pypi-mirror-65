4b1079471a74b4d7bf93eadbf23ea7498c4a11df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:45 +0000

    Add package putio-automator/2.0.0.dev5/putio-automator-2.0.0.dev5.tar.gz

---
+++ packages/putio-automator/putio-automator-2.0.0.dev5.tar.gz/putio-automator-2.0.0.dev5/putio_automator/commands/__init__.py
@@ -0,0 +1,9 @@
+"""
+Flask commands
+"""
+from .account import manager as account
+from .config import manager as config
+from .db import manager as db
+from .files import manager as files
+from .torrents import manager as torrents
+from .transfers import manager as transfers
