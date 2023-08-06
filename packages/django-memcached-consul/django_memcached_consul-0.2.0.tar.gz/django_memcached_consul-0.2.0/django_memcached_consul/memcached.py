fd875f9a45787167c21ee380943e00eb81eed735
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:07 +0000

    Add package django-memcached-consul/0.2.0/django_memcached_consul-0.2.0.tar.gz

---
+++ packages/django-memcached-consul/django_memcached_consul-0.2.0.tar.gz/django_memcached_consul-0.2.0/django_memcached_consul/memcached.py
@@ -0,0 +1,70 @@
+from django.core.cache import caches
+from django.core.cache.backends import memcached
+
+
+import consul
+import requests
+import logging
+
+
+def get_servers(params):
+    """Get the list of cache servers either from cache or directly from Consul."""
+    try:
+        consul_cache = caches[params['CONSUL_CACHE']]
+    except KeyError:
+        consul_cache = False
+
+    cache_key = params["CONSUL_SERVICE"]
+    alt_cache_key = "alt:%s" % params["CONSUL_SERVICE"]
+
+    if consul_cache:
+        cached = consul_cache.get(cache_key)
+    else:
+        cached = False
+
+    if cached:
+        return cached
+    else:
+        try:
+            servers = get_servers_list_from_consul(params)
+            if consul_cache:
+                consul_cache.set(cache_key, servers, params["CONSUL_TTL"])
+                consul_cache.set(alt_cache_key, servers, params["CONSUL_ALT_TTL"])
+        except(requests.exceptions.ConnectionError):
+            logging.warning("Cannot connect to Consul")
+            if consul_cache:
+                alt_cached = consul_cache.get(alt_cache_key)
+                if alt_cached:
+                    return alt_cached
+
+            servers = []
+        return servers
+
+
+def get_servers_list_from_consul(params):
+    """Get the list of cache servers using Consul."""
+    servers = []
+    consul_api = consul.Consul(host=params["CONSUL_HOST"], port=params["CONSUL_PORT"])
+    index, data = consul_api.health.service(service=params["CONSUL_SERVICE"], passing=True)
+    for node in data:
+        addr = (
+            node["Service"]["Address"]
+            if "Address" in node["Service"] and node["Service"]["Address"]
+            else node["Node"]["Address"]
+        )
+        servers.append("%s:%s" % (addr, node["Service"]["Port"]))
+    return servers
+
+
+class BaseMemcachedCache(memcached.BaseMemcachedCache):
+    def __init__(self, server, params, library, value_not_found_exception):
+        server = get_servers(params)
+        memcached.BaseMemcachedCache.__init__(
+            self, server, params, library, value_not_found_exception)
+
+
+class MemcachedCache(memcached.MemcachedCache, BaseMemcachedCache):
+    def __init__(self, server, params):
+        import memcache
+        BaseMemcachedCache.__init__(
+            self, server, params, library=memcache, value_not_found_exception=ValueError)
---
+++ packages/django-memcached-consul/django_memcached_consul-0.2.0.tar.gz/django_memcached_consul-0.2.0/setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# Copyright 2018 Criteo
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import os
+from setuptools import find_packages, setup
+
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+def _read(relpath):
+    fullpath = os.path.join(os.path.dirname(__file__), relpath)
+    with open(fullpath) as f:
+        return f.read()
+
+
+def _read_reqs(relpath):
+    fullpath = os.path.join(os.path.dirname(__file__), relpath)
+    with open(fullpath) as f:
+        return [s.strip() for s in f.readlines()
+                if (s.strip() and not s.startswith("#"))]
+
+
+_REQUIREMENTS_TXT = _read_reqs("requirements.txt")
+
+setup(
+    name="django_memcached_consul",
+    version="0.2.0",
+    maintainer="Criteo",
+    maintainer_email="github@criteo.com",
+    description="Django module that use consul to generate the list of memcached servers",
+    long_description=_read("README.md"),
+    long_description_content_type="text/markdown",
+    license="Apache Software License",
+    keywords="django memcached consul",
+    url="https://github.com/criteo/django-memcached-consul",
+    packages=find_packages(),
+    test_suite="tests",
+    install_requires=_REQUIREMENTS_TXT,
+    dependency_links=_REQUIREMENTS_TXT,
+    tests_require=_read_reqs("tests-requirements.txt"),
+)
