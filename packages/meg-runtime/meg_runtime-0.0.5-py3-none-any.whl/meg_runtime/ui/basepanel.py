f0496d86edd9c90757bd87ffce3cd51d29d72929
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:10 +0000

    Add package meg-runtime/0.0.5/meg_runtime-0.0.5-py3-none-any.whl

---
+++ packages/meg-runtime/meg_runtime-0.0.5-py3-none-any.whl/meg_runtime/ui/basepanel.py
@@ -0,0 +1,27 @@
+"""MEG UI Base Panel
+"""
+from PyQt5 import QtWidgets, uic
+
+import pkg_resources
+
+from meg_runtime.logger import Logger
+
+
+class BasePanel(QtWidgets.QMainWindow):
+    """Base widget panel."""
+
+    def __init__(self, **kwargs):
+        """UI manager constructor."""
+        super().__init__(**kwargs)
+        # Load the UI file
+        path = pkg_resources.resource_filename(
+            __name__,
+            f'/{self.__class__.__name__.lower()}.ui'
+        )
+        try:
+            uic.loadUi(path, self)
+        except Exception as e:
+            Logger.warning('MEG: BasePanel: {}'.format(e))
+            Logger.warning('MEG: BasePanel: Could not load path {}'.format(path))
+
+
