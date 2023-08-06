7e5bd4a5286a258c176bbe629a0091208a1ff613
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:08 +0000

    Add package Gaussian-binomial-ML/0.1/Gaussian_binomial_ML-0.1.tar.gz

---
+++ packages/Gaussian-binomial-ML/Gaussian_binomial_ML-0.1.tar.gz/Gaussian_binomial_ML-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='Gaussian_binomial_ML',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['Gaussian_binomial_ML'],
+      author='IkraP',
+      zip_safe=False)
