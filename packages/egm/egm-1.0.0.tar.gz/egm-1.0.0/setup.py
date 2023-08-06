9c393956bae3b5ee5fc9dde2cb85a02dc18ae171
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:21 +0000

    Add package egm/1.0.0/egm-1.0.0.tar.gz

---
+++ packages/egm/egm-1.0.0.tar.gz/egm-1.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name="egm",
+    version="1.0.0",
+    author="Mohit Bhatnagar",
+    author_email="mohit@uplytics.com",
+    keywords=['Plotly', 'evidence gap map', 'bubble graphs', 'interactive graphs'],
+    description="A python package for plotting Evidence Gap Maps using Plotly",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://https://github.com/mb7419/egm/",
+    packages=find_packages(),
+    install_requires=[
+          'plotly',
+          'pandas',
+    ],
+    zip_safe=False)
