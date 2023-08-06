af0a2517873a4ed2e54fa0c041d0bdc82d3aef9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:49 +0000

    Add package imread/0.7.3/imread-0.7.3-cp27-cp27mu-manylinux2010_x86_64.whl

---
+++ packages/imread/imread-0.7.3-cp27-cp27mu-manylinux2010_x86_64.whl/imread/tests/test_bad.py
@@ -0,0 +1,42 @@
+from nose.tools import raises
+from imread import imread
+from . import file_path
+
+
+BAD_FILES = [
+    'bad-files/LSM/00r/00r01.lsm',
+    'bad-files/LSM/00r/00r03.lsm',
+    'bad-files/LSM/00r/00r02.lsm',
+    'bad-files/LSM/00r/00r00.lsm',
+
+    'bad-files/LSM/01r/01r00.lsm',
+    'bad-files/LSM/01r/01r01.lsm',
+    'bad-files/LSM/01r/01r02.lsm',
+
+    'bad-files/LSM/02r/02r00.lsm',
+    'bad-files/LSM/02r/02r01.lsm',
+    'bad-files/LSM/02r/02r02.lsm',
+
+    'bad-files/LSM/03w/03w01.lsm',
+    'bad-files/LSM/03w/03w00.lsm',
+    'bad-files/LSM/03w/03w02.lsm',
+    'bad-files/LSM/03w/03w03.lsm',
+
+    'bad-files/TIFF/00r00.tiff',
+
+    'bad-files/BMP/00r/00r00.bmp',
+    'bad-files/BMP/00r/00r01.bmp',
+    'bad-files/BMP/00r/00r02.bmp',
+    'bad-files/BMP/00r/00r03.bmp',
+    'bad-files/BMP/00r/00r04.bmp',
+    'bad-files/BMP/00r/00r05.bmp',
+    'bad-files/BMP/00r/00r06.bmp',
+
+]
+def test_read():
+    @raises(RuntimeError)
+    def read1(fname):
+        imread(file_path(fname))
+        assert False
+    for fname in BAD_FILES:
+        read1(fname)
