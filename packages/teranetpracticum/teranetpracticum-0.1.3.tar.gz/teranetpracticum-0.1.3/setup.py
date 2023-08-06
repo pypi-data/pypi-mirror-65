934cd33e6df56de6b85de140546c0373def346db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:06 +0000

    Add package teranetpracticum/0.1.3/teranetpracticum-0.1.3.tar.gz

---
+++ packages/teranetpracticum/teranetpracticum-0.1.3.tar.gz/teranetpracticum-0.1.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+# with open("README.md", "r") as re:
+#     long_description = re.read()
+
+setup(
+    name='teranetpracticum',
+    version='0.1.3',
+    author='kaiyuwang',
+    author_email='kaiyuwangwork@gmail.com',
+    url='https://github.com/kaiyuwang-work',
+    description=u'teranet practicum package test',
+    scripts=["__init__.py"],
+    packages=['teranetpracticum'],
+    install_requires=[],
+
+    entry_points={
+        'console_scripts': [
+            'national=teranetpracticum:national',
+            'bc=teranetpracticum:bc'
+        ]
+    }
+)
