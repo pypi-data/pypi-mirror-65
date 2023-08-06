661ffacf173ab93a0942c6a29cd126f75735153d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:25 +0000

    Add package django-admin-locking/0.8.3/django-admin-locking-0.8.3.tar.gz

---
+++ packages/django-admin-locking/django-admin-locking-0.8.3.tar.gz/django-admin-locking-0.8.3/setup.py
@@ -0,0 +1,42 @@
+import os
+import codecs
+from setuptools import setup, find_packages
+
+
+def read(*parts):
+    filename = os.path.join(os.path.dirname(__file__), *parts)
+    with codecs.open(filename, encoding='utf-8') as fp:
+        return fp.read()
+
+
+setup(
+    name="django-admin-locking",
+    version='0.8.3',
+    description="A reusable app for locking objects within the admin while another editor"
+    "is editing the page.",
+    long_description=open('README.rst').read(),
+    author="Jonas und der Wolf GmbH",
+    author_email="opensource@jonasundderwolf.de",
+    url="https://github.com/jonasundderwolf/django-admin-locking",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
