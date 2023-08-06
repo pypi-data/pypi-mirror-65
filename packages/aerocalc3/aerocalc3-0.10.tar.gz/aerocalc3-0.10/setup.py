1c7ddbbfcd5f2ac864787fae10b8e2cf55c055f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:59 +0000

    Add package aerocalc3/0.10/aerocalc3-0.10.tar.gz

---
+++ packages/aerocalc3/aerocalc3-0.10.tar.gz/aerocalc3-0.10/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+
+setup(
+    name="aerocalc3",
+    version="0.10",
+    description="Aeronautical Engineering Calculations",
+    long_description="""
+                        AeroCalc is a pure python package that performs various aeronautical
+                        engineering calculations.  Currently it provides airspeed conversions,
+                        standard atmosphere calculations, static source error correction calculations
+                        and unit conversions.
+                    """,
+    author="Geoffrey Nyaga",
+    author_email="geoffrey@geoffreynyaga.com",
+    url="https://geoffreynyaga.com/aerocalc3/",
+    download_url="https://github.com/geoffreynyaga/aerocalc/archive/v_01.tar.gz",
+    packages=["aerocalc3"],
+    license="BSD",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Scientific/Engineering :: Atmospheric Science",
+        "Programming Language :: Python :: 3",
+    ],
+)
