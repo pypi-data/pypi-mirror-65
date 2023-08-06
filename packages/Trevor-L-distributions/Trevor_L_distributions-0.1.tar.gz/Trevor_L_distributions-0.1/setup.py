af681047cd4ffd789c46652cc74364b4c7013490
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:58 +0000

    Add package Trevor-L-distributions/0.1/Trevor_L_distributions-0.1.tar.gz

---
+++ packages/Trevor-L-distributions/Trevor_L_distributions-0.1.tar.gz/Trevor_L_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Trevor_L_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['Trevor_L_distributions'],
+      author = 'Trevor LaViale',
+      zip_safe=False)
+
