4e67e7144a21377886a0f62af424ec2814a7dd4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:34 +0000

    Add package kalepy/0.3.0/kalepy-0.3.0.tar.gz

---
+++ packages/kalepy/kalepy-0.3.0.tar.gz/kalepy-0.3.0/kalepy/__init__.py
@@ -0,0 +1,106 @@
+"""
+"""
+
+import os
+
+_path = os.path.dirname(os.path.abspath(__file__))
+_vers_path = os.path.join(_path, "VERSION.txt")
+with open(_vers_path) as inn:
+    _version = inn.read().strip()
+
+__version__ = _version
+__author__ = "Luke Zoltan Kelley <lzkelley@northwestern.edu>"
+__copyright__ = "Copyright 2019 - Luke Zoltan Kelley and contributors"
+# __contributors__ = []
+# __bibtex__ = ""
+
+# Numerical padding parameter (e.g. to avoid edge issues, etc)
+_NUM_PAD = 1e-8
+# Zero-out the PDF of kernels with infinite-support beyond this probability
+_TRUNCATE_INFINITE_KERNELS = 1e-8
+# Default bandwidth calculation method
+_BANDWIDTH_DEFAULT = 'scott'
+
+_PATH_NB_OUT = os.path.join(_path, 'notebooks', 'output')
+
+
+from kalepy import kernels   # noqa
+# from kalepy.kernels import *  # noqa
+from kalepy import utils   # noqa
+# from kalepy.utils import *  # noqa
+
+from kalepy.kde_base import KDE  # noqa
+
+# __all__ = []
+# __all__.extend(kernels.__all__)
+# __all__.extend(utils.__all__)
+
+del os
+del inn
+del _version
+del _vers_path
+del _path
+
+
+# High Level API Functions
+# -----------------------------------
+
+def pdf(data, edges=None, reflect=None, **kwargs):
+    """Use a KDE to calculate a PDF of the given data.
+
+    Arguments
+    ---------
+    edges : array_like of scalar or None
+        Locations at which to evaluate the PDF.
+        If `None`: edges are constructed using the `KDE._guess_edges()` method.
+    kwargs : dict
+        Additional key-value pair arguments passed to the `KDE.__init__` constructor.
+
+    Returns
+    -------
+    edges : (N,) array_like of scalar
+        Locations at which the PDF is evaluated.
+    vals : (N,) array_like of scalar
+        PDF evaluated at the given points
+
+    """
+    kde = KDE(data, **kwargs)
+    if edges is None:
+        edges = kde._guess_edges()
+        if len(edges) == 1:
+            import numpy as np
+            edges = np.array(edges).squeeze()
+
+    vals = kde.pdf(edges, reflect=reflect)
+    return edges, vals
+
+
+def cdf(data, edges=None, **kwargs):
+    """Use a KDE to calculate a CDF of the given data.
+
+    Arguments
+    ---------
+    edges : array_like of scalar or None
+        Locations at which to evaluate the CDF.
+        If `None`: edges are constructed using the `KDE._guess_edges()` method.
+    kwargs : dict
+        Additional key-value pair arguments passed to the `KDE.__init__` constructor.
+
+    Returns
+    -------
+    edges : (N,) array_like of scalar
+        Locations at which the CDF is evaluated.
+    vals : (N,) array_like of scalar
+        CDF evaluated at the given points
+
+    """
+    kde = KDE(data, **kwargs)
+    # NOTE/FIX: dont need to interpolate to `_guess_edges` as they are the interpolation points!
+    if edges is None:
+        edges = kde._guess_edges()
+        if len(edges) == 1:
+            import numpy as np
+            edges = np.array(edges).squeeze()
+
+    vals = kde.cdf(edges)
+    return edges, vals
---
+++ packages/kalepy/kalepy-0.3.0.tar.gz/kalepy-0.3.0/kalepy/plot.py
@@ -0,0 +1,175 @@
+"""Plotting methods
+"""
+
+import os
+import six
+
+import numpy as np
+import matplotlib as mpl  # noqa
+from matplotlib import pyplot as plt
+
+from kalepy import utils
+
+# __all__ = ["align_axes_loc", "nbshow"]
+
+
+def align_axes_loc(tw, ax, ymax=None, ymin=None, loc=0.0):
+    if ((ymax is None) and (ymin is None)) or ((ymin is not None) and (ymax is not None)):
+        raise ValueError("Either `ymax` or `ymin`, and not both, must be provided!")
+
+    ylim = np.array(ax.get_ylim())
+    # beg = np.array(tw.get_ylim())
+
+    hit = np.diff(ylim)[0]
+    frac_up = (loc - ylim[0]) / hit
+    frac_dn = 1 - frac_up
+
+    new_ylim = [0.0, 0.0]
+    if ymax is not None:
+        new_ylim[1] = ymax
+        new_hit = (ymax - loc) / frac_dn
+        new_ylim[0] = ymax - new_hit
+
+    if ymin is not None:
+        new_ylim[0] = ymin
+        new_hit = (loc - ymin) / frac_up
+        new_ylim[1] = ymax - new_hit
+
+    tw.set_ylim(new_ylim)
+    return new_ylim
+
+
+def draw_carpet_fuzz(xx, ax=None, ystd=None, yave=None, fancy=False, random='normal', **kwargs):
+    """Draw a carpet plot on the given axis in the 'fuzz' style.
+
+    Arguments
+    ---------
+    xx : values to plot
+    ax : matplotlib.axis.Axis
+    kwargs : key-value pairs
+        Passed to `matplotlib.axes.Axes.scatter()`
+
+    """
+    if ax is None:
+        ax = plt.gca()
+        if ystd is None:
+            ystd = 0.02
+
+    # Dispersion (yaxis) of the fuzz values
+    if ystd is None:
+        if yave is None:
+            ystd = ax.get_ylim()[1] * 0.02
+        else:
+            ystd = np.fabs(yave) / 5.0
+
+    # Baseline on the yaxis at which the fuzz is plotted
+    if yave is None:
+        yave = -5.0 * ystd
+
+    # General random y-values for the fuzz
+    if random.lower() == 'normal':
+        yy = np.random.normal(yave, ystd, size=xx.size)
+    elif random.lower() == 'uniform':
+        yy = np.random.uniform(yave-ystd, yave+ystd, size=xx.size)
+    else:
+        raise ValueError("Unrecognized `random` = '{}'!".format(random))
+
+    # Choose an appropriate opacity
+    alpha = np.clip(10 / np.sqrt(xx.size), 1e-4, 3e-1)
+
+    # Choose sizes proportional to their deviation (to make outliers more visible)
+    size = np.clip(300 / np.sqrt(xx.size), 5, 100)
+
+    if fancy:
+        # Estimate the deviation of each point from the median
+        dev = np.fabs(xx - np.median(xx)) / np.std(xx)
+        # Rescale the y-values based on their deviation from median
+        yy = (yy - yave) / (np.sqrt(dev) + 1) + yave
+        # Choose sizes proportional to their deviation (to make outliers more visible)
+        size = (size / 1.5) * (1.5 + dev)
+
+    # Set parameters
+    color = kwargs.pop('color', 'red')
+    kwargs.setdefault('facecolor', color)
+    kwargs.setdefault('edgecolor', 'none')
+    kwargs.setdefault('marker', '.')
+    kwargs.setdefault('alpha', alpha)
+    kwargs.setdefault('s', size)
+
+    return ax.scatter(xx, yy, **kwargs)
+
+
+def smap(args=[0.0, 1.0], cmap=None, norm=None,
+         under='0.8', over='0.8', left=None, right=None):
+    args = np.asarray(args)
+
+    if not isinstance(cmap, mpl.colors.Colormap):
+        if cmap is None:
+            cmap = 'viridis'
+        if isinstance(cmap, six.string_types):
+            cmap = plt.get_cmap(cmap)
+
+    if under is not None:
+        cmap.set_under(under)
+    if over is not None:
+        cmap.set_over(over)
+
+    vmin, vmax = utils.minmax(args)
+    # norm = mpl.colors.LogNorm(vmin=min, vmax=max)
+    # norm = MidpointLogNormalize(vmin=min, vmax=max, midpoint=midpoint)
+    # norm = MidpointNormalize(vmin=min, vmax=max, midpoint=midpoint)
+    norm = mpl.colors.Normalize(vmin=vmin, vmax=vmax)
+
+    # Create scalar-mappable
+    smap = mpl.cm.ScalarMappable(norm=norm, cmap=cmap)
+    # Bug-Fix something something
+    smap._A = []
+
+    return smap
+
+
+def nbshow():
+    return utils.run_if_notebook(plt.show, otherwise=lambda: plt.close('all'))
+
+
+def save_fig(fig, fname, path=None, quiet=False, rename=True, **kwargs):
+    """Save the given figure to the given filename, with some added niceties.
+    """
+    if path is None:
+        path = os.path.abspath(os.path.curdir)
+    fname = os.path.join(path, fname)
+    utils.check_path(fname)
+    if rename:
+        fname = utils.modify_exists(fname)
+    fig.savefig(fname, **kwargs)
+    if not quiet:
+        print("Saved to '{}'".format(fname))
+    return fname
+
+
+class plot_control:
+
+    def __init__(self, fname, *args, **kwargs):
+        self.fname = fname
+        self.args = args
+        self.kwargs = kwargs
+        return
+
+    def __enter__(self):
+        # if not _is_notebook():
+        #     raise _DummyError
+
+        plt.close('all')
+        return self
+
+    def __exit__(self, type, value, traceback):
+        # if isinstance(value, _DummyError):
+        #     return True
+
+        plt.savefig(self.fname, *self.args, **self.kwargs)
+        if utils._is_notebook():
+            plt.show()
+        else:
+            plt.close('all')
+
+        return
