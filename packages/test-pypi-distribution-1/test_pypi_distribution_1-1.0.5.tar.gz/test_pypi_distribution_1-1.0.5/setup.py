a64f4a5cc7bd53625b4607599cb8066d6df328a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:43 +0000

    Add package test-pypi-distribution-1/1.0.5/test_pypi_distribution_1-1.0.5.tar.gz

---
+++ packages/test-pypi-distribution-1/test_pypi_distribution_1-1.0.5.tar.gz/test_pypi_distribution_1-1.0.5/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name="test_pypi_distribution_1",
+    version="1.0.5",
+    py_modules=["test_pypi_distribution_1"],
+    author="lfalanga",
+    author_email="lfalanga@gmail.com",
+    url="https://github.com/lfalanga/test_pypi_distribution_1",
+    description="My first PyPi distribution.",
+    )
---
+++ packages/test-pypi-distribution-1/test_pypi_distribution_1-1.0.5.tar.gz/test_pypi_distribution_1-1.0.5/test_pypi_distribution_1.py
@@ -0,0 +1,7 @@
+def echo_indented_list(item, level=0):
+    for x in item:
+        if isinstance(x, list):
+            echo_indented_list(x, level + 1)
+        else:
+            print("\t"*level + "-> ", end="")
+            print(x)
