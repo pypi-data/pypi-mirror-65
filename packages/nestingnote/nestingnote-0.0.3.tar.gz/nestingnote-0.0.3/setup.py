936582850fa26fa30d1e7e635e62ae35a0b02a22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:17:13 +0000

    Add package nestingnote/0.0.3/nestingnote-0.0.3.tar.gz

---
+++ packages/nestingnote/nestingnote-0.0.3.tar.gz/nestingnote-0.0.3/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+from sys import platform
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nestingnote",
+    version="0.0.3",
+    author="Woodson Miles",
+    author_email="woodomiles@gmail.com",
+    description="Terminal note-taking application",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/woodsonmiles/nesting-note",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>3.6',
+    install_requires=[] + ( ["windows-curses"] if platform.startswith("win") else [] )
+)
