3d4fdf19cdf2906f501f9a02bac4ef3117266618
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:13 +0000

    Add package feishupy/1.1.13/feishupy-1.1.13.tar.gz

---
+++ packages/feishupy/feishupy-1.1.13.tar.gz/feishupy-1.1.13/feishupy/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Author: WUWUTech
+# Contact: wuwu@wuwu.tech
+#
+# References
+#   https://open.feishu.cn/document/ukTMukTMukTM/uITNz4iM1MjLyUzM
+
+
+TOKEN_TYPE_APP_ACCESS_TOKEN_INTERNAL = 'app_access_token_internal'
+
+from .http_client import HttpClient
+from .feishu_client import FeiShuClient
