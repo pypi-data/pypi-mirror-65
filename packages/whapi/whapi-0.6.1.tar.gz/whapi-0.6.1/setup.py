a8892e178a3d47f5629f17d974ac6a36f7e22091
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:53 +0000

    Add package whapi/0.6.1/whapi-0.6.1.tar.gz

---
+++ packages/whapi/whapi-0.6.1.tar.gz/whapi-0.6.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='whapi',
+    version='0.6.1',
+    packages=['whapi'],
+    url='https://github.com/killhamster/WHAPI',
+    install_requires=["requests", "bs4"],
+    license='MIT',
+    author='killhamster',
+    author_email='killhamster@gmail.com',
+    description='An unofficial WikiHow API wrapper',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
