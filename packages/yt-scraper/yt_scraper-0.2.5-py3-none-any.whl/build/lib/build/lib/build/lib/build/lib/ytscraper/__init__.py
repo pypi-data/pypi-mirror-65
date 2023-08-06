e55d2a0b633710d081967ab19bc21157ef34bd48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:27 +0000

    Add package yt-scraper/0.2.5/yt_scraper-0.2.5-py3-none-any.whl

---
+++ packages/yt-scraper/yt_scraper-0.2.5-py3-none-any.whl/build/lib/build/lib/build/lib/build/lib/ytscraper/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+__title__ = 'ytscraper'
+__app_name__ = 'Youtube Scraper'
+__version__ = '0.2.5'
+__author__ = 'Michael Brauweiler'
+__license__ = 'GPLv3'
+__copyright__ = 'Copyright 2020, Michael Brauweiler'
+
