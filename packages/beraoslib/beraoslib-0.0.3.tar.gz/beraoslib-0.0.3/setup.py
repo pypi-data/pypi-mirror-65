a4b73f94a44d7b42f2afbde3e3ed9940b08d3f96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:58 +0000

    Add package beraoslib/0.0.3/beraoslib-0.0.3.tar.gz

---
+++ packages/beraoslib/beraoslib-0.0.3.tar.gz/beraoslib-0.0.3/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+import os
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="beraoslib", # Replace with your own username
+    version="0.0.3",
+    author="Ethan Brammah",
+    author_email="bearoshelp@gmail.com",
+    description="A small example package of bear os",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Dragon445/bearoslib/blob/master/__init__.py",
+    packages=setuptools.find_packages(),
+    install_requires=["pyinstaller","cryptography","sounddevice","playsound", "pyperclip"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+os.system("pip install pyinstaller")
+os.system("pip install cryptography")
+os.system("pip install sounddevice")
+os.system("pip install playsound")
+os.system("pip install pyperclip")
