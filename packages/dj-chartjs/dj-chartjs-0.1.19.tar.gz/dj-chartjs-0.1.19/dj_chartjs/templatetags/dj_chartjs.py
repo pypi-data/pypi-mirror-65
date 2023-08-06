0d9d7b1b89e57d5bdf72b1cb3a30e2daa869d802
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:01 +0000

    Add package dj-chartjs/0.1.19/dj-chartjs-0.1.19.tar.gz

---
+++ packages/dj-chartjs/dj-chartjs-0.1.19.tar.gz/dj-chartjs-0.1.19/dj_chartjs/templatetags/dj_chartjs.py
@@ -0,0 +1,16 @@
+from django import template
+
+register = template.Library()
+
+
+@register.inclusion_tag("dj_chartjs/chart.html", takes_context=True)
+def render_chart(context, values):
+    chart = {
+        "chart": values,
+        "type_chart": context["type"],
+        "legend_text": context.get("legend") if context.get("legend") else "",
+        "width": context.get("width"),
+        "height": context.get("height"),
+    }
+
+    return chart
