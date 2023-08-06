e68ecc61e10e4f4a90bdc523b0790ef47c2da643
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:26 +0000

    Add package canvas-client/0.1.1/canvas_client-0.1.1.tar.gz

---
+++ packages/canvas-client/canvas_client-0.1.1.tar.gz/canvas_client-0.1.1/setup.py
@@ -0,0 +1,34 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="canvas_client",
+    version="0.1.1",
+    description="Canvas LMS client: Download submissions, upload grades and comments from excel",
+    long_desccription=README,
+    long_description_content_type="text/markdown",
+    author="Csanad Sandor",
+    author_email="scsanad@cs.ubbcluj.ro",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+    ],
+    packages=["canvas_client"],
+    include_package_data=True,
+    install_requires=[  "jsonpickle", "requests", "pandas", \
+                        "pyunpack", "xlwt", "xlrd"],
+    entry_points={
+        "console_scripts": [
+            "canvas_client=canvas_client.__main__:main",
+        ]
+    },
+)
