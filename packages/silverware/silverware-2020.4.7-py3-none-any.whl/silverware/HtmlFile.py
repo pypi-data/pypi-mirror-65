752c9d89172a7b0c71051056fa0e5051506f4175
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:42 +0000

    Add package silverware/2020.4.7/silverware-2020.4.7-py3-none-any.whl

---
+++ packages/silverware/silverware-2020.4.7-py3-none-any.whl/silverware/HtmlFile.py
@@ -0,0 +1,40 @@
+from disk import Path
+from bs4 import BeautifulSoup
+
+
+from .Spoon import Spoon
+
+
+class HtmlFile(Path):
+	def __init__(self, string, show_size=False):
+		super().__init__(string=string, show_size=show_size)
+		self._soup = None
+		self._spoon = None
+		self._paragraphs = None
+
+	@property
+	def soup(self):
+		"""
+		:rtype: BeautifulSoup
+		"""
+		if self._soup is None:
+			self._soup = BeautifulSoup('\n'.join(self.read_lines()), 'lxml')
+		return self._soup
+
+	@property
+	def spoon(self):
+		"""
+		:rtype: Spoon
+		"""
+		if self._spoon is None:
+			self._spoon = Spoon(soup=self.soup)
+		return self._spoon
+
+	@property
+	def paragraphs(self):
+		"""
+		:rtype: list[str]
+		"""
+		if self._paragraphs is None:
+			self._paragraphs = self.spoon.paragraphs
+		return self._paragraphs
