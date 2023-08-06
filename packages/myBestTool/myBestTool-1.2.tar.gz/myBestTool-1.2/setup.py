550ffd984d1887380e6a8546c9ae88ae42fba1dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:28 +0000

    Add package myBestTool/1.2/myBestTool-1.2.tar.gz

---
+++ packages/myBestTool/myBestTool-1.2.tar.gz/myBestTool-1.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+setup(name='myBestTool',
+      version='1.2',
+      description='my Tool',
+      author='Du HongYu',
+      author_email='837058201@qq.com',
+      packages=['tool'],
+      zip_safe=False,
+      install_requires=[
+            'pika',
+            'requests',
+            'lxml',
+            'redis',
+      ]
+      )
