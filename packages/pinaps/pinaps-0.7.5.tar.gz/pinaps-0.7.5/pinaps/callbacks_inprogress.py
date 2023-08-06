3bc84fdb689033740bb32a34a2345104e22ed916
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:16:31 +0000

    Add package pinaps/0.7.5/pinaps-0.7.5.tar.gz

---
+++ packages/pinaps/pinaps-0.7.5.tar.gz/pinaps-0.7.5/pinaps/callbacks_inprogress.py
@@ -0,0 +1,41 @@
+from piNapsController import PiNapsController
+from NeuroParser import NeuroParser
+
+def printCallback(packet):
+    if(packet.code == NeuroParser.DataPacket.kPoorQuality):
+        print("Poor quality: " + str(packet.poorQuality))
+    if(packet.code == NeuroParser.DataPacket.kAttention):
+        print("Attention: " + str(packet.attention))
+    if(packet.code == NeuroParser.DataPacket.kMeditation):
+        print("Meditation: " + str(packet.meditation))
+    if(packet.code == NeuroParser.DataPacket.kRawSignal):
+        print("Raw: " + str(packet.rawSamples))
+    if(packet.code == NeuroParser.DataPacket.kIntEEGPowers):
+        print("Delta Powers: " + str(packet.delta))
+        print("Theta Powers: " + str(packet.theta))
+        print("Low Alpha Powers: " + str(packet.lAlpha))
+        print("High Alpha Powers: " + str(packet.hAlpha))
+        print("Low Beta Powers: " + str(packet.lBeta))
+        print("High Beta Powers: " + str(packet.hBeta))
+        print("Low Gamma Powers: " + str(packet.lGamma))
+        print("Medium Gamma Powers: " + str(packet.mGamma))
+
+pinapsController = PiNapsController()
+
+def main():
+    pinapsController.setControlInterfaceI2C()
+    pinapsController.setEEGSensorInterfaceI2C()
+    pinapsController.setFullMode()
+
+    pinapsController.deactivateAllLEDs()
+
+    aParser = NeuroParser()
+
+    while True:
+        data = pinapsController.readEEGSensor()
+        #print(pinapsController.dataWaiting())
+        for d in data:
+            aParser.parse(d, printCallback)
+
+if __name__ == '__main__':
+    main()
