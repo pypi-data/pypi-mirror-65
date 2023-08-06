bcfd5a8fd72da17689b7e22208a5fb6379cc5d0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:17 +0000

    Add package tdlogging/0.0.5/tdlogging-0.0.5.tar.gz

---
+++ packages/tdlogging/tdlogging-0.0.5.tar.gz/tdlogging-0.0.5/example/cool/cooler/sleep.py
@@ -0,0 +1,7 @@
+
+import time
+
+class Sleep:
+    @staticmethod
+    def sleep(n):
+        time.sleep(n)
---
+++ packages/tdlogging/tdlogging-0.0.5.tar.gz/tdlogging-0.0.5/example/logger_instance.py
@@ -0,0 +1,5 @@
+from tdlogging.tdlogger import TDLogger
+
+logger = TDLogger(alias="My Custom Logger").config()
+
+
