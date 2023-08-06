f72db696bdb6767e65e3dd538757c5e7c4502e2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:57 +0000

    Add package qf-package/0.1/qf-package-0.1.tar.gz

---
+++ packages/qf-package/qf-package-0.1.tar.gz/qf-package-0.1/create_package.py
@@ -0,0 +1,8 @@
+from qf_package import create
+
+create(
+    version='0.1',
+    name='qf-package', packages=['qf_package'],
+    url='https://github.com/quality-first/qf-package',
+    description='Useful things for QF projects sharing',
+)
