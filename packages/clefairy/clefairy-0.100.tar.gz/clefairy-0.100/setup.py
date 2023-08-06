b991e5be2db96bae059c983820602a1f42e47781
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:51 +0000

    Add package clefairy/0.100/clefairy-0.100.tar.gz

---
+++ packages/clefairy/clefairy-0.100.tar.gz/clefairy-0.100/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import os
+
+setup(
+    # Application name:
+    name="clefairy",
+
+    # Version number:
+    version="0.100",
+
+    # Application author details:
+    author="Vanessa Sochat",
+    author_email="vsochat@stanford.edu",
+
+    # Packages
+    packages=find_packages(),
+
+    # Data files
+    include_package_data=True,
+    zip_safe=False,
+
+    # Details
+    url="http://www.github.com/vsoch/pokemon",
+
+    license="LICENSE",
+    description="ascii database of pokemon... in python!",
+    keywords='pokemon, avatar, ascii, gravatar',
+
+    entry_points = {
+        'console_scripts': [
+            'pokemon=pokemon.scripts:main',
+        ],
+    },
+
+)
