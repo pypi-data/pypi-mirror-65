78bab139ae5fa2d69b56040e95d0e6026ab8252d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:50 +0000

    Add package omnitools/0.0.28/omnitools-0.0.28.tar.gz

---
+++ packages/omnitools/omnitools-0.0.28.tar.gz/omnitools-0.0.28/README.md
@@ -0,0 +1,37 @@
+# Omnitools
+
+<i>Miscellaneous functions written in short forms.</i>
+
+# Hierarchy
+```
+omnitools
+|---- p()
+|---- charenc()
+'---- Obj()
+```
+
+# Example
+
+## python
+```python
+from omnitools import *
+
+# print and always flush buffer
+p("abc")
+# abc
+# 
+
+# detect character encoding
+p(charenc(b"\xe3\x81\x82"))
+# utf-8
+
+# turn (nested) dict into an object
+p(Obj({"a":{"b":{"c":123}}}).a.b.c)
+# 123
+```
+
+## shell
+```shell script
+rem omnitools.exe <function name> [argument] ...
+omnitools.exe p abc
+```
---
+++ packages/omnitools/omnitools-0.0.28.tar.gz/omnitools-0.0.28/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+from importlib import import_module
+import os
+import re
+
+
+name = os.path.basename(os.path.dirname(os.path.abspath(__file__)))
+locals()[name] = import_module(name)
+readme = open("README.md").read()
+description = re.search(r"<i>(.*?)</i>", readme)[1]
+setup(
+    name="omnitools",
+    version=locals()[name].version,
+    keywords=locals()[name].keywords,
+    packages=[name],
+    url="https://github.com/foxe6-temp/omnitools",
+    license="AGPL-3.0",
+    author="f̣ộx̣ệ6",
+    author_email="foxe6@protonmail.com",
+    description=description,
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    install_requires=open("requirements.txt").read().splitlines(),
+    python_requires=">=3.7",
+    entry_points=dict(console_scripts=[name+"="+name+"."+locals()[name].entry])
+)
