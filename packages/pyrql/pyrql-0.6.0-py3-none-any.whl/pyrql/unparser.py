fedd6f89f1f363278f2e9755661dd86cca7feeb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:20 +0000

    Add package pyrql/0.6.0/pyrql-0.6.0-py3-none-any.whl

---
+++ packages/pyrql/pyrql-0.6.0-py3-none-any.whl/pyrql/unparser.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+
+class Unparser:
+    def unparse(self, expr):
+        name = expr.get("name")
+
+        args = []
+
+        for a in expr.get("args", []):
+            if isinstance(a, dict):
+                arg = self.unparse(a)
+
+            elif isinstance(a, tuple):
+                arg = self.unparse_tuple(a)
+
+            else:
+                arg = a
+
+            args.append(arg)
+
+        return "{}({})".format(name, ",".join(map(str, args)))
+
+    def unparse_tuple(self, arg):
+        prefix = ""
+        tokens = []
+
+        if arg[0] in {"+", "-"}:
+            prefix = arg[0]
+            arg = arg[1:]
+
+        for a in arg:
+            if isinstance(a, tuple):
+                tokens.append(self.unparse_tuple(a))
+            else:
+                tokens.append(a)
+
+        if len(tokens) == 1:
+            return prefix + tokens[0]
+
+        else:
+            return prefix + "(" + ",".join(tokens) + ")"
