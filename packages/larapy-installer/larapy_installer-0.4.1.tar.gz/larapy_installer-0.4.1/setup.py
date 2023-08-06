c39699598be7799163fa03188193316677bf84ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:03 +0000

    Add package larapy-installer/0.4.1/larapy_installer-0.4.1.tar.gz

---
+++ packages/larapy-installer/larapy_installer-0.4.1.tar.gz/larapy_installer-0.4.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name='larapy_installer',
+    version='0.4.01',
+    scripts=['larapy'],
+    author="Bedram Tamang",
+    author_email="tmgbedu@gmail.com",
+    description="Larapy installer for crafting larapy application.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LaravelPython/installer",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'cleo',
+        'requests',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
