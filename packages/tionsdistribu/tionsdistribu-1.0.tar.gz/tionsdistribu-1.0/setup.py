48c904afc80a08c93117d877b92743c152157ea4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:20 +0000

    Add package tionsdistribu/1.0/tionsdistribu-1.0.tar.gz

---
+++ packages/tionsdistribu/tionsdistribu-1.0.tar.gz/tionsdistribu-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='tionsdistribu',
+    version='1.0',
+    description='Code for distributions',
+    packages=['tionsdistribu'],
+    zip_safe=False
+)
