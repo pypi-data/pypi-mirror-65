69ccf9701a9471f9b2e49d557241d2fed5c3c4e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:30 +0000

    Add package kea-cerberus/0.1/kea_cerberus-0.1.tar.gz

---
+++ packages/kea-cerberus/kea_cerberus-0.1.tar.gz/kea_cerberus-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+ 
+setup(name='kea_cerberus',
+      version='0.1',
+      url='http://kea.mx/',
+      license='MIT',
+      author='Cesar Ocotitla',
+      author_email='cesar@kea.mx',
+      description='Control client, users and service execution of your projects in python',
+      zip_safe=False)
