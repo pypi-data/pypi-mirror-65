7785e3eb68fa77143e035ad3d17ceefb0441d3e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:53 +0000

    Add package sparks-distributions/0.2/sparks-distributions-0.2.tar.gz

---
+++ packages/sparks-distributions/sparks-distributions-0.2.tar.gz/sparks-distributions-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='sparks-distributions',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['sparks-distributions'],
+      author = 'Martin Gleditzsch',
+      zip_safe=False)
