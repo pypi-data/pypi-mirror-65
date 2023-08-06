f0e104edefee7914be45b13fc500a4beb5f527e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:09 +0000

    Add package zshpower/0.3.0/zshpower-0.3.0-py3-none-any.whl

---
+++ packages/zshpower/zshpower-0.3.0-py3-none-any.whl/zshpower/utils/check.py
@@ -0,0 +1,26 @@
+from shutil import which
+from os.path import join, exists
+from snakypy import printer, FG
+from zshpower.config import package
+
+
+def tools_requirements(*args):
+    for tool in args:
+        if which(tool) is None:
+            raise Exception(
+                f'The package \'{package.info["name"]}\' needs the "{tool}" tool.'
+                " Tool not found. Aborted."
+            )
+
+
+def checking_init(home):
+    """Function that ends commands that depend on the created repository, but
+    the repository was not created."""
+    if not exists(join(home, f".{package.info['pkg_name']}")):
+        printer(
+            f'Command "{package.info["pkg_name"]} init" has not been started.'
+            "Aborted",
+            foreground=FG.WARNING,
+        )
+        exit(1)
+    return True
