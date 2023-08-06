8eb114b2c6e79b5816b1bf41016f9bd2af178f2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:02 +0000

    Add package ZoomFoundry/6.3.2/ZoomFoundry-6.3.2.tar.gz

---
+++ packages/ZoomFoundry/ZoomFoundry-6.3.2.tar.gz/ZoomFoundry-6.3.2/zoom/models.py
@@ -0,0 +1,304 @@
+"""
+    zoom.models
+
+    common models
+"""
+
+import zoom
+from zoom.utils import DefaultRecord, id_for
+from zoom.helpers import link_to, url_for_item, url_for
+from zoom.utils import Record, id_for
+from zoom.records import RecordStore
+from zoom.users import Users
+
+
+class Model(DefaultRecord):
+    """Model Superclass
+
+    Provide basic model properties and functions.
+
+    Subclass this to create a Model that can be stored in
+    a RecordStore, EntityStore or some other type of store.
+
+    Assumes every record has an id attribute.  If not, you
+    will need to provide one via an additional property
+    defined in the subclass.
+
+    The key can end up being just the str of the id, however
+    it is provided separately to make it easy to provide human
+    friendly keys typically used in REST style URLs.  If used
+    this way the key should generated such that it is unique
+    for each record.
+
+    >>> zoom.system.site = site = zoom.sites.Site()
+    >>> zoom.system.request = zoom.utils.Bunch(route=[])
+    >>> class MyModel(Model):
+    ...     pass
+    >>> thing = MyModel(name='Pat Smith')
+
+    >>> thing.name
+    'Pat Smith'
+
+    >>> thing.key
+    'pat-smith'
+
+    >>> url_for_item('pat-smith')
+    '/pat-smith'
+
+    >>> thing.url
+    '/pat-smith'
+
+    >>> thing.link
+    '<a href="/pat-smith" name="link-to-pat-smith">Pat Smith</a>'
+
+    >>> thing.allows('user', 'edit')
+    False
+    """
+
+    @property
+    def key(self):
+        """Return the key"""
+        return id_for(self.name)
+
+    @property
+    def url(self):
+        """Return a valid URL"""
+        return url_for_item(self.key)
+
+    @property
+    def link(self):
+        """Return a link"""
+        return link_to(self.name, self.url)
+
+    def allows(self, user, action):
+        return False
+
+
+def get_users(db, group):
+    """Get users of a Group
+
+    Gets the users that are members of a group from a given database.
+
+    >>> site = zoom.sites.Site()
+    >>> users_group = Groups(site.db).first(name='users')
+    >>> get_users(site.db, users_group)
+    {2}
+    """
+    my_users = {
+        user_id
+        for user_id, in db("""
+        select distinct
+            users.id
+            from users, members
+            where
+                users.id = members.user_id
+                and group_id = %s
+        """,
+        group._id)
+    }
+    return my_users
+
+
+class Member(Record):
+    pass
+
+
+class Members(RecordStore):
+
+    def __init__(self, db, entity=Member):
+        RecordStore.__init__(
+            self,
+            db,
+            entity,
+            name='members',
+            key='id'
+            )
+
+
+class Group(Record):
+    """Zoom Users Group
+
+    >>> zoom.system.site = site = zoom.sites.Site()
+    >>> groups = Groups(site.db)
+    >>> group = groups.first(name='users')
+
+    >>> user = site.users.first(username='admin')
+    >>> group.allows(user, 'edit')
+    True
+
+    >>> group.key
+    '2'
+
+    >>> group.url
+    '/admin/groups/2'
+
+    >>> group.link
+    '<a href="/admin/groups/2" name="link-to-users">users</a>'
+
+    >>> group.roles
+    {4}
+
+    >>> zoom.utils.pp(group.apps)
+    {
+      10,
+      12,
+      20,
+      28,
+      29
+    }
+
+    >>> groups.first(name='everyone').subgroups
+    {2, 3}
+
+    >>> groups.first(name='users').user_ids
+    [2]
+    >>> {u.username for u in site.users.get(groups.first(name='users').user_ids)}
+    {'user'}
+
+    """
+
+    @property
+    def group_id(self):
+        return self._id
+
+    @property
+    def key(self):
+        return str(self._id)
+
+    @property
+    def url(self):
+        """user view url"""
+        return url_for('/admin/groups/{}'.format(self.key))
+
+    @property
+    def link(self):
+        """user as link"""
+        return link_to(self.name, self.url)
+
+    def allows(self, user, action):
+        system_groups = ['administrators', 'everyone', 'guests', 'managers', 'users']
+        return self.name not in system_groups or action != 'delete'
+
+    def get_users(self):
+        return get_users(self['__store'].db, self)
+
+    @property
+    def users(self):
+        """Return list of users that are part of this group"""
+        # TODO:
+        # Ideally, this should have returned users as it advertises.  Instead
+        # it returns user IDs.  We're introducing the user_ids property below
+        # to take the place of this property prior to switching this it
+        # over to fixing it so clients from this point forward have a property
+        # that returns value consistent with its name.  Plan to do a scan of
+        # existing systems before switching this over so we don't break things.
+        return self.get_users()
+
+    @property
+    def user_ids(self):
+        """Return list of user IDs of users that are in the group"""
+        return list(self.get_users())
+
+    def add_user(self, user):
+        store = self.get('__store')
+        members = Members(store.db)
+        membership = members.first(group_id=self._id, user_id=user._id)
+        if not membership:
+            members.put(Member(group_id=self._id, user_id=user._id))
+
+    @property
+    def roles(self):
+        db = self['__store'].db
+        my_roles = {
+            group_id
+            for group_id, in db("""
+            select distinct
+                groups.id
+                from `groups`, subgroups
+                where
+                    groups.id = subgroups.group_id
+                    and subgroup_id = %s
+                    and groups.type = 'U'
+            """,
+            self._id)
+        }
+        return my_roles
+
+    @property
+    def apps(self):
+        """Return set of apps that group can access"""
+        db = self['__store'].db
+        my_apps = {
+            group_id
+            for group_id, in db("""
+            select distinct
+                group_id
+                from subgroups, groups
+                where
+                    groups.id = subgroups.group_id
+                    and subgroup_id = %s
+                    and groups.type = 'A'
+            """,
+            self._id)
+        }
+        return my_apps
+
+    @property
+    def subgroups(self):
+        """Return set of subgroups that are part of this group"""
+        db = self['__store'].db
+        my_apps = {
+            group_id
+            for group_id, in db("""
+            select distinct
+                subgroup_id
+                from subgroups, groups
+                where
+                    id = group_id
+                    and group_id = %s
+                    and groups.type = 'U'
+            """,
+            self._id)
+        }
+        return my_apps
+
+    @property
+    def administrators(self):
+        store = self['__store']
+        admin_group = store.get(self['admin_group_id'])
+        if admin_group:
+            return admin_group.name
+        return 'nothing'
+
+
+class Groups(RecordStore):
+
+    def __init__(self, db, entity=Group):
+        RecordStore.__init__(
+            self,
+            db,
+            entity,
+            name='groups',
+            key='id'
+            )
+
+    def locate(self, locator):
+        """locate a group whether it is referred to by reference, id or name"""
+        return (
+            isinstance(locator, Group) and locator or
+            type(locator) == int and self.get(locator) or
+            type(locator) == str and self.first(name=locator)
+        )
+
+
+class SystemAttachment(Record):
+    pass
+
+
+Attachment = SystemAttachment
+
+
+def handler(request, handler, *rest):
+    request.site.groups = Groups(request.site.db)
+    request.site.users = Users(request.site.db)
+    return handler(request, *rest)
