18e9f76b686540c4cd317086ab506d19c4a52bb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:32 +0000

    Add package dodo-commands/0.30.1/dodo_commands-0.30.1.tar.gz

---
+++ packages/dodo-commands/dodo_commands-0.30.1.tar.gz/dodo_commands-0.30.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+setup(name='dodo_commands',
+      version='0.30.1',
+      description=
+      'Project-aware development environments, inspired by django-manage',
+      url='https://github.com/mnieber/dodo_commands',
+      download_url='https://github.com/mnieber/dodo_commands/tarball/0.30.1',
+      author='Maarten Nieber',
+      author_email='hallomaarten@yahoo.com',
+      license='MIT',
+      packages=find_packages(),
+      package_data={
+          'dodo_commands': [
+              'bin/*.fish',
+              'bin/*.sh',
+              'extra/dodo_standard_commands/*.meta',
+              'extra/dodo_docker_commands/*.meta',
+          ]
+      },
+      entry_points={'console_scripts': [
+          'dodo=dodo_commands.dodo:main',
+      ]},
+      data_files=[('/etc/bash_completion.d', [
+          'dodo_commands/bin/dodo_autocomplete.sh',
+          'dodo_commands/bin/sdodo_autocomplete.sh'
+      ]), ('/etc/fish/conf.d', ['dodo_commands/bin/dodo_autocomplete.fish'])],
+      install_requires=[],
+      zip_safe=False)
