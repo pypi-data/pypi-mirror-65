43e5e34b8018054cc87807997a6b46958692a20a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:01 +0000

    Add package ColorDetect/0.1.5/ColorDetect-0.1.5.tar.gz

---
+++ packages/ColorDetect/ColorDetect-0.1.5.tar.gz/ColorDetect-0.1.5/tests/test_unit.py
@@ -0,0 +1,32 @@
+"""
+Unit tests: testing a small bit of code like function or class in isolation of the system
+From the developers perspective
+"""
+# Whether ()dictionary has the correct image count
+# test whether text in the dictionary is what is being written to the image
+
+from ..colordetect import ColorDetect
+import os
+import pytest
+
+
+def test_image_parsed_to_class(image):
+    """
+    test whether an image is parsed to the class ColorDetect(<image>)
+    Check whether an instance is created
+    """
+    isinstance(ColorDetect(image), object)
+
+
+def test_dictionary_has_correct_color_count():
+    pass
+
+
+def test_what_is_in_dictionary_is_being_written(datadir):
+    """
+    What is in the dictionary should be what is being written
+    """
+    # compare input and output images
+    # expected_image = datadir.join('image2.jpg')
+
+    pass
