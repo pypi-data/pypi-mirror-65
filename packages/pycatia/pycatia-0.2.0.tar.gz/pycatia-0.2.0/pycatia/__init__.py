4df337d6a08d4fecd16e8c672c58ff90ca3dc9f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:32 +0000

    Add package pycatia/0.2.0/pycatia-0.2.0.tar.gz

---
+++ packages/pycatia/pycatia-0.2.0.tar.gz/pycatia-0.2.0/pycatia/__init__.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python3.6
+
+import os
+
+__author__ = 'Paul Bourne'
+__author_email = 'evereux@gmail.com'
+__description__ = 'A python module to access the CATIA Measurable object.'
+__name__ = "pycatia"
+__version__ = "0.2.0"
+__url__ = "https://github.com/evereux/pycatia"
+
+name = __name__
+
+macro_path = os.path.join(os.path.realpath(__file__), r'macros')
---
+++ packages/pycatia/pycatia-0.2.0.tar.gz/pycatia-0.2.0/pycatia/base_interfaces/context.py
@@ -0,0 +1,73 @@
+#! /usr/bin/python3.6
+
+import os
+import warnings
+
+from pycatia.base_interfaces.catia_application import CATIAApplication
+from pycatia.exception_handling.exceptions import CATIAApplicationException
+
+
+class CATIADocHandler:
+    """
+    A context manager to open or create a CATIA document. The CATIA document will be closed upon exit.
+
+    Only file_name __or__ new_document are required as one document is handled at a time.
+
+    :Example - Open a CATPart:
+
+        >>> from pycatia.base_interfaces import CATIADocHandler
+        >>> catia_part = 'tests\\CF_catia_measurable_part.CATPart'
+        >>> with CATIADocHandler(catia_part) as handler:
+        >>>     # create the CATIA() object.
+        >>>     catia = handler.catia
+        >>>     # create the documents object.
+        >>>     documents = handler.documents
+        >>>     # create the document object.
+        >>>     document = handler.document
+        >>>     # do some stuff
+
+    :Example - Create a new CATPart:
+
+        >>> from pycatia.base_interfaces import CATIADocHandler
+        >>> with CATIADocHandler(new_document='Part') as handler:
+        >>>     # create the CATIA() object.
+        >>>     catia = handler.catia
+        >>>     # create the documents object.
+        >>>     documents = handler.documents
+        >>>     # create the document object.
+        >>>     document = handler.document
+
+    :param str file_name: (optional) path filename to file
+    :param str new_document: (optional) 'Part', 'Product' or 'Drawing'.
+    """
+
+    def __init__(self, file_name=None, new_document=None):
+
+        self.catia = CATIAApplication()
+        self.documents = self.catia.documents()
+        self.file_name = file_name
+        self.new_document = new_document
+
+        if self.file_name and not os.path.isfile(self.file_name):
+            raise CATIAApplicationException(f'Could not find file: {file_name}')
+
+    def __enter__(self):
+        self.catia = CATIAApplication()
+        self.documents = self.catia.documents()
+        self.document = None
+
+        if self.file_name:
+            self.documents.open(self.file_name)
+            self.document = self.catia.document()
+        elif self.new_document:
+            self.documents.add(self.new_document)
+            self.document = self.catia.document()
+
+        return self
+
+    def __exit__(self, *args):
+
+        if self.document:
+            self.document.close()
+        else:
+            warnings.warn('The document handler could not detect a document to close.')
