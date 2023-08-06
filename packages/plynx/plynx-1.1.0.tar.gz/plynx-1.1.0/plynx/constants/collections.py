c7ccb00ef6e1a27c940a727b9f9e21ea4323bac8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:38 +0000

    Add package plynx/1.1.0/plynx-1.1.0.tar.gz

---
+++ packages/plynx/plynx-1.1.0.tar.gz/plynx-1.1.0/plynx/constants/collections.py
@@ -0,0 +1,7 @@
+class Collections:
+    WORKER_STATE = 'worker_state'
+    NODE_CACHE = 'node_cache'
+    TEMPLATES = 'templates'
+    RUNS = 'runs'
+    RUN_CANCELLATIONS = 'run_cancellations'
+    USERS = 'users'
