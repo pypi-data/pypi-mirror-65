85ab44b55a72400a67b0c228ad0fb16128983341
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:45 +0000

    Add package gsdcli/0.1.1/gsdcli-0.1.1.tar.gz

---
+++ packages/gsdcli/gsdcli-0.1.1.tar.gz/gsdcli-0.1.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['gsdcli']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Click>=7.1.1,<8.0.0']
+
+entry_points = \
+{'console_scripts': ['gsd-cli = gsdcli.gsdcli:gsdcli']}
+
+setup_kwargs = {
+    'name': 'gsdcli',
+    'version': '0.1.1',
+    'description': 'A cli for installing game servers',
+    'long_description': None,
+    'author': 'egee-irl',
+    'author_email': 'brian@egee.io',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
