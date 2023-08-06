379a26490caef02c60087ec847d837d0554ff523
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:28 +0000

    Add package leaptask/2020.4.14/leaptask-2020.4.14.tar.gz

---
+++ packages/leaptask/leaptask-2020.4.14.tar.gz/leaptask-2020.4.14/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+# -$ python setup.py sdist bdist_wheel
+# -$ twine upload dist/* -u VicWang -p PYPI******
+
+setuptools.setup(
+    name="leaptask",
+    version="2020.4.14",
+    author="Vic Wang",
+    author_email="305880887@qq.com",
+    description='leap task',
+    long_description="leap tasks",
+    long_description_content_type="text/markdown",
+    url='http://github.com/XRDX/pyleap',
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ),
+)
