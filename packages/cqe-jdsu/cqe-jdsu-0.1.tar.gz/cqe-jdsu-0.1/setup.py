4981a4b211cb3078610d2191af93765f53a6c8ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:09 +0000

    Add package cqe-jdsu/0.1/cqe-jdsu-0.1.tar.gz

---
+++ packages/cqe-jdsu/cqe-jdsu-0.1.tar.gz/cqe-jdsu-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='cqe-jdsu',
+      version='0.1',
+      description='package required for JDSU API',
+      url='http://github.com/cafykit/cqe-jdsu',
+      author='Deepesh Kaushal',
+      author_email='kaushal_deepesh@yahoo.in',
+      license='VIAVI',
+      packages=['jdsu'],
+      zip_safe=False)
