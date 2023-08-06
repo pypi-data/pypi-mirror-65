0a529512caeb569d27d0d83296f010ff45d2d42e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:16 +0000

    Add package knotify/0.0.1/knotify-0.0.1.tar.gz

---
+++ packages/knotify/knotify-0.0.1.tar.gz/knotify-0.0.1/knotify/utils.py
@@ -0,0 +1,19 @@
+from typing import List, Type
+
+
+def build_uri(scheme: str, loc: str, paths: List[str] = [], **kwargs) -> str:
+    """
+    Used to build URIs
+    :param scheme: scheme e.g. http in http://google.com/
+    :param loc: netloc e.g. google.com in http://google.com/
+    :param paths: a list of paths e.g. ["example", "path"] in http://google.com/example/path
+    :param kwargs: parameters e.g. {NETLOC}?k1=v&k2=v
+    :return:
+    """
+    path = "/" + "/".join(paths) if paths else ""
+    params = "?" + "&".join([f"{k}={kwargs[k]}" for k in kwargs.keys()]) if kwargs else ""
+    return "{}://{}{}{}".format(scheme, loc, path, params)
+
+
+def get_cls_name(cls):
+    return cls.__name__.lower()
