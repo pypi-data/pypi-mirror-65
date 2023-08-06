e1186fbe69c98c6d9552864166ef34bc8fd11ccd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:13 +0000

    Add package fast-password-validation/0.1.2/fast_password_validation-0.1.2.linux-x86_64.tar.gz

---
+++ packages/fast-password-validation/fast_password_validation-0.1.2.linux-x86_64.tar.gz/usr/local/lib/python3.7/dist-packages/test/settings.py
@@ -0,0 +1,11 @@
+DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'fast_password_validation'
+    }
+}
+
+USE_I18N = False
+LOGGING = {}
