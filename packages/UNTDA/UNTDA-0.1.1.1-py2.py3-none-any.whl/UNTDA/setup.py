a9b601dc97abc76f7023f13f56a11d442b4c82f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:26 +0000

    Add package UNTDA/0.1.1.1/UNTDA-0.1.1.1-py2.py3-none-any.whl

---
+++ packages/UNTDA/UNTDA-0.1.1.1-py2.py3-none-any.whl/UNTDA/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+  name = 'UNTDA',
+  version = '0.1.0.0.3',
+  author = 'Isaac Zainea',
+  author_email = 'isaaczainea@gmail.com',  
+  description = 'paquete de aprendizaje de TDA - UNAL Colombia',
+  url = 'https://github.com/izainea/UNATD', # use the URL to the github repo
+  keywords = ['testing', 'ATD', 'TDA'],
+  packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
