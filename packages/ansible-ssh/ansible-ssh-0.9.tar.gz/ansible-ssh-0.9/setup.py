fbf9bba8f4ff4e8be4c4af9ea43b93a69d749c9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:53 +0000

    Add package ansible-ssh/0.9/ansible-ssh-0.9.tar.gz

---
+++ packages/ansible-ssh/ansible-ssh-0.9.tar.gz/ansible-ssh-0.9/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='ansible-ssh',  
+     version='0.9',
+     scripts=['ansible-ssh'] ,
+     author="Dimos Alevizos",
+     author_email="dimos@alevizos.gr",
+     description="Interactive SSH for Ansible",
+     long_description=long_description,
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+)
