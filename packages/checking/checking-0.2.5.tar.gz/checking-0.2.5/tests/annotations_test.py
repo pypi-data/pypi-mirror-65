10ae82d0f4a5326072dc84615d0c32cd03f724dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:34 +0000

    Add package checking/0.2.5/checking-0.2.5.tar.gz

---
+++ packages/checking/checking-0.2.5.tar.gz/checking-0.2.5/tests/annotations_test.py
@@ -0,0 +1,191 @@
+from unittest import TestCase, main
+
+from checking.annotations import *
+from checking.exceptions import *
+from checking.classes.basic_suite import TestSuite
+from checking.runner import start
+from checking.classes.listeners.basic import Listener
+from tests.fixture_behaviour_test import clear
+
+
+def valid():
+    pass
+
+
+def valid_for_data():
+    return [1, 2]
+
+
+def valid_for_provider(arg):
+    pass
+
+
+def non_valid_for_provider(arg, arg1):
+    pass
+
+
+class T:
+    def m(self):
+        pass
+
+
+def f():
+    """Test"""
+    pass
+
+
+class TestAnnotations(TestCase):
+
+    def test_valid_func_works(self):
+        clear()
+        initial_count = TestSuite.get_instance().tests_count()
+        test(valid)
+        self.assertEqual(initial_count + 1, TestSuite.get_instance().tests_count())
+
+    def test_not_add_if_disabled(self):
+        clear()
+        initial_count = TestSuite.get_instance().tests_count()
+        test(enabled=False)(valid)
+        self.assertEqual(initial_count, TestSuite.get_instance().tests_count())
+
+    def test_not_add_if_enabled(self):
+        clear()
+        initial_count = TestSuite.get_instance().tests_count()
+        test(enabled=True)(valid)
+        self.assertEqual(initial_count + 1, TestSuite.get_instance().tests_count())
+
+    def test_raises_when_func_for_provider_has_no_arg(self):
+        with self.assertRaises(WrongAnnotationPlacement) as e:
+            test(data_provider="good")(valid)
+        self.assertEqual("Function 'valid' marked with data_provider has no argument!", e.exception.args[0])
+
+    def test_raises_when_no_provider(self):
+        clear()
+        test(data_provider="missing")(valid_for_provider)
+        with self.assertRaises(UnknownProviderName) as e:
+            start(TestSuite.get_instance(), listener=Listener(0))
+        self.assertTrue("Cant find provider with name(s) ['missing']" in e.exception.args[0])
+
+    def test_ignore_all_if_test_disabled(self):
+        test(enabled=False, data_provider="wrong")(valid)
+
+    def test_raises_when_using_test_with_class(self):
+        with self.assertRaises(WrongAnnotationPlacement) as ex:
+            test(T)
+        self.assertTrue('test' in ex.exception.args[0])
+
+    def test_raises_when_using_function_with_arg(self):
+        with self.assertRaises(WrongAnnotationPlacement) as ex:
+            test(valid_for_provider)
+        self.assertTrue('test' in ex.exception.args[0])
+
+    def test_raises_when_using_test_with_class_methods(self):
+        with self.assertRaises(WrongAnnotationPlacement) as ex:
+            t = T()
+            test(t.m)
+        self.assertTrue('test' in ex.exception.args[0])
+
+    def test_name_param_works(self):
+        clear()
+        test(name='new_name')(valid)
+        self.assertTrue('new_name' in [t.name for t in list(TestSuite.get_instance().groups.values())[0].tests])
+
+    def test_raises_if_only_is_not_callable(self):
+        clear()
+        with self.assertRaises(ValueError):
+            test(only_if=False)(valid)
+
+    def test_data_works(self):
+        clear()
+        data(name="any_name")(valid_for_data)
+        self.assertTrue('any_name' in TestSuite.get_instance().providers)
+
+    def test_data_raises_when_duplicate_name(self):
+        with self.assertRaises(DuplicateNameException) as e:
+            data(valid_for_data)
+            data(valid_for_data)
+        self.assertEqual('Provider with name "valid_for_data" already exists! Only unique names allowed!',
+                         e.exception.args[0])
+
+    def test_data_raises_when_two_args(self):
+        with self.assertRaises(WrongAnnotationPlacement) as e:
+            data(name='another2')(lambda: [1, 2])
+            test(data_provider='another2')(non_valid_for_provider)
+        self.assertEqual("Function 'non_valid_for_provider' marked with data_provider has more than 1 argument!",
+                         e.exception.args[0])
+
+    def test_data_name_works(self):
+        clear()
+        data(name="another")(valid_for_data)
+        self.assertTrue('another' in TestSuite.get_instance().providers)
+
+    def test_data_ignore_if_disabled(self):
+        clear()
+        data(enabled=False, name="no")(valid)
+        self.assertFalse('no' in TestSuite.get_instance().providers)
+
+    def test_before_all_when_before_first(self):
+        clear()
+        before_ = valid
+        before(before_)
+        t = Test('first', print)
+        list(TestSuite.get_instance().groups.values())[0].add_test(t)
+        self.assertTrue(t.before)
+        self.assertEqual(before_, t.before[0])
+
+    def test_before_all_when_before_last(self):
+        clear()
+        t = Test('second', print)
+        TestSuite.get_instance().get_or_create("new").add_test(t)
+        bef = lambda: None
+        before(group_name='new')(bef)
+        self.assertTrue(bef in t.before)
+
+    def test_no_timeout_default(self):
+        clear()
+        test(valid)
+        self.assertEqual(0, list(TestSuite.get_instance().groups.values())[0].tests[0].timeout)
+
+    def test_timeout_same(self):
+        clear()
+        test(timeout=10)(valid)
+        self.assertEqual(10, list(TestSuite.get_instance().groups.values())[0].tests[0].timeout)
+
+    def test_timeout_same_float(self):
+        clear()
+        test(timeout=10.02)(valid)
+        self.assertEqual(10, list(TestSuite.get_instance().groups.values())[0].tests[0].timeout)
+
+    def test_timeout_same_negative(self):
+        clear()
+        test(timeout=-1)(valid)
+        self.assertEqual(0, list(TestSuite.get_instance().groups.values())[0].tests[0].timeout)
+
+    def test_description_empty(self):
+        clear()
+        test(valid)
+        self.assertIsNone(list(TestSuite.get_instance().groups.values())[0].tests[0].description)
+        self.assertEqual('annotations_test.valid ', str(list(TestSuite.get_instance().groups.values())[0].tests[0]))
+
+    def test_description_from_param(self):
+        clear()
+        test(description='test')(valid)
+        self.assertEqual(list(TestSuite.get_instance().groups.values())[0].tests[0].description, 'test')
+        self.assertEqual("annotations_test.valid ('test')",
+                         str(list(TestSuite.get_instance().groups.values())[0].tests[0]))
+
+    def test_description_from_docs(self):
+        clear()
+        test(f)
+        self.assertEqual(list(TestSuite.get_instance().groups.values())[0].tests[0].description, 'Test')
+        self.assertEqual("annotations_test.f ('Test')", str(list(TestSuite.get_instance().groups.values())[0].tests[0]))
+
+    def test_description_from_param_wins_docs(self):
+        clear()
+        test(description='test')(f)
+        self.assertEqual(list(TestSuite.get_instance().groups.values())[0].tests[0].description, 'test')
+        self.assertEqual("annotations_test.f ('test')", str(list(TestSuite.get_instance().groups.values())[0].tests[0]))
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/checking/checking-0.2.5.tar.gz/checking-0.2.5/tests/asserts_test.py
@@ -0,0 +1,234 @@
+from unittest import main, TestCase
+
+from checking.asserts import *
+from checking.exceptions import TestBrokenException, ExceptionWrapper
+
+
+class AssertsTest(TestCase):
+
+    def test_is_zero(self):
+        is_zero(0)
+
+    def test_is_zero_float(self):
+        is_zero(0.0)
+
+    def test_is_zero_fails_if_wrong_type(self):
+        with self.assertRaises(TestBrokenException):
+            is_zero("0")
+
+    def test_is_zero_fails_if_positive(self):
+        with self.assertRaises(AssertionError):
+            is_zero(1)
+
+    def test_is_zero_fails_if_negative(self):
+        with self.assertRaises(AssertionError):
+            is_zero(-1)
+
+    def test_is_zero_fails_if_positive_float(self):
+        with self.assertRaises(AssertionError):
+            is_zero(0.1)
+
+    def test_is_positive(self):
+        is_positive(1)
+
+    def test_is_positive_str(self):
+        is_positive('1')
+
+    def test_is_positive_list(self):
+        is_positive([1,2])
+
+    def test_is_positive_fail(self):
+        with self.assertRaises(AssertionError):
+            is_positive(0)
+
+    def test_is_positive_fail_if_not_seq(self):
+        with self.assertRaises(TestBrokenException):
+            is_positive((g for g in range(2)))
+
+    def test_is_positive_fail_str(self):
+        with self.assertRaises(AssertionError):
+            is_positive('')
+
+    def test_is_negative(self):
+        is_negative(-1)
+
+    def test_is_negative_float(self):
+        is_negative(-0.213)
+
+    def test_is_negative_fail_zero(self):
+        with self.assertRaises(AssertionError):
+            is_negative(0)
+
+    def test_is_negative_fail(self):
+        with self.assertRaises(AssertionError):
+            is_negative(5)
+
+    def test_is_negative_fail_if_wrong_type(self):
+        with self.assertRaises(TestBrokenException):
+            is_negative('1')
+
+    def test_equals_ok(self):
+        equals(1, 1)
+
+    def test_equals_failed(self):
+        with self.assertRaises(AssertionError) as e:
+            equals(1, 2)
+        self.assertEqual('Expected "1" <int>, but got "2"<int>!', e.exception.args[0])
+
+    def test_equals_failed_with_message(self):
+        with self.assertRaises(AssertionError) as e:
+            equals(1, 2, 'message')
+        self.assertEqual('message\nExpected "1" <int>, but got "2"<int>!', e.exception.args[0])
+
+    def test_is_none_ok(self):
+        is_none(None)
+
+    def test_is_none_failed(self):
+        with self.assertRaises(AssertionError) as e:
+            is_none(1)
+        self.assertEqual('Object 1<int> is not None!', e.exception.args[0])
+
+    def test_is_none_failed_with_message(self):
+        with self.assertRaises(AssertionError) as e:
+            is_none(1, 'message')
+        self.assertEqual('message\nObject 1<int> is not None!', e.exception.args[0])
+
+    def test_not_none_ok(self):
+        not_none(1)
+
+    def test_not_none_failed(self):
+        with self.assertRaises(AssertionError) as e:
+            not_none(None)
+        self.assertEqual('Unexpected None!', e.exception.args[0])
+
+    def test_not_none_failed_with_message(self):
+        with self.assertRaises(AssertionError) as e:
+            not_none(None, 'message')
+        self.assertEqual('message\nUnexpected None!', e.exception.args[0])
+
+    def test_waiting_exception_with_BaseException_failed(self):
+        with self.assertRaises(TestBrokenException) as e:
+            with waiting_exception(BaseException):
+                pass
+        self.assertEqual('You must use concrete exception, except of BaseException!', e.exception.args[0])
+
+    def test_waiting_exception_with_not_Exception_failed(self):
+        with self.assertRaises(TestBrokenException) as e:
+            with waiting_exception('wrong'):
+                pass
+        self.assertEqual('Exception or its subclasses expected, but got "wrong"<str>', e.exception.args[0])
+
+    def test_waiting_exception_ok_with_Exception(self):
+        with waiting_exception(Exception) as e:
+            x = 1 / 0
+        self.assertEqual(e.message, 'division by zero')
+        self.assertEqual(type(e.value.__class__), type(ZeroDivisionError))
+
+    def test_waiting_exception_ok_with_concrete_exception(self):
+        with waiting_exception(ZeroDivisionError) as e:
+            x = 1 / 0
+        self.assertEqual(e.message, 'division by zero')
+        self.assertEqual(type(e.value.__class__), type(ZeroDivisionError))
+
+    def test_waiting_exception_with_wrong_Exception_failed(self):
+        with self.assertRaises(AssertionError) as e:
+            with waiting_exception(ZeroDivisionError):
+                int('a')
+        self.assertEqual("Expect <class 'ZeroDivisionError'>, but raised ValueError"
+                         " (\"invalid literal for int() with base 10: 'a'\")", e.exception.args[0])
+
+    def test_waiting_exception_with_no_Exception_failed(self):
+        with self.assertRaises(ExceptionWrapper) as e:
+            with waiting_exception(ZeroDivisionError):
+                pass
+        self.assertEqual("Expect exception, but none raised!", e.exception.args[0])
+
+    def test_failing_test_default(self):
+        with self.assertRaises(AssertionError) as e:
+            test_fail()
+        self.assertEqual("Test was forcibly failed!", e.exception.args[0])
+
+    def test_failing_test_with_message(self):
+        with self.assertRaises(AssertionError) as e:
+            test_fail('message')
+        self.assertEqual("message", e.exception.args[0])
+
+    def test_braking_test_default(self):
+        with self.assertRaises(TestBrokenException) as e:
+            test_brake()
+        self.assertEqual("Test was forcibly broken!", e.exception.args[0])
+
+    def test_braking_test_with_message(self):
+        with self.assertRaises(TestBrokenException) as e:
+            test_brake('message')
+        self.assertEqual("message", e.exception.args[0])
+
+    def test_no_exception_expected_ok(self):
+        with no_exception_expected():
+            pass
+
+    def test_no_exception_expected_failed(self):
+        with self.assertRaises(AssertionError) as e:
+            with no_exception_expected():
+                raise ValueError('message')
+        self.assertEqual("Expect no exception, but raised ValueError (\"message\")", e.exception.args[0])
+
+    def test_contains_ok_for_str(self):
+        contains('1', '123')
+
+    def test_contains_ok_for_list(self):
+        contains('1', ['1', '2'])
+
+    def test_contains_ok_for_tuple(self):
+        contains('1', ('123', '1'))
+
+    def test_contains_ok_for_dict(self):
+        contains('1', {'1': 1})
+
+    def test_contains_failed_for_None(self):
+        with self.assertRaises(TestBrokenException) as e:
+            contains('1', None)
+        self.assertEqual("\"None\"<NoneType> is not iterable and cant be check for contains!", e.exception.args[0])
+
+    def test_contains_failed_for_not_iterable(self):
+        with self.assertRaises(TestBrokenException) as e:
+            contains('1', 1)
+        self.assertEqual("\"1\"<int> is not iterable and cant be check for contains!", e.exception.args[0])
+
+    def test_contains_failed_if_not_contains_str(self):
+        with self.assertRaises(AssertionError) as e:
+            contains('1', '234')
+        self.assertEqual("Object \"1\" <str>, is not part of \"234\"<str>!", e.exception.args[0])
+
+    def test_contains_failed_if_not_contains_list(self):
+        with self.assertRaises(AssertionError) as e:
+            contains('1', [2, 3])
+        self.assertEqual("Object \"1\" <str>, is not part of \"[2, 3]\"<list>!", e.exception.args[0])
+
+    def test_contains_failed_if_wrong_types(self):
+        with self.assertRaises(TestBrokenException) as e:
+            contains(1, '123')
+        self.assertEqual("Object \"1\" <int> and \"123\"<str> are of different types and cant be check for contains!",
+                         e.exception.args[0])
+
+    def test_is_true(self):
+        is_true(True)
+        is_true(1 == 1)
+        is_true([1, 2])
+
+    def test_is_true_failed(self):
+        with self.assertRaises(AssertionError):
+            is_true(False)
+
+    def test_is_false(self):
+        is_false(False)
+        is_false(1 == 2)
+        is_false([])
+
+    def test_is_false_failed(self):
+        with self.assertRaises(AssertionError):
+            is_false(True)
+
+
+if __name__ == '__main__':
+    main()
