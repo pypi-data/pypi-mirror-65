e5049a1af63d13687120fbae459e8515ee13480f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:19 +0000

    Add package pulsar-galaxy-lib/0.14.0.dev3/pulsar-galaxy-lib-0.14.0.dev3.tar.gz

---
+++ packages/pulsar-galaxy-lib/pulsar-galaxy-lib-0.14.0.dev3.tar.gz/pulsar-galaxy-lib-0.14.0.dev3/.travis/setup_tests.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+set -v
+
+sudo apt-get update
+sudo apt-get install libxml2-dev libxslt1-dev libcurl3 python-pycurl openssh-server
+#pip install -r requirements$REQUIREMENTS_SUFFIX.txt --use-mirrors || true
+#pip install -r dev-requirements.txt --use-mirrors || true
+pip install coveralls  # Required fro coveralls reporting.
+sudo apt-get install slurm-llnl slurm-llnl-torque # slurm-drmaa1 slurm-drmaa-dev
+sudo apt-get install libswitch-perl  # A missing dependency of slurm-llnl-torque
+wget https://depot.galaxyproject.org/old/deb/slurm-drmaa1_1.2.0-dev.57ebc0c_amd64.deb
+sudo dpkg -i slurm-drmaa1_1.2.0-dev.57ebc0c_amd64.deb
+wget https://depot.galaxyproject.org/old/deb/slurm-drmaa-dev_1.2.0-dev.57ebc0c_amd64.deb
+sudo dpkg -i slurm-drmaa-dev_1.2.0-dev.57ebc0c_amd64.deb
+sudo /usr/sbin/create-munge-key
+sudo service munge start
+sudo python scripts/configure_test_slurm.py
+echo "export DRMAA_LIBRARY_PATH=/usr/lib/slurm-drmaa/lib/libdrmaa.so" >> local_env.sh
+echo ". $VIRTUAL_ENV/bin/activate" >> local_env.sh
+sudo useradd --home-dir /home/u1 --shell /bin/bash --create-home --comment TEST,,, --groups travis u1  ## Create user for run-as-user test.
+mkdir -p ~/.ssh && cp test_data/testkey.pub ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys
