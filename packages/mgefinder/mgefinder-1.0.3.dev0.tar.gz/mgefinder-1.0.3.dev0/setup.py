b8042bdb918af3ccbe6817ed666d15a263d98858
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:22 +0000

    Add package mgefinder/1.0.3.dev0/mgefinder-1.0.3.dev0.tar.gz

---
+++ packages/mgefinder/mgefinder-1.0.3.dev0.tar.gz/mgefinder-1.0.3.dev0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mgefinder",
+    version='1.0.3_dev',
+    description='A toolbox for identifying mobile genetic element (MGE) insertions from short-read sequencing data of bacterial isolates.',
+    url='https://github.com/bhattlab/MGEfinder',
+    author="Matt Durrant",
+    author_email="mdurrant@stanford.edu",
+    license="MIT",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'click',
+        'pandas',
+        'biopython',
+        'pysam',
+        'editdistance',
+        'scipy',
+        'networkx',
+        'tqdm'
+    ],
+    zip_safe=False,
+    entry_points = {
+        'console_scripts': [
+            'mgefinder = mgefinder.main:cli'
+        ],
+}
+)
