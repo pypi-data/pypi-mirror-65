be5b8ddea6c09d5692884827a0974109072a27a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:18:16 +0000

    Add package falsecolor/1.1.4.5/falsecolor-1.1.4.5.tar.gz

---
+++ packages/falsecolor/falsecolor-1.1.4.5.tar.gz/falsecolor-1.1.4.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open('requirements.txt','r') as f:
+    requires = f.read().splitlines()
+f.close()
+
+setup(name='falsecolor',
+    packages = ['falsecolor'],
+    version='1.1.4.5',
+    license = 'GNU Affero General Public License v3.0',
+    description='Methods for H&E pseudo coloring of grayscale fluorescent images',
+    author='Robert Serafin',
+    author_email='serrob23@uw.edu',
+    url = 'https://github.com/serrob23/FalseColor',
+    download_url = 'https://github.com/serrob23/falsecolor/releases/download/v1.1.4.4/falsecolor-1.1.4.4.tar.gz',
+    keywords = ['Image Analysis', 
+                            'Fluorescence Microscopy', 
+                            'Color Translation', 
+                            'Virtual Staining',
+                            'Histology'],
+    install_requires = requires,
+    python_requires='>=3.6',
+
+)
