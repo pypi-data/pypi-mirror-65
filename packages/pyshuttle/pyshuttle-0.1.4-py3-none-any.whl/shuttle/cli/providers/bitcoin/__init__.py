20b4eac6a84fc6e69db33ec954b7ad4d4366c42c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:37 +0000

    Add package pyshuttle/0.1.4/pyshuttle-0.1.4-py3-none-any.whl

---
+++ packages/pyshuttle/pyshuttle-0.1.4-py3-none-any.whl/shuttle/cli/providers/bitcoin/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+# IMPORT ALL PACKAGES
+from shuttle.cli import click
+
+from .signature import sign
+from .decode import decode
+from .submit import submit
+
+
+@click.group("bitcoin", options_metavar="[OPTIONS]",
+             short_help="Select Bitcoin provider.")
+def bitcoin():
+    pass
+
+
+# Adding bitcoin sign
+bitcoin.add_command(sign)
+# Adding bitcoin decoder
+bitcoin.add_command(decode)
+# Adding bitcoin submit
+bitcoin.add_command(submit)
---
+++ packages/pyshuttle/pyshuttle-0.1.4-py3-none-any.whl/shuttle/cli/providers/bytom/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+# IMPORT ALL PACKAGES
+from shuttle.cli import click
+
+from .signature import sign
+from .decode import decode
+from .submit import submit
+
+
+@click.group("bytom", options_metavar="[OPTIONS]",
+             short_help="Select Bytom provider.")
+def bytom():
+    pass
+
+
+# Adding bytom sign
+bytom.add_command(sign)
+# Adding bytom decoder
+bytom.add_command(decode)
+# Adding bytom submit
+bytom.add_command(submit)
