011984d1dea4d0f2c35e5a7bec693d877868aeb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:33 +0000

    Add package monoff/0.0.1/monoff-0.0.1-py2.py3-none-any.whl

---
+++ packages/monoff/monoff-0.0.1-py2.py3-none-any.whl/monoff/__init__.py
@@ -0,0 +1,27 @@
+from sys import platform
+from os import system
+
+__version__ = '0.0.1'
+
+def monoff():
+    if platform == 'linux':
+        system('xset dpms force off')
+    elif platform == 'win32' or platform == 'cygwin':
+        from win32gui import SendMessage
+        from win32con import HWND_BROADCAST, WM_SYSCOMMAND
+        from os import getpid
+        from threading import Timer
+        SC_MONITORPOWER = 0xF170
+        SC_MONITORPOWER_OFF = 2
+        def end_thread():
+            pid = getpid()
+            system('taskkill /pid %s /f' % pid)
+        monoff_thread = Timer(1,end_thread)
+        monoff_thread.start()
+        SendMessage(HWND_BROADCAST,WM_SYSCOMMAND,SC_MONITORPOWER,SC_MONITORPOWER_OFF)
+        monoff_thread.cancel()
+    elif platform == 'darwin':
+        from os import system
+        system('pmset displaysleepnow')
+    else:
+        raise Exception('Monoff does not support this platform: %s' % repr(platform))
