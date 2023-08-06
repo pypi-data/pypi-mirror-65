31add3f6cb45c032a70581f20df1e924a25ec4de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:39 +0000

    Add package fruit/0.2.5/fruit-0.2.5.tar.gz

---
+++ packages/fruit/fruit-0.2.5.tar.gz/fruit-0.2.5/fruit/globals.py
@@ -0,0 +1,10 @@
+import click
+
+FRUITCONFIG_NAME = "fruitconfig.py"
+
+FMT_STEPHEADER = "🥝 Step {number}: {name}\n" + "-"* (click.get_terminal_size()[0] - 5)
+SHELLCHAR = '$ '
+
+ICON_SUCC = "✅"
+ICON_FAIL    = "❌"
+ICON_SKIP    = "⏭"
