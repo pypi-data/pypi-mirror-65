f341d2f3bb55cded5c77d991e40843f4197025c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:39 +0000

    Add package matplot-fmt-pi/1.3.3/matplot-fmt-pi-1.3.3.tar.gz

---
+++ packages/matplot-fmt-pi/matplot-fmt-pi-1.3.3.tar.gz/matplot-fmt-pi-1.3.3/matplot_fmt_pi/__init__.py
@@ -0,0 +1,14 @@
+"""
+Format numbers as multiples of pi for matplotlib plots.
+
+The class MultiplePi contains the math and formatting to accomplish this.
+MultiplePi has methods to return a locator and formatter for use on a plot.
+
+
+classes include:
+    MultiplePi
+        contains everything needed
+"""
+from .multiple import MultiplePi
+
+__version__ = "1.3.3"
