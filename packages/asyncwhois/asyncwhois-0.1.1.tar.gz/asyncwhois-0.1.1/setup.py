b1ac618af7a5fc601735a1480e9d996df81f7dbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:58 +0000

    Add package asyncwhois/0.1.1/asyncwhois-0.1.1.tar.gz

---
+++ packages/asyncwhois/asyncwhois-0.1.1.tar.gz/asyncwhois-0.1.1/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="asyncwhois",
+    version="0.1.1",
+    author="Joseph Obarzanek",
+    author_email="pogzyb@umich.edu",
+    description="Async-compatible Python module for retrieving WHOIS information for domains.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    install_requires=[
+        "aiodns",
+    ],
+    classifiers=[
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP",
+        "Programming Language :: Python :: 3",
+        "Framework :: AsyncIO"
+    ],
+    url="https://github.com/pogzyb/asyncwhois",
+    packages=["asyncwhois"],
+    package_dir={"asyncwhois": "asyncwhois"},
+    python_requires=">=3.6",
+    include_package_data=True
+)
