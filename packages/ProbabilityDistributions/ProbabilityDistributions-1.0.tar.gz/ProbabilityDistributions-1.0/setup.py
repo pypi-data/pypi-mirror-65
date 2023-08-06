9b76bea0cdae3212e629f73dca09e1123d4efa74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:09 +0000

    Add package ProbabilityDistributions/1.0/ProbabilityDistributions-1.0.tar.gz

---
+++ packages/ProbabilityDistributions/ProbabilityDistributions-1.0.tar.gz/ProbabilityDistributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ProbabilityDistributions',
+      version='1.0',
+      description='Gaussian Binomial distributions',
+      packages=['ProbabilityDistributions'],
+      author='Pankhil prajapati',
+      author_email="puruprajapti8@gmail.com",
+      zip_safe=False)
