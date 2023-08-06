5e0b33c9c63a2492a49c3cf18f93206b98affba7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:07 +0000

    Add package floydpink.jsii-native-python/0.1.0/floydpink.jsii_native_python-0.1.0-py3-none-any.whl

---
+++ packages/floydpink.jsii-native-python/floydpink.jsii_native_python-0.1.0-py3-none-any.whl/hello_world/hello_world.py
@@ -0,0 +1,11 @@
+class HelloWorld:
+    def say_hello(self, name):
+        return 'Hello, ' + name
+
+    def fibonacci(self, num):
+        if num == 0:
+            return 0
+        elif num == 2 or num == 1:
+            return 1
+        else:
+            return self.fibonacci(num - 2) + self.fibonacci(num - 1)
