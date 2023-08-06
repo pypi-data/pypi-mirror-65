22e9d7c4413f68d4eff900316cad509a695511b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:42 +0000

    Add package ndtreepy/0.0.2/ndtreepy-0.0.2.tar.gz

---
+++ packages/ndtreepy/ndtreepy-0.0.2.tar.gz/ndtreepy-0.0.2/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+
+requirements = []
+
+setup(
+      name="ndtreepy",
+      version = "0.0.2", #@version@#
+      description="handle,.in progressing..,APIs",
+      author="ihgazni2",
+      url="https://github.com/ihgazni2/ndtreepy",
+      author_email='', 
+      license="MIT",
+      long_description = "refer to .md files in https://github.com/ihgazni2/ndtreepy",
+      classifiers=[
+          'Environment :: Console',
+          'Environment :: Web Environment',
+          'Intended Audience :: End Users/Desktop',
+          'Intended Audience :: Developers',
+          'Intended Audience :: System Administrators',
+          'Programming Language :: Python',
+          ],
+      packages= find_packages(),
+      entry_points={
+          'console_scripts': [
+              'ndtreepy=ndtreepy.bin:main'
+          ]
+      },
+      package_data={
+          'resources':['RESOURCES/*']
+      },
+      include_package_data=True,
+      install_requires=requirements,
+      py_modules=['ndtreepy'], 
+)
+
+
+# python3 setup.py bdist --formats=tar
+# python3 setup.py sdist
+
+
