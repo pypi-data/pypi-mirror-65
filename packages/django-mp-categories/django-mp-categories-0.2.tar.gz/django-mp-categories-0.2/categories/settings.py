7e1c072fa7022b9c1b3c2c1103d861856239a416
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:19 +0000

    Add package django-mp-categories/0.2/django-mp-categories-0.2.tar.gz

---
+++ packages/django-mp-categories/django-mp-categories-0.2.tar.gz/django-mp-categories-0.2/categories/settings.py
@@ -0,0 +1,13 @@
+
+from categories import defaults
+
+
+class CategorySettings(object):
+
+    IS_CATEGORY_LOGO_ENABLED = defaults.IS_CATEGORY_LOGO_ENABLED
+    IS_CATEGORY_ICON_ENABLED = defaults.IS_CATEGORY_ICON_ENABLED
+    IS_CATEGORY_DESCRIPTION_ENABLED = defaults.IS_CATEGORY_DESCRIPTION_ENABLED
+
+    @property
+    def INSTALLED_APPS(self):
+        return super().INSTALLED_APPS + ['categories']
