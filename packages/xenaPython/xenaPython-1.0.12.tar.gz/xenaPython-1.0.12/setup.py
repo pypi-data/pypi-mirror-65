f5d901ad948020f22c2c958261bf015bb3f05464
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:47 +0000

    Add package xenaPython/1.0.12/xenaPython-1.0.12.tar.gz

---
+++ packages/xenaPython/xenaPython-1.0.12.tar.gz/xenaPython-1.0.12/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+print(find_packages())
+setup(
+    name="xenaPython",
+    version="1.0.12",
+    packages=find_packages(),
+    include_package_data=True,
+    author = '@jingchunzhu, @acthp',
+    author_email = 'craft@soe.ucsc.com',
+    description = 'XENA python API',
+    url = 'https://github.com/ucscXena/xenaPython',
+    keywords = ['xena', 'ucsc', 'xenaAPI', 'xenaPython'],
+    license='Apache 2.0',
+    extras_require={
+        "convert": ["scanpy", "loom", "cellbrowser"],
+    }
+)
