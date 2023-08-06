0a61230245c32731cbbd3dca7b46ed548139e6c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:33 +0000

    Add package amphipathic/0.3.4/amphipathic-0.3.4.tar.gz

---
+++ packages/amphipathic/amphipathic-0.3.4.tar.gz/amphipathic-0.3.4/tests/fixtures/sequences.py
@@ -0,0 +1,20 @@
+import pytest
+
+
+@pytest.fixture(name="sequence1")
+def fixture_sequence_1():
+    return 'NLYIQWLKDGGPSSGRPPPS'
+
+
+@pytest.fixture(name="sequence2")
+def fixture_sequence_2():
+    return 'MALPVTALLLPLALLLHAARPSQFRVSPLDRTWNLGETVELKCQVLLSNPTSGCSWLFQPRGA'
+
+
+@pytest.fixture(name="sequence3")
+def fixture_sequence_3():
+    return 'ggctgacggctgggttaagaagtttactgaggcagttaacgcctttaagggtttggactggatc'\
+           'cgctgccaagttgtccaagttccttgattggatcaaatccaagatcatcccggagctcagagag'\
+           'agagcggagtttgttaagaatcttaggcagcttcctcttctcgaggcccagatcaccactttgg'\
+           'agcactccaaccctaatcaggagacccaagaacagcttttctcgaacgtccaatacctggcaca'\
+           'ccactgtaggaagaacgctccgctctatgcagcggaagcccggagagttttcgcactagagaaa'
