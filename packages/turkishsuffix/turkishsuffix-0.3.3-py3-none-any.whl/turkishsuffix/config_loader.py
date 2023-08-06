ce3fadba214da05c92503799c69aeaf5ce9383f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:26 +0000

    Add package turkishsuffix/0.3.3/turkishsuffix-0.3.3-py3-none-any.whl

---
+++ packages/turkishsuffix/turkishsuffix-0.3.3-py3-none-any.whl/turkishsuffix/config_loader.py
@@ -0,0 +1,22 @@
+import collections
+import pkg_resources
+import os
+
+config = {}
+
+
+class ConfigLoader:
+    def __init__(self):
+        executed = {}
+        for file in pkg_resources.resource_listdir(__name__, "config"):
+            if file.endswith(".py"):
+                name = file[:-3]
+                path = pkg_resources.resource_filename(__name__, os.path.join("config", file))
+                executed[name] = {}
+                exec(open(path, encoding='utf8').read(), executed[name])
+                values = [value for value in executed[name] if value != "__builtins__"]
+                Tuple = collections.namedtuple(name, values)
+                config[name] = Tuple(*[executed[name][value] for value in values if value != "__builtins__"])
+
+
+ConfigLoader()
