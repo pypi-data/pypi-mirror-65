b1c292da09bac367c00e20b7719e26e5e76fe07c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:42 +0000

    Add package skreep/0.1.2/skreep-0.1.2-py3-none-any.whl

---
+++ packages/skreep/skreep-0.1.2-py3-none-any.whl/skreep/skreep.py
@@ -0,0 +1,81 @@
+from selenium import webdriver
+from time import sleep
+
+class Skreep:
+    def __init__(self):
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        # options.add_argument('--log-level=3')
+        user_agent = 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36'
+        options.add_argument('user-agent={0}'.format(user_agent))
+        self.driver = webdriver.Chrome(executable_path='.\\chromedriver.exe', options=options)
+        
+    def quit(self):
+        self.driver.quit()
+        
+    def get(self, url, sc=0):
+        self.driver.get(url)
+        sleep(sc)
+        
+    def tag(self, *args, set='default', sc=0):
+        if 'default' == set:
+            tag_name = self.driver.find_element_by_tag_name(args[0])
+            sleep(sc)
+            return tag_name
+
+        if 'in' == set:
+            tag_name = args[0].find_element_by_tag_name(args[1])
+            sleep(sc)
+            return tag_name
+ 
+    def cls(self, *args, set='default', sc=0):
+        if 'default' == set:
+            class_name = self.driver.find_element_by_class_name(args[0])
+            sleep(sc)
+            return class_name
+
+        if 'in' == set:
+            class_name = args[0].find_element_by_class_name(args[1])
+            sleep(sc)
+            return class_name
+
+    def id(self, *args, set='default', sc=0):
+        if 'default' == set:
+            id_name = self.driver.find_element_by_id(args[0])
+            sleep(sc)
+            return id_name
+        
+        if 'in' == set:
+            id_name = args[0].find_element_by_id(args[1])
+            sleep(sc)
+            return id_name
+
+    def path(self, *args, set='default', sc=0):
+        if 'default' == set:
+            xpath_name = self.driver.find_element_by_xpath('//*[@'+ args[0] +']')
+            sleep(sc)
+            return xpath_name
+
+        if 'in' == set:
+            xpath_name = args[0].find_element_by_xpath('//*[@'+ args[1] +']')
+            sleep(sc)
+            return xpath_name
+
+    def paths(self, xpath, sc=0):
+        xpath_name = self.driver.find_elements_by_xpath('//*[@'+ xpath +']')
+        sleep(sc)
+        return xpath_name
+
+    def img(self, *args, set="pa", sc=0):
+        
+        if "pa" == set:
+            sleep(sc)
+            return [i.get_attribute(args[1]) for i in self.driver.find_elements_by_xpath('//*[@'+ args[0] +']')]
+
+        elif "pta" == set:
+            sleep(sc)
+            return [i.find_element_by_tag_name(args[1]).get_attribute(args[2]) for i in self.driver.find_elements_by_xpath('//*[@'+ args[0] +']')]
+
+        elif "ota" == set:
+            sleep(sc)
+            return [i.get_attribute(args[2]) for i in args[0].find_elements_by_tag_name(args[1])]
