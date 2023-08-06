ab8ef6126449a607b72e1d7693ed849a20fab838
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:59 +0000

    Add package markdown-vimwiki/0.1.0/markdown_vimwiki-0.1.0.tar.gz

---
+++ packages/markdown-vimwiki/markdown_vimwiki-0.1.0.tar.gz/markdown_vimwiki-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='markdown_vimwiki',
+    version='0.1.0',
+    description='Markdown extension for vimwiki todos and tags',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Madison Scott-Clary',
+    author_email='makyo@drab-makyo.com',
+    packages=['markdown_vimwiki'],
+    install_requires=['markdown'],
+    url='https://github.com/makyo/markdown-vimwiki',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Text Processing',
+        'Programming Language :: Python :: 3',
+    ])
