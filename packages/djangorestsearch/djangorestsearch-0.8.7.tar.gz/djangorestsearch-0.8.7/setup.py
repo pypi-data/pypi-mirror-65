50d467d11aab351ed81941ea090ceab2e8928c15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:27 +0000

    Add package djangorestsearch/0.8.7/djangorestsearch-0.8.7.tar.gz

---
+++ packages/djangorestsearch/djangorestsearch-0.8.7.tar.gz/djangorestsearch-0.8.7/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='djangorestsearch',
+    version='0.8.7',
+    url='https://github.com/wemap/django-rest-search',
+    license='BSD',
+    description='ElasticSearch integration for Django.',
+    long_description=open('README.rst', 'r').read(),
+    author='Jeremy Lainé',
+    author_email='jeremy@getwemap.com',
+    packages=['rest_search'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    install_requires=[
+        'aws-requests-auth>=0.3.0',
+        'elasticsearch>=6.0.0,<7.0.0',
+    ],
+    python_requires='>=3',
+)
