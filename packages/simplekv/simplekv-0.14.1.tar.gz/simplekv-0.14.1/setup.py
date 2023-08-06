71f1451e446500c9fe3b38aa42da56fd5b05fb6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:17:35 +0000

    Add package simplekv/0.14.1/simplekv-0.14.1.tar.gz

---
+++ packages/simplekv/simplekv-0.14.1.tar.gz/simplekv-0.14.1/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding=utf8
+
+import os
+
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(name='simplekv',
+      version='0.14.1',
+      description=('A key-value storage for binary data, support many '
+                   'backends.'),
+      long_description=read('README.rst'),
+      author='Marc Brinkmann',
+      author_email='git@marcbrinkmann.de',
+      url='http://github.com/mbr/simplekv',
+      license='MIT',
+      packages=find_packages(exclude=['test']),
+      install_requires=[],
+      classifiers=[
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+      ])
