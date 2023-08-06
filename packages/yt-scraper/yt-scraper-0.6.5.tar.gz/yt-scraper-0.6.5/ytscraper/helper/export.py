8a7ad5cce2244f872a37356285fa3c1a377700df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:17 +0000

    Add package yt-scraper/0.6.5/yt-scraper-0.6.5.tar.gz

---
+++ packages/yt-scraper/yt-scraper-0.6.5.tar.gz/yt-scraper-0.6.5/ytscraper/helper/export.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import csv
+import os
+import sys
+import re
+
+
+def export_to_csv(nodes, output_dir):
+    nodes_path = os.path.join(output_dir, "nodes.csv")
+    edges_path = os.path.join(output_dir, "edges.csv")
+
+    edges = set(
+        (node["videoId"], child) for node in nodes for child in node["relatedVideos"]
+    )
+    print(edges)
+    with open(edges_path, "w", newline="") as out:
+        csv_out = csv.writer(out, delimiter="\t")
+        csv_out.writerow(["origin", "target"])
+        for origin, target in edges:
+            csv_out.writerow([origin, target])
+
+    with open(nodes_path, "w", newline="") as out:
+        csv_out = csv.writer(out, delimiter="\t")
+        csv_out.writerow(nodes[0].keys())
+        for node in nodes:
+            del node["relatedVideos"]
+            del node["description"]
+            csv_out.writerow(node.values())
+
+
+def get_call_directory():
+    return os.path.dirname(sys.argv[0])
+
+
+def filter_text(text, encoding="ascii"):
+    if encoding == 'smart':
+        text = re.sub(r"<[^>]*>", " ", text)
+        text = re.sub(r"\[[^\]]*\]", " ", text)
+        text = re.sub(r'[^a-zA-ZäöüÄÖÜß\s]*', '', text)
+        text = re.sub(r"\s+", " ", text).strip()
+    else:
+        text = text.encode(encoding, "ignore").decode()
+    return text
