d15a971aed729e681875a1dd24ce637b123f64df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:16 +0000

    Add package pythonspell/0.7/pythonspell-0.7.tar.gz

---
+++ packages/pythonspell/pythonspell-0.7.tar.gz/pythonspell-0.7/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+with open('README.rst') as f:
+    long_description = f.read()
+setup(name='pythonspell',
+    version='0.7',
+    description='A simple python spellchecker built on BK Trees and Damerau Levenshtein distance',
+    url='https://github.com/AidanJSmith',
+    long_description = long_description,
+    author='Aidan Smith',
+    author_email='100023755@mvla.net',
+    license='MIT',
+    keywords='spelling corrector autocorrect',
+    packages=['pyspell'],
+    zip_safe=False)
