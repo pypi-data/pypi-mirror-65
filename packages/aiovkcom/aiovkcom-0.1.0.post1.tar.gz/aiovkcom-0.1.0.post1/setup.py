ba1165c9b97acbe0c3d40e88852c4685b9cde462
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:52 +0000

    Add package aiovkcom/0.1.0.post1/aiovkcom-0.1.0.post1.tar.gz

---
+++ packages/aiovkcom/aiovkcom-0.1.0.post1.tar.gz/aiovkcom-0.1.0.post1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='aiovkcom',
+    version='0.1.0.post1',
+    author='Konstantin Togoi',
+    author_email='konstantin.togoi@protonmail.com',
+    url='https://github.com/KonstantinTogoi/aiovkcom',
+    description='Python vk.com API wrapper',
+    long_description=open('README.rst').read(),
+    license='BSD',
+    packages=find_packages(),
+    python_requires='>=3.5',
+    install_requires='aiohttp>=3.0.0',
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest-asyncio', 'pytest-localserver'],
+    keywords=['vk.com rest api asyncio'],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
