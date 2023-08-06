6bb0680828ae9556526668735151c6d8b88de8de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:36 +0000

    Add package nlknhjc/1.0.0/nlknhjc-1.0.0.tar.gz

---
+++ packages/nlknhjc/nlknhjc-1.0.0.tar.gz/nlknhjc-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='nlknhjc',
+    version='1.0.0',
+    py_modules= ['nlknhjc'],
+    author='knhjc',
+    author_email='rbshuangjinchao@163.com',
+    url='http://www.baidu.com',
+    description='A simple printer of nested lists',
+)
