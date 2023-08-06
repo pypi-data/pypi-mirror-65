eddad28ff83247303f7107556e9f3e96811318e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:21 +0000

    Add package pyhera/0.5.3/pyhera-0.5.3.tar.gz

---
+++ packages/pyhera/pyhera-0.5.3.tar.gz/pyhera-0.5.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, Extension
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+  name = 'pyhera',
+  packages = ['pyhera'],
+  version = '0.5.3',
+  license='MIT',
+  description = 'pyhera is an optimized in-memory database',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  author = 'Ali Moghimi',
+  author_email = 'thisisjavid@gmail.com',
+  url = 'https://github.com/lstil/pyhera',
+  keywords = ['dbms', 'database', 'nosql', 'pyhera db',
+            'hera', 'pyhera', 'pyhera database'],
+  classifiers=[
+    'Development Status :: 4 - Beta',      # "3 - Alpha" | "4 - Beta" | "5 - Production/Stable"
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License'
+  ],
+)
