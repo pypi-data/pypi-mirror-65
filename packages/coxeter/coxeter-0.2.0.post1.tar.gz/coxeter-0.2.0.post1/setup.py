e9b058bbc4a1dc4fb06f6d1c4215e8630d244be6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:18:05 +0000

    Add package coxeter/0.2.0.post1/coxeter-0.2.0.post1.tar.gz

---
+++ packages/coxeter/coxeter-0.2.0.post1.tar.gz/coxeter-0.2.0.post1/setup.py
@@ -0,0 +1,54 @@
+from setuptools import setup, find_packages
+import os
+
+# Read README for PyPI, fallback if it fails.
+desc = 'Tools for creating and manipulating shapes.'
+try:
+    readme_file = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), 'README.md')
+    with open(readme_file) as f:
+        readme = f.read()
+except ImportError:
+    readme = desc
+
+version = '0.2.0.post1'
+
+
+################################################
+# Set up for the various optional dependencies
+# that may be installed for additional features.
+################################################
+
+test_deps = [
+    'pytest',
+    'hypothesis[numpy]',
+]
+
+bounding_deps = [
+    'miniball',
+]
+
+extras = {
+    'test': test_deps + bounding_deps,
+    'bounding_sphere': bounding_deps,
+}
+
+setup(
+    name='coxeter',
+    version=version,
+    description=desc,
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    url='https://github.com/glotzerlab/coxeter',
+    author='Vyas Ramasubramani',
+    author_email='vramasub@umich.edu',
+    packages=find_packages(),
+    package_data={'coxeter': ['science.1220869.json']},
+    install_requires=[
+        'numpy',
+        'rowan>=1.2',
+        'scipy',
+    ],
+    tests_require=test_deps,
+    extras_require=extras,
+    zip_safe=True)
