e3ee9295403ea547ce31e8c82d5afc53cec8789a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:14 +0000

    Add package distributions-GB-SA/0.1/distributions_GB_SA-0.1.tar.gz

---
+++ packages/distributions-GB-SA/distributions_GB_SA-0.1.tar.gz/distributions_GB_SA-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_GB_SA',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      packages=['distributions_GB_SA'],
+      author='SOUKAINA ABELHAD',
+      author_email='abelhad.soukaina@gmail.com',
+      zip_safe=False)
