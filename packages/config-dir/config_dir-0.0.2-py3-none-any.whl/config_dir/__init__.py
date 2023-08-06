ecc36477eeed43ef4f9b8b273ce43ab7578b5931
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:21 +0000

    Add package config-dir/0.0.2/config_dir-0.0.2-py3-none-any.whl

---
+++ packages/config-dir/config_dir-0.0.2-py3-none-any.whl/config_dir/__init__.py
@@ -0,0 +1,44 @@
+__license__ = "Apache License 2.0"
+__author__ = "jan grant <config-dir@ioctl.org>"
+
+import os
+import pathlib
+import yaml
+
+
+def config_dir(name=None, sub_dir=None):
+    home = pathlib.Path.home() / name
+    if not home.exists():
+        print("Making config dir")
+        home.mkdir(0o700)
+    if not home.is_dir():
+        raise FileExistsError("Expected a directory:", home)
+
+    if sub_dir is None:
+        return home
+
+    sub_dir = home / sub_dir
+    if not sub_dir.exists():
+        print("Making config dir")
+        sub_dir.mkdir(0o700)
+    if not sub_dir.is_dir():
+        raise FileExistsError("Expected a directory:", sub_dir)
+    return sub_dir
+
+
+def load_config(name=None, default={}, sub_dir=None, sub_name="config.yaml"):
+    conf = config_dir(name=name, sub_dir=sub_dir) / sub_name
+    if not conf.exists():
+        config = dict(default)
+        with os.fdopen(os.open(conf, os.O_CREAT | os.O_WRONLY, 0o600), "w") as f:
+            yaml.safe_dump(config, f)
+    else:
+        with conf.open() as f:
+            config = yaml.safe_load(f)
+    return config
+
+
+def save_config(name=None, config=None, sub_dir=None, sub_name="config.yaml"):
+    conf = config_dir(name=name, sub_dir=sub_dir) / sub_name
+    with os.fdopen(os.open(conf, os.O_CREAT | os.O_WRONLY, 0o600), "w") as f:
+        yaml.safe_dump(config, f)
