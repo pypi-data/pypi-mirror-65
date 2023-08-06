46c2dc0d168d71162e582fb77c7ba0c2ca90a560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:48 +0000

    Add package idem-windows/1.3/idem_windows-1.3-py3-none-any.whl

---
+++ packages/idem-windows/idem_windows-1.3-py3-none-any.whl/idem_windows/tests/integration/test_version.py
@@ -0,0 +1,11 @@
+# import version
+import pytest
+
+
+class TestVersion:
+    """
+    Automatically generated test
+    """
+
+    def test_get_ver(self, hub):
+        mock_hub = hub.pop.testing.mock_hub()
