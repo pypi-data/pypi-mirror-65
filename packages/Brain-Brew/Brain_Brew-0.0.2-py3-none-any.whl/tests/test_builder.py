c8c9e5353a963780eaaca5b73e25f49df52a8a06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:56 +0000

    Add package Brain-Brew/0.0.2/Brain_Brew-0.0.2-py3-none-any.whl

---
+++ packages/Brain-Brew/Brain_Brew-0.0.2-py3-none-any.whl/tests/test_builder.py
@@ -0,0 +1,24 @@
+from unittest.mock import patch
+
+from brain_brew.build_tasks.generate_guids import GenerateGuids
+from brain_brew.build_tasks.source_crowd_anki import SourceCrowdAnki
+from brain_brew.build_tasks.source_csv import SourceCsv
+from brain_brew.build_tasks.source_csv_collection import SourceCsvCollection
+from brain_brew.builder import Builder
+from brain_brew.representation.configuration.yaml_file import YamlFile
+from tests.test_files import TestFiles
+from tests.test_helpers import global_config
+
+
+class TestConstructor:
+    def test_runs(self, global_config):
+
+        with patch.object(SourceCsvCollection, "__init__", lambda x, y, z: None), \
+             patch.object(SourceCsv, "__init__", lambda x, y, z: None), \
+             patch.object(GenerateGuids, "__init__", lambda x, y, z: None), \
+             patch.object(SourceCrowdAnki, "__init__", lambda x, y, z: None):
+
+            data = YamlFile.read_file(TestFiles.BuildConfig.ONE_OF_EACH_TYPE)
+            builder = Builder(data, global_config, None, read_now=False)
+
+            assert len(builder.build_tasks) == 4
---
+++ packages/Brain-Brew/Brain_Brew-0.0.2-py3-none-any.whl/tests/test_files.py
@@ -0,0 +1,54 @@
+
+
+class TestFiles:
+    class Headers:
+        LOC = "tests/test_files/deck_parts/headers/"
+
+        FIRST = "default header"
+        FIRST_COMPLETE = "default-header.json"
+
+    class NoteFiles:
+        LOC = "tests/test_files/deck_parts/"
+
+        NO_GROUPING_OR_SHARED_TAGS = "csvtonotes1_withnogroupingorsharedtags.json"
+        WITH_GROUPING = "csvtonotes1_withgrouping.json"
+        WITH_SHARED_TAGS = "csvtonotes1_withsharedtags.json"
+        WITH_SHARED_TAGS_EMPTY_AND_GROUPING = "csvtonotes1_withsharedtagsandgrouping_butnothingtogroup.json"
+        WITH_SHARED_TAGS_AND_GROUPING = "csvtonotes2_withsharedtagsandgrouping.json"
+
+    class NoteModels:
+        LOC = "tests/test_files/deck_parts/note_models/"
+
+        TEST = "Test Model"
+        TEST_COMPLETE = "Test-Model.json"
+
+        LL_WORD = "LL Word"
+        LL_WORD_COMPLETE = "LL_Word.json"
+
+    class CsvFiles:
+        LOC = "tests/test_files/csv/"
+
+        TEST1 = LOC + "test1.csv"
+        TEST2 = LOC + "test2.csv"
+        TEST1_SPLIT1 = LOC + "test1_split1.csv"
+        TEST1_SPLIT2 = LOC + "test1_split2.csv"
+
+    class UnfinishedData:
+        LOC = "tests/test_files/unfinished_data/"
+
+        FIRST_SET = LOC + "test1_after_csv_mapping.json"
+        SECOND_SET = LOC + "test2_after_csv_mapping.json"
+        FIRST_SET_SPLIT1 = LOC + "test3_after_csv_mapping.json"
+        FIRST_SET_SPLIT2 = LOC + "test4_after_csv_mapping.json"
+
+    class CrowdAnkiExport:
+        LOC = "tests/test_files/crowd_anki/"
+
+        TEST1_FOLDER = LOC + "crowdanki_example_1/"
+        TEST1_FOLDER_WITHOUT_SLASH = LOC + "crowdanki_example_1"
+        TEST1_JSON = TEST1_FOLDER + "deck.json"
+
+    class BuildConfig:
+        LOC = "tests/test_files/build_files/"
+
+        ONE_OF_EACH_TYPE = LOC + "builder1.yaml"
