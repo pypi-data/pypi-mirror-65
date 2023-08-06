d21f046e5f6faf46061ad703c11566326302e27c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:03 +0000

    Add package undmainchain/0.0.1/undmainchain-0.0.1.tar.gz

---
+++ packages/undmainchain/undmainchain-0.0.1.tar.gz/undmainchain-0.0.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+requires = [
+    'click>=6.7',
+]
+
+setuptools.setup(
+    name="undmainchain",
+    version="0.0.1",
+    author="Unification Foundation",
+    author_email="indika@unification.com",
+    description="Helper tools for administering the Unification Mainchain",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/unification-com/mainchain-helpers",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=requires,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
