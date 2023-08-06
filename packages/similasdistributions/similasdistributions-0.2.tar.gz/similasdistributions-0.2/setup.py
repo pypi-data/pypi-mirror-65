a476d4089b165f4ac19b9c0f2629e7dfa219e06b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:02 +0000

    Add package similasdistributions/0.2/similasdistributions-0.2.tar.gz

---
+++ packages/similasdistributions/similasdistributions-0.2.tar.gz/similasdistributions-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='similasdistributions',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['similasdistributions'],
+      zip_safe=False)
