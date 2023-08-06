11aab819d681aa17dd0f66279f3918981d2afd7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:21 +0000

    Add package adafruit-circuitpython-led-animation/1.1.3/adafruit-circuitpython-led%20animation-1.1.3.tar.gz

---
+++ packages/adafruit-circuitpython-led-animation/adafruit-circuitpython-led%20animation-1.1.3.tar.gz/adafruit-circuitpython-led animation-1.1.3/adafruit_led_animation/color.py
@@ -0,0 +1,23 @@
+"""Color variables made available for import.
+
+RAINBOW is a list of colors to use for cycling through.
+"""
+RED = (255, 0, 0)
+YELLOW = (255, 150, 0)
+ORANGE = (255, 40, 0)
+GREEN = (0, 255, 0)
+TEAL = (0, 255, 120)
+CYAN = (0, 255, 255)
+BLUE = (0, 0, 255)
+PURPLE = (180, 0, 255)
+MAGENTA = (255, 0, 20)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+GOLD = (255, 222, 30)
+PINK = (242, 90, 255)
+AQUA = (50, 255, 255)
+JADE = (0, 255, 40)
+AMBER = (255, 100, 0)
+
+RAINBOW = (RED, ORANGE, YELLOW, GREEN, BLUE, PURPLE)
