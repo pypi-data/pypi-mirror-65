0fe334f10dc0c54adfde45e6370ad4f7f31daed8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:10 +0000

    Add package pyuni/0.0.3/pyuni-0.0.3.tar.gz

---
+++ packages/pyuni/pyuni-0.0.3.tar.gz/pyuni-0.0.3/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name='pyuni',  # Replace with your own username
+    version='0.0.3',
+    author='Anh Pham',
+    author_email="anhphamduy@outlook.com",
+    description='A tool to get classes timetable',
+    url="https://github.com/anhphamduy/pyuni",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'selenium',
+    ],
+    classifiers=[],
+    python_requires='>=3.6',
+)
