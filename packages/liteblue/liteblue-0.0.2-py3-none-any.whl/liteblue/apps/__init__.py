ddf4a873d3283371db6336dde869102c3755c728
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:32 +0000

    Add package liteblue/0.0.2/liteblue-0.0.2-py3-none-any.whl

---
+++ packages/liteblue/liteblue-0.0.2-py3-none-any.whl/liteblue/apps/__init__.py
@@ -0,0 +1,23 @@
+'''
+    This is a our invoke program
+'''
+import tornado.log
+from invoke import Program, Collection
+from . import run
+from . import db
+
+VERSION = '0.0.2'
+
+_NAMESPACE_ = Collection()
+
+_NAMESPACE_.add_task(run.run)
+_NAMESPACE_.add_task(run.create)
+_NAMESPACE_.add_task(db.upgrade)
+_NAMESPACE_.add_task(db.downgrade)
+_NAMESPACE_.add_task(db.revise)
+
+tornado.log.enable_pretty_logging()
+
+program = Program(
+    version=VERSION, namespace=_NAMESPACE_
+)  # pylint: disable=C0103
