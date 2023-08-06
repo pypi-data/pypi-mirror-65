94b9f975487d4c7b43864f79226fe86dbe1155fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:30 +0000

    Add package image-processor-client/2.1.4/image_processor_client-2.1.4.tar.gz

---
+++ packages/image-processor-client/image_processor_client-2.1.4.tar.gz/image_processor_client-2.1.4/image_processor_client/__init__.py
@@ -0,0 +1,16 @@
+"""
+Python Client for Image Processor
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+A python client for .. _Image Processor API  Server: https://github.com/thec0sm0s/image-processor.
+
+:copyright: (c) 2019 □ | The Cosmos
+:license: MIT, see LICENSE for more details.
+
+"""
+
+
+__version__ = "2.1.4"
+__author__ = "□ | The Cosmos"
+
+from .client import Client
