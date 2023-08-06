31ebcb88502ae0d3a483f9dd232172aab011609a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:22 +0000

    Add package toil-http-parser/0.8.9/toil-http-parser-0.8.9.tar.gz

---
+++ packages/toil-http-parser/toil-http-parser-0.8.9.tar.gz/toil-http-parser-0.8.9/toil_http_parser/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -
+#
+# This file is part of toil_http_parser released under the MIT license.
+# See the NOTICE for more information.
+
+version_info = (0, 8, 9)
+__version__ = ".".join(map(str, version_info))
