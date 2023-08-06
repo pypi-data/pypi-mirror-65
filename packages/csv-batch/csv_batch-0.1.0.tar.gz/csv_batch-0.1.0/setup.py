3f1d46b9399723f8d4e018e1bf85bce180b9a052
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:01 +0000

    Add package csv-batch/0.1.0/csv_batch-0.1.0.tar.gz

---
+++ packages/csv-batch/csv_batch-0.1.0.tar.gz/csv_batch-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+with open('requirements/requirements-test.txt') as f:
+    test_requirements = f.read().splitlines()
+
+
+setup(
+    name='csv_batch',
+    author='Mitchell Lisle',
+    author_email='m.lisle90@gmail.com',
+    description="A Python Encryption Helper Library",
+    packages=find_packages(),
+    setup_requires=[],
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/mitchelllisle/csv_batch',
+    version='0.1.0',
+    zip_safe=False,
+)
