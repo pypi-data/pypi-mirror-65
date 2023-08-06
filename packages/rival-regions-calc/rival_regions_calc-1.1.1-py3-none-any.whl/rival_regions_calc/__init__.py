398a6838461ca9d762a466d966831a5c671a60fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:54 +0000

    Add package rival-regions-calc/1.1.1/rival_regions_calc-1.1.1-py3-none-any.whl

---
+++ packages/rival-regions-calc/rival_regions_calc-1.1.1-py3-none-any.whl/rival_regions_calc/__init__.py
@@ -0,0 +1,13 @@
+
+"""
+Unofficial calculator for Rival Regions. Easily calculate common known
+formulas from the game.
+"""
+
+from .item import Item
+from .value import Value 
+from .resource_coefficient import ResourceCoefficient
+from .work_production import WorkProduction
+from .building import Building
+from .construction_costs import ConstructionCosts
+from .deep_exploration import DeepExploration
