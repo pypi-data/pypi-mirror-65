a80f0bca4795fbc4c07464bea9fd8f6490aeb73d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:47 +0000

    Add package matrixprofile/1.0.2/matrixprofile-1.0.2-cp37-cp37m-manylinux2010_i686.whl

---
+++ packages/matrixprofile/matrixprofile-1.0.2-cp37-cp37m-manylinux2010_i686.whl/matrixprofile/__init__.py
@@ -0,0 +1,11 @@
+name = "matrixprofile"
+
+
+from matrixprofile.compute import compute
+from matrixprofile.visualize import visualize
+from matrixprofile.analyze import analyze
+from matrixprofile import discover
+from matrixprofile import utils
+from matrixprofile import io
+from matrixprofile import algorithms
+from matrixprofile import datasets
