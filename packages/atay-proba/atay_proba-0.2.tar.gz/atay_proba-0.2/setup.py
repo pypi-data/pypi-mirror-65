085b6e5e82cc098236fc182c76ee35b58d8dc031
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:38 +0000

    Add package atay-proba/0.2/atay_proba-0.2.tar.gz

---
+++ packages/atay-proba/atay_proba-0.2.tar.gz/atay_proba-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='atay_proba',
+      version='0.2',
+      description='Gaussian and binomial distributions',
+      author="Ali El Adi",
+      author_email="alieladi@gmail.com",
+      packages=['atay_proba'],
+      zip_safe=False)
