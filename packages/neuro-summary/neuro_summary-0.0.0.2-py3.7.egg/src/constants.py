b339a77639f34b2085215053e72475b2a3f00af5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:18:04 +0000

    Add package neuro-summary/0.0.0.2/neuro_summary-0.0.0.2-py3.7.egg

---
+++ packages/neuro-summary/neuro_summary-0.0.0.2-py3.7.egg/src/constants.py
@@ -0,0 +1,3 @@
+spike_threshold=5.5 #Scaler threshold for what is considered a spike based on standard deviation
+wave_size=(5, 5) #Constrain wave plots
+acf_lags=14000 #Used to constrain the population spiking vector autocorr lags
