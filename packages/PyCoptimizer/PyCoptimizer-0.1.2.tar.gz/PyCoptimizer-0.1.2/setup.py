b30eba9a8181509542cc302f9162a58fd75923ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:11 +0000

    Add package PyCoptimizer/0.1.2/PyCoptimizer-0.1.2.tar.gz

---
+++ packages/PyCoptimizer/PyCoptimizer-0.1.2.tar.gz/PyCoptimizer-0.1.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='PyCoptimizer',
+    version='0.1.2',
+    description='Python Code Optimization using compilation flags',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Leticia Figueiredo',
+    author_email='letfreirefigueiredo@gmail.com',
+    packages=find_packages(),
+    license='Apache License',
+    install_requires=['setuptools', 'numpy', 'matplotlib'],
+    package_dir={'COptimizer': './PyCoptimizer/coptimizer',
+                 'UserParameters': './PyCoptimizer/user_parameters',
+                 'Results': './PyCoptimizer/results',
+                 'CompClean': './PyCoptimizer/framework/comp_clean',
+                 'Island': './PyCoptimizer/framework/island'}
+)
