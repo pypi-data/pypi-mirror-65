7e49dc0f00239dd800fbd9b2b0e1068406b84c49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:25 +0000

    Add package runningz-automl/0.0.2/runningz_automl-0.0.2.tar.gz

---
+++ packages/runningz-automl/runningz_automl-0.0.2.tar.gz/runningz_automl-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup, Extension
+import pathlib
+# The directory containing this file
+
+setup(name='runningz_automl',
+      version='0.0.2',
+      description='runningz_automl',
+      url='https://github.com/RunxingZhong/stacking_automl_001',
+      author='zhongrunxinig',
+      author_email='zhongrunxing@gmail.com',
+      license='MIT',
+      include_package_data=True,
+      packages=['runningz_automl'],
+      zip_safe=False)
