0e29a63c38ecc75f6158ad54df3a3e2773c67726
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:26 +0000

    Add package jiuming-tools/1.18/jiuming_tools-1.18-py3-none-any.whl

---
+++ packages/jiuming-tools/jiuming_tools-1.18-py3-none-any.whl/src/config/config_manager.py
@@ -0,0 +1,96 @@
+import os
+
+import click
+import yaml
+
+from src.config.global_config import default_config_dir
+
+# 默认配置file
+sync_config_default_file = default_config_dir + '/sync_config.yaml'
+
+
+def details():
+    checkSyncConfig()
+    y_read_file = open(sync_config_default_file, 'r')
+    config = yaml.safe_load(y_read_file)
+    if config is None:
+        click.echo("暂无同步仓库配置信息!")
+        return
+    else:
+        sc = SyncConfig.to_obj(config[0])
+        click.echo("仓库地址:{},用户名:{},密码:{}".format(sc.url, sc.username, sc.password))
+
+
+def create(url, username, password):
+    """
+    创建配置  多次创建 会依次覆盖
+    :param url:  同步仓库url
+    :param username:  同步仓库用户名
+    :param password:  同步仓库密码
+    :return:
+    """
+    y_file = open(sync_config_default_file, 'w')
+    sc = SyncConfig(url=url, username=username, password=password)
+    yaml.safe_dump([sc.__dict__], y_file)
+    click.echo('\n录入的同步仓库信息:\n地址:{},用户名:{},密码:{}'.format(url, username, password))
+
+
+def remove():
+    """
+    删除 配置文件
+    :return:
+    """
+    try:
+        checkSyncConfig()
+        os.unlink(sync_config_default_file)
+    except OSError:
+        click.echo("删除同步仓库配置失败")
+    else:
+        click.echo("删除同步仓库配置")
+
+
+def pull():
+    checkSyncConfig()
+    click.echo("sync pull config")
+
+
+def push():
+    checkSyncConfig()
+    click.echo("sync push config ")
+
+
+def checkSyncConfig():
+    """
+    检查 配置文件是否存在
+    :return:
+    """
+    if not os.path.exists(sync_config_default_file):
+        raise Exception(click.echo("同步仓库配置不存在,请创建同步配置"))
+
+
+class SyncConfig(object):
+    """
+    同步配置 对象
+    暂时只支持 http/https 协议git 操作
+    """
+    # 地址
+    url: str
+    # git用户名
+    username: str
+    # 密码
+    password: str
+
+    def __init__(self, url, username, password):
+        self.url = url
+        self.username = username
+        self.password = password
+
+    @staticmethod
+    def to_obj(d: dict):
+        """
+        将读取的dict 转换为 serverConfig
+        :param d:  dict
+        :return: ServerConfig
+        """
+        return SyncConfig(url=d['url'], username=d['username'],
+                          password=d['password'])
