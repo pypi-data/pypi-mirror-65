1d82393efae2395e66eeb1683b2695fe0b31ba3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:02 +0000

    Add package madprofiling/0.6/madprofiling-0.6.tar.gz

---
+++ packages/madprofiling/madprofiling-0.6.tar.gz/madprofiling-0.6/madprofiling/__init__.py
@@ -0,0 +1,1 @@
+from madprofiling.madprofiling import madprofiling
---
+++ packages/madprofiling/madprofiling-0.6.tar.gz/madprofiling-0.6/madprofiling/madprofiling.py
@@ -0,0 +1,23 @@
+class madprofiling:
+
+    def madprofiling(df):
+        import pandas as pd
+        names=list(df.columns)
+        unique=[]
+        for var_i in names:
+            unique.append(len(df[var_i].unique()))
+        df_unique=pd.DataFrame(unique,index=list(df.columns))
+        df_unique.columns=['unique']
+        
+        profiling=pd.DataFrame(df.dtypes)
+        profiling.columns=['type']
+        profiling.loc[profiling.type=='object', 'categorical'] = 1  
+        profiling.loc[profiling.type!='object', 'categorical'] = 0
+        profiling['total']=(len(df))
+        profiling=pd.concat([profiling,df_unique],axis=1,sort=False)
+        profiling['null']=pd.DataFrame(df.isnull().sum())
+        profiling['na']=pd.DataFrame(df.isna().sum())
+        profiling['null_pcnt']=round(profiling.null/profiling.total,1)
+        profiling['na_pcnt']=round(profiling.na/profiling.total,1)
+        profiling=profiling.sort_values(by='null_pcnt')
+        return profiling
