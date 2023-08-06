15e87740dcca940147fe9c167e6a2999df1c99d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:21 +0000

    Add package os-rq-scrapy/0.0.7/os-rq-scrapy-0.0.7.tar.gz

---
+++ packages/os-rq-scrapy/os-rq-scrapy-0.0.7.tar.gz/os-rq-scrapy-0.0.7/README.md
@@ -0,0 +1,45 @@
+# os-rq-scrapy
+
+[![Build Status](https://www.travis-ci.org/cfhamlet/os-rq-scrapy.svg?branch=master)](https://www.travis-ci.org/cfhamlet/os-rq-scrapy)
+[![codecov](https://codecov.io/gh/cfhamlet/os-rq-scrapy/branch/master/graph/badge.svg)](https://codecov.io/gh/cfhamlet/os-rq-scrapy)
+[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/os-rq-scrapy.svg)](https://pypi.python.org/pypi/os-rq-scrapy)
+[![PyPI](https://img.shields.io/pypi/v/os-rq-scrapy.svg)](https://pypi.python.org/pypi/os-rq-scrapy)
+
+
+A framework for [Scrapy](https://github.com/scrapy/scrapy) working with [os-rq-pod](https://github.com/cfhamlet/os-rq-pod) and [os-rq-hub](https://github.com/cfhamlet/os-rq-hub) to build ["broad crawls"](https://docs.scrapy.org/en/latest/topics/broad-crawls.html) system.
+
+As you know, Scrapy is a very popular python crawler framework. It is suit for "focused crawl", start from several URLs of specific sites, fetch html, extract and save "structured data" also with patternd links to crawl recursively. But for large scale, long time crawling especially "broad crawls", scrapy is incompetent. Basically, you have to decouple the whole crawling system into several sub-systems, high-performance full-featured distributed fetcher, task scheduler, html extractor, link database, data storage, proxy and a lot of auxiliary equipments. It will be more complex when your system is for multi-tenancy.
+
+The os-rq-scrapy and [os-rq-pod](https://github.com/cfhamlet/os-rq-pod) project are basic components to build "broad crawls" system. The core conceptions are very simple, os-rq-pod is multi-sites request queue have http api to recieve requests. os-rq-scrapy is fetcher, getting requests from os-rq-pod and crawl multi-sites concurrently.  [os-rq-hub](https://github.com/cfhamlet/os-rq-hub) can also be used to connect multi pod and scrapy instances to work simultaneously.
+
+
+## Requirements
+
+* Python 3.6+ (pypy3.6+)
+* [Scrapy](https://github.com/scrapy/scrapy) 2.0
+
+extra requirements:
+
+* [ujson](https://github.com/ultrajson/ultrajson), for json performance
+
+## Install
+
+```
+pip install os-rq-scrapy
+```
+
+## Usage
+
+### Command line
+
+``rq-scrapy`` command enhance the basic ``scrapy`` command. When RQ_API is configured, the ``crawl`` subcommand will run on rq mode, get requests from rq.
+
+## Unit Tests
+
+```
+tox
+```
+
+## License
+
+MIT licensed.
---
+++ packages/os-rq-scrapy/os-rq-scrapy-0.0.7.tar.gz/os-rq-scrapy-0.0.7/src/rq_scrapy/commands/crawl.py
@@ -0,0 +1,86 @@
+from scrapy.commands.crawl import Command as ScrapyCommand
+from scrapy.exceptions import UsageError
+from scrapy.utils.log import logger
+
+from rq_scrapy.crawlers import get_crawlers_dict, load_crawler_class
+
+
+class Command(ScrapyCommand):
+    def add_options(self, parser):
+        super(Command, self).add_options(parser)
+        self.crawlers = get_crawlers_dict("rq_scrapy.crawlers")
+        parser.add_option(
+            "-c",
+            "--crawler-class",
+            metavar="CRAWLER_CLASS",
+            help=(
+                "set crawler class. "
+                "choose from %r "
+                "default 'scrapy' if RQ_API is not configured. "
+                "crawler class path is also acceptable"
+            )
+            % list(self.crawlers),
+        )
+        parser.add_option(
+            "-r",
+            "--rq-api",
+            metavar="RQ_API",
+            help=("set rq api uri. " "the default crawler class is 'rq-demo'"),
+        )
+
+    def process_options(self, args, opts):
+        super(Command, self).process_options(args, opts)
+        self.settings.set("CRAWLER_CLASS", "scrapy", "command")
+        if opts.crawler_class:
+            self.settings.set("CRAWLER_CLASS", opts.crawler_class, "cmdline")
+        if opts.rq_api:
+            self.settings.set("RQ_API", opts.rq_api, "cmdline")
+
+        if self.settings.get("RQ_API", None):
+            self.settings.set("CRAWLER_CLASS", "rq-demo", "command")
+            self.settings.set(
+                "TWISTED_REACTOR",
+                "twisted.internet.asyncioreactor.AsyncioSelectorReactor",
+                "command",
+            )
+
+    def _create_crawler(self, spname):
+        c = self.settings.get("CRAWLER_CLASS")
+        crawler_class = None
+        if c in self.crawlers:
+            crawler_class = self.crawlers[c]
+        else:
+            crawler_class = load_crawler_class(c)
+        return crawler_class(
+            self.crawler_process.spider_loader.load(spname), self.settings
+        )
+
+    def run(self, args, opts):
+        if len(args) < 1:
+            raise UsageError()
+        elif len(args) > 1:
+            raise UsageError(
+                (
+                    "running 'rq-scrapy crawl' ",
+                    "with more than one spider is no longer supported",
+                )
+            )
+        spname = args[0]
+
+        crawler = self._create_crawler(spname)
+        logger.debug("Crawler %s.%s" % (crawler.__module__, crawler.__class__.__name__))
+
+        crawl_defer = self.crawler_process.crawl(crawler, **opts.spargs)
+
+        if getattr(crawl_defer, "result", None) is not None and issubclass(
+            crawl_defer.result.type, Exception
+        ):
+            self.exitcode = 1
+        else:
+            self.crawler_process.start()
+
+            if self.crawler_process.bootstrap_failed or (
+                hasattr(self.crawler_process, "has_exception")
+                and self.crawler_process.has_exception
+            ):
+                self.exitcode = 1
