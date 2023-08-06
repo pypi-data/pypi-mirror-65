9c66bd4d7378a12d3a4e637bac775decacaa9b03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:31 +0000

    Add package magic-admin-django/0.0.0.0/magic-admin-django-0.0.0.0.tar.gz

---
+++ packages/magic-admin-django/magic-admin-django-0.0.0.0.tar.gz/magic-admin-django-0.0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='magic-admin-django',
+    version='0.0.0.0',
+    description='Troll Goat',
+    url='https://magic.link',
+    author='Troll Goat',
+    author_email='arthur@magic.link',
+    license='MIT',
+    packages=['magic_admin_django'],
+    zip_safe=False,
+)
