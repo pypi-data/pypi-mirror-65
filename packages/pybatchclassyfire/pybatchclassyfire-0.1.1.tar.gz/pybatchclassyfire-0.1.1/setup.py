9b44600aa0820f54dac91b289d2093d0b3e689ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:21 +0000

    Add package pybatchclassyfire/0.1.1/pybatchclassyfire-0.1.1.tar.gz

---
+++ packages/pybatchclassyfire/pybatchclassyfire-0.1.1.tar.gz/pybatchclassyfire-0.1.1/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+    
+
+setup(
+    name='pybatchclassyfire',
+
+    version='0.1.1',
+
+    description='A python client for the ClassyFire API',
+
+    url='https://gitlab.unige.ch/Pierre-Marie.Allard/pybatchclassyfire.git',
+
+    author='Pierre-Marie Allard',
+    author_email='pierre-marie.allard@unige.ch',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Chemistry',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    keywords='Cheminformatics ClassyFire ontology',
+
+    packages=['pybatchclassyfire'],
+
+    install_requires=['requests'],
+
+    extras_require={
+        'sdf_query': ['rdkit'],
+    },
+)
