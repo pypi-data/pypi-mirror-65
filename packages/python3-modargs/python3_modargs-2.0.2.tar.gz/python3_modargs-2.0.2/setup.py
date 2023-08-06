b75f7f4616e7bfa3fde18e7ecba38f15db231b8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:57 +0000

    Add package python3-modargs/2.0.2/python3_modargs-2.0.2.tar.gz

---
+++ packages/python3-modargs/python3_modargs-2.0.2.tar.gz/python3_modargs-2.0.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+config = {
+ 'description': 'modargs is a simple command line argument parsing library that infers arguments from functions in a module',
+ 'author': 'Zed A. Shaw',
+ 'maintainer' : 'Ana Nelson',
+ 'maintainer_email' : 'ana@ananelson.com',
+ 'url': 'http://pypi.python.org/pypi/python3-modargs',
+ 'version': '2.0.2',
+ 'packages': ['modargs'],
+ 'name': 'python3_modargs'
+}
+setup(**config)
