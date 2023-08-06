5f30d79285f0fa91ae59f2d57bb33c50c571588a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:14 +0000

    Add package ronto/0.1.0/ronto-0.1.0-py3-none-any.whl

---
+++ packages/ronto/ronto-0.1.0-py3-none-any.whl/ronto/model/__init__.py
@@ -0,0 +1,45 @@
+import os
+import yaml
+
+from ronto import verbose
+
+# module global variable
+model_ = {}
+# List of variables
+variables = {}
+
+
+def get_model():
+    return model_
+
+
+def read_rontofile(file):
+    global model_
+    verbose("Read Rontofile.yml")
+    model_ = yaml.load(file, Loader=yaml.FullLoader)
+    update_defaults()
+
+
+def env_val(key, value):
+    env = os.getenv(key)
+    if env:
+        verbose(f"Read from environment {key}: {env}")
+        return env
+    else:
+        verbose(f"Use default for {key}: {value}")
+        return value
+
+
+def update_defaults():
+    """
+    Section defaults to deal with, like:
+
+    defaults:
+        FOO: 'foo'
+        BAR: 'bar'
+    """
+    global variables
+    verbose("Update default variables")
+    if "defaults" in model_ and isinstance(model_["defaults"], dict):
+        for default in model_["defaults"]:
+            variables[default] = env_val(default, model_["defaults"][default])
