976689a11f84cf7a4f0b026f33bc3057f27e80f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:25 +0000

    Add package utils-functions-plus/0.0.6/utils_functions_plus-0.0.6-py3-none-any.whl

---
+++ packages/utils-functions-plus/utils_functions_plus-0.0.6-py3-none-any.whl/utils_plus/decorator.py
@@ -0,0 +1,90 @@
+import functools
+import threading
+from datetime import datetime
+
+REPORT_ERROR = lambda message: print(message)
+
+NOTIFY = lambda args, result, argument: print(result)
+
+
+def init_decorator(report_error, notify):
+    global REPORT_ERROR, NOTIFY
+    REPORT_ERROR = report_error
+    NOTIFY = notify
+
+
+class lazy_property(object):
+    def __init__(self, function):
+        self.function = function
+        functools.update_wrapper(self, function)
+
+    def __get__(self, obj, type_):
+        if obj and self.function.__name__ in obj.__dict__:
+            return obj.__dict__[self.function.__name__]
+        val = _function_execution(lambda: self.function(obj), func_name=self.function.__name__)
+        obj.__dict__[self.function.__name__] = val
+        return val
+
+
+def _function_execution(function, func_name="function_execution"):
+    try:
+        result = function()
+    except Exception as exc:
+        REPORT_ERROR(func_name, exc)
+        return
+
+    return result
+
+
+def notify_thread(argument):
+    def decorator(function):
+        def wrapper(*args, **kwargs):
+            result = _function_execution(lambda: function(*args, **kwargs), func_name=function.__name__)
+            NOTIFY(args, result, argument)
+            return result
+
+        return wrapper
+
+    return decorator
+
+
+def timeout(time_count):
+    def decorator(function):
+        def wrapper(*args, **kwargs):
+            response = {"result": None}
+
+            def process(resp):
+                resp["result"] = _function_execution(lambda: function(*args, **kwargs), func_name=function.__name__)
+
+            task = threading.Thread(target=process, args=(response,), daemon=True)
+            task.start()
+            task.join(time_count)
+
+            return response["result"]
+
+        return wrapper
+
+    return decorator
+
+
+def measure_time(callback):
+    def decorator(function):
+        def wrapper(*args, **kwargs):
+            start_time = datetime.now()
+            result = function(*args, **kwargs)
+            end_time = datetime.now()
+            delta_in_minutes = (end_time - start_time).seconds / 60
+            callback(args[0], round(delta_in_minutes, 2))
+            return result
+
+        return wrapper
+
+    return decorator
+
+
+def prevent_exception(function):
+    def wrapper(*args, **kwargs):
+        result = _function_execution(lambda: function(*args, **kwargs), func_name=function.__name__)
+        return result
+
+    return wrapper
---
+++ packages/utils-functions-plus/utils_functions_plus-0.0.6-py3-none-any.whl/utils_plus/utility.py
@@ -0,0 +1,65 @@
+import logging
+import shutil
+import sys
+import traceback
+import os
+
+USE_LOGGING = False
+
+
+def init_log(project_name, use_logging=False):
+    global USE_LOGGING
+    USE_LOGGING = use_logging
+    if USE_LOGGING:
+        path = f'c:\\Temp\\{project_name}.log'
+        if not os.path.isfile(path):
+            open(path, "w").write("")
+
+        logging.basicConfig(
+            filename=path,
+            level=logging.DEBUG,
+            format=f'[{project_name}] %(asctime)-15s %(levelname)-7.7s %(message)s', datefmt='%Y-%m-%d %H:%M:%S'
+        )
+
+
+def print_log(*message):
+    if USE_LOGGING:
+        print(message)
+    else:
+        logging.debug(message)
+
+
+def print_error():
+    logging.error('-' * 60)
+    traceback.print_exc(file=sys.stdout)
+    formatted_lines = traceback.format_exc()
+    if USE_LOGGING:
+        print(formatted_lines)
+    else:
+        logging.error(formatted_lines)
+    logging.error('-' * 60)
+    return formatted_lines
+
+
+def waiting(tasks):
+    for task in tasks:
+        task.start()
+
+    for task in tasks:
+        task.join()
+
+
+def rename_files(path, index, generate_path):
+    attaches = []
+    for f in os.listdir(path):
+        if os.path.isdir(str(path) + "/" + f):
+            attaches += rename_files(str(path) + "/" + f, index, generate_path)
+        else:
+            new_name = generate_path(index)
+            try:
+                shutil.move(str(path) + "/" + f, new_name)
+                attaches.append(new_name)
+            except:
+                print_error()
+            index += 1
+    return attaches
