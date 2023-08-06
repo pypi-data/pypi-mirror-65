1c54a10f9c518486e59d92c8e4d4dfb09880122e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:02 +0000

    Add package smapy/0.0.3.dev0/smapy-0.0.3.dev0-py2.py3-none-any.whl

---
+++ packages/smapy/smapy-0.0.3.dev0-py2.py3-none-any.whl/smapy/actions/hello.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from smapy.action import BaseAction
+
+
+class World(BaseAction):
+    """Test to check API connection, Returns Hello World"""
+
+    def process(self, message):
+        self.logger.info("Hello world!")
+        message["hello"] = "world!"
