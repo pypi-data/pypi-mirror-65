85ef53451cb7e6ac73681999dfe7b451b5815f45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:39 +0000

    Add package hein-utilities/3.7.1/hein_utilities-3.7.1.tar.gz

---
+++ packages/hein-utilities/hein_utilities-3.7.1.tar.gz/hein_utilities-3.7.1/hein_utilities/files_gui.py
@@ -0,0 +1,26 @@
+import warnings
+
+warnings.warn('This module has been moved to the hein_gui_utilities repo. Install the hein_utilities_gui '
+              'package and look under hein_gui_utilities.files_gui for this class',
+              DeprecationWarning)
+
+class Component:
+    def __init__(self,
+                 name: str,
+                 path: str,
+                 ):
+        raise NotImplementedError('This class has been moved to the hein_gui_utilities repo. Install the '
+                                  'hein_utilities_gui '
+                                  'package and look under hein_gui_utilities.files_gui for this class')
+
+
+
+class Folder(Component):
+    def __init__(self,
+                 folder_name: str,
+                 folder_path: str,
+                 ):
+        raise NotImplementedError('This class has been moved to the hein_gui_utilities repo. '
+                                  'Install the hein_utilities_gui '
+                                  'package and look under hein_gui_utilities.files_gui for this class')
+
---
+++ packages/hein-utilities/hein_utilities-3.7.1.tar.gz/hein_utilities-3.7.1/tests/test_watcher.py
@@ -0,0 +1,36 @@
+"""unit tests for Watcher class"""
+import unittest
+import os
+from hein_utilities.files import Watcher
+
+
+class TestWatcher(unittest.TestCase):
+    def test_patterns(self):
+        watch = Watcher(
+            os.getcwd(),
+            '.py',
+        )
+        self.assertNotEqual(
+            len(watch),
+            0,
+        )
+
+        # update partial match
+        watch.watchfor = 'watcher.py'
+        self.assertNotEqual(
+            len(watch),
+            0
+        )
+        # switch to exact matching (there should be no matches now)
+        watch.match_exact = True
+        self.assertEqual(
+            len(watch),
+            0
+        )
+
+        # update watch for to match exact file
+        watch.watchfor = 'test_watcher.py'
+        self.assertEqual(
+            len(watch),
+            1
+        )
