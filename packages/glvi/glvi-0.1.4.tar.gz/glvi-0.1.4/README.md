9d298d3b52a9e2b8590e63deda21f71d6c4b73ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:36 +0000

    Add package glvi/0.1.4/glvi-0.1.4.tar.gz

---
+++ packages/glvi/glvi-0.1.4.tar.gz/glvi-0.1.4/README.md
@@ -0,0 +1,6 @@
+- Two measures to calculate local variable importance with global models. It means that calcuate variable importance for subset of global data set using global model trained by global data set. I provide two files including 'todi.py' and 'meda.py' to realise these two methods. Both file include a class called 'lovim' inheriting from 'RandomForestRegressor' in 'scikit-learn'.
+- For example:
+- l = lovim(500, max_features=0.3, n_jobs=-1))# just be same with the class 'RandomForestRegressor' in 'scikit-learn'.
+- l.fit(X,Y)# fitting the model.
+- local_variable importance = l.compute_feature_importance(X,Y,partition_feature = partition_feature, norm=True,n_jobs=-1) # calculate local variable importance.
+- Of course, we upload the file to 'https://pypi.org/'. You can install it just like other packages such as numpy and pandas. Please input 'pip install glvi' in "cmd" for windows or Terminal Emulator for Linux to install the package.
---
+++ packages/glvi/glvi-0.1.4.tar.gz/glvi-0.1.4/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='glvi',
+                 version='0.1.4',
+                 description='Globally local variable importance',
+                 long_description=long_description,
+                 long_description_content_type="text/markdown",
+                 author='TaoLi',
+                 author_email='lp1559345469@gmail.com',
+                 url='https://github.com/PowderL/Globally-local-variable-importance-algorithm-in-Python',
+                 keywords='random forests variable importance ',
+                 packages=setuptools.find_packages(),
+                 zip_safe=False,
+                 include_package_data=True,
+                 classifiers=[
+                     "Programming Language :: Python",
+                     "Intended Audience :: Science/Research",
+                     "Operating System :: Microsoft :: Windows :: Windows 10",
+                     "License :: Freeware",
+	                 "Natural Language :: English"
+                 ])
