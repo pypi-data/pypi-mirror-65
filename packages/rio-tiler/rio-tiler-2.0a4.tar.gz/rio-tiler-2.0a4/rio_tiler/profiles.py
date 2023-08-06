bb091055597193ca00e3e29339a4105cbbeffaac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:52 +0000

    Add package rio-tiler/2.0a4/rio-tiler-2.0a4.tar.gz

---
+++ packages/rio-tiler/rio-tiler-2.0a4.tar.gz/rio-tiler-2.0a4/rio_tiler/profiles.py
@@ -0,0 +1,50 @@
+"""Image file profiles."""
+
+from rasterio.profiles import Profile  # type: ignore
+
+
+class JPEGProfile(Profile):
+    """JPEG creation options ref: https://www.gdal.org/frmt_jpeg.html."""
+
+    defaults = {"quality": 85}
+
+
+class PNGProfile(Profile):
+    """PNG creation options ref: https://www.gdal.org/frmt_png.html."""
+
+    defaults = {"zlevel": 6}
+
+
+class PNGRAWProfile(Profile):
+    """PNG creation options ref: https://www.gdal.org/frmt_png.html."""
+
+    defaults = {"zlevel": 1}
+
+
+class WEBPProfile(Profile):
+    """WEBP creation options ref: https://www.gdal.org/frmt_webp.html."""
+
+    defaults = {"quality": 75, "lossless": False}
+
+
+class ImagesProfiles(dict):
+    """
+    GDAL Image creation options.
+
+    ref: https://github.com/mapnik/mapnik/wiki/Image-IO#default-output-details.
+
+    """
+
+    def __init__(self):
+        """Initialize COGProfiles dict."""
+        self.update(
+            {
+                "jpeg": JPEGProfile(),
+                "png": PNGProfile(),
+                "pngraw": PNGRAWProfile(),
+                "webp": WEBPProfile(),
+            }
+        )
+
+
+img_profiles = ImagesProfiles()
