abbd74b8e5cc91a308982fe343879b583100e352
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:48 +0000

    Add package pdm/0.7.0/pdm-0.7.0.tar.gz

---
+++ packages/pdm/pdm-0.7.0.tar.gz/pdm-0.7.0/pdm/_types.py
@@ -0,0 +1,5 @@
+from typing import Dict, List, Tuple, Union
+
+Source = Dict[str, Union[str, bool]]
+RequirementDict = Union[str, Dict[str, Union[bool, str]]]
+CandidateInfo = Tuple[List[str], str, str]
