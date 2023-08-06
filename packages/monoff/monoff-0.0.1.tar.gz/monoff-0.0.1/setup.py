2fd1b0c0903b9fd2e112297dc15a5eba83350ec9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:33 +0000

    Add package monoff/0.0.1/monoff-0.0.1.tar.gz

---
+++ packages/monoff/monoff-0.0.1.tar.gz/monoff-0.0.1/setup.py
@@ -0,0 +1,43 @@
+from io import open
+from setuptools import find_packages, setup
+
+with open('monoff/__init__.py', 'r') as f:
+    for line in f:
+        if line.startswith('__version__'):
+            version = line.strip().split('=')[1].strip(' \'"')
+            break
+    else:
+        version = '0.0.1'
+with open('README.md','r') as f:
+    readme = f.read()
+
+REQUIRES = []
+
+setup(
+    name='monoff',
+    version=version,
+    description='',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='Matthew Miguel',
+    author_email='mmiguel6288code@gmail.com',
+    maintainer='Matthew Miguel',
+    maintainer_email='mmiguel6288code@gmail.com',
+    url='https://github.com/mmiguel6288code/monoff',
+    license='MIT',
+    keywords=[
+        'monitors','power','dpms','monitor','off','turn off','power down',
+    ],
+    classifiers=[
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+
+    install_requires=REQUIRES,
+    tests_require=[],
+    packages=find_packages(),
+)
