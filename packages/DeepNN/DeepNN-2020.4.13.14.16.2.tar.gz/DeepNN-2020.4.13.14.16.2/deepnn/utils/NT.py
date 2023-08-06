13209cbc8a6e564a3b1115280924f28006475758
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:06 +0000

    Add package DeepNN/2020.4.13.14.16.2/DeepNN-2020.4.13.14.16.2.tar.gz

---
+++ packages/DeepNN/DeepNN-2020.4.13.14.16.2.tar.gz/DeepNN-2020.4.13.14.16.2/deepnn/utils/NT.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : DeepNN.
+# @File         : get_namedtuple
+# @Time         : 2020/4/13 12:04 下午
+# @Author       : yuanjie
+# @Email        : yuanjie@xiaomi.com
+# @Software     : PyCharm
+# @Description  : 
+
+from .. import namedtupled
+
+
+class NT(object):
+
+    def __new__(cls, _nt_name="NT", **kwargs):
+        nt = namedtupled.map(kwargs, _nt_name=_nt_name)
+        return nt
