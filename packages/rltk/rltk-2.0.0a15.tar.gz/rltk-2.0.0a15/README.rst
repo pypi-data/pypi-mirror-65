7fb4bfab0571cd8b4d0fa8d7331aa975b384cb82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:00 +0000

    Add package rltk/2.0.0a15/rltk-2.0.0a15.tar.gz

---
+++ packages/rltk/rltk-2.0.0a15.tar.gz/rltk-2.0.0a15/README.rst
@@ -0,0 +1,57 @@
+RLTK: Record Linkage ToolKit
+============================
+
+.. begin-intro
+.. image:: https://img.shields.io/badge/license-MIT-blue.svg
+    :target: https://raw.githubusercontent.com/usc-isi-i2/rltk/master/LICENSE
+    :alt: License
+
+.. image:: https://api.travis-ci.org/usc-isi-i2/rltk.svg?branch=master
+    :target: https://travis-ci.org/usc-isi-i2/rltk
+    :alt: Travis
+
+.. image:: https://badge.fury.io/py/rltk.svg
+    :target: https://badge.fury.io/py/rltk
+    :alt: pypi
+
+.. image:: https://readthedocs.org/projects/rltk/badge/?version=latest
+    :target: http://rltk.readthedocs.io/en/latest
+    :alt: Documents
+
+The Record Linkage ToolKit (RLTK) is a general-purpose open-source record linkage platform that allows users to build powerful Python programs that link records referring to the same underlying entity. Record linkage is an extremely important problem that shows up in domains extending from social networks to bibliographic data and biomedicine. Current open platforms for record linkage have problems scaling even to moderately sized datasets, or are just not easy to use (even by experts). RLTK attempts to address all of these issues.
+
+RLTK supports a full, scalable record linkage pipeline, including multi-core algorithms for blocking, profiling data, computing a wide variety of features, and training and applying machine learning classifiers based on Python’s sklearn library. An end-to-end RLTK pipeline can be jump-started with only a few lines of code. However, RLTK is also designed to be extensible and customizable, allowing users arbitrary degrees of control over many of the individual components. You can add new features to RLTK (e.g. a custom string similarity) very easily.
+
+RLTK is being built by the `Center on Knowledge Graphs <http://usc-isi-i2.github.io/>`_ at `USC/ISI <https://isi.edu/>`_, with funding from multiple projects funded by the DARPA LORELEI and MEMEX programs and the IARPA CAUSE program.
+RLTK is under active maintenance and we expect to keep adding new features and state-of-the-art record linkage algorithms in the foreseeable future, in addition to continuously supporting our adopters to integrate the platform into their applications.
+
+Getting Started
+---------------
+
+Installation (make sure prerequisites are installed)::
+
+   pip install -U rltk
+
+Example::
+
+   >>> import rltk
+   >>> rltk.levenshtein_distance('abc', 'abd')
+   1
+
+Try RLTK Online
+---------------
+
+* `Stable version <https://mybinder.org/v2/gh/usc-isi-i2/rltk/master>`_
+* `Development version <https://mybinder.org/v2/gh/usc-isi-i2/rltk/dev>`_
+
+.. end-intro
+
+Datasets & Experiments
+----------------------
+* `rltk-experimentation <https://github.com/usc-isi-i2/rltk-experimentation>`_
+
+Documentation
+-------------
+
+* `Tutorials <http://rltk.readthedocs.io>`_
+* `API Reference <http://rltk.readthedocs.io/en/latest/modules.html>`_
---
+++ packages/rltk/rltk-2.0.0a15.tar.gz/rltk-2.0.0a15/rltk/similarity/hybrid.py
@@ -0,0 +1,101 @@
+from scipy.optimize import linear_sum_assignment
+import rltk.utils as utils
+from rltk.similarity.jaro import jaro_winkler_similarity
+
+MIN_FLOAT = float('-inf')
+
+
+def hybrid_jaccard_similarity(set1, set2, threshold=0.5, function=jaro_winkler_similarity, parameters=None):
+    """
+    Generalized Jaccard Measure.
+
+    Args:
+        set1 (set): Set 1.
+        set2 (set): Set 2.
+        threshold (float, optional): The threshold to keep the score of similarity function. \
+            Defaults to 0.5.
+        function (function, optional): The reference of a similarity measure function. \
+            It should return the value in range [0,1]. If it is set to None, \
+            `jaro_winlker_similarity` will be used.
+        parameters (dict, optional): Other parameters of function. Defaults to None.
+
+    Returns:
+        float: Hybrid Jaccard similarity.
+
+    Examples:
+        >>> def hybrid_test_similarity(m ,n):
+        ...     ...
+        >>> rltk.hybrid_jaccard_similarity(set(['a','b','c']), set(['p', 'q']), function=hybrid_test_similarity)
+        0.533333333333
+    """
+
+    utils.check_for_none(set1, set2)
+    utils.check_for_type(set, set1, set2)
+
+    parameters = parameters if isinstance(parameters, dict) else {}
+
+    matching_score = []
+    for s1 in set1:
+        inner = []
+        for s2 in set2:
+            score = function(s1, s2, **parameters)
+            if score < threshold:
+                score = 0.0
+            inner.append(1.0 - score)  # munkres finds out the smallest element
+        matching_score.append(inner)
+
+    row_idx, col_idx = linear_sum_assignment(matching_score)
+
+    score_sum, matching_count = 0.0, 0
+    for r, c in zip(row_idx, col_idx):
+        matching_count += 1
+        score_sum += 1.0 - matching_score[r][c]  # go back to similarity
+
+    if len(set1) + len(set2) - matching_count == 0:
+        return 1.0
+    return float(score_sum) / float(len(set1) + len(set2) - matching_count)
+
+
+def monge_elkan_similarity(bag1, bag2, function=jaro_winkler_similarity, parameters=None):
+    """
+    Monge Elkan similarity.
+
+    Args:
+        bag1 (list): Bag 1.
+        bag2 (list): Bag 2.
+        function (function, optional): The reference of a similarity measure function. \
+            It should return the value in range [0,1]. If it is set to None, \
+            `jaro_winlker_similarity` will be used.
+        parameters (dict, optional): Other parameters of function. Defaults to None.
+
+    Returns:
+        float: Monge Elkan similarity.
+    """
+
+    utils.check_for_none(bag1, bag2)
+    utils.check_for_type(list, bag1, bag2)
+
+    parameters = parameters if isinstance(parameters, dict) else {}
+
+    if len(bag1) == 0:
+        return 0.0
+
+    score_sum = 0
+    for ele1 in bag1:
+        max_score = MIN_FLOAT
+        for ele2 in bag2:
+            max_score = max(max_score, function(ele1, ele2, **parameters))
+        score_sum += max_score
+
+    return float(score_sum) / float(len(bag1))
+
+
+def symmetric_monge_elkan_similarity(bag1, bag2, function=jaro_winkler_similarity, parameters=None):
+    """
+    Symmetric Monge Elkan similarity is computed by \
+    (monge_elkan_similarity(b1, b2) + monge_elkan_similarity(b2, b1)) / 2.
+    """
+
+    s1 = monge_elkan_similarity(bag1, bag2, function, parameters)
+    s2 = monge_elkan_similarity(bag2, bag1, function, parameters)
+    return (s1 + s2) / 2
