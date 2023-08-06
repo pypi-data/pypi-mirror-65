3952d55f62a1fc44c6d0d47239c24aeed7951247
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:18 +0000

    Add package stock-codes/0.5/stock_codes-0.5.tar.gz

---
+++ packages/stock-codes/stock_codes-0.5.tar.gz/stock_codes-0.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='stock_codes',
+      version='0.5',
+      description='Analyze excel files to get codes',
+      url='https://github.com/Fedex75/stock_codes',
+      author='Federico Meza',
+      author_email='mezafabrello@gmail.com',
+      license='MIT',
+      packages=['stock_codes'],
+      install_requires=[
+        'pandas',
+        'xlrd',
+        'openpyxl'
+      ],
+      zip_safe=False)
