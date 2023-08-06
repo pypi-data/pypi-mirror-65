c427d4c6d0b97f82d946a8f19c6331a891afcf78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:47 +0000

    Add package cmake/3.14.4.post1/cmake-3.14.4.post1.tar.gz

---
+++ packages/cmake/cmake-3.14.4.post1.tar.gz/cmake-3.14.4.post1/CMakeUrls.cmake
@@ -0,0 +1,26 @@
+
+#-----------------------------------------------------------------------------
+# CMake sources
+set(unix_source_url       "https://github.com/Kitware/CMake/releases/download/v3.14.4/cmake-3.14.4.tar.gz")
+set(unix_source_sha256    "00b4dc9b0066079d10f16eed32ec592963a44e7967371d2f5077fd1670ff36d9")
+
+set(windows_source_url    "https://github.com/Kitware/CMake/releases/download/v3.14.4/cmake-3.14.4.zip")
+set(windows_source_sha256 "c33bf2dc5fabd4c86566eed62942ba18e05194dbbe5fe62b0d11882a2468ab63")
+
+#-----------------------------------------------------------------------------
+# CMake binaries
+
+set(linux32_binary_url    "NA")  # Linux 32-bit binaries not available
+set(linux32_binary_sha256 "NA")
+
+set(linux64_binary_url    "https://github.com/Kitware/CMake/releases/download/v3.14.4/cmake-3.14.4-Linux-x86_64.tar.gz")
+set(linux64_binary_sha256 "9f414df8e432c4a143c2d6d81e170581badba8d89df1cf8944735b9122765c50")
+
+set(macosx_binary_url    "https://github.com/Kitware/CMake/releases/download/v3.14.4/cmake-3.14.4-Darwin-x86_64.tar.gz")
+set(macosx_binary_sha256 "8a0c98869cb1e74e31c401a9ecf7a7cacbcfe9d4e7944f434e2f1d3995fefdf4")
+
+set(win32_binary_url    "https://github.com/Kitware/CMake/releases/download/v3.14.4/cmake-3.14.4-win32-x86.zip")
+set(win32_binary_sha256 "37faf7051c6318927b67ee1bf7e0c8d34b7159679d38e80ad860f833f92a096f")
+
+set(win64_binary_url    "https://github.com/Kitware/CMake/releases/download/v3.14.4/cmake-3.14.4-win64-x64.zip")
+set(win64_binary_sha256 "7a0901a26a987259f720ca763ede1dff8161b7024488a89d2aa497e5e1508f5e")
