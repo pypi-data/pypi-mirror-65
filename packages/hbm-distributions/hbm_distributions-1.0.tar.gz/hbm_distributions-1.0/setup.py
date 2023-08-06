19062abfb54beb4f7910fc2267238fab56e4e296
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:56 +0000

    Add package hbm-distributions/1.0/hbm_distributions-1.0.tar.gz

---
+++ packages/hbm-distributions/hbm_distributions-1.0.tar.gz/hbm_distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='hbm_distributions',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['hbm_distributions'],
+      author="Hwi Moon",
+      author_email="hbmoon@berkeley.edu",
+      zip_safe=False)
