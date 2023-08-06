b7e46d7c500b2e4173d0825698801cb2af76ab82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:15 +0000

    Add package gias2/0.6.16/gias2-0.6.16-py3-none-any.whl

---
+++ packages/gias2/gias2-0.6.16-py3-none-any.whl/gias2/__init__.py
@@ -0,0 +1,23 @@
+"""
+===============================================================================
+This file is part of GIAS2. (https://bitbucket.org/jangle/gias2)
+
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+===============================================================================
+"""
+
+from gias2.version import *
+from gias2.common import transform3D, geoprimitives
+from gias2.fieldwork.field import geometric_field
+from gias2.learning import PCA, PCA_fitting
+
+import sys
+# needed to open PCA files pickled by gias(1)
+import gias2
+from gias2 import learning
+sys.modules['gias'] = gias2
+sys.modules['gias.learning'] = learning
+sys.modules['gias.learning.PCA'] = PCA
+
