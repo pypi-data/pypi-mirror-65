59491e603ba0bdf1e6a786bbf8703d158c69ad46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:43 +0000

    Add package mkcloud/0.0.4/mkcloud-0.0.4.tar.gz

---
+++ packages/mkcloud/mkcloud-0.0.4.tar.gz/mkcloud-0.0.4/README.md
@@ -0,0 +1,5 @@
+### A clittle chat bot
+#### 打包 
+python setup.py sdist
+#### 上传 
+python setup.py sdist upload -r pypi 
---
+++ packages/mkcloud/mkcloud-0.0.4.tar.gz/mkcloud-0.0.4/mkcloud/robot.py
@@ -0,0 +1,22 @@
+# -*- coding: UTF-8 -*-
+import json
+import requests
+class robot:
+    key = ''
+    lanauage = 'zh'
+    def __init__(self, key = '',lanauage = 'zh'):
+        self.key = key
+        self.lanauage = lanauage
+
+    def setLanguage(self, lanauage):
+        self.lanauage = lanauage
+
+    def chat(self, receivedMessage):
+        if self.lanauage == 'zh':
+            sess = requests.get('https://api.ownthink.com/bot?spoken='+receivedMessage + '?')
+            answer = sess.text
+            answer = json.loads(answer)
+            return answer['data']['info']['text']
+        else:
+            return('英文服务尚未提供,敬请期待')
+            
