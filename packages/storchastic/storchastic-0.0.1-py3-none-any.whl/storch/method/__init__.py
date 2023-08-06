8f3b8a7603f8130d7394586f83e3dc50cfcd31b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:22 +0000

    Add package storchastic/0.0.1/storchastic-0.0.1-py3-none-any.whl

---
+++ packages/storchastic/storchastic-0.0.1-py3-none-any.whl/storch/method/__init__.py
@@ -0,0 +1,11 @@
+from storch.method.method import (
+    GumbelSoftmax,
+    ScoreFunction,
+    Infer,
+    Method,
+    Expect,
+    Reparameterization,
+    MonteCarloMethod,
+)
+from storch.method.relax import RELAX, REBAR, LAX
+from storch.method.baseline import Baseline, MovingAverageBaseline
