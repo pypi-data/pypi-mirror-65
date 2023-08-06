ed3217ea1d7fc549751caa3dabb94969cc2aeccd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:38 +0000

    Add package beneath/1.1.1/beneath-1.1.1.tar.gz

---
+++ packages/beneath/beneath-1.1.1.tar.gz/beneath-1.1.1/beneath/admin/streams.py
@@ -0,0 +1,232 @@
+from beneath.connection import Connection
+from beneath.utils import format_entity_name
+
+
+class Streams:
+
+  def __init__(self, conn: Connection):
+    self.conn = conn
+
+  async def find_by_id(self, stream_id):
+    result = await self.conn.query_control(
+      variables={
+        'streamID': stream_id,
+      },
+      query="""
+        query StreamByID($streamID: UUID!) {
+          streamByID(streamID: $streamID) {
+            streamID
+            name
+            description
+            createdOn
+            updatedOn
+            project {
+              projectID
+              name
+            }
+            schema
+            avroSchema
+            streamIndexes {
+              fields
+              primary
+              normalize
+            }
+            external
+            batch
+            manual
+            retentionSeconds
+            instancesCreatedCount
+            instancesCommittedCount
+            currentStreamInstanceID
+          }
+        }
+      """
+    )
+    return result['streamByID']
+
+  async def find_by_organization_project_and_name(self, organization_name, project_name, stream_name):
+    result = await self.conn.query_control(
+      variables={
+        'organizationName': format_entity_name(organization_name),
+        'projectName': format_entity_name(project_name),
+        'streamName': format_entity_name(stream_name),
+      },
+      query="""
+        query StreamByOrganizationProjectAndName($organizationName: String!, $projectName: String!, $streamName: String!) {
+          streamByOrganizationProjectAndName(
+            organizationName: $organizationName,
+            projectName: $projectName,
+            streamName: $streamName, 
+          ) {
+            streamID
+            name
+            description
+            createdOn
+            updatedOn
+            project {
+              projectID
+              name
+            }
+            schema
+            avroSchema
+            streamIndexes {
+              fields
+              primary
+              normalize
+            }
+            external
+            batch
+            manual
+            retentionSeconds
+            instancesCreatedCount
+            instancesCommittedCount
+            currentStreamInstanceID
+          }
+        }
+      """
+    )
+    return result['streamByOrganizationProjectAndName']
+
+  async def create(self, schema, project_id, manual=False, batch=False):
+    result = await self.conn.query_control(
+      variables={
+        'projectID': project_id,
+        'schema': schema,
+        'batch': batch,
+        'manual': bool(manual),
+      },
+      query="""
+        mutation CreateExternalStream($projectID: UUID!, $schema: String!, $batch: Boolean!, $manual: Boolean!) {
+          createExternalStream(
+            projectID: $projectID,
+            schema: $schema,
+            batch: $batch,
+            manual: $manual
+          ) {
+            streamID
+            name
+            description
+            createdOn
+            updatedOn
+            project {
+              projectID
+              name
+            }
+            schema
+            avroSchema
+            streamIndexes {
+              fields
+              primary
+              normalize
+            }
+            external
+            batch
+            manual
+            retentionSeconds
+            instancesCreatedCount
+            instancesCommittedCount
+            currentStreamInstanceID
+          }
+        }
+      """
+    )
+    return result['createExternalStream']
+
+  async def update(self, stream_id, schema=None, manual=None):
+    variables = {'streamID': stream_id}
+    if schema:
+      variables['schema'] = schema
+    if manual:
+      variables['manual'] = bool(manual)
+
+    result = await self.conn.query_control(
+      variables=variables,
+      query="""
+        mutation UpdateExternalStream($streamID: UUID!, $schema: String, $manual: Boolean) {
+          updateExternalStream(
+            streamID: $streamID,
+            schema: $schema,
+            manual: $manual
+          ) {
+            streamID
+            name
+            description
+            createdOn
+            updatedOn
+            project {
+              projectID
+              name
+            }
+            schema
+            avroSchema
+            streamIndexes {
+              fields
+              primary
+              normalize
+            }
+            external
+            batch
+            manual
+            retentionSeconds
+            instancesCreatedCount
+            instancesCommittedCount
+            currentStreamInstanceID
+          }
+        }
+      """
+    )
+    return result['updateExternalStream']
+
+  async def delete(self, stream_id):
+    result = await self.conn.query_control(
+      variables={
+        'streamID': stream_id,
+      },
+      query="""
+        mutation DeleteExternalStream($streamID: UUID!) {
+          deleteExternalStream(streamID: $streamID)
+        }
+      """
+    )
+    return result['deleteExternalStream']
+
+  async def create_batch(self, stream_id):
+    result = await self.conn.query_control(
+      variables={
+        'streamID': stream_id,
+      },
+      query="""
+        mutation CreateExternalStreamBatch($streamID: UUID!) {
+          createExternalStreamBatch(streamID: $streamID) {
+            instanceID
+          }
+        }
+      """
+    )
+    return result['createExternalStreamBatch']
+
+  async def commit_batch(self, instance_id):
+    result = await self.conn.query_control(
+      variables={
+        'instanceID': instance_id,
+      },
+      query="""
+        mutation CommitExternalStreamBatch($instanceID: UUID!) {
+          commitExternalStreamBatch(instanceID: $instanceID)
+        }
+      """
+    )
+    return result['commitExternalStreamBatch']
+
+  async def clear_pending_batches(self, stream_id):
+    result = await self.conn.query_control(
+      variables={
+        'streamID': stream_id,
+      },
+      query="""
+        mutation ClearPendingExternalStreamBatches($streamID: UUID!) {
+          clearPendingExternalStreamBatches(streamID: $streamID) 
+        }
+      """
+    )
+    return result['clearPendingExternalStreamBatches']
---
+++ packages/beneath/beneath-1.1.1.tar.gz/beneath-1.1.1/beneath/cli/stream.py
@@ -0,0 +1,161 @@
+from beneath.client import Client
+from beneath.utils import ProjectQualifier, StreamQualifier
+from beneath.cli.utils import async_cmd, pretty_print_graphql_result, str2bool
+
+
+def add_subparser(root):
+  stream = root.add_parser('stream').add_subparsers()
+  root_stream = root.add_parser('root-stream').add_subparsers()
+  root_stream_batch = root_stream.add_parser('batch').add_subparsers()
+
+  _list = stream.add_parser('list')
+  _list.set_defaults(func=async_cmd(show_list))
+  _list.add_argument('project_path', type=str)
+
+  _create = root_stream.add_parser('create')
+  _create.set_defaults(func=async_cmd(create_root))
+  _create.add_argument(
+    '-f',
+    '--file',
+    type=str,
+    required=True,
+    help="This file should contain the GraphQL schema for the stream you would like to create"
+  )
+  _create.add_argument('-p', '--project-path', type=str, required=True)
+  _create.add_argument('--manual', type=str2bool, nargs='?', const=True, default=False)
+  _create.add_argument('--batch', type=str2bool, nargs='?', const=True, default=False)
+
+  _update = root_stream.add_parser('update')
+  _update.set_defaults(func=async_cmd(update_root))
+  _update.add_argument('stream_path', type=str)
+  _update.add_argument(
+    '-f',
+    '--file',
+    type=str,
+    required=True,
+    help=
+    "This file should contain the stream's schema and an updated description. Only the description should change, the schema itself should not change."
+  )
+  _update.add_argument('--manual', type=str2bool, nargs='?', const=True, default=False)
+
+  _delete = root_stream.add_parser('delete')
+  _delete.set_defaults(func=async_cmd(delete_root))
+  _delete.add_argument('stream_path', type=str)
+
+  _batch_create = root_stream_batch.add_parser('create')
+  _batch_create.set_defaults(func=async_cmd(batch_create))
+  _batch_create.add_argument('stream_path', type=str)
+
+  _batch_commit = root_stream_batch.add_parser('commit')
+  _batch_commit.set_defaults(func=async_cmd(batch_commit))
+  _batch_commit.add_argument('instance', type=str)
+
+  _batches_clear = root_stream_batch.add_parser('clear')
+  _batches_clear.set_defaults(func=async_cmd(batches_clear))
+  _batches_clear.add_argument('stream_path', type=str)
+
+
+async def show_list(args):
+  client = Client()
+  pq = ProjectQualifier.from_path(args.project_path)
+  project = await client.admin.projects.find_by_organization_and_name(pq.organization, pq.project)
+  if len(project['streams']) == 0:
+    print("There are no streams currently in this project")
+  for streamname in project['streams']:
+    print(f"{pq.organization}/{pq.project}/{streamname['name']}")
+
+
+async def create_root(args):
+  with open(args.file, "r") as f:
+    schema = f.read()
+
+  client = Client()
+  pq = ProjectQualifier.from_path(args.project_path)
+  project = await client.admin.projects.find_by_organization_and_name(pq.organization, pq.project)
+  stream = await client.admin.streams.create(
+    schema=schema,
+    project_id=project['projectID'],
+    manual=args.manual,
+    batch=args.batch,
+  )
+
+  _pretty_print_stream(stream)
+
+
+async def update_root(args):
+  schema = None
+  if args.file:
+    with open(args.file, "r") as f:
+      schema = f.read()
+
+  client = Client()
+  sq = StreamQualifier.from_path(args.stream_path)
+  stream = await client.admin.streams.find_by_organization_project_and_name(
+    organization_name=sq.organization,
+    project_name=sq.project,
+    stream_name=sq.stream,
+  )
+
+  stream = await client.admin.streams.update(stream['streamID'], schema=schema, manual=args.manual)
+  _pretty_print_stream(stream)
+
+
+async def delete_root(args):
+  client = Client()
+  sq = StreamQualifier.from_path(args.stream_path)
+  stream = await client.admin.streams.find_by_organization_project_and_name(
+    organization_name=sq.organization,
+    project_name=sq.project,
+    stream_name=sq.stream,
+  )
+  result = await client.admin.streams.delete(stream['streamID'])
+  pretty_print_graphql_result(result)
+
+
+async def batch_create(args):
+  client = Client()
+  sq = StreamQualifier.from_path(args.stream_path)
+  stream = await client.admin.streams.find_by_organization_project_and_name(
+    organization_name=sq.organization,
+    project_name=sq.project,
+    stream_name=sq.stream,
+  )
+  batch = await client.admin.streams.create_batch(stream['streamID'])
+  pretty_print_graphql_result(batch)
+
+
+async def batch_commit(args):
+  client = Client()
+  result = await client.admin.streams.commit_batch(instance_id=args.instance)
+  pretty_print_graphql_result(result)
+
+
+async def batches_clear(args):
+  client = Client()
+  sq = StreamQualifier.from_path(args.stream_path)
+  stream = await client.admin.streams.find_by_organization_project_and_name(
+    organization_name=sq.organization,
+    project_name=sq.project,
+    stream_name=sq.stream,
+  )
+  result = await client.admin.streams.clear_pending_batches(stream['streamID'])
+  pretty_print_graphql_result(result)
+
+
+def _pretty_print_stream(stream):
+  pretty_print_graphql_result(
+    stream, [
+      "streamID",
+      "name",
+      "description",
+      "createdOn",
+      "updatedOn",
+      "external",
+      "batch",
+      "manual",
+      "retentionSeconds"
+      "project",
+      "streamIndexes",
+      "currentStreamInstanceID",
+    ]
+  )
