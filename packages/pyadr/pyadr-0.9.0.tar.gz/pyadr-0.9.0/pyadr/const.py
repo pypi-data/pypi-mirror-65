8458a98470e31e37550b9fe3275b46d985ffb47c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:26 +0000

    Add package pyadr/0.9.0/pyadr-0.9.0.tar.gz

---
+++ packages/pyadr/pyadr-0.9.0.tar.gz/pyadr-0.9.0/pyadr/const.py
@@ -0,0 +1,13 @@
+"""Package constants"""
+import os
+from pathlib import Path
+
+STATUS_PROPOSED = "proposed"
+STATUS_ACCEPTED = "accepted"
+STATUS_REJECTED = "rejected"
+
+CWD = Path(os.getcwd())
+DEFAULT_ADR_PATH = Path("docs", "adr")
+
+DEFAULT_CONFIG_FILE_NAME = ".adr"
+DEFAULT_CONFIG_FILE_PATH = Path(DEFAULT_CONFIG_FILE_NAME)
