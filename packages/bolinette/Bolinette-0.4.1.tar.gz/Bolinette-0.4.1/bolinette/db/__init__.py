beef7cc6c24ab08435c07d70fa39764545293f17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:03 +0000

    Add package bolinette/0.4.1/Bolinette-0.4.1.tar.gz

---
+++ packages/bolinette/Bolinette-0.4.1.tar.gz/Bolinette-0.4.1/bolinette/db/__init__.py
@@ -0,0 +1,4 @@
+from bolinette.db import types, defs
+from bolinette.db.models import model, models
+from bolinette.db.mixins import with_mixin, mixins, mixin
+from bolinette.db.engine import engine
