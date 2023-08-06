c4b2351807969c47d73f48f8520879c0e207b80f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:22 +0000

    Add package jb4jupyter/0.13/jb4jupyter-0.13-py3-none-any.whl

---
+++ packages/jb4jupyter/jb4jupyter-0.13-py3-none-any.whl/jb4jupyter/__init__.py
@@ -0,0 +1,24 @@
+from .plotpretty import *
+import os
+
+def joke(): return 'nah'
+
+def exporthtml():
+	os.system('jupyter nbconvert --to=html ' +  os.path.abspath(__file__))
+
+def exportpdf():
+	os.system('jupyter nbconvert --to=pdf --template=jb4jupyter ' + os.path.abspath(__file__))
+
+def configlatexprint():
+	lines = [
+		'((* extends \'article.tplx\' *)) \n',
+		'((* block input_group *)) \n',
+		'((* endblock input_group *)) \n',
+		'((* block execute_result scoped *)) \n',
+		'    ((* block display_data scoped *)) \n',
+		'        ((( super() ))) \n',
+		'    ((* endblock display_data *)) \n',
+		'((* endblock execute_result *)) \n'
+		]
+	with open('jb4jupyter.tplx','w') as txtfile:
+		txtfile.writelines(lines)
