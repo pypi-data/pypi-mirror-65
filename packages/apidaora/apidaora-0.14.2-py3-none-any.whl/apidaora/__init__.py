9cf4daf4b0afd4c12308bcf15e38b12dba24d146
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:14 +0000

    Add package apidaora/0.14.2/apidaora-0.14.2-py3-none-any.whl

---
+++ packages/apidaora/apidaora-0.14.2-py3-none-any.whl/apidaora/__init__.py
@@ -0,0 +1,41 @@
+"""
+ASGI App using dataclasses module for request/response objects
+"""
+
+__version__ = '0.14.2'
+
+from apidaora.app import appdaora
+from apidaora.bodies import GZipFactory
+from apidaora.content import ContentType
+from apidaora.exceptions import BadRequestError
+from apidaora.header import Header
+from apidaora.method import MethodType
+from apidaora.middlewares import CorsMiddleware, Middlewares
+from apidaora.responses import (
+    Response,
+    html,
+    json,
+    no_content,
+    not_found,
+    text,
+)
+from apidaora.route.decorator import route
+
+
+__all__ = [
+    'BadRequestError',
+    'GZipFactory',
+    'ContentType',
+    'MethodType',
+    'appdaora',
+    'Header',
+    'html',
+    'json',
+    'text',
+    'route',
+    'no_content',
+    'not_found',
+    'Response',
+    'Middlewares',
+    'CorsMiddleware',
+]
