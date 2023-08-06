b482ecb6cfb262c23d0617779cd2f34df30f8d8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:04 +0000

    Add package slack-notifications-datateam/0.0.3/slack-notifications-datateam-0.0.3.tar.gz

---
+++ packages/slack-notifications-datateam/slack-notifications-datateam-0.0.3.tar.gz/slack-notifications-datateam-0.0.3/slack_notifications/slack_messages.py
@@ -0,0 +1,18 @@
+
+def send_message(webhook_url=None,
+                text=None):
+
+    if webhook_url is None:
+            raise ValueError('No valid Slack token.')
+    if text is not None:
+        raise ValueError('A text is needed.')
+
+    response = requests.post(
+        self.webhook_url, data=json.dumps(self.slack_data),
+        headers={'Content-Type': 'application/json'}
+        )
+    if response.status_code != 200:
+        raise ValueError(
+            'Request to slack returned an error %s, the response is:\n%s'
+            % (response.status_code, response.text)
+        )
