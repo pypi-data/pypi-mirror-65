d0668ec9912da3580bf5dd8801ebbfc4a2800fdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:09 +0000

    Add package django-extra-referrals/0.0.1/django-extra-referrals-0.0.1.tar.gz

---
+++ packages/django-extra-referrals/django-extra-referrals-0.0.1.tar.gz/django-extra-referrals-0.0.1/README.md
@@ -0,0 +1,23 @@
+# Django Extra Referrals
+Simpel referrals app for Django with simple multilevel fee system
+## Installation
+```
+$ pip install django-extra-referrals
+```
+or
+```
+$ pip install git+https://github.com/sasriawesome/django_extra_referrals.git#egg=django-extra-referrals
+```
+
+## Settings
+
+```
+REFERRAL_MODEL = 'django_referrals.Referral'
+REFERRAL_COOKIE_KEY = 'ref_id'
+REFERRAL_PARAM_KEY = 'ref_id
+REFERRAL_MAX_DAY = 7 * 24 * 60 * 60 # a week
+REFERRAL_SCHEMA = 'FLAT'
+REFERRAL_FLAT_CAMPAIGN_RATE = 4
+REFERRAL_FLAT_UPLINES_RATE = [5, 2, 1]
+
+```
---
+++ packages/django-extra-referrals/django-extra-referrals-0.0.1.tar.gz/django-extra-referrals-0.0.1/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+from django_extra_referrals import __version__
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='django-extra-referrals',
+    version=__version__,
+    description='Django referral with multilevel fee system',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    maintainer='Rizki Sasri Dwitama',
+    maintainer_email='sasri.project@gmail.com',
+    license="MIT",
+    url='https://github.com/sasriawesome/django_extra_referrals',
+    packages=[
+        'django_extra_referrals',
+        'django_extra_referrals.migrations',
+        'django_extra_referrals.utils',
+    ],
+    install_requires=[
+        "django>=2.2",
+        "django-numerators>=0.0.1",
+        "django-polymorphic>=2.1.2",
+        "django-mptt>=0.11.0",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
