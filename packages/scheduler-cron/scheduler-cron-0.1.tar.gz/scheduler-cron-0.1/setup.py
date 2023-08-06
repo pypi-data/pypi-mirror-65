5261a51c4dc027870f89fbc448a76a73db7421b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:18:08 +0000

    Add package scheduler-cron/0.1/scheduler-cron-0.1.tar.gz

---
+++ packages/scheduler-cron/scheduler-cron-0.1.tar.gz/scheduler-cron-0.1/setup.py
@@ -0,0 +1,24 @@
+import codecs
+from setuptools import setup
+
+
+def read_file(filename):
+    """
+    Read a utf8 encoded text file and return its contents.
+    """
+    with codecs.open(filename, 'r', 'utf8') as f:
+        return f.read()
+
+
+setup(
+    name='scheduler-cron',
+    packages=['scheduler'],
+    description='Python task scheduling with cron expression',
+    long_description=read_file('README.rst'),
+    version='0.1',
+    url='https://github.com/mehrdadmhd/scheduler-py',
+    author='Mehrdad Mahmoudi',
+    author_email='mehrdad@asiatech.ir',
+    keywords=['pip','schedule','schedule','python','python3','job','cron'],
+    install_requires=['croniter']
+)
