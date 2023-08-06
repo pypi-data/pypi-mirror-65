4172bf34dd93358b38a88c175fa5ac19db43d458
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:37 +0000

    Add package libpythonproteste/0.1/libpythonproteste-0.1.tar.gz

---
+++ packages/libpythonproteste/libpythonproteste-0.1.tar.gz/libpythonproteste-0.1/libpythonpro/guithub_api.py
@@ -0,0 +1,14 @@
+import requests
+
+
+def buscar_avatar(usuario):
+    """
+    Buscar avatar
+    """
+    url = f'https://api.github.com/users/{usuario}'
+    resp = requests.get(url)
+    return resp.json()['avatar_url']
+
+
+if __name__ == '__main__':
+    print(buscar_avatar('asmmiguel'))
