a5450d08acf580f0cb2a03cc7ec9631ed553b45e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:55 +0000

    Add package document-your-code/0.1.10/document-your-code-0.1.10.tar.gz

---
+++ packages/document-your-code/document-your-code-0.1.10.tar.gz/document-your-code-0.1.10/setup.py
@@ -0,0 +1,25 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+setup(
+    name="document-your-code",
+    version="0.1.10",
+    author="Mohammad Albakri",
+    author_email="mohammad.albakri93@gmail.com",
+    packages=find_packages(),
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Zarad1993/dyc",
+    install_requires=[
+        "click==7.0",
+        "pyyaml>=4.2b1",
+        "gitpython==2.1.11",
+        "watchdog==0.9.0",
+        "pytest==4.4.0",
+        "pre-commit==1.18.1",
+    ],
+    entry_points={"console_scripts": ["dyc=dyc.dyc:main"]},
+    package_data={'': ['*.yaml']},
+)
