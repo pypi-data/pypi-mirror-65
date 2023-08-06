5fcda6de8e958c8d2bb6fe9ff5676fd9cb882413
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:24 +0000

    Add package Flask-Apidoc-Extend/0.1.1/Flask-Apidoc-Extend-0.1.1.tar.gz

---
+++ packages/Flask-Apidoc-Extend/Flask-Apidoc-Extend-0.1.1.tar.gz/Flask-Apidoc-Extend-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='Flask-Apidoc-Extend',
+    version='0.1.1',
+    packages=['flask_apidoc_extend'],
+    url='https://github.com/sunyuanxiang/flask-apidoc-extend',
+    license='MIT',
+    author='sun yuanxiang',
+    author_email='apidocer@163.com',
+    description='Add ApiDoc support to Flask',
+    keywords=['flask', 'apidoc', 'doc', 'documentation','restful'],
+    install_requires=['flask>=0.11'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: Implementation :: CPython'
+    ],
+    python_requires='>=3.5',
+)
