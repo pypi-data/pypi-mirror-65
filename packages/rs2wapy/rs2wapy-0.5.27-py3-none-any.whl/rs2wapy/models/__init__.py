7e123c1ebdad2ceb46b505c82d395e94a480601a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:56 +0000

    Add package rs2wapy/0.5.27/rs2wapy-0.5.27-py3-none-any.whl

---
+++ packages/rs2wapy/rs2wapy-0.5.27-py3-none-any.whl/rs2wapy/models/__init__.py
@@ -0,0 +1,20 @@
+from .models import *
+
+__all__ = [
+    "STEAM_ID_TYPE",
+    "AllTeam",
+    "AccessPolicy",
+    "ChatMessage",
+    "ChatChannel",
+    "ChatChannelTeam",
+    "ChatChannelAll",
+    "CurrentGame",
+    "Player",
+    "PlayerScoreboard",
+    "Squad",
+    "Team",
+    "TeamScoreboard",
+    "BlueTeam",
+    "RedTeam",
+    "MapCycle",
+]
