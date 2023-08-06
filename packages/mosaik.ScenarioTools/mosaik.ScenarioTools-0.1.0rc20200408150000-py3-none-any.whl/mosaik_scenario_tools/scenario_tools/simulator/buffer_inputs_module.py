5d7fd0b9fe923cff60e9e693fea5136db4e47034
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:16 +0000

    Add package mosaik.ScenarioTools/0.1.0rc20200408150000/mosaik.ScenarioTools-0.1.0rc20200408150000-py3-none-any.whl

---
+++ packages/mosaik.ScenarioTools/mosaik.ScenarioTools-0.1.0rc20200408150000-py3-none-any.whl/mosaik_scenario_tools/scenario_tools/simulator/buffer_inputs_module.py
@@ -0,0 +1,17 @@
+def buffer_inputs(function):
+    def wrapper(self, time, inputs):
+        try:
+            inputs_old = self.__last_inputs
+        except AttributeError:
+            self.__last_inputs = {}
+            inputs_old = self.__last_inputs
+
+        inputs_new = inputs_old
+        inputs_new.update(inputs)
+        setattr(self, '__inputs', inputs_new)
+
+        value = function(self=self, time=time, inputs=inputs_new)
+
+        return value
+
+    return wrapper
