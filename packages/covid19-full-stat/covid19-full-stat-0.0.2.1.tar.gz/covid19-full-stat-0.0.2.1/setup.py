369f0d945fbdd388859358332fd4b0f696dd0bc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:30 +0000

    Add package covid19-full-stat/0.0.2.1/covid19-full-stat-0.0.2.1.tar.gz

---
+++ packages/covid19-full-stat/covid19-full-stat-0.0.2.1.tar.gz/covid19-full-stat-0.0.2.1/setup.py
@@ -0,0 +1,13 @@
+﻿from setuptools import setup
+
+setup(
+    name='covid19-full-stat',
+    version='0.0.2.1',
+    url='https://github.com/Sergii-Lak/covid19-info-stats',
+    package_dir={'': 'C:\\Proje\\'},
+    license='MIT',
+    author='Sergii-Lak',
+    install_requires=["pandas", "numpy"],
+    author_email='serg1509@yandex.ru',
+    description='Сovid19 statistics per countries and for the world'
+)
