da35349b9a1f014ff856a050046b5c550a6a8575
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:47 +0000

    Add package glean-sdk/27.0.0/glean_sdk-27.0.0-cp35-abi3-macosx_10_7_x86_64.whl

---
+++ packages/glean-sdk/glean_sdk-27.0.0-cp35-abi3-macosx_10_7_x86_64.whl/glean/testing/__init__.py
@@ -0,0 +1,56 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+"""
+Utilities for writing unit tests involving Glean.
+"""
+
+
+from pathlib import Path
+from typing import Optional
+
+
+from glean import Configuration
+from .error_type import ErrorType  # noqa
+
+
+def reset_glean(
+    *,
+    application_id: str,
+    application_version: str,
+    configuration: Optional[Configuration] = None,
+    clear_stores: bool = True
+):
+    """
+    Resets the Glean singleton.
+
+    Args:
+        application_id (str): The application id to use when sending pings.
+        application_version (str): The version of the application sending
+            Glean data.
+        configuration (glean.config.Configuration): (optional) An object with
+            global settings.
+    """
+    from glean import Glean
+    from glean._dispatcher import Dispatcher
+
+    Dispatcher._testing_mode = True
+
+    data_dir = None  # type: Optional[Path]
+    if not clear_stores:
+        Glean._destroy_data_dir = False
+        data_dir = Glean._data_dir
+
+    Glean.reset()
+    Glean.initialize(
+        application_id=application_id,
+        application_version=application_version,
+        upload_enabled=True,
+        configuration=configuration,
+        data_dir=data_dir,
+    )
+
+
+__all__ = ["reset_glean", "ErrorType"]
