8958201cc7e92c98fb0bb26a033db6095e881499
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:31 +0000

    Add package httpmethods/0.0.1/httpmethods-0.0.1.tar.gz

---
+++ packages/httpmethods/httpmethods-0.0.1.tar.gz/httpmethods-0.0.1/httpmethods/__init__.py
@@ -0,0 +1,36 @@
+"""
+ * methods
+ * Copyright(c) 2013-2014 TJ Holowaychuk
+ * Copyright(c) 2015-2016 Douglas Christopher Wilson
+ * Copyright(c) 2020-2020 Braian Staimer
+ * MIT Licensed
+"""
+def getHttpMethods():
+    return [
+        'get',
+        'post',
+        'put',
+        'head',
+        'delete',
+        'options',
+        'trace',
+        'copy',
+        'lock',
+        'mkcol',
+        'move',
+        'purge',
+        'propfind',
+        'proppatch',
+        'unlock',
+        'report',
+        'mkactivity',
+        'checkout',
+        'merge',
+        'm-search',
+        'notify',
+        'subscribe',
+        'unsubscribe',
+        'patch',
+        'search',
+        'connect'
+    ]
