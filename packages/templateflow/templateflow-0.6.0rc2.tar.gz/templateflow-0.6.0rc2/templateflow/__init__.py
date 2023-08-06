a8ba93c2c73ee312891fabfe283ff930594b101b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:34 +0000

    Add package templateflow/0.6.0rc2/templateflow-0.6.0rc2.tar.gz

---
+++ packages/templateflow/templateflow-0.6.0rc2.tar.gz/templateflow-0.6.0rc2/templateflow/__init__.py
@@ -0,0 +1,26 @@
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+"""TemplateFlow is the Zone of Templates."""
+__packagename__ = "templateflow"
+__copyright__ = "2020, The TemplateFlow developers"
+try:
+    from ._version import __version__
+except ModuleNotFoundError:
+    from pkg_resources import get_distribution, DistributionNotFound
+    try:
+        __version__ = get_distribution(__packagename__).version
+    except DistributionNotFound:
+        __version__ = "unknown"
+    del get_distribution
+    del DistributionNotFound
+
+from . import api
+from .conf import update
+
+__all__ = [
+    '__copyright__',
+    '__packagename__',
+    '__version__',
+    'api',
+    'update',
+]
