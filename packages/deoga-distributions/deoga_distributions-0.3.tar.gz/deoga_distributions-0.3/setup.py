695103758b4a124fb459b323a52aa8379121cfc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:34 +0000

    Add package deoga-distributions/0.3/deoga_distributions-0.3.tar.gz

---
+++ packages/deoga-distributions/deoga_distributions-0.3.tar.gz/deoga_distributions-0.3/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='deoga_distributions',
+      version='0.3',
+      description='Mathematical distributions',
+      packages=['deoga_distributions'],
+      zip_safe=False)
