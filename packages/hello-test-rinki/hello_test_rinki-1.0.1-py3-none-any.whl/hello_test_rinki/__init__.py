21645f6f09f971512d9923311d10455d32bb39ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:25 +0000

    Add package hello-test-rinki/1.0.1/hello_test_rinki-1.0.1-py3-none-any.whl

---
+++ packages/hello-test-rinki/hello_test_rinki-1.0.1-py3-none-any.whl/hello_test_rinki/__init__.py
@@ -0,0 +1,2 @@
+# Version of the hello-world package
+__version__ = "1.0.1"
---
+++ packages/hello-test-rinki/hello_test_rinki-1.0.1-py3-none-any.whl/hello_test_rinki/__main__.py
@@ -0,0 +1,9 @@
+from importlib import resources  # Python 3.7+
+import sys
+
+def hello_p():
+    #name=input("Input your name")
+    return "hello world my first ok package"
+
+if __name__ == "__main__":
+    hello_p()
