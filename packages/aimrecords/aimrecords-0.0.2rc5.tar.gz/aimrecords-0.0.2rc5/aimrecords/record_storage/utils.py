bbf555e9b9297e81d3d05683c5601d5569f6c379
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:16:41 +0000

    Add package aimrecords/0.0.2rc5/aimrecords-0.0.2rc5.tar.gz

---
+++ packages/aimrecords/aimrecords-0.0.2rc5.tar.gz/aimrecords-0.0.2rc5/aimrecords/record_storage/utils.py
@@ -0,0 +1,32 @@
+import os
+import json
+
+
+def get_data_fname(path: str) -> str:
+    return os.path.join(path, 'data_chunk_00000.bin')
+
+
+def get_record_offsets_fname(path: str) -> str:
+    return os.path.join(path, 'record_offsets.bin')
+
+
+def get_bucket_offsets_fname(path: str) -> str:
+    return os.path.join(path, 'bucket_offsets.bin')
+
+
+def get_metadata_fname(path: str) -> str:
+    return os.path.join(path, 'metadata.json')
+
+
+def metadata_exists(path: str) -> bool:
+    return os.path.isfile(get_metadata_fname(path))
+
+
+def write_metadata(path: str, metadata: dict):
+    with open(get_metadata_fname(path), 'w') as f_out:
+        json.dump(metadata, f_out)
+
+
+def read_metadata(path: str) -> dict:
+    with open(get_metadata_fname(path), 'r') as f_in:
+        return json.load(f_in)
