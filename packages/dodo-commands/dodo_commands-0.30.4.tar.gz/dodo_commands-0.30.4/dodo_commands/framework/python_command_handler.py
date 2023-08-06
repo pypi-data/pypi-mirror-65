f27eb190e3a9845b3c5908136fa60e5395d6b950
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:11 +0000

    Add package dodo-commands/0.30.4/dodo_commands-0.30.4.tar.gz

---
+++ packages/dodo-commands/dodo_commands-0.30.4.tar.gz/dodo_commands-0.30.4/dodo_commands/framework/python_command_handler.py
@@ -0,0 +1,112 @@
+import os
+import platform
+import re
+import subprocess
+from importlib import import_module
+
+from dodo_commands.dependencies import yaml_round_trip_load
+from dodo_commands.dependencies.get import plumbum
+from dodo_commands.framework.command_error import CommandError
+from dodo_commands.framework.command_map import CommandMapItem
+from dodo_commands.framework.command_path import extend_sys_path
+from dodo_commands.framework.config import Paths
+from dodo_commands.framework.util import query_yes_no
+
+CommandNotFound = plumbum.commands.processes.CommandNotFound
+
+
+class PythonCommandMapItem(CommandMapItem):
+    def __init__(self, group, filename):
+        super(PythonCommandMapItem, self).__init__(group, filename, 'py')
+        self.package_path = group
+
+
+class PythonCommandHandler:
+    def add_commands_to_map(self, command_dirs, file_map, command_map):
+        extend_sys_path(command_dirs)
+        for command_dir, files in file_map.items():
+            for file in files:
+                command_name, ext = os.path.splitext(os.path.basename(file))
+                if ext == '.py' and not command_name.startswith('_'):
+                    command_map[command_name] = PythonCommandMapItem(
+                        group=os.path.basename(command_dir), filename=file)
+
+    def execute(self, command_map_item, command_name):
+        def meta_data_filename():
+            base_path = import_module(
+                command_map_item.package_path).__path__[0]
+            return os.path.join(base_path, command_name + ".meta")
+
+        def meta_data():
+            if os.path.exists(meta_data_filename()):
+                with open(meta_data_filename()) as f:
+                    return yaml_round_trip_load(f.read())
+            return None
+
+        def install_packages(meta_data, dependency):
+            """Pip install packages found in meta_data."""
+            packager_name = 'brew' if platform.system() == 'Darwin' else 'apt'
+            package_data = meta_data['packages'].get(packager_name, {})
+            recipe = package_data.get(dependency, None)
+            if recipe:
+                commands = [recipe] if isinstance(recipe, str) else recipe
+
+                print("This command wants to run:\n%s\n" % '\n'.join(commands))
+                if query_yes_no("Install (yes), or abort (no)?"):
+                    for package_cmd in commands:
+                        subprocess.check_call(package_cmd.split())
+                    print("--- Done ---\n\n")
+
+        def install_python_packages(meta_data, dependency):
+            """Pip install packages found in meta_data."""
+            requirements = [
+                x for x in meta_data['requirements']
+                if re.search(r"\b" + dependency + r"\b", x)
+            ]
+            if requirements:
+                if len(requirements) > 1:
+                    raise CommandError(
+                        "Found more than 1 candidate for python package %s in %s"
+                        % (dependency, meta_data_filename()))
+
+                requirement = requirements[0]
+                print("This command wants to install %s:\n" % requirement)
+                if query_yes_no("Install (yes), or abort (no)?"):
+                    subprocess.check_call(
+                        [sys.executable, "-m", "pip", "install", requirement])
+                    print("--- Done ---\n\n")
+
+        def get_program_from_exception(e):
+            m = re.search(r"cannot import name '(\w+)' from 'plumbum.cmd'",
+                          e.args[0]) if len(e.args) else None
+            return m.groups(1)[0] if m else None
+
+        import_path = '%s.%s' % (command_map_item.package_path, command_name)
+        if command_map_item.package_path in ("", None, "."):
+            import_path = command_name
+
+        # Try the import, return if success
+        try:
+            return import_module(import_path)
+        except ImportError as e:
+            exception_type = type(e)
+            program = get_program_from_exception(e)
+            if program:
+                dependency, install = program, install_packages
+            else:
+                dependency, install = e.name, install_python_packages
+        except CommandNotFound as e:
+            exception_type = type(e)
+            dependency, install = e.name, install_python_packages
+
+        # Try the import again, but first run install
+        try:
+            md = meta_data()
+            if md:
+                install(md, dependency)
+            import_module(import_path)
+        except exception_type:
+            fail_msg = ('Could not find executable: %s' % dependency if
+                        (install is install_packages) else
+                        'Could not import python module: %s' % dependency)
+            raise CommandError(fail_msg)
---
+++ packages/dodo-commands/dodo_commands-0.30.4.tar.gz/dodo_commands-0.30.4/setup.py
@@ -0,0 +1,72 @@
+import os
+import subprocess
+import sys
+from distutils.command.install_data import install_data
+
+from setuptools import find_packages, setup
+
+
+class InstallPrivatePackages(install_data):
+    def _packages_dirname(self):
+        def find_module_path():
+            for p in sys.path:
+                if os.path.isdir(p) and "dodo_commands" in os.listdir(p):
+                    return os.path.join(p, "dodo_commands")
+
+        install_path = find_module_path()
+        return os.path.join(install_path, "dependencies", "packages")
+
+    def _install_packages(self, packages_dirname):
+        if not os.path.exists(packages_dirname):
+            os.makedirs(packages_dirname)
+
+        for dependency in [
+                'python-dotenv==0.12.0',
+                'plumbum==1.6.8',
+                'ruamel.yaml==0.16.10',
+                'parsimonious==0.8.1',
+                'six==1.14.0',
+                'funcy==1.14',
+                'ansimarkup==1.4.0',
+                'argcomplete==1.11.1',
+                'semantic_version==2.8.4',
+        ]:
+            try:
+                subprocess.check_call([
+                    sys.executable, '-m', 'pip', 'install', '--target',
+                    packages_dirname, dependency
+                ])
+            except:  # noqa
+                pass
+
+    def run(self):
+        super().run()
+        self._install_packages(self._packages_dirname())
+
+
+setup(name='dodo_commands',
+      version='0.30.4',
+      description=
+      'Project-aware development environments, inspired by django-manage',
+      url='https://github.com/mnieber/dodo_commands',
+      download_url='https://github.com/mnieber/dodo_commands/tarball/0.30.4',
+      author='Maarten Nieber',
+      author_email='hallomaarten@yahoo.com',
+      license='MIT',
+      packages=find_packages(),
+      package_data={
+          'dodo_commands': [
+              'extra/dodo_standard_commands/*.meta',
+              'extra/dodo_docker_commands/*.meta',
+          ]
+      },
+      entry_points={'console_scripts': [
+          'dodo=dodo_commands.dodo:main',
+      ]},
+      data_files=[('/etc/bash_completion.d', [
+          'dodo_commands/bin/dodo_autocomplete.sh',
+          'dodo_commands/bin/sdodo_autocomplete.sh'
+      ]), ('/etc/fish/conf.d', ['dodo_commands/bin/dodo_autocomplete.fish'])],
+      cmdclass={'install_data': InstallPrivatePackages},
+      install_requires=[],
+      zip_safe=False)
