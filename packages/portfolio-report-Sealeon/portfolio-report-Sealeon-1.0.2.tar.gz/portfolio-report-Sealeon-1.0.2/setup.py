5cfed37286091fb52243d7c8651a76d2a1e468e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:30 +0000

    Add package portfolio-report-Sealeon/1.0.2/portfolio-report-Sealeon-1.0.2.tar.gz

---
+++ packages/portfolio-report-Sealeon/portfolio-report-Sealeon-1.0.2.tar.gz/portfolio-report-Sealeon-1.0.2/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="portfolio-report-Sealeon",
+    version="1.0.2",
+    author="Emily Knight",
+    author_email="emily.knight2@mohawkcollege.ca",
+    description="A stock report generator",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sheridan-python/assignment-7-Sealeon",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Natural Language :: English",
+    ],
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            "portfolio_report=portfolio.portfolio_report:main",
+        ],
+    },
+)
