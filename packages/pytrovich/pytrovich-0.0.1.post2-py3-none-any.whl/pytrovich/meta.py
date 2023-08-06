09065b609188598c4add2f013b678d28a3c36817
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:04 +0000

    Add package pytrovich/0.0.1.post2/pytrovich-0.0.1.post2-py3-none-any.whl

---
+++ packages/pytrovich/pytrovich-0.0.1.post2-py3-none-any.whl/pytrovich/meta.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+package = "pytrovich"
+version = "0.0.1post2"
+authors = ["Anton Alekseev"]
+authors_string = ", ".join(authors)
+emails = ["anton.m.alexeyev@gmail.com"]
+license = "MIT"
+copyright = "2020 " + authors_string
