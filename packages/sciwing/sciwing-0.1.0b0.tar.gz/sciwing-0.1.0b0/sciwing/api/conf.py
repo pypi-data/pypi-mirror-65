a8c9e4e502a30c55c18d73bec8508fe4715f7400
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:00 +0000

    Add package sciwing/0.1.0b0/sciwing-0.1.0b0.tar.gz

---
+++ packages/sciwing/sciwing-0.1.0b0.tar.gz/sciwing-0.1.0b0/sciwing/api/conf.py
@@ -0,0 +1,8 @@
+import pathlib
+import sciwing.constants as constants
+
+PATHS = constants.PATHS
+OUTPUT_DIR = PATHS["OUTPUT_DIR"]
+
+PDF_STORE_LOCATION = pathlib.Path("/tmp/")
+BIN_FOLDER = pathlib.Path("./bin/")
