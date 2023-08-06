d6da0a4fd595b08ae52beaaf99214d07f19db474
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:46 +0000

    Add package due-deligence/1.3/due_deligence-1.3.tar.gz

---
+++ packages/due-deligence/due_deligence-1.3.tar.gz/due_deligence-1.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import re
+
+
+def get_requirements():
+    for requirement in open('./requirements.txt').read().splitlines():
+        removed_version = re.sub(r'==.*', '', requirement)
+        yield removed_version.replace('-', '')
+
+
+setup(
+    name="due_deligence",
+    version='1.3',
+    description='EDINETから有価証券報告書を取得して企業の割安度を判定します。',
+    author='Kobori Akira',
+    # maintainer='',
+    # maintainer_email='',
+    author_email='private.beats@gmail.com',
+    url='https://github.com/koboriakira/due_deligence',
+    packages=find_packages(),
+    install_requires=open('requirements.txt').read().splitlines(),
+    # requires=get_requirements(),
+    # requires=open('./requirements.txt').read().splitlines(),
+    license='MIT',
+    entry_points="""
+      # -*- Entry points: -*-
+      [console_scripts]
+      duedeli = due_deligence.cli:main
+    """
+)
