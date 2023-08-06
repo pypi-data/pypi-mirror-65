2ea268c67853b9a5f6e14b1b4ae158ea207bdbb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:52 +0000

    Add package transform-BELLIKAN/0.0.1/transform-BELLIKAN-0.0.1.tar.gz

---
+++ packages/transform-BELLIKAN/transform-BELLIKAN-0.0.1.tar.gz/transform-BELLIKAN-0.0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="transform-BELLIKAN",  # Replace with your own username
+    version="0.0.1",
+    author="Birol Bellikan",
+    author_email="birol.bellikan@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    setup_requires=['wheel'],
+)
+# python3 setup.py sdist bdist_wheel
