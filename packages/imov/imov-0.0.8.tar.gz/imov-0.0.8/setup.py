ba024e1dd7f63055de0c7cb3874cdbac616102d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:50 +0000

    Add package imov/0.0.8/imov-0.0.8.tar.gz

---
+++ packages/imov/imov-0.0.8.tar.gz/imov-0.0.8/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+setup(
+  name='imov',
+  version='0.0.8',
+  packages=find_packages(),
+  entry_points={
+    'console_scripts': [
+      'imov=imov:run',
+    ]
+  },
+  install_requires=[
+    "requests",
+    "openpyxl",
+    "lxml",
+    "setuptools",
+  ],
+  url='https://github.com/doctoroyy/IMDB-TOP-250',
+  author='doctoroyy',
+  author_email='doctor.oyy@gmail.com',
+  description='A Python Crawler for IMDB-TOP-250.'
+)
