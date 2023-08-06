f3f925fb719c4ddab85d53e3ef1203b97dbf8f9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:51 +0000

    Add package easy-tweet-deleter/0.0.2/easy_tweet_deleter-0.0.2.tar.gz

---
+++ packages/easy-tweet-deleter/easy_tweet_deleter-0.0.2.tar.gz/easy_tweet_deleter-0.0.2/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+from distutils.core import setup
+setup(
+  name = 'easy_tweet_deleter',
+  packages = ['easy_tweet_deleter'],
+  version = '0.0.2',
+  license='MIT',
+  description = 'A simple app to delete tweets',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+  author = 'Stefan Selby',
+  author_email = 'stefanselby@gmail.com', 
+  url = 'https://github.com/TheTallBlonde1/easy_tweet_deleter', 
+  download_url = 'https://github.com/TheTallBlonde1/easy_tweet_deleter/archive/v_0_0_2.tar.gz',    # I explain this later on
+  keywords = ['DELETE', 'TWEETS', 'TWITTER'],
+  install_requires=[
+    'python-twitter',
+    'GetOldTweets3'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Public Domain",
+        "Operating System :: OS Independent",
+    ]
+)
