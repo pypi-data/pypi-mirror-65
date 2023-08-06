1d140257fd57a253366f0b18a919684d3119c2fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:16 +0000

    Add package uutils/0.0.3/uutils-0.0.3.tar.gz

---
+++ packages/uutils/uutils-0.0.3.tar.gz/uutils-0.0.3/setup.py
@@ -0,0 +1,20 @@
+#-*- coding:utf-8 -*-
+
+import setuptools
+
+setuptools.setup(
+    name="uutils",
+    version="0.0.3",
+    author="CzKerwin",
+    author_email="czksnk@outlook.com",
+    description="uutils",
+    long_description="uutils",
+    long_description_content_type="text/markdown",
+    url="https://github.com/CzKerwin/uutils.git",
+    packages=setuptools.find_packages(),
+    license = "MIT Licence",
+    python_requires='>=3.5',
+    include_package_data = True,
+    platforms = "any",
+    install_requires = [],
+)
