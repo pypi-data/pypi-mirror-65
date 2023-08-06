1d648a04769cabc586a3e1f510cd8594c3e4ad81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:16 +0000

    Add package datastructs-sparkles58/1.0.0/datastructs-sparkles58-1.0.0.tar.gz

---
+++ packages/datastructs-sparkles58/datastructs-sparkles58-1.0.0.tar.gz/datastructs-sparkles58-1.0.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="datastructs-sparkles58", # Replace with your own username
+    version="1.0.0",
+    author="Ezra SB",
+    author_email="ezzysuger717@gmail.com",
+    description="Queues and trees.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sparklesunicorn/datastructs",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
