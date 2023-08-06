78d81d8ecde017ff9f1a5cf7ea04cbe8cb7350a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:35 +0000

    Add package lazycli/0.6.1/lazycli-0.6.1-py3-none-any.whl

---
+++ packages/lazycli/lazycli-0.6.1-py3-none-any.whl/lazycli-0.6.1.dist-info/DESCRIPTION.rst
@@ -0,0 +1,320 @@
+lazycli
+=======
+lazycli is a module which provides a decorator which will generate cli
+scripts from function signatures. The intention is to allow the creation
+of cli-scripts with as little extra work as possible. It was originally
+going to be called ``sig2cli``, but someone else already `had the same
+idea`_ and got the name on PyPI in ten months before I did.
+
+The one and only goal of lazycli is to facilitate the creation of CLI
+interfaces with *minimum effort*.
+
+lazycli wraps ``argparse`` from the Python standard library and exposes
+some parts of the `argparse api`_. The abstraction it provides is a
+little leaky, but it's not too bad, because it's relatively simple and
+is not intended to provide the full range functionality. If you need
+flexibility, use ``argparse`` directly or something more powerful like
+click_.
+
+.. _had the same idea: https://github.com/PaoloSarti/sig2cli
+.. _argparse api: https://docs.python.org/3/library/argparse.html
+.. _click: https://click.palletsprojects.com/
+
+.. contents::
+
+Basics
+------
+Consider this simple clone of the ``cp`` command, ``cp.py``:
+
+.. code:: Python
+
+  #!/usr/bin/env python3
+  import lazycli
+  import shutil
+  import sys
+
+
+  @lazycli.script
+  def cp(*src, dst, recursive=False):
+      """copy around files"""
+      for path in src:
+          try:
+              shutil.copy2(path, dst)
+          except IsADirectoryError as err:
+              if recursive:
+                  shutil.copytree(path, dst)
+              else:
+                  print(err, file=sys.stderr)
+
+
+  if __name__ == '__main__':
+      cp.run()
+
+.. code:: sh
+
+  $ ./cp.py -h
+  usage: cp.py [-h] [-r] [src [src ...]] dst
+
+  copy around files
+
+  positional arguments:
+    src
+    dst
+
+  optional arguments:
+    -h, --help       show this help message and exit
+    -r, --recursive
+
+It works like you'd expect. I chose ``cp`` because shutil_ can do all
+the heavy lifting, and the body of the function isn't important. The
+important thing in this script are these three lines:
+
+.. code:: python
+
+  @lazycli.script
+  def cp(*src, dst, recursive=False):
+
+  # ... and ...
+
+  cp.run()
+
+- All parameters without defaults become positional arguments.
+- All parameters with defaults become optional arguments.
+- ``*args`` arguments will translate into variadic arguments at the
+  command line as well. *There can always be zero of them.*
+- Parameters with boolean default values are treated as boolean flags
+  and don't accept arguments.
+- Short versions of flags are generated automatically from the first
+  letter of the parameter.
+- A ``.run`` function is tacked on to the ``cp`` function which
+  triggers argument parsing and applies the results to ``cp``. The
+  ``cp`` function itself is unaltered and can be called elsewhere if
+  desired.
+
+I'm not entirely sure how useful this last point is, since script
+entry-point functions tend not to be very general-purpose, but, eh, who
+knows.
+
+Be aware that, presently, ``**kwargs``-style parameters are ignored
+by lazycli.
+
+**Note on short flags:**
+  Short flags are generated for optional arguments based on the first
+  letter of parameter names. If that flag has been used by a previous
+  parameter, the flag will be uppercased. If that has already been used,
+  no short flag is generated. Because of this, changing the order of
+  arguments can potentially break the backward compatibility of your
+  CLI.
+
+**Note on boolean defaults:**
+  A boolean default set to ``False`` produces the output seen above. If
+  we change the parameter default to ``recursive=True``, the name of the
+  flag is inverted:
+
+  .. code::
+
+    optional arguments:
+      -h, --help          show this help message and exit
+      -r, --no-recursive
+
+.. _shutil: https://docs.python.org/3/library/shutil.html
+
+Types
+-----
+lazycli attempts to determine argument types based first on type
+annotations in the function signature and then based on the type of the
+default argument.
+
+- If the type of parameter is an iterable (besides mappings, strings and
+  files), it will become a variadic when interpreted. If it's a
+  subscripted type from the typing_ module, like
+  ``typing.Iterable[int]``, the subscript will be used as the type.
+- If the type is determined to be a mapping or is annotated as
+  ``object``, the argument should be a json literal (though it could
+  theoretically be a string, number, array or object).
+
+The inferred type is then used as a constructor to parse the argument
+string. This means only constructors that can take strings as input may
+be used.
+
+.. code:: python
+
+  #!/usr/bin/env python3
+  import typing as t
+  import lazycli
+
+  @lazycli.script
+  def mysum(numbers: t.Iterable[float]):
+      return sum(numbers)
+
+  if __name__ == '__main__':
+      mysum.run()
+
+
+.. code:: sh
+
+  $ ./sum.py -h
+  usage: sum.py [-h] [numbers [numbers ...]]
+
+  positional arguments:
+    numbers     type: float
+
+  optional arguments:
+    -h, --help  show this help message and exit
+
+  $ ./sum.py 5 8
+  13.0
+
+Though the style is questionable, this means you can use arbitrary
+callables as type annotations:
+
+.. code:: python
+
+
+  #!/usr/bin/env python3
+  import sys
+  import lazycli
+
+
+  @lazycli.script
+  def upcat(
+          infile: open = sys.stdin,
+          outfile: lambda f: open(f, 'w') = sys.stdout
+  ):
+      """cat, but upper-cases everything."""
+      for line in infile:
+          outfile.write(line.upper())
+
+
+  if __name__ == '__main__':
+      upcat.run()
+
+.. code:: sh
+
+  usage: upcat.py [-h] [-i INFILE] [-o OUTFILE]
+
+  cat, but upper-cases everything.
+
+  optional arguments:
+    -h, --help            show this help message and exit
+    -i INFILE, --infile INFILE
+                          type: open; default: <stdin>
+    -o OUTFILE, --outfile OUTFILE
+                          type: <lambda>; default: <stdout>
+
+This looks pretty bad, and mypy_ is going to hate it. A better way to
+do this is probably just parsing the string inside the script.
+
+However, because the pattern of having an optional file argument and
+falling back to standard streams is so common, ``lazycli`` provides
+special classes for making this less ugly:
+
+.. code:: Python
+
+  #!/usr/bin/env python3
+  import sys
+  from lazycli import script, ReadFile, WriteFile
+
+
+  @script
+  def upcat2(infile:ReadFile=sys.stdin, outfile:WriteFile=sys.stdout):
+      """cat, but upper-cases everything."""
+      for line in infile:
+          outfile.write(line.upper())
+
+
+  if __name__ == '__main__':
+      upcat2.run()
+
+
+These classes will provide users more helpful type information and will
+return true if used in instance checks of text file types (including
+``sys.{stdin,stdout,stderr}`` and non-bytes output of the ``open``
+builtin function). These classes don't create instances of themselves,
+but rather instances of ``io.TextIOWrapper``. However, they still break
+mypy. Funny how metaclasses will do that.
+
+In addition to ReadFile and WriteFile, there is also an AppendFile
+constructor.
+
+.. _typing: https://docs.python.org/3/library/typing.html
+.. _mypy: http://mypy-lang.org/
+
+Output
+------
+So far, output has simply been printed. However, If the function has a
+return value, that will also be printed. If it is an iterable (besides a
+string or mapping), each item will be printed on a new line.
+
+Subcommands
+-----------
+I'll expand this section of the documentation later, but here's a sample
+script, modeled on info in this `blog post`_
+
+.. code:: Python
+
+  #!/usr/bin/env python3
+  import lazycli
+
+
+  @lazycli.script
+  def main(version=False):
+    if version:
+      return 1.0
+
+
+  @main.subcommand
+  def hello(name, greeting="Hello", caps=False):
+      return greet(name, greeting, caps)
+
+
+  @main.subcommand
+  def goodbye(name, greeting="Goodbye", caps=False):
+      return greet(name, greeting, caps)
+
+
+  def greet(name, greeting, caps):
+      if caps:
+          return f'{greeting}, {name}!'.upper()
+      return f'{greeting}, {name}!'
+
+
+  if __name__ == '__main__':
+      script.run()
+
+
+Any parameters definied on the script function (main, in this case)
+will be passed to the script function, and parameters definied on the
+subcommands are parsed to them. If one wishes to pass values from the
+top-level function to subcommands, and external mechanism must be used.
+(e.g. a global variable)
+
+.. code:: shell
+
+  $ ./test_sub.py -h
+  usage: test_sub.py [-h] [-v] {hello,goodbye} ...
+
+  positional arguments:
+    {hello,goodbye}
+
+  optional arguments:
+    -h, --help       show this help message and exit
+    -v, --version
+  $
+  $
+  $ ./test_sub.py hello -h
+  usage: test_sub.py hello [-h] [-c] [-g GREETING] name
+
+  positional arguments:
+    name
+
+  optional arguments:
+    -h, --help            show this help message and exit
+    -c, --caps
+    -g GREETING, --greeting GREETING
+                          default: Hello
+
+.. _blog post:
+  https://realpython.com/comparing-python-command-line-parsing-libraries-argparse-docopt-click/
+
+
---
+++ packages/lazycli/lazycli-0.6.1-py3-none-any.whl/lazycli/core.py
@@ -0,0 +1,368 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+import argparse
+import json
+import inspect
+import io
+import libaaron
+import functools
+import typing as t
+
+
+# fake types
+Iter = t.Iterable
+PositionalParams = Iter[inspect.Parameter]
+FlagsParams = Iter[t.Tuple[inspect.Parameter, t.Optional[str]]]
+OptionParams = FlagsParams
+Parser = argparse.ArgumentParser
+HelpDict = t.Dict[str, str]
+
+
+class FileBase(io.TextIOBase):
+    """Children of this class really just pass the arguments from their
+    constructor to `open`. There are no real instances of the the classes.
+    the mode argument is determined in the child.
+    """
+    mode = ''
+
+    def __new__(cls, filename: str):
+        return open(filename, cls.mode)
+
+
+class ReadFile(FileBase):
+    """opens a file in text mode for reading"""
+    mode = 'r'
+
+
+class WriteFile(FileBase):
+    """opens a file in text mode for writing (truncates)"""
+    mode = 'w'
+
+
+class AppendFile(FileBase):
+    """opens a file in text mode for writing (appends)"""
+    mode = 'a'
+
+
+def getshortflag(char: str, shortflags: t.Set[str]) -> t.Optional[str]:
+    """Check if a character has alread used as a short flag. If so, uppercase
+    it, otherwise return the character. If the uppercase character is also
+    used, return None. No shortflag is generated.
+
+    char -- the character to use
+
+    shortflags -- a set of flags already used. This function updates the set,
+                  so usage should be like this:
+
+    >>> used = set()
+    >>> for char in 'abccdce':
+    ...     flag = getshortflag(char, used)
+    ...     # etc
+    """
+    if char in shortflags:
+        char = char.upper()
+        if char in shortflags:
+            return None
+
+    shortflags.add(char)
+    return char
+
+
+def sort_params(params: Iter[inspect.Parameter]) -> (
+        t.Tuple[PositionalParams, FlagsParams, OptionParams]):
+    positionals = []
+    flags = []
+    options = []
+    shortflags: t.Set[str] = set()
+    for param in params:
+        if param.kind is param.VAR_KEYWORD:
+            pass
+        elif param.annotation is bool or isinstance(param.default, bool):
+            flags.append((param, getshortflag(param.name[0], shortflags)))
+        elif param.default == param.empty:
+            positionals.append(param)
+        else:
+            options.append((param, getshortflag(param.name[0], shortflags)))
+
+    return positionals, flags, options
+
+
+class ArgType(t.NamedTuple):
+    iterable: bool
+    constructor: t.Optional[t.Callable[[str], t.Any]]
+
+
+def real_type(T: t.Type) -> ArgType:
+    """determine argument type from a concrete python type"""
+    if T is object or issubclass(T, t.Mapping):
+        return ArgType(False, json.loads)
+
+    if issubclass(T, (io.IOBase, str)):
+        return ArgType(False, T)
+
+    if issubclass(T, t.Sequence):
+        return ArgType(True, None)
+
+    return ArgType(False, T)
+
+
+def typing_type(T: t.Type) -> ArgType:
+    """determine argument type from a type that is from the `typing` modlue"""
+    iterable, _ = real_type(T.__origin__)
+    if iterable:
+        try:
+            subscript = T.__args__[0]
+            _, constructor = real_type(subscript)
+            return ArgType(True, constructor)
+        except IndexError:
+            return ArgType(True, None)
+
+    return ArgType(False, None)
+
+
+def annotation_type(annotation: t.Type) -> ArgType:
+    """determine argument type from type in annotation"""
+    if inspect.isfunction(annotation) or inspect.isbuiltin(annotation):
+        return ArgType(False, annotation)
+    if isinstance(annotation, t.GenericMeta):
+        return typing_type(annotation)
+    if isinstance(annotation, t.Type):
+        return real_type(annotation)
+    return ArgType(False, None)
+
+
+def varargs_type(param: inspect.Parameter) -> ArgType:
+    """determine argument type for variadic positional parameter"""
+    if param.annotation is param.empty:
+        return ArgType(True, None)
+
+    _, constructor = annotation_type(param.annotation)
+    return ArgType(True, constructor)
+
+
+def default_type(default) -> ArgType:
+    """infer the type from the default argument"""
+    if default is None:
+        return ArgType(False, None)
+    iterable, constructor = real_type(type(default))
+    if iterable:
+        try:
+            _, constructor = real_type(type(default[0]))
+        except IndexError:
+            pass
+    return ArgType(iterable, constructor)
+
+
+def infer_type(param: inspect.Parameter, positional: bool = False) -> ArgType:
+    """determine argument type from a function parameter"""
+    if param.kind is param.VAR_POSITIONAL:
+        return varargs_type(param)
+
+    if param.annotation is not param.empty:
+        return annotation_type(param.annotation)
+
+    if positional:
+        return ArgType(False, None)
+
+    return default_type(param.default)
+
+
+def add_arg(
+        parser: Parser,
+        name: str,
+        param: inspect.Parameter,
+        kwargs,
+        help: HelpDict,
+        shortflag: str = None,
+        positional: bool = False
+):
+    """add an argument to the parser with the given name. additional info is
+    derived from the function parameter. kwargs is a dictionary of keyword
+    arguments that will be passed to add_arg. This dictionary will be mutated.
+    """
+    try:
+        kwargs["help"] = help[name]
+    except KeyError:
+        pass
+
+    iterable, constructor = infer_type(param, positional)
+    if iterable:
+        kwargs['nargs'] = '*'
+    if constructor:
+        kwargs['type'] = constructor
+        tname = 'json' if constructor == json.loads else constructor.__name__
+        tstring = 'type: ' + tname
+        try:
+            kwargs['help'] += '; ' + tstring
+        except KeyError:
+            kwargs['help'] = tstring
+
+    if param.default is not param.empty:
+        defstr = 'default: %s' % getattr(param.default, 'name', param.default)
+        try:
+            kwargs['help'] += '; ' + defstr
+        except KeyError:
+            kwargs['help'] = defstr
+
+    if shortflag:
+        parser.add_argument('-' + shortflag, name.replace('_', '-'), **kwargs)
+    else:
+        parser.add_argument(name.replace('_', '-'), **kwargs)
+
+
+def mkpositional(params: PositionalParams, parser: Parser, help: HelpDict):
+    """add positional parameters to the parser"""
+    for param in params:
+        add_arg(parser, param.name, param, {}, help, positional=True)
+
+
+def mkflags(params: FlagsParams, parser: Parser, help: HelpDict):
+    """add flags to the parser"""
+    for param, shortflag in params:
+        kwargs = {'action': 'store_true'}
+        name = '--'
+        if param.default and param.default is not param.empty:
+            name += 'no-'
+            kwargs['action'] = 'store_false'
+        name += param.name
+        try:
+            kwargs['help'] = help[name]
+        except KeyError:
+            pass
+        if shortflag:
+            parser.add_argument(  # type: ignore
+                '-' + shortflag, name.replace('_', '-'), **kwargs)
+        else:
+            parser.add_argument(  # type: ignore
+                name.replace('_', '-'), **kwargs)
+
+
+def mkoptions(params: OptionParams, parser: Parser, help: HelpDict):
+    """add optional params to the parser"""
+    for param, shortflag in params:
+        add_arg(
+            parser,
+            '--' + param.name,
+            param,
+            {'default': param.default},
+            help,
+            shortflag,
+        )
+
+
+class Script:
+    def __init__(
+            self,
+            function: t.Callable = None,
+            parser: type = Parser,
+            help: HelpDict = None,
+    ):
+        """make a parser for a script.
+        """
+        self.function = function
+        self.parsertype = parser
+        self.help = help or {}
+        self.positionals = []
+        self.flags = []
+        self.options = []
+
+    @libaaron.reify
+    def parser(self) -> Parser:
+        """returns the argparse.ArgumentParser for the instance"""
+        if not self.function:
+            return self.parsertype()
+        sig = inspect.signature(self.function)
+        self.positionals, self.flags, self.options = sort_params(
+            sig.parameters.values())
+        parser = self.parsertype(description=self.function.__doc__)
+        mkpositional(self.positionals, parser, self.help)
+        mkflags(self.flags, parser, self.help)
+        mkoptions(self.options, parser, self.help)
+        return parser
+
+    @property
+    def params(self) -> t.Iterator[inspect.Parameter]:
+        """iterate over all parameters"""
+        yield from self.positionals
+        yield from (i[0] for i in self.flags)
+        yield from (i[0] for i in self.options)
+
+    @libaaron.reify
+    def subparsers(self) -> Parser:
+        """get a subparser for the instance"""
+        return self.parser.add_subparsers()
+
+    def run(self, *args, iterprint=False, **kwargs):
+        """run the generated cli script. *args and **kwargs are passed to
+        argparse.ArgumentParser.parse_args
+        """
+        args = {
+            k.replace('-', '_'): v for k, v in
+            vars(self.parser.parse_args(*args, **kwargs)).items()
+        }
+        delegate = args.pop('_func', None)
+        if delegate:
+            top_args = {p.name: args.pop(p.name) for p in self.params}
+        else:
+            top_args = args
+        funcs = [(self._func, top_args)] if self.function else []
+
+        if delegate:
+            funcs.append((delegate, args))
+
+        for func, args in funcs:
+            out = func(args)
+
+            if isinstance(out, Iter) and not isinstance(out, (str, t.Mapping)):
+                if iterprint:
+                    for line in out:
+                        print(line)
+                else:
+                    print(*out, sep='\n')
+            elif out is not None:
+                print(out)
+
+    def _func(self, args: t.MutableMapping):
+        # map args back onto the signature.
+        pargs = []  # type: t.List[t.Any]
+        for param in self.positionals:
+            if param.kind == param.VAR_POSITIONAL:
+                pargs.extend(args.pop(param.name))
+            elif param.kind == param.POSITIONAL_OR_KEYWORD:
+                pargs.append(args.pop(param.name))
+
+        for key, value in args.items():
+            if key.startswith('no_'):
+                args[key[3:]] = args.pop(key)
+                continue
+
+        return (self.function or (lambda: None))(*pargs, **args)
+
+    def subcommand(
+            self, func: t.Callable = None, help: HelpDict = None
+    ) -> t.Callable:
+        if not func:
+            return functools.partial(self.subcommand, help=help)
+
+        subparser = functools.partial(
+            self.subparsers.add_parser, func.__name__
+        )
+        subscript = Script(func, subparser, help=help)
+        subscript.parser.set_defaults(_func=subscript._func)
+        return func
+
+
+def script(func: t.Callable = None, help: HelpDict = None):
+    if not func:
+        if help:
+            return functools.partial(script, help=help)
+        else:
+            return Script()
+
+    scrpt = Script(func, help=help)
+    print(scrpt)
+    func.run = scrpt.run
+    func.subcommand = scrpt.subcommand
+    func._script = scrpt
+    return func
