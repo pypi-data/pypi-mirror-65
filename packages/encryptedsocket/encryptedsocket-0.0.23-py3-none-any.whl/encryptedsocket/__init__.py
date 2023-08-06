7a1acdef5cae8e6c57b773274b28b5c9ac1e32dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:13 +0000

    Add package encryptedsocket/0.0.23/encryptedsocket-0.0.23-py3-none-any.whl

---
+++ packages/encryptedsocket/encryptedsocket-0.0.23-py3-none-any.whl/encryptedsocket/__init__.py
@@ -0,0 +1,6 @@
+from .server import *
+from .client import *
+
+version = "0.0.23"
+keywords = ["encrypted socket server"]
+entry = "test:main"
---
+++ packages/encryptedsocket/encryptedsocket-0.0.23-py3-none-any.whl/encryptedsocket/utils.py
@@ -0,0 +1,34 @@
+from aescipher import *
+import hmac
+import hashlib
+import json
+import pickle
+import random
+
+
+__ALL__ = ["encrypt", "decrypt", "randno"]
+
+
+def encrypt(key: str_or_bytes, plaintext: str_or_bytes) -> bytes:
+    plaintext = AESCipher(key).encrypt(plaintext)
+    hash = hmac.new(key.encode("utf-8"), plaintext.encode("utf-8"), hashlib.sha3_512).hexdigest()
+    return f"{hash} {plaintext}".encode("utf-8")
+
+
+def decrypt(key: str_or_bytes, ciphertext: str) -> Any:
+    mac, ciphertext = ciphertext.split(" ")
+    hash = hmac.new(key.encode("utf-8"), ciphertext.encode("utf-8"), hashlib.sha3_512).hexdigest()
+    if hash == mac:
+        ciphertext = AESCipher(key).decrypt(ciphertext)
+        try:
+            return json.loads(ciphertext)
+        except:
+            return pickle.loads(ciphertext)
+    else:
+        raise Exception("current connection might be spoofed due to different hmac.")
+
+
+def randno(power=6):
+    return random.randint(10 ** power, 10 ** (power + 1) - 1)
+
+
