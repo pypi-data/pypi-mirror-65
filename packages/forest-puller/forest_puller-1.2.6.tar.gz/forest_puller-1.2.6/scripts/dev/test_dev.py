80a2df5a3b44c575f89d6ff7dc92300bae571263
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:24 +0000

    Add package forest-puller/1.2.6/forest_puller-1.2.6.tar.gz

---
+++ packages/forest-puller/forest_puller-1.2.6.tar.gz/forest_puller-1.2.6/scripts/dev/test_dev.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Written by Lucas Sinclair and Paul Rougieux.
+
+JRC Biomass Project.
+Unit D1 Bioeconomy.
+
+Development script to test some of the methods in `forest_puller`
+
+Typically you would run this file from a command line like this:
+
+     ipython3 -i -- ~/deploy/forest_puller/scripts/dev/test_dev.py
+"""
+
+# Built-in modules #
+from pprint import pprint
+
+# Third party modules #
+import pandas
+from tqdm import tqdm
+
+###############################################################################
+from forest_puller.core.continent import continent
+print(continent.report())
+
