a7f49ec77921ff7947c1b618a53918c9e06064dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:26 +0000

    Add package l0bnb/0.0.3/l0bnb-0.0.3.tar.gz

---
+++ packages/l0bnb/l0bnb-0.0.3.tar.gz/l0bnb-0.0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+LICENSE = """MIT"""
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(
+    name="l0bnb",
+    description="Scalable algorithms for L0 L2-regularized regression",
+    author="Hussein Hazimeh, Rahul Mazumder, Ali Saab",
+    author_email="alikassemsaab@gmail.com",
+    url='https://github.com/alisaab/l0bnb',
+    download_url="https://github.com/alisaab/l0bnb/archive/0.0.3.tar.gz",
+    install_requires=["numpy >= 1.18.2", "scipy >= 1.4.1", "numba >= 0.48.0",
+                      "matplotlib >= 3.2.1"],
+    version="0.0.3",
+    packages=find_packages(),
+    include_package_data=True,
+    license=LICENSE,
+    long_description=readme(),
+    long_description_content_type="text/markdown"
+)
