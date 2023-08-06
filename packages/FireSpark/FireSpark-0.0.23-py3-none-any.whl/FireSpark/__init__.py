e6ba2b72f8b24b58bc64ea6f606fe2da5efa369b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:48 +0000

    Add package FireSpark/0.0.23/FireSpark-0.0.23-py3-none-any.whl

---
+++ packages/FireSpark/FireSpark-0.0.23-py3-none-any.whl/FireSpark/__init__.py
@@ -0,0 +1,12 @@
+from .proto_utils import ProtoTemplate, ProtoDataset
+from .data_utils import SparkDataset
+from .protos.mas import annotation_pb2 as annotation_proto
+from .protos.mas import image_data_pb2 as image_proto
+from .protos.mas import lyftbag_pb2 as lyftbag_proto
+from .aws_utils import FireSparkAWS as S3
+from .torch_utils import TorchLoaderBase as torch_loader
+from .numpy_utils import NumpyLoaderBase as np_loader
+from .tensorflow_utils import TFLoaderBase as tf_loader
+from .lyftbag_utils import CamLoader
+
+__version__ = '0.0.23'
