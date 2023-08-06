87a3304814bcaaa75da68c1b313cfd65ba1cbec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:25 +0000

    Add package sms-toolkit/1.0.3/sms-toolkit-1.0.3.tar.gz

---
+++ packages/sms-toolkit/sms-toolkit-1.0.3.tar.gz/sms-toolkit-1.0.3/setup.py
@@ -0,0 +1,16 @@
+import os
+from setuptools import find_packages, setup
+
+
+setup(
+    name='sms-toolkit',
+    version='1.0.3',
+    description='A collection of tools to work with SMS messages.',
+    long_description=open('README.rst').read(),
+    author='Klaviyo',
+    author_email='community@klaviyo.com',
+    url='https://github.com/chrisconlon-klaviyo/sms-toolkit',
+    license='MIT',
+    packages=find_packages(),
+    package_data={'': ['README.rst']},
+)
