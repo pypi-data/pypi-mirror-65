98babda2eca95e076a86fbe0f75330fb0eacf00d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:07 +0000

    Add package pmlab-lite/0.3.9.6/pmlab_lite-0.3.9.6.tar.gz

---
+++ packages/pmlab-lite/pmlab_lite-0.3.9.6.tar.gz/pmlab_lite-0.3.9.6/setup.py
@@ -0,0 +1,27 @@
+import setuptools, pathlib
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
+setuptools.setup(
+	name='pmlab_lite',
+    version='0.3.9.6',
+    description='Process Mining scripting environment',
+    long_description=README,
+    long_description_content_type='text/markdown',
+	classifiers=[
+			'Development Status :: 2 - Pre-Alpha',
+			'Programming Language :: Python :: 3'
+		  ],
+    url='https://github.com/pmlab/pmlab-lite',
+    author=['Josep Carmona',
+			'Matthias Weidlich',
+			'Andrea Burattin',
+			'jmunozgama',
+			'Simon Remy'],
+    author_email='',
+    license='GPL-3.0',
+    packages=setuptools.find_packages(),
+    zip_safe=False
+)
