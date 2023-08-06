38e54eefd5093728c8e306d9a28b03cc084cdc43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:53 +0000

    Add package mpl-tools/0.1.5/mpl_tools-0.1.5-py2.py3-none-any.whl

---
+++ packages/mpl-tools/mpl_tools-0.1.5-py2.py3-none-any.whl/mpl_tools/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from pkg_resources import get_distribution, DistributionNotFound
+
+try:
+    # Change here if project is renamed and does not equal the package name
+    dist_name = 'mpl-tools'
+    __version__ = get_distribution(dist_name).version
+except DistributionNotFound:
+    __version__ = 'unknown'
+finally:
+    del get_distribution, DistributionNotFound
+
+
+from mpl_tools.misc import *
+from mpl_tools.ax_placement import *
+from mpl_tools.log_toggler import *
