4a1dcb34c72d8415c598d0aed4bf5aa5aaa8d75a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:47 +0000

    Add package nlknhjc/1.2.0/nlknhjc-1.2.0.tar.gz

---
+++ packages/nlknhjc/nlknhjc-1.2.0.tar.gz/nlknhjc-1.2.0/nlknhjc.py
@@ -0,0 +1,12 @@
+"""the description"""
+
+
+def nested_list(the_list, level=0):
+    """the description"""
+    for item in the_list:
+        if isinstance(item, list):
+            nested_list(item, level+1)
+        else:
+            for tab_stop in range(level):
+                print("\t", end="")
+            print(item)
