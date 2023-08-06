36e6194a1d0711ce765cd4243e3ab98d8c587ff1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:09 +0000

    Add package django-dynamic-models-readonly/0.1.1/django_dynamic_models_readonly-0.1.1-py3-none-any.whl

---
+++ packages/django-dynamic-models-readonly/django_dynamic_models_readonly-0.1.1-py3-none-any.whl/settings/base.py
@@ -0,0 +1,24 @@
+import os
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SECRET_KEY = 'supersecret'
+
+INSTALLED_APPS = [
+    'tests',
+    'dynamic_models',
+    'django.contrib.contenttypes',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.dummy'
+    }
+}
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'
+    }
+}
