8a2483e355068bcf443f55c09a689175402500a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:47 +0000

    Add package idem-windows/1.2/idem_windows-1.2-py3-none-any.whl

---
+++ packages/idem-windows/idem_windows-1.2-py3-none-any.whl/idem_windows/tests/conftest.py
@@ -0,0 +1,18 @@
+import pop.hub
+import pytest
+import sys
+import unittest.mock as mock
+
+
+@pytest.fixture(scope="session")
+def hub():
+    """
+    provides a full hub that is used as a reference for mock_hub
+    """
+    hub = pop.hub.Hub()
+
+    # strip pytest args
+    with mock.patch.object(sys, "argv", sys.argv[:1]):
+pass
+
+    return hub
