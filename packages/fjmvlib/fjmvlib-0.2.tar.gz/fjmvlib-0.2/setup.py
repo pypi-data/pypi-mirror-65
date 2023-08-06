37a226174522e474f0c6b6b3bb379f7c4f9b80fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:11 +0000

    Add package fjmvlib/0.2/fjmvlib-0.2.tar.gz

---
+++ packages/fjmvlib/fjmvlib-0.2.tar.gz/fjmvlib-0.2/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fjmvlib", # Replace with your own username
+    version="0.2",
+    author="Oxana Sannikova",
+    author_email="osanniko@cisco.com",
+    description="A small package with helper functions to validate Fire Jumper Programmability Mission submissions.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/oxsannikova/fjmvlib",
+    download_url="https://github.com/oxsannikova/fjmvlib/archive/v_01.tar.gz",
+    #install_requires=[            
+    #      'requests',
+    #      'json',
+    #  ],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
