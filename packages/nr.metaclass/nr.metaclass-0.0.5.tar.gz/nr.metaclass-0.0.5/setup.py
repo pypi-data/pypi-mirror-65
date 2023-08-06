5f309f56b183c6c239e7a94002806b96255ea433
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:15 +0000

    Add package nr.metaclass/0.0.5/nr.metaclass-0.0.5.tar.gz

---
+++ packages/nr.metaclass/nr.metaclass-0.0.5.tar.gz/nr.metaclass-0.0.5/setup.py
@@ -0,0 +1,37 @@
+# automatically created by shore
+
+import io
+import re
+import setuptools
+import sys
+
+with io.open('src/nr/metaclass/__init__.py', encoding='utf8') as fp:
+  version = re.search(r"__version__\s*=\s*'(.*)'", fp.read()).group(1)
+
+long_description = None
+
+requirements = []
+
+setuptools.setup(
+  name = 'nr.metaclass',
+  version = version,
+  author = 'Niklas Rosenstein',
+  author_email = 'rosensteinniklas@gmail.com',
+  description = 'Metaclass utilities.',
+  long_description = long_description,
+  long_description_content_type = 'text/plain',
+  url = 'https://git.niklasrosenstein.com/NiklasRosenstein/nr-python-libs',
+  license = 'MIT',
+  packages = setuptools.find_packages('src', ['test', 'test.*', 'docs', 'docs.*']),
+  package_dir = {'': 'src'},
+  include_package_data = False,
+  install_requires = requirements,
+  extras_require = {},
+  tests_require = [],
+  python_requires = None, # TODO: None,
+  data_files = [],
+  entry_points = {},
+  cmdclass = {},
+  keywords = [],
+  classifiers = [],
+)
