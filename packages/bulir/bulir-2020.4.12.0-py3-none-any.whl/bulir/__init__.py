c3675c3b4bd82a3b846f6cfe26cf4a844fd1fa1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:16 +0000

    Add package bulir/2020.4.12.0/bulir-2020.4.12.0-py3-none-any.whl

---
+++ packages/bulir/bulir-2020.4.12.0-py3-none-any.whl/bulir/__init__.py
@@ -0,0 +1,11 @@
+from .bulir import (
+    BrazilianLawsuitIDsRetriever,
+    retrieve_brazilian_uf_lawsuit_ids,
+    __version__,
+)
+
+
+__all__ = [
+    "BrazilianLawsuitIDsRetriever",
+    "retrieve_brazilian_uf_lawsuit_ids",
+]
