4b00e53827ee836cf6873b0d964073bb0578a9f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:25 +0000

    Add package collections-example/1.0/collections_example-1.0.tar.gz

---
+++ packages/collections-example/collections_example-1.0.tar.gz/collections_example-1.0/Collections/Lists/ArrayList.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+class ArrayList:
+    def __init__(self, *args):
+        self.__data = []
+        for data in args:
+            self += data
+
+    def getdata(self, index:int):
+        if index >= 0 and index < len(self.__data):
+            return self.__data[index]
+        else:
+            return -1
+
+    def __add__(self, data):
+        self.__data.append(data)
+        return self
+        
+    def __len__(self):
+        return len(self.__data)
+
+    def __iter__(self):
+        return iter(self.__data)
+
+    def __str__(self):
+        return str(self.__data)
