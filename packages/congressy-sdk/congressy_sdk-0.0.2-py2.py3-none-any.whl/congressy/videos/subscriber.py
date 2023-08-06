bdf283746e9cdf4392e8b757b8c60cfa3dfbdd52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:16:12 +0000

    Add package congressy-sdk/0.0.2/congressy_sdk-0.0.2-py2.py3-none-any.whl

---
+++ packages/congressy-sdk/congressy_sdk-0.0.2-py2.py3-none-any.whl/congressy/videos/subscriber.py
@@ -0,0 +1,46 @@
+from congressy.core import synchable_models as models
+from congressy.core.models import fields
+
+
+class Subscriber(models.SynchableModel):
+    class Meta(models.SynchableModel.Meta):
+        list_endpoint = '/v1/videos/subscribers'
+        create_endpoint = list_endpoint
+        item_endpoint = '/v1/videos/subscribers/{pk}'
+
+    uuid = fields.UUIDField(
+        label='ID',
+        required=True,
+        primary_key=True,
+    )
+    project = fields.UUIDField(
+        label='project',
+        required=True,
+    )
+    external_id = fields.StringField(
+        label='external ID',
+        required=True,
+        max_length=255,
+    )
+    restriction_keys = fields.StringField(
+        label='restriction keys',
+        required=True,
+        max_length=255,
+    )
+    created_at = fields.DateTimeField(
+        label='created at',
+        required=True,
+    )
+    updated_at = fields.DateTimeField(
+        label='updated fields',
+        required=False,
+    )
+
+    def clean_project(self, value):
+        if isinstance(value, dict):
+            pks = ['uuid', 'id', 'pk']
+            for pk in pks:
+                if pk in value:
+                    return value['pk']
+
+            return value
