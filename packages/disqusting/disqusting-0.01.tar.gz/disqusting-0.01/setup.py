7b77d9be264399d73a3625cc24bcd4e93aa2601c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:32 +0000

    Add package disqusting/0.01/disqusting-0.01.tar.gz

---
+++ packages/disqusting/disqusting-0.01.tar.gz/disqusting-0.01/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="disqusting",
+    version="0.01",
+    description="Very simple (and incomplete) library for Disqus API",
+    url="https://github.com/vanatteveldt/disqusting",
+    author="Wouter van Atteveldt",
+    author_email="wouter@vanatteveldt.com",
+    packages=["disqusting"],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+    ],
+    install_requires=[
+        "requests",
+    ]
+)
