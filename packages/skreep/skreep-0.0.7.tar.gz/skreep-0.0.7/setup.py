c257255b7c8bbfedfa2199e4a7d28f4053d0c9bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:22 +0000

    Add package skreep/0.0.7/skreep-0.0.7.tar.gz

---
+++ packages/skreep/skreep-0.0.7.tar.gz/skreep-0.0.7/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="skreep",
+    version="0.0.7",
+    author="Korud",
+    author_email="khorud@yahoo.com",
+    description="Data scraper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/khorud",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    # include_package_data=True,
+    install_requires= ['selenium'],
+    python_requires= '>=3',
+)
