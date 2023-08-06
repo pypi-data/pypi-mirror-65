61e14d60437c4273b467ed099ed5f4219cd7b8ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:12 +0000

    Add package pyxing/0.0.4/pyxing-0.0.4.tar.gz

---
+++ packages/pyxing/pyxing-0.0.4.tar.gz/pyxing-0.0.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name            = 'pyxing',
+    version         = '0.0.4',
+    description     = 'python wrapper for eBest Xing API',
+    url             = 'https://github.com/sharebook-kr/pyxing',
+    author          = 'Lukas Yoo, Brayden Jo',
+    author_email    = 'brayden.jo@outlook.com, jonghun.yoo@outlook.com, pystock@outlook.com',
+    install_requires= ['pandas'],
+    license         = 'MIT',
+    packages        = ['pyxing'],
+    zip_safe        = False
+)
