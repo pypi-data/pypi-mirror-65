c489bbcac6b59d4530b7abff92d735e449396c11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:29 +0000

    Add package django-dirtyfields/1.4/django-dirtyfields-1.4.tar.gz

---
+++ packages/django-dirtyfields/django-dirtyfields-1.4.tar.gz/django-dirtyfields-1.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+def listify(filename):
+    return [line for line in open(filename, 'r').read().split('\n') if line]
+
+
+setup(
+    name="django-dirtyfields",
+    version="1.4",
+    url='http://github.com/romgar/django-dirtyfields',
+    license='BSD',
+    description=("Tracking dirty fields on a Django model instance "
+                 "(actively maintained)"),
+    long_description=open('README.rst', 'r').read(),
+    author='Romain Garrigues',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=listify('requirements.txt'),
+    classifiers=listify('CLASSIFIERS.txt')
+)
