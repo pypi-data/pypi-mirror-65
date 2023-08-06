bf6e874d7b1f868545d290df3918e1b450ac7b2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:57 +0000

    Add package pathtub/1.1.2/pathtub-1.1.2.tar.gz

---
+++ packages/pathtub/pathtub-1.1.2.tar.gz/pathtub-1.1.2/pathtub/__init__.py
@@ -0,0 +1,13 @@
+from .pathtools import (
+    ensure,  # Probably the only thing you'll need
+    clean,
+    get_path,
+    add_to_path,
+    remove_from_path,
+    is_in_path,
+)
+
+from .dll import (
+    ensure_dll,
+    forget_dll,
+)
