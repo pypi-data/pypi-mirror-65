c814cad8727ef77bebd0e301e7f2142fa771c5da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:38 +0000

    Add package InPynamoDB/4.1.1/InPynamoDB-4.1.1.tar.gz

---
+++ packages/InPynamoDB/InPynamoDB-4.1.1.tar.gz/InPynamoDB-4.1.1/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['inpynamodb', 'inpynamodb.connection']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['aiobotocore>=0.10.3,<0.11.0',
+ 'async-property>=0.2.1,<0.3.0',
+ 'coveralls>=2.0.0,<3.0.0',
+ 'pynamodb==4.1.0']
+
+setup_kwargs = {
+    'name': 'inpynamodb',
+    'version': '4.1.1',
+    'description': 'asyncio wrapper of PynamoDB',
+    'long_description': None,
+    'author': 'sunghyun-lee',
+    'author_email': 'sunghyunlee@mymusictaste.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
