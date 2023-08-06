02b85b52c9814e75927c3c4a9529aae6b463a48a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:27 +0000

    Add package pypassgens/1.0/pypassgens-1.0.tar.gz

---
+++ packages/pypassgens/pypassgens-1.0.tar.gz/pypassgens-1.0/setup.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+import setuptools
+
+setuptools.setup(
+    name='pypassgens',
+    version=1.0,
+    long_description=Path('README.md').read_text(),
+    packages=setuptools.find_packages(exclude=['test', 'data'])
+)
