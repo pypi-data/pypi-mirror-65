6bd32cee87a141070caf9a246cdab8f386c55413
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:51 +0000

    Add package caverimx-db/0.0.3/caverimx-db-0.0.3.tar.gz

---
+++ packages/caverimx-db/caverimx-db-0.0.3.tar.gz/caverimx-db-0.0.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="caverimx-db", # Replace with your own username
+    version="0.0.3",
+    author="Alexis Betancourt",
+    author_email="alexis@caveri.mx",
+    description="A small file system based database package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/caverimx/caverimx-db.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
