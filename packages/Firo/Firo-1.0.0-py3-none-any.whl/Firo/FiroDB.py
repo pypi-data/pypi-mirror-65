4653421d4b061d7251926fec6c190fe978bd9f1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:03 +0000

    Add package Firo/1.0.0/Firo-1.0.0-py3-none-any.whl

---
+++ packages/Firo/Firo-1.0.0-py3-none-any.whl/Firo/FiroDB.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+class Database(object):
+  con = none
+  cur = none
+  def __init__(dat):
+    con = sqlite3.connect(dat)
+    cur = con.cursor()
+    table_sql = "CREATE TABLE IF NOT EXISTS info (name TEXT NOT NULL UNIQUE, val TEXT NOT NULL)"
+    cur.execute(table_sql)
+  def set(name, data):
+    in_sql = "INSERT INTO info(name, val) VALUES (?, ?)"
+    cur.execute(in_sql, (name, data))
+  def get(name):
+    out_sql = "SELECT FROM info WHERE name=?"
+    cur.execute(out_sql, (name))
+    rows = cur.fetchall()
+    for row in rows:
+      return row
