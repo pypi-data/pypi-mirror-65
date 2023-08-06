672e34c9f412ae1882608005d8535c9c7f8008c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:09 +0000

    Add package black-mamba/0.3.1/black_mamba-0.3.1-py3-none-any.whl

---
+++ packages/black-mamba/black_mamba-0.3.1-py3-none-any.whl/black_mamba/initialization/init_files/settings_sample.py
@@ -0,0 +1,12 @@
+networks = {
+    "development": {
+        "mode": "HTTP",
+        "host": "localhost",
+        "port": 7545,
+        "network_id": "*"
+    }
+    #"development": {
+    #    "mode": "IPC",
+    #    "url": "/home/sarahconnor/ethereum/data/geth.ipc"
+    #}
+}
