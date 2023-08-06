a1c9e2fe98f7d5b4ee3575bb28d6542fa84944a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:56 +0000

    Add package liang/0.0.2/liang-0.0.2.tar.gz

---
+++ packages/liang/liang-0.0.2.tar.gz/liang-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='liang',
+      version='0.0.2',
+      description='only me',
+      url='https://github.com/fengfengxiong123/liang.git',
+      author='梁峰',
+      author_email='359975715@qq.com',
+      license='MIT',
+      packages=['liang'],
+      zip_safe=False)
