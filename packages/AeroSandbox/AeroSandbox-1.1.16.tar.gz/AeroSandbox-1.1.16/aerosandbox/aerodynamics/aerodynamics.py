cdf0efcc46690de8c6fd76170475994594127e57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:36 +0000

    Add package AeroSandbox/1.1.16/AeroSandbox-1.1.16.tar.gz

---
+++ packages/AeroSandbox/AeroSandbox-1.1.16.tar.gz/AeroSandbox-1.1.16/aerosandbox/aerodynamics/aerodynamics.py
@@ -0,0 +1,10 @@
+from ..geometry import *
+from ..performance import *
+
+class AeroProblem(AeroSandboxObject):
+    def __init__(self,
+                 airplane, # type: Airplane
+                 op_point, # type: OperatingPoint
+                 ):
+        self.airplane = airplane
+        self.op_point = op_point
